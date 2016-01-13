4db729b43c5dfbf4885df792b97e26fd98157ce4
jaapjan <jaapjan@goalgorilla.com>
2016-01-13 07:12:39 +0100

    Do not show Unable to send e-mail errors during installation with drush.

--- docker_build/drupal8/install_script.sh
+++ docker_build/drupal8/install_script.sh
@@ -3,5 +3,5 @@
 FILE_EXISTS=`docker exec -it social_db_1 test -f /var/lib/mysql/social/users.ibd; echo $?`
 
 if [ "$FILE_EXISTS" -eq 1 ]; then
-  docker exec -it social_web_1 drush site-install social --db-url=mysql://root:root@db:3306/social --account-pass=admin
+  docker exec -it social_web_1 drush -y site-install social --db-url=mysql://root:root@db:3306/social --account-pass=admin install_configure_form.update_status_module='array(FALSE,FALSE)'
 fi
