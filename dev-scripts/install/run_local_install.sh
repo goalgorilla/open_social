796d88d687c7169037f9aed0c8dd5334feec85d0
jaapjan <jaapjan@goalgorilla.com>
2016-02-04 12:28:53 +0100

    Docker improvements, remove unneccessary script.

--- dev-scripts/install/run_local_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OPTION=$1
-FILE_EXISTS=`docker-compose run db test -f /var/lib/mysql/social/users.ibd; echo $?`
-
-if [ "$OPTION"  == "reset" ] || [ "$FILE_EXISTS" -eq 1 ]; then
-  docker-compose run web bash /root/dev-scripts/install/install_script.sh
-fi
