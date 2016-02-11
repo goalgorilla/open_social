032543e955e8212f76bf5bcbf908680c84a0f2e6
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-02-11 10:41:43 +0100

    DS-284 by nielsvandermolen: Updated phpunit to use one  configuration file to run the tests and put config in the install profile. Should be easier to run the unit tests both in phpstorm and in the docker container.

--- dev-scripts/unit-tests.sh
+++ dev-scripts/unit-tests.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 DRUPAL_ROOT=/var/www/html
-DEV_SCRIPTS=/root/dev-scripts
 
-php -d xdebug.extended_info=0 -d xdebug.remote_autostart=0 -d xdebug.coverage_enable=0 -d xdebug.profiler_enable=0 -d xdebug.remote_enable=0 $DRUPAL_ROOT/vendor/bin/phpunit -c $DEV_SCRIPTS/phpunit.xml.dist --testsuite social
+php -d xdebug.extended_info=0 -d xdebug.remote_autostart=0 -d xdebug.coverage_enable=0 -d xdebug.profiler_enable=0 -d xdebug.remote_enable=0 $DRUPAL_ROOT/vendor/bin/phpunit -c $DRUPAL_ROOT/profiles/social/phpunit.xml.dist --testsuite social
