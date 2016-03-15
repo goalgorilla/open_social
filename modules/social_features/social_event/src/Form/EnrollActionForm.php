ccb216e79a06faea5d8f7d77dc7aa10180191e7f
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-03-15 18:01:11 +0100

    DS-252 by nielsvandermolen: Setup the dependency injection dependencies for the event enrollment form.

--- modules/social_features/social_event/src/Form/EnrollActionForm.php
+++ modules/social_features/social_event/src/Form/EnrollActionForm.php
@@ -7,15 +7,41 @@
 
 namespace Drupal\social_event\Form;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class EnrollActionForm.
  *
  * @package Drupal\social_event\Form
  */
-class EnrollActionForm extends FormBase {
+class EnrollActionForm extends FormBase implements ContainerInjectionInterface {
+
+
+  /**
+   * The routing matcher to get the nid.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMath;
+
+  /**
+   * The node storage.
+   *
+   * @var \Drupal\Core\entity\EntityStorageInterface
+   */
+  protected $entityStorage;
+
+  /**
+   * The user storage.
+   *
+   * @var \Drupal\user\UserStorageInterface
+   */
+  protected $userStorage;
 
   /**
    * {@inheritdoc}
@@ -24,10 +50,33 @@ class EnrollActionForm extends FormBase {
     return 'enroll_action_form';
   }
 
+  function __construct(RouteMatchInterface $route_match, EntityStorageInterface $entity_storage, UserStorageInterface $user_storage) {
+    $this->routeMatch = $route_match;
+    $this->entityStorage = $entity_storage;
+    $this->userStorage = $user_storage;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_route_match'),
+      $container->get('entity.manager')->getStorage('event_enrollment'),
+      $container->get('entity.manager')->getStorage('user')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    global $user;
+
+    $nid = $this->routeMatch->getRawParameter('node');
+
+    $form['event'] = array(
+      '#type' => 'hidden',
+      '#value' => $nid,
+    );
+
     $form['enroll_for_this_event'] = array(
       '#type' => 'submit',
       '#value' => $this->t('Enroll for this event'),
@@ -40,7 +89,20 @@ class EnrollActionForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    var_dump($form);
 
+    // Let's create some entities.
+//    $enrollment = EventEnrollment::create([
+//      'langcode' => $eventenrollment['language'],
+//      'name' => substr($eventenrollment['title'], 0, 50),
+//      'user_id' => $user_id,
+//      'created' => REQUEST_TIME,
+//      'field_event' => $event_id,
+//      'field_enrollment_status' => $eventenrollment['field_enrollment_status'],
+//      'field_account' => $user_id
+//    ]);
+//
+//    $enrollment->save();
   }
 
 }
