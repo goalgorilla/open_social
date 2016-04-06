ef22eb9b4e21dd89473cd2e25908533def7f1c45
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-04-06 15:47:32 +0200

    DS-329 by nielsvandermolen: Added sleeps and output lines to the install script to fix issue with nightly build

--- dev-scripts/install/install_script.sh
+++ dev-scripts/install/install_script.sh
@@ -8,10 +8,20 @@ cd /var/www/html/;
 # composer update --lock
 
 drush -y site-install social --db-url=mysql://root:root@db:3306/social --account-pass=admin install_configure_form.update_status_module='array(FALSE,FALSE)';
+sleep 5
+echo "installed drupal"
 chown -R www-data:www-data /var/www/html/
+sleep 5
+echo "set the correct owner"
 php -r 'opcache_reset();';
+sleep 5
+echo "opcache reset"
 chmod 444 sites/default/settings.php
+sleep 5
+echo "settings.php"
 drush pm-enable social_demo -y
+sleep 5
+echo "enabled module"
 drush cc drush
 drush sda file user topic event eventenrollment comment # Add the demo content
 #drush sdr file user topic event eventenrollment comment # Remove the demo content
