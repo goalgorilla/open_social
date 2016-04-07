53f7eae51731a31318840b21b359d816d7969c71
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-04-07 13:28:40 +0300

    DS-462 by ribel: Remove Facets module. Add custom Search Content block. Alter Views exposed filter block

--- modules/contrib/facets/src/Exception/InvalidQueryTypeException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\facets\Exception;
-
-/**
- * Represents an exception that occurred when calling an invalid query type.
- */
-class InvalidQueryTypeException extends \Exception {}
