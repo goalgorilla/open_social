5772d77508856266e66e60b806c9a0c53e1f3da9
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-04-11 18:42:38 +0300

    DS-255 by ribel: Add 'Search Users' form and block. Enhance exposed filters

---
+++ public_html/profiles/social/modules/social_features/social_profile/src/Plugin/Block/SearchUsersBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\social_profile\Plugin\Block\SearchUsersBlock.
+ */
+
+namespace Drupal\social_profile\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'SearchUsersBlock' block.
+ *
+ * @Block(
+ *  id = "search_users_block",
+ *  admin_label = @Translation("Search users block"),
+ * )
+ */
+class SearchUsersBlock extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $form = \Drupal::formBuilder()->getForm('Drupal\social_profile\Form\SearchUsersForm');
+    $build['search_content_form'] = $form;
+
+    return $build;
+  }
+
+}
