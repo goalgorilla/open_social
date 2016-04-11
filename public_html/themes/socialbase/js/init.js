d06af519f748f68dfc195279f0880e72ec317f12
Maikel Koopman <maikel.koopman@goalgorilla.com>
2016-04-11 14:53:55 +0200

    DS-291 by maikel-koopman: Add template for create event, replace form markup site-wide

--- public_html/themes/socialbase/js/init.js
+++ public_html/themes/socialbase/js/init.js
@@ -3,31 +3,6 @@
 
     var window_width = $(window).width();
 
-    // convert rgb to hex value string
-    function rgb2hex(rgb) {
-      if (/^#[0-9A-F]{6}$/i.test(rgb)) { return rgb; }
-
-      rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-
-      if (rgb === null) { return "N/A"; }
-
-      function hex(x) {
-          return ("0" + parseInt(x).toString(16)).slice(-2);
-      }
-
-      return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }
-
-    $('.dynamic-color .col').each(function () {
-      $(this).children().each(function () {
-        var color = $(this).css('background-color'),
-            classes = $(this).attr('class');
-        $(this).html(rgb2hex(color) + " " + classes);
-        if (classes.indexOf("darken") >= 0 || $(this).hasClass('black')) {
-          $(this).css('color', 'rgba(255,255,255,.9');
-        }
-      });
-    });
 
     // Floating-Fixed table of contents
     if ($('.table-of-contents').length) {
@@ -39,8 +14,6 @@
     else {
       $('.toc-wrapper').pushpin({ top: 0 });
     }
-
-
 
     // Github Latest Commit
     if ($('.repo-link').length) { // Checks if widget div exists (Index only)
@@ -68,25 +41,6 @@
         });
     });
 
-    // Toggle Containers on page
-    var toggleContainersButton = $('#container-toggle-button');
-    toggleContainersButton.click(function(){
-      $('body .browser-window .container, .had-container').each(function(){
-        $(this).toggleClass('had-container');
-        $(this).toggleClass('container');
-        if ($(this).hasClass('container')) {
-          toggleContainersButton.text("Turn off Containers");
-        }
-        else {
-          toggleContainersButton.text("Turn on Containers");
-        }
-      });
-    });
-
-    // Set checkbox on forms.html to indeterminate
-    var indeterminateCheckbox = document.getElementById('indeterminate-checkbox');
-    if (indeterminateCheckbox !== null)
-      indeterminateCheckbox.indeterminate = true;
 
 
 		$('[data-toggle="tabs"] a').click(function (e) {
@@ -97,7 +51,6 @@
     // Plugin initialization
     $('.scrollspy').scrollSpy();
     $('.button-collapse').sideNav({'edge': 'left'});
-    $('select').not('.disabled').material_select();
 
 
   }); // end of document ready
