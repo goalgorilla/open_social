08f9b375e0ad4a4e03cf351bb4870b11f684e12d
Ronald te Brake <ronald@goalgorilla.com>
2016-05-25 16:52:32 +0200

    DS-761 - Hero now with WIP on caching

--- public_html/profiles/social/modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
+++ public_html/profiles/social/modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
@@ -8,6 +8,9 @@
 namespace Drupal\social_group\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\group\Cache\Context\GroupTypeCacheContext;
+use Drupal\group\Cache\Context\GroupMembershipCacheContext;
 
 /**
  * Provides a 'GroupHeroBlock' block.
@@ -15,6 +18,9 @@ use Drupal\Core\Block\BlockBase;
  * @Block(
  *  id = "group_hero_block",
  *  admin_label = @Translation("Group hero block"),
+ *  context = {
+ *    "group" = @ContextDefinition("entity:group", required = FALSE)
+ *  }
  * )
  */
 class GroupHeroBlock extends BlockBase {
@@ -33,24 +39,39 @@ class GroupHeroBlock extends BlockBase {
         ->load($group);
     }
 
-    dpm($group);
-
     if (!empty($group)) {
       $content = \Drupal::entityTypeManager()
         ->getViewBuilder('group')
         ->view($group, 'hero');
 
-      dpm($content);
-
       $build['content'] = $content;
     }
 
+    // Also see GroupOperationsBlock.php for cache settings.
+    // This sets context per group type (private in future), per permission
+    // since some users may have the same roles in different group types granting
+    // them other permissions.
+    $build['#cache']['contexts'] = ['group.type', 'group_membership.permissions'];
+    $build['#cache']['contexts'][] = 'group_membership';
 
-    // @TODO make sure it fits for all the use cases.
+    $service_1 = \Drupal::service('group.group_route_context');
+    $service_2 = \Drupal::service('current_user');
 
-    $build['#cache'] = array(
-      'max-age' => 0,
+    // Add's route based and current user based cache tags for grouptype & membership.
+    $context_1 = new GroupTypeCacheContext($service_1, $service_2);
+    $context_2 = new GroupMembershipCacheContext($service_1, $service_2);
+
+    // Merge them with existing tags!
+    $tags = Cache::mergeTags(
+      $context_1->getCacheableMetadata()->getCacheTags(),
+      $context_2->getCacheableMetadata()->getCacheTags()
     );
+
+    // Also add custom cache_tag for when a member is added to the group so we
+    // can invalidate. It will be group_membership:gid.
+    $tags[] = 'group_membership:' . $group->id();
+
+    $build['#cache']['tags'] = $tags;
 
     return $build;
   }
