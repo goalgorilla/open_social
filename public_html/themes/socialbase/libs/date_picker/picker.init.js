253418032a0ba1ef2e062d4262e7768d7d7b8ebc
Maikel Koopman <maikel.koopman@goalgorilla.com>
2016-06-01 18:11:25 +0200

    DS-888 by maikel-koopman: disabled init of datepicker, conflict in submit format

--- public_html/themes/socialbase/libs/date_picker/picker.init.js
+++ public_html/themes/socialbase/libs/date_picker/picker.init.js
@@ -3,8 +3,8 @@
   Drupal.behaviors.initDatepicker = {
     attach: function (context, settings) {
 
-      $('.datepicker').pickadate({selectYears: 20, format: 'dd mm yyyy', formatSubmit: 'yyyy-mm-dd', close: 'Done',});
-      $('.timepicker').pickatime({format: 'H:i' , formatSubmit: 'H:i', close: 'Done',})
+      //$('.datepicker').pickadate({selectYears: 20, format: 'dd mm yyyy', formatSubmit: 'yyyy-mm-dd', close: 'Done',});
+      //$('.timepicker').pickatime({format: 'H:i' , formatSubmit: 'H:i', close: 'Done',})
 
     }
   }
