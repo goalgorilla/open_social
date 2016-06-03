a3da23bca984fa3f7bfbe7c02d0854327557e1f4
Ronald te Brake <ronald@goalgorilla.com>
2016-06-03 15:02:29 +0200

    DS-987 - Bubble the hero cache tags to the group hero so we don't have strange things happening

--- modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
+++ modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
@@ -47,6 +47,37 @@ class GroupHeroBlock extends BlockBase {
       $build['content'] = $content;
     }
 
+
+    $build['#cache']['contexts'][] = 'group';
+    $build['#cache']['contexts'][] = 'group.type';
+    $build['#cache']['contexts'][] = 'group_membership.permissions';
+    $build['#cache']['contexts'][] = 'group_membership';
+
+    $tags = $build['#cache']['tags'];
+
+    // Based on GroupOperationsBlock.php we add some more cache tags.
+    $service_1 = \Drupal::service('group.group_route_context');
+    $service_2 = \Drupal::service('current_user');
+    $context_1 = new GroupTypeCacheContext($service_1, $service_2);
+    $context_2 = new GroupMembershipCacheContext($service_1, $service_2);
+
+    // Merge all cache tags together.
+    $merged_tags = Cache::mergeTags(
+      $context_1->getCacheableMetadata()->getCacheTags(),
+      $context_2->getCacheableMetadata()->getCacheTags()
+    );
+
+    // Get all the tags to merge with the new ones.
+    foreach($merged_tags as $merged_tag) {
+      $tags[] = $merged_tag;
+    }
+
+    // This tag will invalidate the hero whenever a new member is added or
+    // updated or deleted. group_membership:2
+    $tags[] = 'group_membership:' . $group->id();
+
+    $build['#cache']['tags'] = $tags;
+
     return $build;
   }
 
