909f365834b3c448cb890f4986a78dad276822c4
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-06-07 22:18:11 +0300

    DS-987 by ribel: Add cachable dependency for Group Hero block

--- modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
+++ modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
@@ -39,15 +39,44 @@ class GroupHeroBlock extends BlockBase {
         ->load($group);
     }
 
+    // Add cache tags for group type & permissions. We need join / leave for
+    // the CTA and also update permissions for the button to edit a group.
+    $build['#cache']['contexts'][] = 'group.type';
+    $build['#cache']['contexts'][] = 'group_membership';
+
     if (!empty($group)) {
+      // Content
       $content = \Drupal::entityTypeManager()
         ->getViewBuilder('group')
         ->view($group, 'hero');
 
       $build['content'] = $content;
-    }
 
-    $build['#cache']['tags'][] = 'group_membership:' . $group->id();
+      // Cache
+      $renderer = \Drupal::service('renderer');
+      $current_user = \Drupal::currentUser();
+      $membership = $group->getMember($current_user);
+
+      $tags = $build['#cache']['tags'];
+
+      // Based on GroupOperationsBlock.php we add some more cache tags.
+      $service_1 = \Drupal::service('group.group_route_context');
+      $service_2 = \Drupal::service('current_user');
+      $context_1 = new GroupTypeCacheContext($service_1, $service_2);
+      $context_2 = new GroupMembershipCacheContext($service_1, $service_2);
+      // Merge all cache tags together.
+      $merged_tags = Cache::mergeTags(
+        $context_1->getCacheableMetadata()->getCacheTags(),
+        $context_2->getCacheableMetadata()->getCacheTags()
+      );
+      // Get all the tags to merge with the new ones.
+      foreach ($merged_tags as $merged_tag) {
+        $tags[] = $merged_tag;
+      }
+      $build['#cache']['tags'] = $tags;
+
+      $renderer->addCacheableDependency($build, $membership);
+    }
 
     return $build;
   }
