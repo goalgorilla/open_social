fc3a0b2a663e8409b20d09964d336f44fd8de71c
jaapjan <mail@jaapjan.nl>
2016-06-08 15:58:10 +0200

    DS-1060 by jaapjan: created activity content entity and activity_creator custom module.

---
+++ public_html/profiles/social/modules/custom/activity_creator/src/Form/ActivityDeleteForm.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_creator\Form\ActivityDeleteForm.
+ */
+
+namespace Drupal\activity_creator\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Activity entities.
+ *
+ * @ingroup activity_creator
+ */
+class ActivityDeleteForm extends ContentEntityDeleteForm {
+
+}
---
+++ public_html/profiles/social/modules/custom/activity_creator/src/Form/ActivitySettingsForm.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_creator\Form\ActivitySettingsForm.
+ */
+
+namespace Drupal\activity_creator\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ActivitySettingsForm.
+ *
+ * @package Drupal\activity_creator\Form
+ *
+ * @ingroup activity_creator
+ */
+class ActivitySettingsForm extends FormBase {
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'Activity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+
+  /**
+   * Defines the settings form for Activity entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['Activity_settings']['#markup'] = 'Settings form for Activity entities. Manage field settings here.';
+    return $form;
+  }
+
+}
