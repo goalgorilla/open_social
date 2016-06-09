637cc25367a9c9fe807090756774a6fa838bf362
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-06-09 17:29:24 +0200

    DS-1060 by nielsvandermolen: Added pluggable Activity Context system and load it in the message config form.

---
+++ modules/custom/activity_creator/src/Plugin/ActivityContext/CommunityActivityContext.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_creator\Plugin\ActivityContext\CommunityActivityContext.
+ */
+
+namespace Drupal\activity_creator\Plugin\ActivityContext;
+
+use Drupal\activity_creator\Plugin\ActivityContextBase;
+
+/**
+ * Provides a 'CommunityActivityContext' acitivy context.
+ *
+ * @ActivityContext(
+ *  id = "community_activity_context",
+ *  label = @Translation("Community activity context"),
+ * )
+ */
+class CommunityActivityContext extends ActivityContextBase {
+
+}
