302724554bafa4049e85ab88e4d122cb43f85221
jaapjan <mail@jaapjan.nl>
2016-06-12 14:30:16 +0200

    DS-1060 by jaapjan: activity display and get recipients in the profileactivitycontext.

--- modules/custom/activity_creator/src/Plugin/ActivityContext/ProfileActivityContext.php
+++ modules/custom/activity_creator/src/Plugin/ActivityContext/ProfileActivityContext.php
@@ -8,6 +8,8 @@
 namespace Drupal\activity_creator\Plugin\ActivityContext;
 
 use Drupal\activity_creator\Plugin\ActivityContextBase;
+use Drupal\Core\Entity\Entity;
+use Drupal\social_post\Entity\Post;
 
 /**
  * Provides a 'ProfileActivityContext' activity context.
@@ -25,13 +27,24 @@ class ProfileActivityContext extends ActivityContextBase {
   public function getRecipients(array $data, $last_uid, $limit) {
     $recipients = [];
 
-    // Only return the referenced entity here.
-    // @TODO Is referenced entity always the profile owner
-    if ($data['entity_type'] && $data['entity_id']) {
-      $recipients[] = [
-        'id' => $data['entity_id'],
-        'type' => $data['entity_type'],
-      ];
+    // We only know the context if there is a related object.
+    if (isset($data['related_object']) && !empty($data['related_object'])) {
+      $referenced_entity = $data['related_object']['0'];
+
+      if ($referenced_entity['target_type'] === 'post') {
+        $post = Post::load($referenced_entity['target_id']);
+
+        $recipient_user = $post->get('field_recipient_user')->getValue();
+        if (!empty($recipient_user)) {
+          $recipients[] = [
+            'target_type' => 'user',
+            'target_id' => $recipient_user['0']['target_id'],
+          ];
+        }
+
+
+      }
+
     }
 
     return $recipients;
