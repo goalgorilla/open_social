930f966af325045db16aeb5800d6420d4ad68442
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-06-13 13:57:25 +0300

    DS-1095 by ribel: Replace jQuery simplyCountable with characterCounter

--- public_html/profiles/social/modules/social_features/social_group/js/social_group.add_form.js
+++ public_html/profiles/social/modules/social_features/social_group/js/social_group.add_form.js
@@ -61,33 +61,8 @@
         }
       }
 
-      $(document).ready(function(){
-        $('input, textarea').once().characterCounter();
-      });
-
-      // DEVELOPER, PLEASE REMOVE THIS WHEN SURE WE CAN USE THE ABOVE
-
-      // // Get "Description" textarea of Group Create form.
-      // var textarea = $('#edit-field-group-description-0-value', context);
-      // // Get "Save" button.
-      // var submitButton = $('#edit-submit', context);
-      // // Insert new counter element after textarea.
-      // $('<div/>').addClass('counter').insertAfter($(textarea));
-      // // Init jQuery Simply Countable plugin
-      // $(textarea).simplyCountable({
-      //   counter: '.counter',
-      //   countType: 'characters',
-      //   maxCount: 200,
-      //   countDirection: 'down',
-      //   safeClass: 'safe',
-      //   overClass: 'over',
-      //   onOverCount: function(count, countable, counter){
-      //     $(submitButton).prop('disabled', true);
-      //   },
-      //   onSafeCount: function(count, countable, counter){
-      //     $(submitButton).prop('disabled', false);
-      //   },
-      // });
+      // Init jQuery characterCounter for all inputs and textareas.
+      $('input, textarea').once().characterCounter();
     }
   };
 
