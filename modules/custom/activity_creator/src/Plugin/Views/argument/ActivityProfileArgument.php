9228636291f30e5a16dcf95f2259460432aebf95
jaapjan <mail@jaapjan.nl>
2016-06-14 20:23:13 +0200

    DS-1060 by jaapjan: on a profile do not show posts by the user as actor on someone else his profile. This is conffusing to the user.

--- modules/custom/activity_creator/src/Plugin/Views/argument/ActivityProfileArgument.php
+++ modules/custom/activity_creator/src/Plugin/Views/argument/ActivityProfileArgument.php
@@ -24,15 +24,24 @@ class ActivityProfileArgument extends ArgumentPluginBase {
     // \Drupal\views\Plugin\views\query\QueryPluginBase
 
     $this->query->addTable('activity__field_activity_recipient_user');
+    $this->query->addTable('activity__field_activity_entity');
+    $this->query->addTable('activity__field_activity_destinations');
 
     $or_condition = db_or();
 
+    // User is a recipient.
     $or_condition->condition('activity__field_activity_recipient_user.field_activity_recipient_user_target_id', $this->argument, '=');
-    $or_condition->condition('activity.user_id', $this->argument, '=');
+
+    // Or posted by the user, but not on someone else his profile..
+    // TODO Because of this set-up we have to use distinct. Not perfect.
+    $by_user = db_and();
+    $by_user->condition('activity.user_id', $this->argument, '=');
+    $by_user->condition('activity__field_activity_recipient_user.field_activity_recipient_user_target_id', NULL, 'IS NULL');
+    // TODO Add condition for posts in a group as well (do not show them)
+
+    $or_condition->condition($by_user);
 
     $this->query->addWhere('activity_profile_argument', $or_condition);
-
-    // @TODO posts created by this user: see PostAccountStream::query
 
   }
 
