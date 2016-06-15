01d204b19cd7497e855ed767101a30b09ee8d7df
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-06-15 11:01:22 +0200

    DS-1060 by nielsvandermolen: Add initial plugin structure for activity destinations

---
+++ public_html/profiles/social/modules/custom/activity_creator/src/Plugin/ActivityDestination/StreamGroupActivityDestination.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_creator\Plugin\ActivityDestination\StreamGroupActivityDestination.
+ */
+
+namespace Drupal\activity_creator\Plugin\ActivityDestination;
+use Drupal\activity_creator\Plugin\ActivityDestinationBase;
+
+/**
+ * Provides a 'StreamGroupActivityDestination' acitivy destination.
+ *
+ * @ActivityContext(
+ *  id = "stream_group",
+ *  label = @Translation("Stream group activity "),
+ * )
+ */
+class StreamGroupActivityDestination extends ActivityDestinationBase {
+
+}
