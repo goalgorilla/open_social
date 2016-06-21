07bbfc0fac70ea0813c03e661d09942eb2a5cb3a
jaapjan <mail@jaapjan.nl>
2016-06-21 13:58:26 +0200

    Issue #2752649 by jaapjan: fix coding standards in activity_logger module.

--- modules/custom/activity_logger/src/Service/ContextGetter.php
+++ modules/custom/activity_logger/src/Service/ContextGetter.php
@@ -1,16 +1,32 @@
 <?php
+/**
+ * @file
+ * Get the context for a given Entity.
+ */
 
 namespace Drupal\activity_logger\Service;
 
 use Drupal\Core\Entity\Entity;
 use Drupal\group\Entity\GroupContent;
 
+/**
+ * Get the context, mostly entity based.
+ */
 class ContextGetter {
 
+  /**
+   * Get the context for a given Entity.
+   *
+   * @param \Drupal\Core\Entity\Entity $entity
+   *    Entity object.
+   *
+   * @return string $context
+   *    Context string.
+   */
   public function getContext(Entity $entity) {
 
     // Check if it's placed in a group (regardless off content type).
-    if ($group_entity = GroupContent::loadByEntity($entity) || strpos(\Drupal::routeMatch()->getRouteName(),'entity.group_content') === 0) {
+    if ($group_entity = GroupContent::loadByEntity($entity) || strpos(\Drupal::routeMatch()->getRouteName(), 'entity.group_content') === 0) {
       $context = $this->getFieldValue('group');
     }
     else {
@@ -35,14 +51,18 @@ class ContextGetter {
     return $context;
   }
 
+  /**
+   * Get field value of the context field.
+   */
   public function getFieldValue($context) {
     // @TODO: Make this more generic, based on the field config.
-    $contextValues = array (
+    $context_values = array(
       'community' => 'community_activity_context',
       'group' => 'group_activity_context',
       'profile' => 'profile_activity_context',
     );
 
-    return $contextValues[$context];
+    return $context_values[$context];
   }
+
 }
