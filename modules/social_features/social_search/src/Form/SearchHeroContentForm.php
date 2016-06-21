74ffa8c0964bbbccc7f21526aeedcf3d7ea24804
Ronald te Brake <ronald@goalgorilla.com>
2016-06-21 12:41:06 +0200

    Coding standards - updated social_search by ronaldtebrake

--- modules/social_features/social_search/src/Form/SearchHeroContentForm.php
+++ modules/social_features/social_search/src/Form/SearchHeroContentForm.php
@@ -17,32 +17,4 @@ class SearchHeroContentForm extends SearchContentForm {
     return 'search_hero_content_form';
   }
 
-  // /**
-  //   * {@inheritdoc}
-  //   */
-  //  public function buildForm(array $form, FormStateInterface $form_state) {
-  //    parent::buildForm()
-  //    return $form;
-  //  }
-  //
-  //  /**
-  //   * {@inheritdoc}
-  //   */
-  //  public function submitForm(array &$form, FormStateInterface $form_state) {
-  //    if(empty($form_state->getValue('search_input'))){
-  //      // Redirect to the search content page with empty search values.
-  //      $search_content_page = Url::fromRoute('view.search_content.page_no_value');
-  //    } else {
-  //      // Redirect to the search content page with filters in the GET parameters.
-  //      $search_input = $form_state->getValue('search_input');
-  //      $search_content_page = Url::fromRoute('view.search_content.page', array('keys' => $search_input));
-  //    }
-  //    $redirect_path = $search_content_page->toString();
-  //
-  //    $query = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
-  //
-  //    $redirect = Url::fromUserInput($redirect_path, array('query' => $query));
-  //
-  //    $form_state->setRedirectUrl($redirect);
-  //  }
 }
