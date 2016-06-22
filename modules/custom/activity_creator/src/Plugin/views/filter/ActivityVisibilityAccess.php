62892ef5ea14a39c6e5841fa2dd78c88f22ff610
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2016-06-22 11:20:28 +0200

    by nielsvandermolen: Fix namespace

--- modules/custom/activity_creator/src/Plugin/views/filter/ActivityVisibilityAccess.php
+++ modules/custom/activity_creator/src/Plugin/views/filter/ActivityVisibilityAccess.php
@@ -5,7 +5,7 @@
  * Contains \Drupal\activity_creator\Plugin\views\filter\ActivityVisibilityAccess.
  */
 
-namespace Drupal\activity_creator\Plugin\Views\Filter;
+namespace Drupal\activity_creator\Plugin\views\filter;
 
 use Drupal\views\Plugin\views\filter\FilterPluginBase;
 
