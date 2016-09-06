a7be54cbadf24875d1e72ba1af1fc439000f1661
maikelkoopman <maikelkoopman@goalgorilla.com>
2016-09-06 19:23:34 +0200

    DS-1636 by maikelkoopman: add timepicker for time fields, change library dependencies

--- themes/socialbase/js/custom/date_picker.js
+++ themes/socialbase/js/custom/date_picker.js
@@ -6,26 +6,37 @@
             if (!isMobile()) {
 
                 // Defaults
+                var $context = $(context);
+
                 var $time = $('.form-time');
                 var $date = $('.form-date');
 
-                // Check whether we have a form element time.
-                if ($time.length) {
-                    // Change it's input to text. Only for date element and only on Desktop.
-                    // If JS is disabled the fallback is the HTML 5 element, not too user friendly.
-                    $time.prop('type', 'text');
-                }
-                // Check whether we have a form element date.
-                if ($date.length) {
-                    // Change it's input to text. Only for date element and only on Desktop.
-                    // If JS is disabled the fallback is the HTML 5 element, not too user friendly.
-                    $date.prop('type', 'text');
-                    // Initiate the datepicker element. So we can make it user friendly again.
-                    $date.datepicker({
-                        altFormat: 'yy-mm-dd',
-                        dateFormat: 'yy-mm-dd' // @Todo we can alter this to show the user a different format.
-                    });
-                }
+                // TIME
+                $context.find($time).once('timePicker').each(function () {
+                  // Change it's input to text. Only for date element and only on Desktop.
+                  // If JS is disabled the fallback is the HTML 5 element, not too user friendly.
+                  $time.prop('type', 'text');
+                  // Initiate the datepicker element. So we can make it user friendly again.
+                  var options = {
+                    'show2400': true,
+                    'scrollDefault': 'now',
+                    'timeFormat': 'H:i'
+                  };
+                  $time.timepicker(options);
+                });
+
+                // DATES
+                $context.find($date).once('datePicker').each(function () {
+                  // Change it's input to text. Only for date element and only on Desktop.
+                  // If JS is disabled the fallback is the HTML 5 element, not too user friendly.
+                  $date.prop('type', 'text');
+                  // Initiate the datepicker element. So we can make it user friendly again.
+                  $date.datepicker({
+                      altFormat: 'yy-mm-dd',
+                      dateFormat: 'yy-mm-dd' // @Todo we can alter this to show the user a different format.
+                  });
+                });
+
             }
         }
     }
