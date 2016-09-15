a724fabb19a6cd02d0e7262efff3b13892747866
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-09-15 20:18:45 +0300

    DS-1034 by ribel: Add OwnerActivityContext plugin

---
+++ modules/custom/activity_basics/src/Plugin/ActivityContext/OwnerActivityContext.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\activity_basics\Plugin\ActivityContext\OwnerActivityContext.
+ */
+
+namespace Drupal\activity_basics\Plugin\ActivityContext;
+
+use Drupal\activity_creator\Plugin\ActivityContextBase;
+use Drupal\activity_creator\ActivityFactory;
+
+
+/**
+ * Provides a 'OwnerActivityContext' activity context.
+ *
+ * @ActivityContext(
+ *  id = "owner_activity_context",
+ *  label = @Translation("Owner activity context"),
+ * )
+ */
+class OwnerActivityContext extends ActivityContextBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRecipients(array $data, $last_uid, $limit) {
+    $recipients = [];
+
+    // We only know the context if there is a related object.
+    if (isset($data['related_object']) && !empty($data['related_object'])) {
+
+      $referenced_entity = ActivityFactory::getActivityRelatedEntity($data);
+
+      if ($referenced_entity['target_type'] === 'post') {
+        $recipients += $this->getRecipientsFromPost($referenced_entity);
+      }
+
+      if ($referenced_entity['target_type'] === 'node') {
+        $recipients += $this->getRecipientsFromNode($referenced_entity);
+      }
+
+    }
+
+    return $recipients;
+  }
+
+  public function isValidEntity($entity) {
+    // Returns the entity to which the comment is attached.
+    if ($entity->getEntityTypeId() === 'comment') {
+      $entity = $entity->getCommentedEntity();
+    }
+
+    if ($entity->getEntityTypeId() === 'post') {
+      if (!empty($entity->get('field_recipient_group')->getValue())) {
+        return FALSE;
+      }
+      elseif (!empty($entity->get('field_recipient_user')->getValue())) {
+        return TRUE;
+      }
+    }
+    return TRUE;
+  }
+
+}
