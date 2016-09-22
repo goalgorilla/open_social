40f0862d2b41da5b038bb13a27c85b1a760f7578
maikelkoopman <maikelkoopman@goalgorilla.com>
2016-09-22 15:11:32 +0200

    DS-1936 by maikelkoopman: update behat test, origin of changed test is changed grid width

--- themes/socialbase/gulpfile.js
+++ themes/socialbase/gulpfile.js
@@ -201,8 +201,7 @@ gulp.task('watch:styles', ['styles'], function () {
 // svgmin minifies our SVG files and strips out unnecessary code that you might inherit from your graphics editor. svgstore binds them together in one giant SVG container called icons.svg. Then cheerio gives us the ability to interact with the DOM components in this file in a jQuery-like way. cheerio in this case is removing any fill attributes from the SVG elements (you’ll want to use CSS to manipulate them) and adds a class of .hide to our parent SVG. It gets deposited into our inc directory with the rest of the HTML partials.
 // ===================================================
 
-var gulp          = require('gulp'),
-    svgmin        = require('gulp-svgmin'),
+var svgmin        = require('gulp-svgmin'),
     svgstore      = require('gulp-svgstore'),
     cheerio       = require('gulp-cheerio');
 
