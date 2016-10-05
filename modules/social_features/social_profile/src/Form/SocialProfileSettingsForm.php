6e3ccf8bb610d3c5b11686406b7e0beafdb57bf3
Taras Kruts <taras.kruts@lemberg.co.uk>
2016-10-05 18:55:07 +0300

    DS-2082 by ribel: Add check for permission to view email address on user profiles

--- modules/social_features/social_profile/src/Form/SocialProfileSettingsForm.php
+++ modules/social_features/social_profile/src/Form/SocialProfileSettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\social_profile\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
 
 /**
  * Configure social profile settings.
@@ -36,9 +37,9 @@ class SocialProfileSettingsForm extends ConfigFormBase {
     );
     $form['privacy']['social_profile_show_email'] = array(
       '#type' => 'checkbox',
-      '#title' => $this->t('Show Email on user profiles'),
+      '#title' => $this->t('Show email on all user profiles'),
       '#default_value' => $config->get('social_profile_show_email'),
-      '#description' => $this->t('Users with permission "view profile email" will be able to see email in any case.'),
+      '#description' => $this->t('If this setting is turned off, users will be able override it for their profiles. Also users with permission "view profile email" will be able to see email in any case.'),
     );
 
     return parent::buildForm($form, $form_state);
@@ -48,9 +49,26 @@ class SocialProfileSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Save config.
     $config = $this->config('social_profile.settings');
     $config->set('social_profile_show_email', $form_state->getValue('social_profile_show_email'))
       ->save();
+
+    // Invalidate profile cache tags.
+    $query = \Drupal::database()->select('profile', 'p');
+    $query->addField('p', 'profile_id');
+    $query->condition('p.type', 'profile');
+    $query->condition('p.status', 1);
+    $ids = $query->execute()->fetchCol();
+
+    if (!empty($ids)) {
+      $cache_tags = [];
+      foreach ($ids as $id) {
+        $cache_tags[] = 'profile:' . $id;
+      }
+      Cache::invalidateTags($cache_tags);
+    }
+
     parent::submitForm($form, $form_state);
   }
 
