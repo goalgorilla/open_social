38293fdfd85f5b2dbb516afae7fdd31fe95e60d6
Frank Graave <frankgraave@gmail.com>
2016-12-19 15:34:06 +0100

    add cache tag for username

--- modules/social_features/social_user/src/Controller/SocialUserController.php
+++ modules/social_features/social_user/src/Controller/SocialUserController.php
@@ -22,4 +22,28 @@ class SocialUserController extends ControllerBase {
     return $this->redirect('entity.user.canonical', array('user' => $user->id()));
   }
 
+  /**
+   * The _title_callback for the users profile stream title.
+   *
+   * @return string
+   *   The first and/or last name with the AccountName as a fallback.
+   *
+   */
+  function setUserStreamTitle(UserInterface $user = NULL) {
+    $accountname = '';
+    // Let's get the First name Last name.
+    /** @var \Drupal\profile\ProfileStorageInterface $storage */
+    $storage = \Drupal::entityTypeManager()->getStorage('profile');
+    if (!empty($storage)) {
+      // Returns false.
+      if ($user_profile = $storage->loadByUser($user, 'profile', TRUE)) {
+        $accountname = trim(t('@first @last', array(
+          '@first' => $user_profile->get('field_profile_first_name')->value,
+          '@last' => $user_profile->get('field_profile_last_name')->value
+        )));
+      }
+    }
+    return $this->t($name = ($accountname !== '') ? $accountname : $user->getAccountName());
+  }
+
 }
