47a0fc54aa6b00f5af4fdc39a803653327cd061f
Ronald te Brake <ronald@goalgorilla.com>
2017-01-06 16:31:49 +0100

    DS-2735 by ronaldtebrake: start tour automatically, and make a backdrop configurable and available

---
+++ modules/social_features/social_tour/js/social_tour.js
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * Attaches behaviors for the Tour module's toolbar tab.
+ */
+
+(function ($, Backbone, Drupal, document) {
+
+    'use strict';
+
+    /**
+     * Attaches the tour's toolbar tab behavior on document load, heavily relies on tour.js
+     *
+     * @type {Drupal~behavior}
+     *
+     * @prop {Drupal~behaviorAttach} attach
+     *   Attach tour functionality on `tour` events.
+     */
+    Drupal.behaviors.social_tour = {
+        attach: function (context) {
+            $('body').once('social_tour').each(function () {
+                var model = new Drupal.tour.models.StateModel();
+                new Drupal.tour.views.ToggleTourView({
+                    el: $(context).find('#toolbar-tab-tour'),
+                    model: model
+                });
+
+                model
+                    // Allow other scripts to respond to tour events.
+                    .on('change:isActive', function (model, isActive) {
+                        $(document).trigger((isActive) ? 'drupalTourStarted' : 'drupalTourStopped');
+                    })
+                    // Initialization: check whether a tour is available on the current
+                    // page.
+                    .set('tour', $(context).find('ol#tour'));
+
+                // Start the tour immediately if it's available.
+                if ($(context).find('ol#tour').length > 0 && model.isActive !== true) {
+                    model.set('isActive', true);
+                }
+            });
+        }
+    };
+
+})(jQuery, Backbone, Drupal, document);
