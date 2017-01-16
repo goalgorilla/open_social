4ef37a73ff9121b8c69d3ebea62226342f5b3dba
Peter Polman <info@peterpolman.nl>
2017-01-16 14:53:29 +0100

    DS-2001 - Added overrides component for exotic selectors.

--- modules/social_features/social_event/src/Form/EnrollActionForm.php
+++ modules/social_features/social_event/src/Form/EnrollActionForm.php
@@ -149,10 +149,9 @@ class EnrollActionForm extends FormBase implements ContainerInjectionInterface {
       $form['enroll_for_this_event']['#attributes'] = array(
         'class' => array(
           'btn',
-          'btn-accent',
+          'btn-accent brand-bg-accent',
           'btn-lg btn-raised',
           'dropdown-toggle',
-          'brand-bg-accent',
         ),
         'autocomplete' => 'off',
         'data-toggle' => 'dropdown',
