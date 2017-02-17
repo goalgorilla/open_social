13f9edf625c9210667dcae3810a6f61d193aab9c
maikelkoopman <maikelkoopman@goalgorilla.com>
2017-02-17 15:24:15 +0100

    ECI-133 by maikelkoopman: update documentaion for gulp in socialbase

--- themes/socialbase/README.md
+++ themes/socialbase/README.md
@@ -2,7 +2,7 @@
 The Socialbase theme is designed as a base theme for Open Social. This base theme holds has a lot of sensible defaults. It doesn't however contain much styling. We expect every developer to want to change this for his/her project. As you can see there is also a theme called **socialblue**. Socialblue is the demo/styling sub theme for Open Social.
 
 # Changing the style of the theme  #
-If you are an experienced Drupal themer you know how to create a theme. There are no tricks in our setup you need to know to prevent errors. The easiest way to start is to grab a copy of Socialblue and rename all instances of socialblue to your new theme name. Socialbase and socialblue will get updates each release, so it is best not to make changes in here you want to keep.
+If you are a Drupal themer you know how to create a (sub)theme. There are no tricks in our setup you need to know for Open Social. The easiest way to start is to grab a copy of Socialblue and rename all instances of socialblue to your new theme name. Socialbase and Socialblue will get updates each release, so it is best not to make changes in here you want to keep.
 
  Make sure that socialbase is always set as the base theme and inherit the regions from socialbase if you do not change the page template.
 
@@ -14,12 +14,12 @@ If you are an experienced Drupal themer you know how to create a theme. There ar
 
  Also in sites default you will find an example.settings.local.php file. Duplicate this file and remove the example part. After you have configured these files you need to rebuild the caches. This will speed up you development process.
 
-### Techniques
+### Techniques used
 
-- Gulp
+- Gulp 
 - Yarn
 - Sass
-- Pugjs (will be replaced later with twig)
+- KSS Node
 - Twig
 
 ## Structure
@@ -43,37 +43,37 @@ This folder contains all twig templates for the theme.
 
 
 
-# Contributing #
+# Contributing to socialbase #
 
-Socialbase uses Gulp.js as a task runner, so that it can do many different tasks automatically:
+Socialbase uses Gulp.js as a task runner, so that it can do many tasks automatically:
  - Build CSS from your Sass using libSass and node-sass.
  - Add vendor prefixes for the browsers you want to support using Autoprefixer.
- - Build a style guide of your components with pug templates.
  - Watch all of your files as you develop and re-build everything on the fly.
 
 Set up your front-end development build tools:
 
 1. [Install Yarn](https://yarnpkg.com/en/docs/install), see their website for documentation
 
-2. The package.json file contains the versions of all the node packages you need. To install them run:
-    ```
-    yarn install
-    ```
-
-3. Install the gulp-cli tool globally. Normally, installing a Node.js globally
+2. Install the gulp-cli tool globally. Normally, installing a Node.js globally
   is not recommended, which is why both Gulp and Grunt have created wrapper
   commands that will allow you to run "gulp" or "grunt" from anywhere, while
   using the local version of gulp or grunt that is installed in your project.
   To install gulp's global wrapper, run:
     ```
     npm install -g gulp-cli
+
+3. The package.json file contains the versions of all the node packages you need. To install them run:
+    ```
+    yarn install
     ```
 
-4. The default gulp task will build the CSS, build the style guide, and lint
-  your Sass and JavaScript. To run the default gulp task, type:
+3. 
     ```
-    gulp
+
+4. There are different gulp tasks. What each gulp task does is decribed in the gulpfile with the task itself. To run a gulp task, type:
     ```
+    gulp [taskname]
+    ``` 
 
 # Notice
 We are constantly improving and updating the theme setup for Open Social. It might be the readme is not always up to date. You can report issues for the drupal.org issue queue. The following items are on our roadmap:
--- themes/socialbase/gulpfile.js
+++ themes/socialbase/gulpfile.js
@@ -1,13 +1,23 @@
 'use strict';
 
 // ===================================================
-// Global packages
+// Load Gulp plugins
 // ===================================================
 
 var importOnce    = require('node-sass-import-once'),
     path          = require('path'),
     notify        = require("gulp-notify"),
-    bower         = require("gulp-bower");
+    bower         = require("gulp-bower"),
+    gulp          = require('gulp'),
+    $             = require('gulp-load-plugins')(),
+    browserSync   = require('browser-sync').create(),
+    del           = require('del'),
+    // gulp-load-plugins will report "undefined" error unless you load gulp-sass manually.
+    sass          = require('gulp-sass'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    mqpacker      = require('css-mqpacker'),
+    runSequence   = require('run-sequence');
 
 var options = {};
 
@@ -20,6 +30,11 @@ options.basetheme = {
   js         : __dirname + '/assets/js/'
 };
 
+options.icons = {
+  src   : options.basetheme.components + '06-libraries/icons/source/',
+  dest  : options.basetheme.build + 'icons/'
+};
+
 
 // Set the URL used to access the Drupal website under development. This will
 // allow Browser Sync to serve the website and update CSS changes on the fly.
@@ -30,7 +45,7 @@ options.drupalURL = '';
 options.sass = {
   importer: importOnce,
   includePaths: [
-    options.basetheme.components,
+    options.basetheme.components
   ],
   outputStyle: 'expanded'
 };
@@ -39,6 +54,11 @@ var sassFiles = [
   options.basetheme.components + '**/*.scss',
   // Do not open Sass partials as they will be included as needed.
   '!' + options.basetheme.components + '**/_*.scss'
+];
+
+var sassProcessors = [
+  autoprefixer({browsers: ['> 1%', 'last 2 versions']}),
+  mqpacker({sort: true})
 ];
 
 var onError = function(err) {
@@ -50,87 +70,95 @@ var onError = function(err) {
   this.emit('end');
 };
 
-options.icons = {
-  src   : options.basetheme.components + '01-base/icons/source/',
-  dest  : options.basetheme.build + 'icons/'
-};
 
 
+// #################
+//
+// Compile the Sass
+//
+// #################
+//
+// This task will look for all scss files and run postcss and rucksack.
+// For performance review we will display the file sizes
+// Then the files will be stored in the assets folder
+// At the end we check if we should inject new styles in the browser
 // ===================================================
-// Build CSS.
-// ===================================================
-
-var gulp          = require('gulp'),
-    $             = require('gulp-load-plugins')(),
-    browserSync   = require('browser-sync').create(),
-    del           = require('del'),
-    // gulp-load-plugins will report "undefined" error unless you load gulp-sass manually.
-    sass          = require('gulp-sass'),
-    postcss       = require('gulp-postcss'),
-    autoprefixer  = require('autoprefixer'),
-    mqpacker      = require('css-mqpacker');
-
-// Must be defined after plugins are called.
-var sassProcessors = [
-  autoprefixer({browsers: ['> 1%', 'last 2 versions']}),
-  mqpacker({sort: true})
-];
 
 gulp.task('styles', ['clean:css'], function () {
   return gulp.src(sassFiles)
-    .pipe($.sourcemaps.init() )
     .pipe($.sass(options.sass).on('error', sass.logError))
     .pipe($.plumber({ errorHandler: onError }) )
     .pipe($.postcss(sassProcessors) )
     .pipe($.rucksack() )
     .pipe($.rename({dirname: ''}))
     .pipe($.size({showFiles: true}))
-    .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest(options.basetheme.css))
     .pipe($.if(browserSync.active, browserSync.stream({match: '**/*.css'})));
 });
 
-gulp.task('styles:production', ['clean:css'], function () {
-  return gulp.src(sassFiles)
-    .pipe(sass(options.sass).on('error', sass.logError))
-    .pipe($.plumber({ errorHandler: onError }) )
-    .pipe($.postcss(sassProcessors) )
-    .pipe($.rucksack() )
-    .pipe($.rename({dirname: ''}))
-    .pipe($.size({showFiles: true}))
-    .pipe(gulp.dest(options.basetheme.css));
-});
 
+
+// #################
+//
+// Run Bower
+//
+// #################
+//
+// This task will read bower.json file and put the library in libraries folder
+// the task minify-scripts will determine what to use and move it to the assets
+// eventually. Libraries will ignored in the repository.
 // ===================================================
-// Run bower
-// ===================================================
+
 gulp.task('bower', function () {
   return bower()
     .pipe(gulp.dest(options.basetheme.bowerDir))
 });
 
+
+
+// #################
+//
+// Minify JS
+//
+// #################
+//
+// First clean the JS folder, then search all components for js files.
+// Also search in the libraries folder for specific files.
+// Then compress the files, give them an explicit .min filename and
+// save them to the assets folder.
 // ===================================================
-// Move and minify JS.
-// ===================================================
-gulp.task('minify-scripts', ['clean:js'], function () {
+
+gulp.task('minify-js', ['clean:js'], function () {
   return gulp.src([
     options.basetheme.components + '**/*.js',
     options.basetheme.bowerDir + '**/raphael.js',
     options.basetheme.bowerDir + '**/morris.js'
     ]
   )
-    .pipe($.uglify())
-    .pipe($.flatten())
-    .pipe($.rename({
-      suffix: ".min"
-    }))
-    .pipe(gulp.dest(options.basetheme.js));
+  .pipe($.uglify())
+  .pipe($.flatten())
+  .pipe($.rename({
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest(options.basetheme.js));
 });
 
 
-// ===================================================
-// Icons
-// svgmin minifies our SVG files and strips out unnecessary code that you might inherit from your graphics editor. svgstore binds them together in one giant SVG container called icons.svg. Then cheerio gives us the ability to interact with the DOM components in this file in a jQuery-like way. cheerio in this case is removing any fill attributes from the SVG elements (you’ll want to use CSS to manipulate them) and adds a class of .hide to our parent SVG. It gets deposited into our inc directory with the rest of the HTML partials.
+// #################
+//
+// Sprite icons
+//
+// #################
+//
+// svgmin minifies our SVG files and strips out unnecessary
+// code that you might inherit from your graphics editor.
+// svgstore binds them together in one giant SVG container called
+// icons.svg. Then cheerio gives us the ability to interact with
+// the DOM components in this file in a jQuery-like way. cheerio
+// in this case is removing any fill attributes from the SVG
+// elements (you’ll want to use CSS to manipulate them)
+// and adds a class of .hide to our parent SVG. It gets
+// deposited into our inc directory with the rest of the HTML partials.
 // ===================================================
 
 var svgmin        = require('gulp-svgmin'),
@@ -151,14 +179,31 @@ gulp.task('sprite-icons', function () {
     .pipe(gulp.dest(options.icons.dest))
 });
 
+
+
+// #################
+//
+// Image icons
+//
+// #################
+//
+// Becsides the sprite we somtimes still need the individual svg files
+// to load as a css background image. This task optimises and copies
+// the icons to the assets folder.
+// ===================================================
+
 gulp.task('image-icons', function () {
   return gulp.src(options.icons.src + '*.svg')
     .pipe(svgmin())
     .pipe(gulp.dest(options.basetheme.build + 'images/icons/'))
 });
 
+
+
 // ##############################
+//
 // Watch for changes and rebuild.
+//
 // ##############################
 
 gulp.task('watch', ['browser-sync']);
@@ -178,13 +223,15 @@ gulp.task('watch:css', ['styles'], function () {
 });
 
 gulp.task('watch:icons', function () {
-  return gulp.watch(options.icons.src + '**/*.svg', ['sprite-icons'] );
+  return gulp.watch(options.icons.src + '**/*.svg', ['sprite-icons', 'image-icons'] );
 });
 
 
 
 // ######################
+//
 // Clean all directories.
+//
 // ######################
 
 // Clean CSS files.
@@ -202,201 +249,17 @@ gulp.task('clean:js', function () {
   ], {force: true});
 });
 
-
-// ##############################################################################################################
-
-// Old style guide -- needs to be removed when twig styleguide is set up
-
-var config = {
-  "dist"        : "dist/",
-  "build"       : "assets/",
-  "components"  : "components/",
-  "content"     : "content/",
-  "css"         : "assets/css/",
-  "font"        : "../socialblue/assets/font/",
-  "icons"       : "assets/icons/",
-  "images"      : "assets/images/",
-  "js"          : "assets/js/",
-  "patterns"    : "pug/",
-  "drupal"      : "../../../../../core/"
-}
-
-var pug           = require('gulp-pug'),
-    connect       = require('gulp-connect'),
-    concat        = require('gulp-concat'),
-    notify        = require('gulp-notify'),
-    path          = require('path'),
-    plumber       = require('gulp-plumber');
-
-
-gulp.task('styleguide', function() {
-  return gulp.src(config.patterns + '**/*.pug')
-  .pipe(plumber({ handleError: onError }))
-  .pipe(pug({ pretty: true })) // pipe to pug plugin
-  .pipe(gulp.dest(config.dist)) // tell gulp our output folder
-});
-
-gulp.task('watch:styleguide', ['styleguide'], function () {
-  return gulp.watch(config.patterns + '**/*', ['styleguide'] );
-});
-
-//copy drupal scripts from drupal to make them available for the styleguide
-gulp.task('scripts-drupal', function() {
-  return gulp.src([
-    config.drupal + 'assets/vendor/domready/ready.min.js',
-    config.drupal + 'assets/vendor/jquery/jquery.min.js',
-    config.drupal + 'assets/vendor/jquery-once/jquery.once.min.js',
-    config.drupal + '/misc/drupalSettingsLoader.js',
-    config.drupal + '/misc/drupal.js',
-    config.drupal + '/misc/debounce.js',
-    config.drupal + '/misc/forms.js',
-    config.drupal + '/misc/tabledrag.js',
-    config.drupal + '/modules/user/user.js',
-    config.drupal + '/modules/file/file.js'
-  ])
-  .pipe( concat('drupal-core.js') )
-  .pipe( gulp.dest(config.dist + '/js') );
-});
-
-
-// ===================================================
-// Copy assets to dist folder
+// ######################
+//
+// Default task (no watching)
+//
+// ######################
+//
+// Before generating all assets, first run bower
 // ===================================================
 
-gulp.task('sg-images', function() {
-  return gulp.src(config.images + '**/*')
-  .pipe( gulp.dest(config.dist + 'assets/images') );
+gulp.task('default', function(done) {
+  runSequence('bower',
+    ['styles', 'image-icons', 'sprite-icons', 'minify-js'],
+    done);
 });
-
-gulp.task('sg-content', function() {
-  return gulp.src(config.content + '**/*')
-  .pipe( gulp.dest(config.dist + 'assets/content') );
-});
-
-gulp.task('sg-icons', function() {
-  return gulp.src(config.icons + '**/*')
-  .pipe( gulp.dest(config.dist + 'assets/icons') );
-});
-
-gulp.task('sg-font', function() {
-  return gulp.src(config.font + '**/*')
-  .pipe( gulp.dest(config.dist + 'assets/font') );
-});
-
-gulp.task('sg-stylesheets', function() {
-  return gulp.src(config.css + '**/*')
-  .pipe( gulp.dest(config.dist + 'assets/css') );
-});
-
-gulp.task('watch:sg-stylesheets', ['styles'], function () {
-  return gulp.watch(config.css + '**/*', ['sg-stylesheets'] );
-});
-
-gulp.task('sg-scripts', function() {
-  return gulp.src(config.js + '/**/*')
-  .pipe( gulp.dest(config.dist + 'assets/js') );
-});
-
-gulp.task('blue-styleguide-stylesheets', function() {
-  return gulp.src('../socialblue/components/styleguide/**/*')
-  .pipe( gulp.dest(config.dist + 'assets/js') );
-});
-
-gulp.task('css-assets-blue', function () {
-  return gulp.src('../socialblue/assets/css/*.css')
-  .pipe(gulp.dest(config.dist + 'assets/css/blue') );
-});
-
-gulp.task('js-assets-blue', function () {
-  return gulp.src('../socialblue/assets/js/*.js')
-  .pipe(gulp.dest(config.dist + 'assets/js/blue') );
-});
-
-
-gulp.task('watch:sg-scripts', function () {
-  return gulp.watch(config.js + '**/*', ['sg-scripts'] );
-});
-
-
-// ===================================================
-// Set up a server
-// ===================================================
-
-gulp.task('connect', function() {
-  connect.server({
-    root: [config.dist],
-    livereload: false,
-    port: 5000
-  });
-});
-
-
-// ===================================================
-// Deploy
-// ===================================================
-
-var rsync         = require('gulp-rsync'),
-    prompt        = require('gulp-prompt'),
-    gutil         = require('gulp-util'),
-    gulpif        = require('gulp-if'),
-    argv          = require('minimist')(process.argv);
-
-  try {
-    var deploy    = require('./deploy_config.json');
-  } catch(error) {
-    console.log('Deploy config file missing');
-  }
-
-
-// Generate an error for deploy if something goes wrong
-function throwError(taskName, msg) {
-  throw new gutil.PluginError({
-    plugin: taskName,
-    message: msg
-  });
-}
-
-gulp.task('build', ['styleguide', 'sg-stylesheets', 'sg-scripts', 'sg-font', 'sg-images', 'sg-icons', 'sg-content']);
-
-gulp.task('deploy', ['build'], function() {
-  // Dirs and Files to sync
-
-  // Default options for rsync
-  var rsyncConf = {
-    progress: true,
-    incremental: true,
-    relative: true,
-    emptyDirectories: true,
-    recursive: true,
-    clean: true,
-    exclude: [],
-  };
-
-  if (argv.production) {
-    rsyncConf.hostname = deploy.hostname; // hostname
-    rsyncConf.username = deploy.username; // ssh username
-    rsyncConf.destination = deploy.destination; // path where uploaded files go
-    rsyncConf.root = config.dist;
-  // Missing/Invalid Target
-  } else {
-    throwError('deploy', gutil.colors.red('Missing or invalid target'));
-  }
-
-  // Use gulp-rsync to sync the files
-  return gulp.src(config.dist + '/**/*')
-  .pipe(gulpif(
-      argv.production,
-      prompt.confirm({
-        message: 'Heads Up! Are you SURE you want to push to PRODUCTION?',
-        default: false
-      })
-  ))
-  .pipe(rsync(rsyncConf));
-
-});
-
-// ===================================================
-// Watch and rebuild tasks
-// ===================================================
-
-gulp.task('default', ['watch:css', 'watch:styleguide', 'watch:sg-stylesheets', 'watch:sg-scripts', 'connect']);
