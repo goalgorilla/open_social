4b4a1104c4acc5b3fa76fca7bc7a3421d73373f9
maikelkoopman <maikelkoopman@goalgorilla.com>
2017-03-07 09:42:57 +0100

    ECI-334 update changelog and clean css in socialblue

--- themes/socialblue/gulpfile.js
+++ themes/socialblue/gulpfile.js
@@ -215,7 +215,7 @@ var sassProcessors = [
 // At the end we check if we should inject new styles in the browser
 // ===================================================
 
-gulp.task('styles', function () {
+gulp.task('styles', ['clean:css'], function () {
   return gulp.src(sassFiles)
     .pipe(sass(options.sass).on('error', sass.logError))
     .pipe($.plumber({ errorHandler: onError }) )
