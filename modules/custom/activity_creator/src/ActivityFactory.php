ca8731957ced901511e492954c04d41f94381040
Ronald te Brake <ronald@goalgorilla.com>
2017-03-08 16:50:47 +0100

    DS-3197 by ronaldtebrake, frankgraave: check on activities creation with new template on democontent

--- modules/custom/activity_creator/src/ActivityFactory.php
+++ modules/custom/activity_creator/src/ActivityFactory.php
@@ -278,10 +278,12 @@ class ActivityFactory extends ControllerBase {
       $comment = $comment_storage->load($related_object['target_id']);
       if($comment){
         $commented_entity = $comment->getCommentedEntity();
-        $related_object = [
-          'target_type' => $commented_entity->getEntityTypeId(),
-          'target_id' => $commented_entity->id(),
-        ];
+        if(!empty($commented_entity)) {
+          $related_object = [
+            'target_type' => $commented_entity->getEntityTypeId(),
+            'target_id' => $commented_entity->id(),
+          ];
+        }
       }
     }
     return $related_object;
