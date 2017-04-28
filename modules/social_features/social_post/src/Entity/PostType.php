8a1871da91a93a348761eca62a01397ba89fde03
tor <mykhailo.semkulych@lemberg.co.uk>
2017-04-28 18:14:47 +0300

    Add support for post bundles

---
+++ modules/social_features/social_post/src/Entity/PostType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\social_post\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Post type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "post_type",
+ *   label = @Translation("Post type"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\social_post\PostTypeListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\social_post\Form\PostTypeForm",
+ *       "edit" = "Drupal\social_post\Form\PostTypeForm",
+ *       "delete" = "Drupal\social_post\Form\PostTypeDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\social_post\PostTypeHtmlRouteProvider",
+ *     },
+ *   },
+ *   config_prefix = "post_type",
+ *   admin_permission = "administer site configuration",
+ *   bundle_of = "post",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/post_type/{post_type}",
+ *     "add-form" = "/admin/structure/post_type/add",
+ *     "edit-form" = "/admin/structure/post_type/{post_type}/edit",
+ *     "delete-form" = "/admin/structure/post_type/{post_type}/delete",
+ *     "collection" = "/admin/structure/post_type"
+ *   }
+ * )
+ */
+class PostType extends ConfigEntityBundleBase implements PostTypeInterface {
+
+  /**
+   * The Post type ID.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The Post type label.
+   *
+   * @var string
+   */
+  protected $label;
+
+}
