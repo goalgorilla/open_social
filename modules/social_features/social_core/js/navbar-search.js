128b17dfe81c2432b961dc64a12edbc2bc803231
Peter Polman <info@peterpolman.nl>
2017-05-16 10:33:02 +0800

    DS-3580 - Closing main menu when user clicks outside of menu when opened on mobile.

--- modules/social_features/social_core/js/navbar-search.js
+++ modules/social_features/social_core/js/navbar-search.js
@@ -10,6 +10,10 @@
     Drupal.behaviors.navbarSearch = {
         attach: function (context, settings) {
             new UISearch(document.getElementById('navbar-search'));
+
+            $('#content').on('click', function(){
+              $('.navbar-toggle:not(.collapsed)').click();
+            });
         }
   };
 
