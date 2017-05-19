63d101a41e17fb0269446161c2ee0d319169d0c8
ksis <ksis@mail.com>
2017-05-19 17:24:12 +0300

    Issue #2879466 by alex.ksis: Add check by permission to each of links

--- modules/social_features/social_user/src/Plugin/Block/AccountHeaderBlock.php
+++ modules/social_features/social_user/src/Plugin/Block/AccountHeaderBlock.php
@@ -9,8 +9,11 @@ use Drupal\Core\Url;
  * Provides a 'AccountHeaderBlock' block.
  *
  * @Block(
- *  id = "account_header_block",
- *  admin_label = @Translation("Account header block"),
+ *   id = "account_header_block",
+ *   admin_label = @Translation("Account header block"),
+ *   context = {
+ *     "user" = @ContextDefinition("entity:user")
+ *   }
  * )
  */
 class AccountHeaderBlock extends BlockBase {
@@ -19,10 +22,10 @@ class AccountHeaderBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $account = \Drupal::currentUser();
+    $account = $this->getContextValue('user');
+
     if ($account->id() !== 0) {
       $account_name = $account->getAccountName();
-      $account_uid = $account->id();
 
       $links = [
         'add' => array(
@@ -34,7 +37,45 @@ class AccountHeaderBlock extends BlockBase {
           'label' => $this->t('New content'),
           'title_classes' => 'sr-only',
           'url' => '#',
-          'below' => array(),
+          'below' => array(
+            'add_event' => array(
+              'classes' => '',
+              'link_attributes' => '',
+              'link_classes' => '',
+              'icon_classes' => '',
+              'icon_label' => '',
+              'title' => $this->t('Create New Event'),
+              'label' => $this->t('New event'),
+              'title_classes' => '',
+              'url' => Url::fromRoute('node.add', [
+                'node_type' => 'event',
+              ]),
+            ),
+            'add_topic' => array(
+              'classes' => '',
+              'link_attributes' => '',
+              'link_classes' => '',
+              'icon_classes' => '',
+              'icon_label' => '',
+              'title' => $this->t('Create New Topic'),
+              'label' => $this->t('New topic'),
+              'title_classes' => '',
+              'url' => Url::fromRoute('node.add', [
+                'node_type' => 'topic',
+              ]),
+            ),
+            'add_group' => array(
+              'classes' => '',
+              'link_attributes' => '',
+              'link_classes' => '',
+              'icon_classes' => '',
+              'icon_label' => '',
+              'title' => $this->t('Create New Group'),
+              'label' => $this->t('New group'),
+              'title_classes' => '',
+              'url' => Url::fromRoute('entity.group.add_page'),
+            ),
+          ),
         ),
         'groups' => array(
           'classes' => '',
@@ -43,57 +84,14 @@ class AccountHeaderBlock extends BlockBase {
           'title' => $this->t('My Groups'),
           'label' => $this->t('My Groups'),
           'title_classes' => 'sr-only',
-          'url' => Url::fromUserInput('/user/' . $account_uid . '/groups'),
+          'url' => Url::fromRoute('view.groups.page_user_groups', [
+            'user' => $account->id(),
+          ]),
         ),
       ];
 
-      // Check if the current user is allowed to create Events.
-      if ($account->hasPermission('create event content')) {
-        $links['add']['below']['add_event'] = array(
-          'classes' => '',
-          'link_attributes' => '',
-          'link_classes' => '',
-          'icon_classes' => '',
-          'icon_label' => '',
-          'title' => $this->t('Create New Event'),
-          'label' => $this->t('New event'),
-          'title_classes' => '',
-          'url' => Url::fromUserInput('/node/add/event'),
-        );
-      }
-
-      // Check if the current user is allowed to create Topics.
-      if ($account->hasPermission('create topic content')) {
-        $links['add']['below']['add_topic'] = array(
-          'classes' => '',
-          'link_attributes' => '',
-          'link_classes' => '',
-          'icon_classes' => '',
-          'icon_label' => '',
-          'title' => $this->t('Create New Topic'),
-          'label' => $this->t('New topic'),
-          'title_classes' => '',
-          'url' => Url::fromUserInput('/node/add/topic'),
-        );
-      }
-
-      // Check if the current user is allowed to create new Groups.
-      if($account->hasPermission('create open_group group')){
-        $links['add']['below']['add_group'] = array(
-          'classes' => '',
-          'link_attributes' => '',
-          'link_classes' => '',
-          'icon_classes' => '',
-          'icon_label' => '',
-          'title' => $this->t('Create New Group'),
-          'label' => $this->t('New group'),
-          'title_classes' => '',
-          'url' => Url::fromUserInput('/group/add/open_group'),
-        );
-      }
-
       // Check if the current user is allowed to create new books.
-      if (\Drupal::moduleHandler()->moduleExists('social_book') && $account->hasPermission('create new books')) {
+      if (\Drupal::moduleHandler()->moduleExists('social_book')) {
         $links['add']['below']['add_book'] = array(
           'classes' => '',
           'link_attributes' => '',
@@ -103,12 +101,15 @@ class AccountHeaderBlock extends BlockBase {
           'title' => $this->t('Create New Book page'),
           'label' => $this->t('New book page'),
           'title_classes' => '',
-          'url' => Url::fromUserInput('/node/add/book'),
+          'url' => Url::fromRoute('node.add', [
+            'node_type' => 'book',
+          ]),
+          'access' => $account->hasPermission('create new books'),
         );
       }
 
       // Check if the current user is allowed to create new pages.
-      if (\Drupal::moduleHandler()->moduleExists('social_page') && $account->hasPermission('create page content')) {
+      if (\Drupal::moduleHandler()->moduleExists('social_page')) {
         $links['add']['below']['add_page'] = array(
           'classes' => '',
           'link_attributes' => '',
@@ -118,13 +119,10 @@ class AccountHeaderBlock extends BlockBase {
           'title' => $this->t('Create New Page'),
           'label' => $this->t('New page'),
           'title_classes' => '',
-          'url' => Url::fromUserInput('/node/add/page'),
+          'url' => Url::fromRoute('node.add', [
+            'node_type' => 'page',
+          ]),
         );
-      }
-
-      // Check if user can create anything of the above if not remove add link.
-      if(count($links['add']['below']) == 0) {
-        unset($links['add']);
       }
 
       if (\Drupal::moduleHandler()->moduleExists('activity_creator')) {
@@ -190,7 +188,7 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('View my profile'),
             'label' => $this->t('My profile'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user'),
+            'url' => Url::fromRoute('user.page'),
           ),
           'my_events' => array(
             'classes' => '',
@@ -201,7 +199,9 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('View my events'),
             'label' => $this->t('My events'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/' . $account_uid . '/events'),
+            'url' => Url::fromRoute('view.events.events_overview', [
+              'user' => $account->id(),
+            ]),
           ),
           'my_topics' => array(
             'classes' => '',
@@ -212,7 +212,9 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('View my topics'),
             'label' => $this->t('My topics'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/' . $account_uid . '/topics'),
+            'url' => Url::fromRoute('view.topics.page_profile', [
+              'user' => $account->id(),
+            ]),
           ),
           'my_groups' => array(
             'classes' => '',
@@ -223,7 +225,9 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('View my groups'),
             'label' => $this->t('My groups'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/' . $account_uid . '/groups'),
+            'url' => Url::fromRoute('view.groups.page_user_groups', [
+              'user' => $account->id(),
+            ]),
           ),
           'divide_account' => array(
             'divider' => 'true',
@@ -239,7 +243,9 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('Edit account'),
             'label' => $this->t('Edit account'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/' . $account_uid . '/edit'),
+            'url' => Url::fromRoute('entity.user.edit_form', [
+              'user' => $account->id(),
+            ]),
           ),
           'edit_profile' => array(
             'classes' => '',
@@ -250,7 +256,11 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('Edit profile'),
             'label' => $this->t('Edit profile'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/' . $account_uid . '/profile'),
+            'url' => Url::fromRoute('entity.profile.type.profile.user_profile_form', [
+              'user' => $account->id(),
+              'profile_type' => 'profile',
+            ]),
+            'access' => $account->hasPermission('add own profile profile') || $account->hasPermission('bypass profile access'),
           ),
           'divide_logout' => array(
             'divider' => 'true',
@@ -266,24 +276,20 @@ class AccountHeaderBlock extends BlockBase {
             'title' => $this->t('Logout'),
             'label' => $this->t('Logout'),
             'title_classes' => '',
-            'url' => Url::fromUserInput('/user/logout'),
+            'url' => Url::fromRoute('user.logout'),
           ),
         ),
       );
 
-      if ($account) {
-        $storage = \Drupal::entityTypeManager()->getStorage('profile');
-        if (!empty($storage)) {
-          $user_profile = $storage->loadByUser($account, 'profile');
-          if ($user_profile) {
-            $content = \Drupal::entityTypeManager()
-              ->getViewBuilder('profile')
-              ->view($user_profile, 'small');
-            $links['account_box']['icon_image'] = $content;
-          }
-        }
-      }
+      $storage = \Drupal::entityTypeManager()->getStorage('profile');
+      $profile = $storage->loadByUser($account, 'profile');
 
+      if ($profile) {
+        $content = \Drupal::entityTypeManager()
+          ->getViewBuilder('profile')
+          ->view($profile, 'small');
+        $links['account_box']['icon_image'] = $content;
+      }
     }
     else {
       $links = [
@@ -300,17 +306,35 @@ class AccountHeaderBlock extends BlockBase {
       ];
     }
 
+    foreach (['add', 'account_box'] as $key) {
+      if (!isset($links[ $key ]['below'])) {
+        continue;
+      }
+
+      foreach ($links[ $key ]['below'] as &$item) {
+        if (!isset($item['access']) && isset($item['url']) && $item['url'] instanceof Url) {
+          $item['access'] = $item['url']->access($account);
+        }
+      }
+    }
+
+    if (isset($links['groups']['url']) && $links['groups']['url'] instanceof Url) {
+      $links['groups']['access'] = $links['groups']['url']->access($account);
+    }
+
     return [
       '#theme' => 'account_header_links',
       '#links' => $links,
-      '#cache' => array(
-        'contexts' => array('user'),
-      ),
-      '#attached' => array(
-        'library' => array(
+      '#cache' => [
+        'contexts' => [
+          'user',
+        ],
+      ],
+      '#attached' => [
+        'library' => [
           'activity_creator/activity_creator.notifications',
-        ),
-      ),
+        ],
+      ],
     ];
   }
 
