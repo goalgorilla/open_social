df19e8e4fcc97ed47c145b5522887dfccfcf5079
Jochem van Nieuwenhuijsen <jochem@goalgorilla.com>
2017-05-19 15:53:45 +0200

    DS-1136 by jochemvn: Also add overrides to social_like. This to override view displays from other modules

---
+++ modules/social_features/social_like/src/SocialLikeConfigOverride.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\social_post_photo\SocialLikeConfigOverride.
+ */
+
+namespace Drupal\social_like;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Example configuration override.
+ */
+class SocialLikeConfigOverride implements ConfigFactoryOverrideInterface {
+  /**
+   * Returns config overrides.
+   *
+   * @param array $names
+   *   A list of configuration names that are being loaded.
+   *
+   * @return array
+   *   An array keyed by configuration name of override data. Override data
+   *   contains a nested array structure of overrides.
+   */
+  public function loadOverrides($names) {
+    $overrides = array();
+
+    $config_factory = \Drupal::service('config.factory');
+
+    // Override post photo default.
+    $config_name = 'core.entity_view_display.post.photo.activity';
+    if (in_array($config_name, $names)) {
+      $config = $config_factory->getEditable($config_name);
+      $content = $config->get('content');
+
+      $content['like_and_dislike'] = [
+        'weight' => 2,
+        'settings' => [],
+        'third_party_settings' => [],
+      ];
+
+      $overrides[$config_name] = [
+        'content' => $content,
+      ];
+    }
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'SocialLikeConfigOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+}
