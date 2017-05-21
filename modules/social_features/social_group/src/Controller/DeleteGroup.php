be9ce6f348249a6b2ecd5fad5472a7d7ce974604
Frank Graave <frankgraave@gmail.com>
2017-05-21 12:46:35 +0200

    DS-3428-closed-groups by frankgraave: added batch controller for multiple deletion of nodes

---
+++ modules/social_features/social_group/src/Controller/DeleteGroup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\social_group\Controller;
+
+use Drupal\Core\Entity\Entity;
+use Drupal\node\Entity\Node;
+
+class DeleteNode {
+
+  public static function deleteNodeExample($nids, &$context) {
+    drupal_set_message(t('Scraping all that stuff...', 'status'));
+    $message = 'Deleting THE SHIT OUT OF IT...';
+    $results = [];
+    foreach ($nids as $nid) {
+      $node = Node::load($nid);
+      //$entity = Entity::load($group_content);
+      $results[] = $node->delete();
+    }
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+
+  function deleteNodeExampleFinishedCallback($success, $results, $operations) {
+    // The 'success' parameter means no fatal PHP errors were detected. All
+    // other error management should be handled using 'results'.
+    if ($success) {
+      $message = \Drupal::translation()->formatPlural(
+        count($results),
+        'One post processed.', '@count posts processed.'
+      );
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);
+  }
+
+}
