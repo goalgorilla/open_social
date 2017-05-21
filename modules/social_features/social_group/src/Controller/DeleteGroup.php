2e1208de77ad93e9a232d1604be7c91c1a3fad9f
Frank Graave <frankgraave@gmail.com>
2017-05-21 15:26:52 +0200

    DS-3428-closed-groups by frankgraave, jaapjan: fixed batch node deletion for all closed group content

--- modules/social_features/social_group/src/Controller/DeleteGroup.php
+++ modules/social_features/social_group/src/Controller/DeleteGroup.php
@@ -2,35 +2,39 @@
 
 namespace Drupal\social_group\Controller;
 
+use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class DeleteNode {
+class DeleteGroup {
 
-  public static function deleteNodeExample($nids, &$context) {
-    drupal_set_message(t('Scraping all that stuff...', 'status'));
-    $message = 'Deleting THE SHIT OUT OF IT...';
+  public static function deleteGroupAndContent($nids, &$context) {
     $results = [];
+
     foreach ($nids as $nid) {
       $node = Node::load($nid);
+      $message = t('Delete @type "@title"', array('@type' => $node->getType(), '@title' => $node->getTitle()));
       $results[] = $node->delete();
     }
     $context['message'] = $message;
     $context['results'] = $results;
   }
 
-  function deleteNodeExampleFinishedCallback($success, $results, $operations) {
+  function deleteGroupAndContentFinishedCallback($success, $results, $operations) {
     // The 'success' parameter means no fatal PHP errors were detected. All
     // other error management should be handled using 'results'.
     if ($success) {
       $message = \Drupal::translation()->formatPlural(
         count($results),
-        'One post processed.', '@count posts processed.'
+        'One item deleted.', '@count items deleted.'
       );
+      // TODO log to the database
     }
     else {
-      $message = t('Finished with an error.');
+      $message = t('There was an unexpected error.');
+      drupal_set_message($message, 'error');
     }
-    drupal_set_message($message);
+    return new RedirectResponse(Url::fromRoute('view.groups.page_user_groups')->setRouteParameter('user', \Drupal::currentUser()->id())->toString());
   }
 
 }
