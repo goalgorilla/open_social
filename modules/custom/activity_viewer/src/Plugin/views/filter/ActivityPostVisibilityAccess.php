a92b97b3e6699938d6b645d7d66519c6ed6f4108
jaapjan <mail@jaapjan.nl>
2017-05-21 22:01:18 +0200

    DS-3428 - Filtering on activity streams for closed groups.

--- modules/custom/activity_viewer/src/Plugin/views/filter/ActivityPostVisibilityAccess.php
+++ modules/custom/activity_viewer/src/Plugin/views/filter/ActivityPostVisibilityAccess.php
@@ -26,7 +26,16 @@ class ActivityPostVisibilityAccess extends FilterPluginBase {
   }
 
   /**
-   * Currently use similar access as for the entity.
+   * Filters out activity items the user is not allowed to see.
+   *
+   * The access to the activity items may be limited by the following:
+   *  1. Value in field_visibility_value on a Post entity.
+   *  2. Node access grants (this includes the field_node_visibility_value and
+   *     the nodes in a closed group)
+   *  3. The comment or post is posted in a (closed) group.
+   *
+   * In addition to the condition used in this filter there may be some other
+   * filters active in the given view (e.g. destination).
    *
    * Probably want to extend this to entity access based on the node grant
    * system when this is implemented.
@@ -34,9 +43,15 @@ class ActivityPostVisibilityAccess extends FilterPluginBase {
    */
   public function query() {
     $account = $this->view->getUser();
-    /* @var \Drupal\views\Plugin\views\query\Sql $this->query */
+
+    $open_groups = social_group_get_all_open_groups();
+    $group_memberships = social_group_get_all_group_members($account->id());
+    $groups = array_merge($open_groups, $group_memberships);
+    $groups_unique = array_unique($groups);
 
     // Add tables and joins.
+    $this->query->addTable('activity__field_activity_recipient_group');
+
     $this->query->addTable('activity__field_activity_entity');
 
     $configuration = array(
@@ -86,6 +101,7 @@ class ActivityPostVisibilityAccess extends FilterPluginBase {
     $and_wrapper = db_and();
     $or = db_or();
 
+    // Nodes: retrieve all the nodes 'created' activity by node access grants.
     $node_access = db_and();
     $node_access->condition('activity__field_activity_entity.field_activity_entity_target_type', 'node', '=');
     $node_access_grants = node_access_grants('view', $account);
@@ -103,46 +119,47 @@ class ActivityPostVisibilityAccess extends FilterPluginBase {
     $node_access->condition($grants);
     $or->condition($node_access);
 
+    // Posts: retrieve all the posts in groups the user is a member of.
+    if ($account->isAuthenticated()) {
+      $posts_in_groups = db_and();
+      $posts_in_groups->condition('activity__field_activity_entity.field_activity_entity_target_type', 'post', '=');
+      $posts_in_groups->condition('post__field_visibility.field_visibility_value', '3', '!=');
+      $posts_in_groups->condition('activity__field_activity_recipient_group.field_activity_recipient_group_target_id', $groups_unique,'IN');
+
+      $or->condition($posts_in_groups);
+    }
+
+    // Posts: all the posts the user has access to by permission.
     $post_access = db_and();
     $post_access->condition('activity__field_activity_entity.field_activity_entity_target_type', 'post', '=');
+    $post_access->isNull('activity__field_activity_recipient_group.field_activity_recipient_group_target_id');
     if (!$account->hasPermission('view public posts')) {
       $post_access->condition('post__field_visibility.field_visibility_value', '1', '!=');
     }
     if (!$account->hasPermission('view community posts')) {
       $post_access->condition('post__field_visibility.field_visibility_value', '2', '!=');
-      // Also do not show recipient posts (e.g. on groups).
+      // Also do not show recipient posts (e.g. on open groups).
       $post_access->condition('post__field_visibility.field_visibility_value', '0', '!=');
     }
 
     $or->condition($post_access);
 
+    // Comments: retrieve comments the user has access to.
     if ($account->hasPermission('access comments')) {
-      // Join comment table.
-      $configuration = array(
-        'left_table' => 'activity__field_activity_entity',
-        'left_field' => 'field_activity_entity_target_id',
-        'table' => 'comment_field_data',
-        'field' => 'cid',
-        'operator' => '=',
-        'extra' => array(
-          0 => array(
-            'left_field' => 'field_activity_entity_target_type',
-            'value' => 'comment',
-          ),
-        ),
-      );
-      $join = Views::pluginManager('join')->createInstance('standard', $configuration);
-      $this->query->addRelationship('comment_field_data', $join, 'comments');
+      $comments_on_content_in_groups = db_and();
+      $comments_on_content_in_groups->condition('activity__field_activity_entity.field_activity_entity_target_type', 'comment', '=');
+      $comments_on_content_in_groups->condition('activity__field_activity_recipient_group.field_activity_recipient_group_target_id', $groups_unique, 'IN');
+      $or->condition($comments_on_content_in_groups);
 
-      $comment_access = db_and();
-      $comment_access->condition('activity__field_activity_entity.field_activity_entity_target_type', 'comment', '=');
-      $or->condition($comment_access);
+      $comments_on_content = db_and();
+      $comments_on_content->condition('activity__field_activity_entity.field_activity_entity_target_type', 'comment', '=');
+      $comments_on_content->isNull('activity__field_activity_recipient_group.field_activity_recipient_group_target_id');
+      $or->condition($comments_on_content);
     }
 
+    // Lets add all the or conditions to the Views query.
     $and_wrapper->condition($or);
-
     $this->query->addWhere('visibility', $and_wrapper);
-
   }
 
 }
