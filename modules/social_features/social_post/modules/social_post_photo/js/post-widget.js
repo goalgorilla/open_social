d589701e564b09caeb9364d97d3a0c5372ddcac8
Peter Polman <info@peterpolman.nl>
2017-05-22 09:26:36 +0200

    DS-1136 by peterpolman: Cleaned script and added delete action.

--- modules/social_features/social_post/modules/social_post_photo/js/post-widget.js
+++ modules/social_features/social_post/modules/social_post_photo/js/post-widget.js
@@ -1,31 +1,26 @@
 /**
  * @file
- * replaces the placeholder of post when a photo is uploaded.
+ * extends the image widget width eventlisteners and triggers for customized presentation.
  */
 
 (function ($) {
 
   'use strict';
 
-  // Change placeholder text when someone adds a photo.
-  Drupal.behaviors.postPlaceholder = {
+  Drupal.behaviors.postPhotoWidget = {
     attach: function (context, settings) {
 
-      // Process this field only once
-      console.log('script init');
-
       $(document, context).once('field-post-image-add').on('click', '#post-photo-add', function(e) {
-        console.log('add image');
-        $('#edit-field-post-image-0-upload').trigger('click');
+        $('input[data-drupal-selector="edit-field-post-image-0-upload"]').trigger('click');
         e.preventDefault();
       });
 
       $(document, context).once('field-post-image-remove').on('click', '#post-photo-remove', function(e) {
-        console.log('remove image');
-        $('button[name="field_post_image_0_remove_button"').trigger('click');
+        $('button[data-drupal-selector="edit-field-post-image-0-remove-button"]').trigger('mousedown');
         e.preventDefault();
       });
 
+      // Change placeholder text when someone adds a photo.
       $('#edit-field-post-image-0-upload').change(function(e) {
         $('#edit-field-post-0-value').attr("placeholder", Drupal.t('Say something about this photo'));
       });
