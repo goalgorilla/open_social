a5fa98d559100ba3581ad18a4474e52a27ffeae9
ksis <ksis@mail.com>
2017-07-05 16:38:49 +0300

    Issue #2892352 by alex.ksis: Add necessary relationship for Event type filter

--- modules/social_features/social_event/modules/social_event_type/src/SocialEventTypeConfigOverride.php
+++ modules/social_features/social_event/modules/social_event_type/src/SocialEventTypeConfigOverride.php
@@ -9,6 +9,7 @@ namespace Drupal\social_event_type;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
 
 /**
  * Example configuration override.
@@ -150,7 +151,10 @@ class SocialEventTypeConfigOverride implements ConfigFactoryOverrideInterface {
           ]
         ];
       }
+    }
 
+    if (isset($overrides['views.view.group_events']['display']['default']['display_options']['filters']['field_event_type_target_id'])) {
+      $overrides['views.view.group_events']['display']['default']['display_options']['filters']['field_event_type_target_id']['relationship'] = 'gc__node';
     }
 
     return $overrides;
