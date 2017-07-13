3a83cf9db30daec1e6a588fca2f040bc6ef27752
ksis <ksis@mail.com>
2017-07-13 16:49:29 +0300

    Issue #2894571 by alex.ksis: Add AccessDeniedHttpException to pretend cyclic redirect

--- modules/social_features/social_group/src/EventSubscriber/RedirectSubscriber.php
+++ modules/social_features/social_group/src/EventSubscriber/RedirectSubscriber.php
@@ -2,17 +2,20 @@
 
 namespace Drupal\social_group\EventSubscriber;
 
-
 use Drupal\Core\Url;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class RedirectSubscriber implements EventSubscriberInterface {
 
-  static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = array('checkForRedirection');
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['checkForRedirection'];
     return $events;
   }
 
@@ -25,9 +28,9 @@ class RedirectSubscriber implements EventSubscriberInterface {
   public function checkForRedirection(GetResponseEvent $event) {
     // Check if there is a group object on the current route.
     $group = _social_group_get_current_group();
-    // Get the current route name for the checks being performed below
-    $routeMatch = \Drupal::routeMatch()->getRouteName();
-    // Get the current user
+    // Get the current route name for the checks being performed below.
+    $route_name = \Drupal::routeMatch()->getRouteName();
+    // Get the current user.
     $user = \Drupal::currentUser();
     // The array of forbidden routes.
     $routes = [
@@ -36,16 +39,23 @@ class RedirectSubscriber implements EventSubscriberInterface {
       'view.group_events.page_group_events',
       'view.group_topics.page_group_topics',
     ];
-    // If a group is set, and the type is closed_group
-    if ($group && $group->getGroupType()->id() == 'closed_group') {
-      if ($user->id() != 1) {
-        if ($user->hasPermission('manage all groups')) {
-          return;
+    // If a group is set, the bundle is closed_group and user does not have
+    // permission to manage all groups.
+    if ($group && $group->bundle() == 'closed_group' && !$user->hasPermission('manage all groups')) {
+      // If the user is not an member of this group.
+      if (!$group->getMember($user) && in_array($route_name, $routes)) {
+        $request = $event->getRequest();
+        $url = Url::fromRoute('view.group_information.page_group_about', [
+          'group' => $group->id(),
+        ]);
+        // To pretend cyclic redirection in cases when the About page is a canonical
+        // page, throw the "access denied" exception.
+        if ($url->toString() === $request->getRequestUri()) {
+          throw new AccessDeniedHttpException();
         }
-        // If the user is not an member of this group
-        elseif (!$group->getMember($user) && in_array($routeMatch, $routes)) {
-          $event->setResponse(new RedirectResponse(Url::fromRoute('view.group_information.page_group_about', ['group' => $group->id()])
-            ->toString()));
+        else {
+          $response = new RedirectResponse($url->toString());
+          $event->setResponse($response);
         }
       }
     }
