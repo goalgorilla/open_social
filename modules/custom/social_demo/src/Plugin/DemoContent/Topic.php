7673d76b05065751f102c823fd949f8baeae0c72
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2017-09-18 20:19:05 +0200

    DS-4005 by bramtenhove: Remove var_dump

--- modules/custom/social_demo/src/Plugin/DemoContent/Topic.php
+++ modules/custom/social_demo/src/Plugin/DemoContent/Topic.php
@@ -79,8 +79,6 @@ class Topic extends DemoNode {
       $entry['field_topic_image'] = $this->prepareImage($item['field_topic_image']);
     }
 
-    var_dump($entry['created']);
-
     return $entry;
   }
 
