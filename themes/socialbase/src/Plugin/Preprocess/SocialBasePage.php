42b06e28e51a3649f55a56e1d792429eb08ede1b
Volodymyr Kenidra <volodymyr.kenidra@lemberg.co.uk>
2017-10-27 17:30:02 +0300

    Issue 2907221 by kedramon: Move content attribute classes to preprocess function.

--- themes/socialbase/src/Plugin/Preprocess/SocialBasePage.php
+++ themes/socialbase/src/Plugin/Preprocess/SocialBasePage.php
@@ -2,11 +2,94 @@
 
 namespace Drupal\socialbase\Plugin\Preprocess;
 
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
+use Drupal\Core\Template\Attribute;
+use Drupal\node\Entity\Node;
+
 /**
  * Pre-processes variables for the "page" theme hook.
  *
  * @ingroup plugins_preprocess
- * @deprecated
- * @see \Drupal\socialbase\Plugin\Preprocess\Page
+ *
+ * @BootstrapPreprocess("page")
  */
-class SocialBasePage extends Page {}
+class SocialBasePage extends PreprocessBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocess(array &$variables, $hook, array $info) {
+
+    $display_page_title = TRUE;
+    // Add needed attributes so later in template we can manipulate with them.
+    $attributes = new Attribute();
+    // Default classes.
+    $attributes->addClass('row', 'container');
+    // If page has title.
+    if ($variables['page']['title']) {
+      $attributes->addClass('with-title-region');
+    }
+
+    // If we have the admin toolbar permission.
+    $user = \Drupal::currentUser();
+
+    // Check for permission.
+    if ($user->hasPermission('access toolbar')) {
+      $variables['#attached']['library'][] = 'socialbase/admin-toolbar';
+    }
+
+    // Add plain title for node preview page templates.
+    if (!empty($variables['page']['#title'])) {
+      $variables['plain_title'] = strip_tags($variables['page']['#title']);
+    }
+
+    // Hide page title for pages where we want to
+    // display it in the Hero instead, like event, topic, basic page.
+    // First determine if we are looking at a node.
+    $nid = \Drupal::routeMatch()->getRawParameter('node');
+    $node = FALSE;
+
+    if (!is_null($nid) && !is_object($nid)) {
+      $node = Node::load($nid);
+    }
+
+    if ($node instanceof Node) {
+
+      // List pages where we want to hide the default page title.
+      $page_to_exclude = [
+        'event',
+        'topic',
+        'page',
+        'book',
+      ];
+
+      // If there is a title and node type is excluded remove class.
+      if (in_array($node->bundle(), $page_to_exclude, TRUE)) {
+        $attributes->removeClass('with-title-region');
+      }
+
+    }
+
+    // Check complementary_top and complementary_bottom variables.
+    if ($variables['page']['complementary_top'] && $variables['page']['complementary_bottom']) {
+      $attributes->addClass('layout--with-complementary');
+    }
+    // Check if sidebars are empty.
+    if (empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
+      $attributes->addClass('layout--with-complementary');
+    }
+    // Sidebars logic.
+    if (empty($variables['page']['complementary_top']) && empty($variables['page']['complementary_bottom'])) {
+      if ($variables['page']['sidebar_first'] && $variables['page']['sidebar_second']) {
+        $attributes->addClass('layout--with-three-columns');
+      }
+      if (!empty($variables['page']['sidebar_second']) || !empty($variables['page']['sidebar_first'])) {
+        $attributes->addClass('layout--with-two-columns');
+      }
+    }
+
+    $variables['content_attributes'] = $attributes;
+
+  }
+
+}
