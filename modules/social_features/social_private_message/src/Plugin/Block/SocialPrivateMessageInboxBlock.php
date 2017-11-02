4eff56c8a7d6005e7105593d904ab27a4e86febf
jochemvn <jochem@goalgorilla.com>
2017-11-02 13:55:09 +0100

    DS-3885 by jochemvn: Override the default inbox block

---
+++ modules/social_features/social_private_message/src/Plugin/Block/SocialPrivateMessageInboxBlock.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\social_private_message\Plugin\Block;
+
+use Drupal\private_message\Plugin\Block\PrivateMessageInboxBlock;
+
+/**
+ * Provides a 'SocialPrivateMessageInboxBlock' block.
+ *
+ * @Block(
+ *   id = "social_private_message_inbox_block",
+ *   admin_label = @Translation("Social Private Message Inbox"),
+ * )
+ */
+class SocialPrivateMessageInboxBlock extends PrivateMessageInboxBlock {
+
+}
