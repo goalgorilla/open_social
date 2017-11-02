e0fe87be1dd041297bc4806cf57ab46f95a3e6e7
jochemvn <jochem@goalgorilla.com>
2017-11-02 10:32:24 +0100

    DS-3885 by jochemvn: Add routing to deny access to old URLs

---
+++ modules/social_features/social_private_message/src/Routing/RouteSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\social_private_message\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * @package Drupal\social_private_message\Routing
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Route the old private message page to the inbox.
+    if ($route = $collection->get('private_message.private_message_page')) {
+      // Deny access to this route.
+      $route->setRequirement('_access', 'FALSE');
+    }
+  }
+}
