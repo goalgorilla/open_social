fea9ea3383c8e90bad07480a5e7850eb9242b8f0
ksis <ksis@mail.com>
2017-11-10 12:45:32 +0200

    Issue #2888728 by alex.ksis: Add controller for redirect to the events overview page

---
+++ modules/social_features/social_event/src/Controller/SocialEventController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\social_event\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class SocialEventController.
+ *
+ * @package Drupal\social_event\Controller
+ */
+class SocialEventController extends ControllerBase {
+
+  /**
+   * Redirectmyevents.
+   *
+   * Redirect to a users events.
+   */
+  public function redirectMyEvents() {
+    return $this->redirect('view.events.events_overview', [
+      'user' => $this->currentUser()->id(),
+    ]);
+  }
+
+}
