abcb2dd0559da31e203057ede7498ae4fea568db
jaapjan <mail@jaapjan.nl>
2017-11-20 17:04:34 +0100

    DS-4357 by jaapjan: let the user set the group of a topic, event node on the node edit page.

---
+++ modules/social_features/social_group/src/SetGroupsForNodeService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\social_group;
+
+use Drupal\group\Entity\Group;
+use Drupal\group\Entity\GroupContent;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+
+/**
+ * Class SetGroupsForNodeService.
+ *
+ * @package Drupal\social_group
+ */
+class SetGroupsForNodeService {
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+  /**
+   * Save groups for a given node.
+   */
+  public static function setGroupsForNode(Node $node, Array $groups_to_remove, Array $groups_to_add) {
+    if ($node->id()) {
+      foreach ($groups_to_remove as $group_id) {
+        $group = Group::load($group_id);
+        self::removeGroupContent($node, $group);
+      }
+      foreach ($groups_to_add as $group_id) {
+        $group = Group::load($group_id);
+        self::addGroupContent($node, $group);
+      }
+    }
+
+    return $node;
+  }
+
+  /**
+   * Creates a group content.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Object of a node.
+   * @param Group $group
+   *   Object of a group.
+   */
+  public static function addGroupContent(NodeInterface $node, Group $group) {
+    // Get the group content plugin.
+    $plugin_id = 'group_node:' . $node->bundle();
+    $plugin = $group->getGroupType()->getContentPlugin($plugin_id);
+    $group_content = GroupContent::create([
+      'type' => $plugin->getContentTypeConfigId(),
+      'gid' => $group->id(),
+      'entity_id' => $node->id(),
+    ]);
+    $group_content->save();
+  }
+
+  /**
+   * Deletes a group content.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Object of a node.
+   * @param Group $group
+   *   Object of a group.
+   */
+  public static function removeGroupContent(NodeInterface $node, Group $group) {
+    // Try to load group content from entity.
+    if ($group_contents = GroupContent::loadByEntity($node)) {
+      /** @var GroupContent $group_content */
+      foreach ($group_contents as $group_content) {
+        if ($group->id() === $group_content->getGroup()->id()) {
+          $group_content->delete();
+        }
+      }
+    }
+  }
+
+}
