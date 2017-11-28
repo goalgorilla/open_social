31c16201bdb9b79d6804d4a2b6aaed0f86f09a56
jaapjan <mail@jaapjan.nl>
2017-11-28 11:09:57 +0100

    DS-4357 by jaapjan: remove activity item when a non-group content has been added to a group.

--- modules/social_features/social_group/src/SetGroupsForNodeService.php
+++ modules/social_features/social_group/src/SetGroupsForNodeService.php
@@ -24,7 +24,18 @@ class SetGroupsForNodeService {
   /**
    * Save groups for a given node.
    */
-  public static function setGroupsForNode(Node $node, array $groups_to_remove, array $groups_to_add) {
+  public static function setGroupsForNode(Node $node, array $groups_to_remove, array $groups_to_add, array $original_groups = []) {
+    // Remove the notifications related to the node if a group is added.
+    if (empty($original_groups) && !empty($groups_to_add)) {
+      $entity_query = \Drupal::entityQuery('activity');
+      $entity_query->condition('field_activity_entity.target_id', $node->id(), '=');
+      $entity_query->condition('field_activity_entity.target_type', 'node', '=');
+      $ids = $entity_query->execute();
+      if (!empty($ids)) {
+        entity_delete_multiple('activity', $ids);
+      }
+    }
+
     if ($node->id()) {
       foreach ($groups_to_remove as $group_id) {
         $group = Group::load($group_id);
