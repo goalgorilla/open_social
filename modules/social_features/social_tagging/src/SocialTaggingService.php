027741ba66b4faf4b4559cd50aec6073439cdd52
jochemvn <jochem@goalgorilla.com>
2017-12-19 10:28:30 +0100

    DS-4352 by jochemvn: Split up fields on a toplevel basis and save them back into the original field

--- modules/social_features/social_tagging/src/SocialTaggingService.php
+++ modules/social_features/social_tagging/src/SocialTaggingService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\social_tagging;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
 
 /**
@@ -24,16 +25,36 @@ class SocialTaggingService {
   protected $termStorage;
 
   /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * SocialTaggingService constructor.
    *
    * @param \Drupal\Core\Entity\EntityManagerInterface $entityManager
    *   Injection of the entitymanager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Injection of the configFactory.
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    */
-  public function __construct(EntityManagerInterface $entityManager) {
+  public function __construct(EntityManagerInterface $entityManager, ConfigFactoryInterface $configFactory) {
     $this->entityManager = $entityManager;
     $this->termStorage = $entityManager->getStorage('taxonomy_term');
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * Returns all the top level term items, that are considered categories.
+   *
+   * @return bool
+   *   Wether tagging is turnded on or not.
+   */
+  public function active() {
+    return (bool) $this->configFactory->get('social_tagging.settings')->get('enable_content_tagging');
   }
 
   /**
@@ -47,6 +68,26 @@ class SocialTaggingService {
     $options = [];
     // Fetch main categories.
     foreach ($this->termStorage->loadTree('social_tagging', 0, 1) as $category) {
+      $options[$category->tid] = $category->name;
+    }
+    // Return array.
+    return $options;
+  }
+
+  /**
+   * Returns the children of top level term items.
+   *
+   * @param int $category
+   *   The category you want to fetch the child items from.
+   *
+   * @return array
+   *   An array of child items.
+   */
+  public function getChildren($category) {
+    // Define as array.
+    $options = [];
+    // Fetch main categories.
+    foreach ($this->termStorage->loadTree('social_tagging', $category, 1) as $category) {
       $options[$category->tid] = $category->name;
     }
     // Return array.
