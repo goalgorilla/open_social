5f3e56b64926111cff30cfe43fc225ee4f9f96f8
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-02-15 17:50:36 +0200

    DS-4617 by chmez: Prepare alternative URL generator.

---
+++ modules/custom/social_language/src/SocialLanguageMetadataBubblingUrlGenerator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\social_language;
+
+use Drupal\Core\Render\MetadataBubblingUrlGenerator;
+
+class SocialLanguageMetadataBubblingUrlGenerator extends MetadataBubblingUrlGenerator {
+
+}
---
+++ modules/custom/social_language/src/SocialLanguageServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\social_language;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+
+/**
+ * Class SocialLanguageServiceProvider.
+ *
+ * @package Drupal\social_language
+ */
+class SocialLanguageServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    $definition = $container->getDefinition('url_generator');
+    $definition->setClass('Drupal\social_language\SocialLanguageMetadataBubblingUrlGenerator');
+  }
+
+}
