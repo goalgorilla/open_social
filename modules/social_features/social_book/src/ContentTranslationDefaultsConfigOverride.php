e62ec5b191326c1b8af7538e67149ef747729110
Alexander Varwijk <alexander@goalgorilla.com>
2018-03-21 13:36:28 +0100

    OSSUPPORT-385 Implement sane content translation defaults
    This groups the sane defaults for translation of basic pages,
    landing pages and book pages. These defaults are only applied
    when the social_language_content module is enabled and the setting
    for that content type is set to true. This allows another module
    (for example a UI) to determine which content types are
    translatable for a sitemanager.
    
    These defaults are applied as a dynamical config override because
    they're contained within other .yml files that we do not want to
    edit because it would create a new dependency (or break without
    the content language module enabled).

---
+++ modules/social_features/social_book/src/ContentTranslationDefaultsConfigOverride.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\social_book;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Provides content translation defaults for the book content type.
+ *
+ * @package Drupal\social_book
+ */
+class ContentTranslationDefaultsConfigOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = [];
+
+    // TODO: This doesn't work if the setting is enabled by an override but allowing the override will create an endless loop.
+    $settings = \Drupal::configFactory()->getEditable('social_language_content.settings');
+    $translate_book = $settings->getOriginal('social_book', FALSE);
+
+    // If the social_language_content settings object doesn't exist or we are
+    // disabled then we perform no overrides.
+    if ($translate_book) {
+      $this->addTranslationOverrides($names, $overrides);
+    }
+
+    return $overrides;
+  }
+  
+  protected function addTranslationOverrides($names, array &$overrides) {
+    $field_overrides = [
+      'core.base_field_override.node.book.title' => [
+        'translatable' => TRUE,
+      ],
+      'core.base_field_override.node.book.menu_link' => [
+        'translatable' => TRUE,
+      ],
+      'core.base_field_override.node.book.path' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.node.book.body' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.node.book.field_page_image' => [
+        'third_party_settings' => [
+          'content_translation' => [
+            'translation_sync' => [
+              'file' => 'file',
+              'alt' => '0',
+              'title' => '0',
+            ],
+          ],
+        ],
+        'translatable' => TRUE,
+      ],
+    ];
+
+    foreach ($field_overrides as $name => $override) {
+      if (in_array($name, $names)) {
+        $overrides[$name] = $override;
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return __CLASS__;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
---
+++ modules/social_features/social_landing_page/src/ContentTranslationDefaultsConfigOverride.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Drupal\social_landing_page;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Provides content translation defaults for the basic page content type.
+ *
+ * @package Drupal\social_page
+ */
+class ContentTranslationDefaultsConfigOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = [];
+
+    $settings = \Drupal::configFactory()->getEditable('social_language_content.settings');
+    $translate_book = $settings->getOriginal('social_landing_page', FALSE);
+
+    // If the social_language_content settings object doesn't exist or we are
+    // disabled then we perform no overrides.
+    if ($translate_book) {
+      $this->addTranslationOverrides($names, $overrides);
+    }
+
+    return $overrides;
+  }
+
+  protected function addTranslationOverrides($names, array &$overrides) {
+    $field_overrides = [
+      'core.base_field_override.node.landing_page.title' => [
+        'translatable' => TRUE,
+      ],
+      'core.base_field_override.node.landing_page.menu_link' => [
+        'translatable' => TRUE,
+      ],
+      'core.base_field_override.node.landing_page.path' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.node.landing_page.body' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.block.field_block_title' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.button.field_button_link_an' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.button.field_button_link_lu' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.featured.field_featured_description' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.featured.field_featured_title' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.hero.field_hero_image' => [
+        'third_party_settings' => [
+          'content_translation' => [
+            'translation_sync' => [
+              'file' => 'file',
+              'alt' => '0',
+              'title' => '0',
+            ],
+          ],
+        ],
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.hero.field_hero_subtitle' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.hero.field_hero_title' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.introduction.field_introduction_text' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.paragraph.introduction.field_introduction_title' => [
+        'translatable' => TRUE,
+      ],
+    ];
+
+    foreach ($field_overrides as $name => $override) {
+      if (in_array($name, $names)) {
+        $overrides[$name] = $override;
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return __CLASS__;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
