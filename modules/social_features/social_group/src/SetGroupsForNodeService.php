9f47684403382b21540a4dc831c3c1d2dd73aed2
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-03-30 18:50:39 +0300

    DS-4551 by chmez: Delete activity about creating content when content is moved.

--- modules/social_features/social_group/src/SetGroupsForNodeService.php
+++ modules/social_features/social_group/src/SetGroupsForNodeService.php
@@ -25,14 +25,23 @@ class SetGroupsForNodeService {
    * Save groups for a given node.
    */
   public static function setGroupsForNode(Node $node, array $groups_to_remove, array $groups_to_add, array $original_groups = []) {
-    // Remove the notifications related to the node if a group is added.
-    if (empty($original_groups) && !empty($groups_to_add)) {
+    // Remove the notifications related to the node if a group is added or
+    // moved.
+    if ((empty($original_groups) || $original_groups != $groups_to_add) && !empty($groups_to_add)) {
       $entity_query = \Drupal::entityQuery('activity');
       $entity_query->condition('field_activity_entity.target_id', $node->id(), '=');
       $entity_query->condition('field_activity_entity.target_type', 'node', '=');
-      $ids = $entity_query->execute();
-      if (!empty($ids)) {
-        entity_delete_multiple('activity', $ids);
+
+      if (!empty($original_groups)) {
+        $template = 'create_' . $node->bundle() . '_group';
+        $messages = \Drupal::entityTypeManager()->getStorage('message')
+          ->loadByProperties(['template' => $template]);
+        $entity_query->condition('field_activity_message.target_id', array_keys($messages), 'IN');
+      }
+
+      if (!empty($ids = $entity_query->execute())) {
+        $controller = \Drupal::entityTypeManager()->getStorage('activity');
+        $controller->delete($controller->loadMultiple($ids));
       }
     }
 
