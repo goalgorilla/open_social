efd5cdba9838078225dcc44a4e20c1b7397ea59e
jochemvn <jochem@goalgorilla.com>
2018-04-11 12:07:50 +0200

    SHN-44 by jochemvn: Add the possibility to turn off my groups on my/other profiles

--- modules/social_features/social_group/src/Controller/SocialGroupController.php
+++ modules/social_features/social_group/src/Controller/SocialGroupController.php
@@ -2,13 +2,44 @@
 
 namespace Drupal\social_group\Controller;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\group\Entity\Group;
+use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Returns responses for Social Group routes.
  */
 class SocialGroupController extends ControllerBase {
+
+  /**
+   * The request.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * SocialTopicController constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The request stack.
+   */
+  public function __construct(RequestStack $requestStack) {
+    $this->requestStack = $requestStack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('request_stack')
+    );
+  }
 
   /**
    * The _title_callback for the view.group_members.page_group_members route.
@@ -61,6 +92,40 @@ class SocialGroupController extends ControllerBase {
    */
   public function groupRemoveContentTitle() {
     return $this->t('Remove a member');
+  }
+
+  /**
+   * Function that checks access on the my topic pages.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account we need to check access for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   If access is allowed.
+   */
+  public function myGroupAccess(AccountInterface $account) {
+    // Fetch user from url.
+    $user = $this->requestStack->getCurrentRequest()->get('user');
+    // If we don't have a user in the request, assume it's my own profile.
+    if (is_null($user)) {
+      // Usecase is the user menu, which is generated on all LU pages.
+      $user = User::load($account->id());
+    }
+
+    // If not a user then just return neutral.
+    if (!$user instanceof User) {
+      $user = User::load($user);
+
+      if (!$user instanceof User) {
+        return AccessResult::neutral();
+      }
+    }
+
+    // Own profile?
+    if ($user->id() === $account->id()) {
+      return AccessResult::allowedIfHasPermission($account, 'view groups on my profile');
+    }
+    return AccessResult::allowedIfHasPermission($account, 'view groups on other profiles');
   }
 
 }
