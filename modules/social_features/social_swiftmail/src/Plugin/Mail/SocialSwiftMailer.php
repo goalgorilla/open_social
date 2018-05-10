eae19316458a4b6e17b93e2bdbc32fb280a0e725
jaapjan <mail@jaapjan.nl>
2018-05-10 15:37:41 +0200

    Issue #2971969 by jaapjan: coding standards.

--- modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
+++ modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\social_swiftmail\Plugin\Mail;
 
-use Drupal\Core\Render\Markup;
-use Drupal\Core\Site\Settings;
 use Drupal\swiftmailer\Plugin\Mail\SwiftMailer;
 
 /**
@@ -17,18 +15,5 @@ use Drupal\swiftmailer\Plugin\Mail\SwiftMailer;
  * )
  */
 class SocialSwiftMailer extends SwiftMailer {
-
-  /**
-   * Massages the message body into the format expected for rendering.
-   *
-   * @param array $message
-   *   The message.
-   *
-   * @return array
-   *   The massaged message.
-   */
-  public function massageMessageBody(array $message) {
-    return parent::massageMessageBody($message);
-  }
 
 }
