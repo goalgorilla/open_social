6ac8b7cde546deef4c083f8bf596986cc635efe8
jaapjan <mail@jaapjan.nl>
2018-05-10 15:00:13 +0200

    Issue #2971969 by jaapjan: remove old and obsolete code in the massageMessageBody() method.

--- modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
+++ modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
@@ -28,19 +28,7 @@ class SocialSwiftMailer extends SwiftMailer {
    *   The massaged message.
    */
   public function massageMessageBody(array $message) {
-
-    // @see: SwiftMailer::massageMessageBody()
-    $line_endings = Settings::get('mail_line_endings', PHP_EOL);
-    $message['body'] = Markup::create(implode($line_endings, array_map(function ($body) {
-      // If the field contains no html tags we can assume newlines will need be
-      // converted to <br>.
-      if (strlen(strip_tags($body)) === strlen($body)) {
-        $body = str_replace("\r", '', $body);
-        $body = str_replace("\n", '<br>', $body);
-      }
-      return check_markup($body, 'full_html');
-    }, $message['body'])));
-    return $message;
+    return parent::massageMessageBody($message);
   }
 
 }
