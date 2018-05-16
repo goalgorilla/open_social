fbc2cbca3122c73e01970e20120d8265897b6a7b
Jaap Jan <jaapjan@users.noreply.github.com>
2018-05-16 15:51:04 +0200

    Issue #2971969 by jaapjan: update swiftmailer 1.0-beta1 to 1.0-beta2 (#875)
    * Issue #2971969 by jaapjan: update swiftmailer 1.0-beta1 to 1.0-beta2.
    
    * Issue #2971969 by jaapjan: added the patch for swiftmailer missing filter_format.
    
    * Issue #2971969 by jaapjan: remove old and obsolete code in the massageMessageBody() method.
    
    * Issue #2971969 by jaapjan: coding standards.

--- modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
+++ modules/social_features/social_swiftmail/src/Plugin/Mail/SocialSwiftMailer.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\social_swiftmail\Plugin\Mail;
 
-use Drupal\Core\Render\Markup;
-use Drupal\Core\Site\Settings;
 use Drupal\swiftmailer\Plugin\Mail\SwiftMailer;
 
 /**
@@ -17,30 +15,5 @@ use Drupal\swiftmailer\Plugin\Mail\SwiftMailer;
  * )
  */
 class SocialSwiftMailer extends SwiftMailer {
-
-  /**
-   * Massages the message body into the format expected for rendering.
-   *
-   * @param array $message
-   *   The message.
-   *
-   * @return array
-   *   The massaged message.
-   */
-  public function massageMessageBody(array $message) {
-
-    // @see: SwiftMailer::massageMessageBody()
-    $line_endings = Settings::get('mail_line_endings', PHP_EOL);
-    $message['body'] = Markup::create(implode($line_endings, array_map(function ($body) {
-      // If the field contains no html tags we can assume newlines will need be
-      // converted to <br>.
-      if (strlen(strip_tags($body)) === strlen($body)) {
-        $body = str_replace("\r", '', $body);
-        $body = str_replace("\n", '<br>', $body);
-      }
-      return check_markup($body, 'full_html');
-    }, $message['body'])));
-    return $message;
-  }
 
 }
