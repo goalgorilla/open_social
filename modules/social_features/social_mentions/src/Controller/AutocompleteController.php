8d13abd248db3450e7f4348ca7eeb18959b94223
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-05-17 19:05:18 +0300

    DS-5206 by chmez: Implement searching members for private message by full name.

--- modules/social_features/social_mentions/src/Controller/AutocompleteController.php
+++ modules/social_features/social_mentions/src/Controller/AutocompleteController.php
@@ -5,8 +5,8 @@ namespace Drupal\social_mentions\Controller;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Connection;
-use Drupal\Core\Database\Database;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\social_profile\SocialProfileTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\user\Entity\User;
@@ -20,6 +20,8 @@ use Symfony\Component\HttpFoundation\Request;
  * @package Drupal\social_mentions\Controller
  */
 class AutocompleteController extends ControllerBase {
+
+  use SocialProfileTrait;
 
   /**
    * The config factory.
@@ -81,51 +83,8 @@ class AutocompleteController extends ControllerBase {
   public function suggestions(Request $request) {
     $name = $request->get('term');
     $config = $this->configFactory->get('mentions.settings');
-    $connection = Database::getConnection();
-    $name = $connection->escapeLike($name);
     $suggestion_format = $config->get('suggestions_format');
-
-    $query = $this->database->select('users', 'u');
-    $query->join('users_field_data', 'uf', 'uf.uid = u.uid');
-
-    switch ($suggestion_format) {
-      case SOCIAL_MENTIONS_SUGGESTIONS_USERNAME:
-        $query->condition('uf.name', '%' . $name . '%', 'LIKE');
-        break;
-
-      case SOCIAL_MENTIONS_SUGGESTIONS_FULL_NAME:
-        $query->join('profile', 'p', 'p.uid = u.uid');
-        $query->join('profile__field_profile_first_name', 'fn', 'fn.entity_id = p.profile_id');
-        $query->join('profile__field_profile_last_name', 'ln', 'ln.entity_id = p.profile_id');
-
-        $or = $query->orConditionGroup();
-        $or
-          ->condition('fn.field_profile_first_name_value', '%' . $name . '%', 'LIKE')
-          ->condition('ln.field_profile_last_name_value', '%' . $name . '%', 'LIKE');
-        $query->condition($or);
-        break;
-
-      case SOCIAL_MENTIONS_SUGGESTIONS_ALL:
-        $query->leftJoin('profile', 'p', 'p.uid = u.uid');
-        $query->leftJoin('profile__field_profile_first_name', 'fn', 'fn.entity_id = p.profile_id');
-        $query->leftJoin('profile__field_profile_last_name', 'ln', 'ln.entity_id = p.profile_id');
-
-        $or = $query->orConditionGroup();
-        $or
-          ->condition('uf.name', '%' . $name . '%', 'LIKE')
-          ->condition('fn.field_profile_first_name_value', '%' . $name . '%', 'LIKE')
-          ->condition('ln.field_profile_last_name_value', '%' . $name . '%', 'LIKE');
-        $query->condition($or);
-        break;
-    }
-
-    $result = $query
-      ->fields('u', ['uid'])
-      ->condition('uf.status', 1)
-      ->range(0, 8)
-      ->execute()
-      ->fetchCol();
-
+    $result = $this->getUserIdsFromName($name, 0, $suggestion_format);
     $response = [];
     $accounts = User::loadMultiple($result);
     $storage = $this->entityTypeManager->getStorage('profile');
@@ -141,7 +100,7 @@ class AutocompleteController extends ControllerBase {
         'profile_id' => '',
       ];
 
-      if ($storage && ($profile = $storage->loadByUser($account, 'profile', TRUE)) && $suggestion_format != SOCIAL_MENTIONS_SUGGESTIONS_USERNAME) {
+      if ($storage && ($profile = $storage->loadByUser($account, 'profile', TRUE)) && $suggestion_format != SOCIAL_PROFILE_SUGGESTIONS_USERNAME) {
         $build = $view_builder->view($profile, 'autocomplete_item');
         $item['html_item'] = render($build);
         $item['profile_id'] = $profile->id();
