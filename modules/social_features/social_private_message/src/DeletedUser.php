25b7aedd0977a6923602a394678b5aded2964168
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-05-18 12:58:16 +0300

    DS-5048 by chmez: Allow sending a private message in a current thread when someone from members is deleted.

---
+++ modules/social_features/social_private_message/src/DeletedUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\social_private_message;
+
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Class DeletedUser.
+ *
+ * @package Drupal\social_private_message
+ */
+class DeletedUser implements AccountInterface {
+
+  /**
+   * The user ID.
+   *
+   * @var int
+   */
+  protected $id;
+
+  /**
+   * DeletedUser constructor.
+   *
+   * @param int $id
+   *   The user ID.
+   */
+  public function __construct($id) {
+    $this->id = $id;
+  }
+
+  /**
+   * Get user ID.
+   *
+   * @return int
+   *   The user ID.
+   */
+  public function id() {
+    return $this->id;
+  }
+
+}
