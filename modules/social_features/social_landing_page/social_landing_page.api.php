b63f0ee87f0e12ef389be4519173820b1fc467bd
Volodymyr Martsynenko <volodymyr.martsynenko@lemberg.co.uk>
2018-06-07 19:08:33 +0300

    Issue #2978155 by Makishima: Add alter hook for the ActivityOverviewBlock content

---
+++ modules/social_features/social_landing_page/social_landing_page.api.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Hooks provided by the Social Landing Page module.
+ */
+
+/**
+ * Alter ActivityOverviewBlock.
+ * @param array $content
+ *   Block content.
+ */
+function hook_social_landing_page_activity_overview_alter(array &$content) {
+
+}
