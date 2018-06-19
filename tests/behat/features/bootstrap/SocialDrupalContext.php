7b3a3ca22a24bb6b1a26e224420e45458249ac25
jaapjan <mail@jaapjan.nl>
2018-06-19 12:00:28 +0200

    OSSUPPORT-904 by jaapjan: use usermanager to retrieve the current user in behat.

--- tests/behat/features/bootstrap/SocialDrupalContext.php
+++ tests/behat/features/bootstrap/SocialDrupalContext.php
@@ -33,7 +33,10 @@ class SocialDrupalContext extends DrupalContext {
    * @Given I am viewing my :type( content):
    */
   public function assertViewingMyNode($type, TableNode $fields) {
-    if (!isset($this->user->uid)) {
+
+    $user_manager = $this->getUserManager();
+    $user = $user_manager->getCurrentUser();
+    if (!$user) {
       throw new \Exception(sprintf('There is no current logged in user to create a node for.'));
     }
 
@@ -47,7 +50,7 @@ class SocialDrupalContext extends DrupalContext {
       $node->{$field} = $value;
     }
 
-    $node->uid = $this->user->uid;
+    $node->uid = $user->uid;
 
     $saved = $this->nodeCreate($node);
 
