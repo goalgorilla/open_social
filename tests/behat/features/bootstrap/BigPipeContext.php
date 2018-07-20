d8cffaa5b072faee757fd9d5c83940d98a94e163
jaapjan <mail@jaapjan.nl>
2018-07-20 12:49:13 +0200

    Add support for Big Pipe. Make sure homepage is set to the correct homepage in the tests.

---
+++ tests/behat/features/bootstrap/BigPipeContext.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ *
+ * Add support for Bigpipe in Behat tests.
+ *
+ * Original PR here:
+ * https://github.com/jhedstrom/drupalextension/pull/325
+ */
+
+use Drupal\big_pipe\Render\Placeholder\BigPipeStrategy;
+use Behat\Mink\Exception\UnsupportedDriverActionException;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Big Pipe context.
+ */
+class BigPipeContext extends RawDrupalContext {
+
+  /**
+   * Prepares Big Pipe NOJS cookie if needed.
+   *
+   * @BeforeScenario
+   */
+  public function prepareBigPipeNoJsCookie()
+  {
+    try {
+      // Check if JavaScript can be executed by Driver.
+      $this->getSession()->getDriver()->executeScript('true');
+    }
+    catch (UnsupportedDriverActionException $e) {
+      // Set NOJS cookie.
+      $this
+        ->getSession()
+        ->setCookie(BigPipeStrategy::NOJS_COOKIE, true);
+    }
+    catch (\Exception $e) {
+      // Mute exceptions.
+    }
+  }
+
+}
