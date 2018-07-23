aab6924af9e6507dcbb815a1bf4326f37149ba27
ronaldtebrake <ronald@goalgorilla.com>
2018-07-23 19:57:52 +0200

    DS-5479 by ronaldtebrake - Add validation and form submit for adding multiple members to a group

--- modules/social_features/social_group/src/Element/SocialGroupEntityAutocomplete.php
+++ modules/social_features/social_group/src/Element/SocialGroupEntityAutocomplete.php
@@ -5,6 +5,7 @@ namespace Drupal\social_group\Element;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\social_core\Entity\Element\EntityAutocomplete;
 use Drupal\Component\Utility\Tags;
+use Drupal\user\Entity\User;
 
 /**
  * Provides an Group member autocomplete form element.
@@ -20,15 +21,26 @@ class SocialGroupEntityAutocomplete extends EntityAutocomplete {
    * Form element validation handler for entity_autocomplete elements.
    */
   public static function validateEntityAutocomplete(array &$element, FormStateInterface $form_state, array &$complete_form) {
-    // For other types not using members, we have entity_id as multiple
-    // options. For example adding multiple members to a group.
-    $input_values = Tags::explode($element['#value']);
+    $duplicated_values = [];
 
+    // Load the current Group so we can see if there are existing members.
+    $group = _social_group_get_current_group();
+
+    // Grab all the input values so we can get the ID's out of them.
+    $input_values = Tags::explode($element['#value']);
     foreach ($input_values as $input) {
       $match = static::extractEntityIdFromAutocompleteInput($input);
       if ($match === NULL) {
+        $options = $element['#selection_settings'] + [
+          'target_type' => $element['#target_type'],
+          'handler' => $element['#selection_handler'],
+        ];
+
+        /* @var /Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface $handler */
+        $handler = \Drupal::service('plugin.manager.entity_reference_selection')->getInstance($options);
         // Try to get a match from the input string when the user didn't use
         // the autocomplete but filled in a value manually.
+        // Got this from the parent::validateEntityAutocomplete.
         $match = static::matchEntityByTitle($handler, $input, $element, $form_state, !$autocreate);
       }
 
@@ -37,22 +49,42 @@ class SocialGroupEntityAutocomplete extends EntityAutocomplete {
           'target_id' => $match,
         ];
 
+        $account = User::load($match);
+        // User is already a member, add it to an array for the Form element
+        // to render an error after all checks are gone.
+        if ($group->getMember($account)) {
+          $duplicated_values[] = $input;
+        }
+
         // Validate input for every single user. This way we make sure that
-        // The element validates one, two or more users added in the autocomplete.
-        // This is because Group doesnt allow adding multiple users at once,
+        // The element validates one, or more users added in the autocomplete.
+        // This is because Group doesn't allow adding multiple users at once,
         // so we need to validate single users, if they all pass we can add
         // them all in the _social_group_action_form_submit.
-
-        $form_state->setValue('entity_id', $value[$match]['target_id']);
         parent::validateEntityAutocomplete($element, $form_state, $complete_form);
       }
     }
 
-    if ($value !== null) {
-      $form_state->setValue('entity_id_new', $value);
+    // If we have duplicates, provide an error message.
+    if (!empty($duplicated_values)) {
+      $usernames = "";
+      foreach ($duplicated_values as $values) {
+        $usernames .= $values . " ";
+      }
+
+      $message = t('Users: @usernames are already members of the group, 
+      you can\'t add them again', ['@usernames' => $usernames]);
+
+      // We have to kick in a form set error here, or else the
+      // GroupContentCardinalityValidator will kick in and show a faulty
+      // error message. Alter this later when Group supports multiple members.
+      $form_state->setError($element, $message);
+      return;
     }
 
-//    parent::validateEntityAutocomplete($element, $form_state, $complete_form);
+    if ($value !== NULL) {
+      $form_state->setValue('entity_id_new', $value);
+    }
   }
 
 }
