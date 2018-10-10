b508e95267861658dfc25e0cda9156fdd942b00b
ronaldtebrake <ronald@goalgorilla.com>
2018-10-10 19:09:47 +0200

    DS-5598 coding standards

--- tests/blackfire/consumer.php
+++ tests/blackfire/consumer.php
@@ -1,46 +1,46 @@
 <?php
 
-/*
+/**
  * This file is part of the Open Social Blackfire testing.
  */
 
-require_once __DIR__.'../../../../../../vendor/autoload.php';
+//require_once __DIR__.'../../../../../../vendor/autoload.php';
 
 use Blackfire\LoopClient;
 use Blackfire\Client;
-use Blackfire\Profile\Configuration as ProfileConfiguration;
+use Blackfire\Profile\Configuration;
 
 // Grab the values from the command line so we can push any data to github.
-$commandLine = getopt(null, ["commit:uuid"]);
+$commandLine = getopt(NULL, ["commit:uuid"]);
 $uuid = $commandLine['uuid'];
 $commit = $commandLine['commit'];
 
 if (empty($uuid) || empty($commit)) {
-    return 0;
+  return 0;
 }
 
-$config = new \Blackfire\Profile\Configuration();
-$blackfire = new \Blackfire\Client();
+$config = new Configuration();
+$blackfire = new Client();
 $probe = $blackfire->createProbe($config);
 
 // Create a build that will show up on our dashboard in blackfire.
 $build = $blackfire->startBuild(
-    $uuid, array(
-    'title' => 'Build from GitHub Commit: ' . $commit,
-    'external_id' => $commit,
-    'trigger_name' => 'Travis CI - Commit: ' . $commit,
-    )
+    $uuid, [
+      'title' => 'Build from GitHub Commit: ' . $commit,
+      'external_id' => $commit,
+      'trigger_name' => 'Travis CI - Commit: ' . $commit,
+    ]
 );
 
 // create a scenario (if the $build argument is null, a new build will be created)
 $scenario = $blackfire->startScenario(
-    $build, array(
-    'title' => 'Test to see if it works.',
-    )
+    $build, [
+      'title' => 'Test to see if it works.',
+    ]
 );
 
 // create a configuration
-$config = new \Blackfire\Profile\Configuration();
+$config = new Configuration();
 $config->setScenario($scenario);
 
 // create as many profiles as you need
