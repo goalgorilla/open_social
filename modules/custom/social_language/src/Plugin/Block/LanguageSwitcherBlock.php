6697c294dd6ee17244da6c3e32d3d6024bf87912
Alexander Varwijk <alexander@goalgorilla.com>
2018-10-29 16:24:27 +0100

    Refactor LanguageSwitcherBlock to use core LanguageSwitcher
    This refactors the language switcher block to reduce the amount of
    code and fix an issue with prefixes. It also adds the block to both
    socialbase and socialblue when the social_language module is enabled.
    
    The block should ensure that it renders nothing if there's only a
    single language for the user.
    
    Optionally the block and optional config should be moved back to
    the social_content_translation module to not alter the behaviour of
    the social_language module.

---
+++ modules/custom/social_language/src/Plugin/Block/LanguageSwitcherBlock.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\social_language\Plugin\Block;
+
+use Drupal\language\Plugin\Block\LanguageBlock;
+use Drupal\Core\Url;
+
+/**
+ * Provides a 'LanguageSwitcherBlock' block.
+ *
+ * This replaces the Drupal core language switcher block because that block
+ * breaks with the SocialLanguageMetadataBubblingUrlGenerator which is needed to
+ * keep users within the same language when viewing content (such as posts) in
+ * a language other than their current.
+ *
+ * @Block(
+ *  id = "language_switcher_block",
+ *  admin_label = @Translation("Language switcher block"),
+ * )
+ */
+class LanguageSwitcherBlock extends LanguageBlock {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    /** @var \Drupal\Core\Language\Language $defaultLanguage */
+    $defaultLanguage = $this->languageManager->getCurrentLanguage();
+
+    // Build the menu.
+    $links = [
+      'language' => [
+        'classes' => 'dropdown',
+        'link_attributes' => 'data-toggle=dropdown aria-expanded=true aria-haspopup=true role=button',
+        'link_classes' => 'dropdown-toggle clearfix',
+        'icon_classes' => 'icon-language',
+        'label' => $defaultLanguage->getId(),
+        'title' => $defaultLanguage->getName() . " (" . $defaultLanguage->getId() . ")",
+        'title_classes' => 'navlabel-language pull-left',
+        'url' => '#',
+      ],
+    ];
+
+    // Get the languages.
+    $route_name = \Drupal::routeMatch()->getRouteName();
+    $languagelinks = $this->languageManager->getLanguageSwitchLinks('language_interface', Url::fromRoute($route_name));
+    $current_path = \Drupal::service('path.current')->getPath();
+
+    // TODO: use configuration for the prefix instead of assuming ISO.
+    $prefixes = \Drupal::config('language.negotiation')->get('url.prefixes');
+
+    // Add languages as links.
+    foreach ($languagelinks->links as $iso => $languagelink) {
+      $links['language']['below'][$iso] = [
+        'classes' => '',
+        'link_attributes' => '',
+        'link_classes' => ($iso === $defaultLanguage) ? 'active' : '',
+        'icon_classes' => '',
+        'icon_label' => '',
+        'label' => $languagelink['title'] . " (" . $iso . ")",
+        'title' => $languagelink['title'] . " (" . $iso . ")",
+        'title_classes' => '',
+        'url' => (($prefixes[$iso] === '') ? '' : '/' . $prefixes[$iso]) . $current_path,
+      ];
+    }
+
+    return [
+      '#theme' => 'account_header_links',
+      '#links' => $links,
+      '#cache' => [
+        'contexts' => ['user', 'url.path', 'languages'],
+      ],
+    ];
+  }
+
+}
