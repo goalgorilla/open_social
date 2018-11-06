800b59ffc8735054eedc1f9ef4657e420e1b6390
ronaldtebrake <ronald@goalgorilla.com>
2018-11-06 07:03:11 +0100

    DS-5504 by ronaldtebrake: updated visibility code

--- modules/social_features/social_group/src/GroupContentVisibilityUpdate.php
+++ modules/social_features/social_group/src/GroupContentVisibilityUpdate.php
@@ -155,7 +155,7 @@ class GroupContentVisibilityUpdate {
     if ($success) {
       // Here we could do something meaningful with the results.
       // We just display the number of nodes we processed...
-      drupal_set_message(t('@count results processed.', ['@count' => count($results])));
+      drupal_set_message(t('@count results processed.', ['@count' => count($results)]));
     }
     else {
       // An error occurred.
