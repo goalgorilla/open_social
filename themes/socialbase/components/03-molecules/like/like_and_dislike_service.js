857a6cd04acb444a39e62632fc03a3038ec7393d
Truls S. Yggeseth <truls@signwo.com>
2018-11-29 18:18:56 +0100

    Issue #3004793 by truls1502, robertragas: Updated the js/like_and_dislike_service.js file

--- themes/socialbase/components/03-molecules/like/like_and_dislike_service.js
+++ themes/socialbase/components/03-molecules/like/like_and_dislike_service.js
@@ -4,28 +4,34 @@
  */
 (function ($, Drupal) {
 
-  window.likeAndDislikeService = (function() {
+  'use strict';
+
+  window.likeAndDislikeService = window.likeAndDislikeService || (function() {
     function likeAndDislikeService() {}
     likeAndDislikeService.vote = function(entity_id, entity_type, tag) {
       $.ajax({
-        type: "GET",
+        type: "POST",
         url: drupalSettings.path.baseUrl + 'like_and_dislike/' + entity_type + '/' + tag + '/' + entity_id,
         success: function(response) {
           // Expected response is a json object where likes is the new number
           // of likes, dislikes is the new number of dislikes, message_type is
           // the type of message to display ("status" or "warning") and message
           // is the message to display.
-          // @todo: Add/remove classes via jQuery.
-          $('#like-container-' + entity_type + '-' + entity_id + ' a').get(0).className = response.operation.like;
-          $('#dislike-container-' + entity_type + '-' + entity_id + ' a').get(0).className = response.operation.dislike;
+          ['like', 'dislike'].forEach(function (iconType) {
+            var selector = '#' + iconType + '-container-' + entity_type + '-' + entity_id;
+            var $aTag = $(selector + ' a');
+            if ($aTag.length == 0) {
+              return;
+            }
+            response.operation[iconType] ? $aTag.addClass('voted') : $aTag.removeClass('voted');
+            $(selector + ' .count').text(response[iconType + 's']);
+          });
 
-          // Updates the likes and dislikes count.
-          if (response.likes == '1') {
-            var countText = Drupal.t(' like');
-          } else {
-            var countText = Drupal.t(' likes');
-          }
-          $('#like-container-' + entity_type + '-' + entity_id).nextAll('.vote__count').find('a').html(response.likes + countText).attr('data-dialog-options', '{"title":"' + response.likes + countText + '", "width":"auto"}');
+          // Display a message whether the vote was registered or an error
+          // happened.
+          // @todo - this will work only for case when theme has messages in
+          // highlighted region.
+          $('.region.region-highlighted').html("<div class='messages__wrapper layout-container'><div class='messages messages--" + response.message_type + " role='contentinfo'>" + response.message + "</div></div>");
         }
       });
     };
