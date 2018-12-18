0b3a16d97c535f1a2c3593fae0cd17fb030a6899
Jaap Jan <jaapjan@users.noreply.github.com>
2018-12-18 12:01:04 +0100

    Issue #3018721 by jaapjan, chmez: As a SM, I want to send an e-mail to members via bulk operation (#1151)
    * Issue #3018721 by jaapjan: add views bulk operation to send an e-mail to the /admin/people overview.
    
    * Issue #3018721 by jaapjan: add dependency to views_bulk_operations.
    
    * Issue #3018721 by jaapjan: add the necessary user action for cancelling and items per page to 50.
    
    * Issue #3018721 by jaapjan: rewrite social_user_export to views bulk operations.
    
    * Issue #3018721 by jaapjan: install views_bulk_operations.
    
    * Issue #3018721 by jaapjan: some more improvements on the view, incl. clearing when changing exposed filters and enabling the module in social_user instead of social_user_export.
    
    * Issue #3018721 by jaapjan: update ExportUser inject ContainerFactoryPluginInterface.
    
    * Issue #3018721 by jaapjan, chmez: some coding standards issues.
    
    * Issue #3018721 by jaapjan, chmez: Fix bug when opening configures form of action for sending E-mails.

--- modules/social_features/social_user_export/src/SocialUserExportOverrides.php
+++ modules/social_features/social_user_export/src/SocialUserExportOverrides.php
@@ -21,94 +21,19 @@ class SocialUserExportOverrides implements ConfigFactoryOverrideInterface {
 
     if (in_array($config_name, $names)) {
       $overrides[$config_name] = [
-        'dependencies' => [
-          'module' => [
-            'group',
-            'social_user_export',
-            'user',
-          ],
-        ],
         'display' => [
           'default' => [
             'display_options' => [
-              'group_by' => TRUE,
-              'filters' => [
-                'id' => [
-                  'id' => 'id',
-                  'table' => 'groups_field_data',
-                  'field' => 'id',
-                  'relationship' => 'gid',
-                  'admin_label' => '',
-                  'operator' => '=',
-                  'value' => [
-                    'min' => '',
-                    'max' => '',
-                    'value' => '',
+              'fields' => [
+                'views_bulk_operations_bulk_form' => [
+                  'selected_actions' => [
+                    'social_user_export_user_action' => 'social_user_export_user_action',
                   ],
-                  'group' => 1,
-                  'exposed' => TRUE,
-                  'expose' => [
-                    'operator_id' => 'id_op',
-                    'label' => 'Group',
-                    'description' => '',
-                    'use_operator' => FALSE,
-                    'operator' => 'id_op',
-                    'identifier' => 'group',
-                    'required' => FALSE,
-                    'remember' => FALSE,
-                    'multiple' => FALSE,
-                    'remember_roles' => [
-                      'authenticated' => 'authenticated',
-                      'anonymoous' => 0,
-                      'administrator' => 0,
-                      'contentmanager' => 0,
-                      'sitemanager' => 0,
+                  'preconfiguration' => [
+                    'social_user_export_user_action' => [
+                      'label_override' => '',
                     ],
                   ],
-                  'is_grouped' => FALSE,
-                  'group_info' => [
-                    'label' => '',
-                    'description' => '',
-                    'identifier' => '',
-                    'optional' => TRUE,
-                    'widget' => 'select',
-                    'multiple' => FALSE,
-                    'remember' => FALSE,
-                    'default_group' => 'All',
-                    'default_group_multiple' => [],
-                    'group_items' => [],
-                  ],
-                  'entity_type' => 'group',
-                  'entity_field' => 'id',
-                  'plugin_id' => 'numeric',
-                ],
-              ],
-              'relationships' => [
-                'group_content' => [
-                  'id' => 'group_content',
-                  'table' => 'users_field_data',
-                  'field' => 'group_content',
-                  'relationship' => 'none',
-                  'group_type' => 'group',
-                  'admin_label' => 'User group content',
-                  'required' => FALSE,
-                  'group_content_plugins' => [
-                    'group_membership' => 'group_membership',
-                  ],
-                  'entity_type' => 'user',
-                  'plugin_id' => 'group_content_to_entity_reverse',
-                ],
-                'gid' => [
-                  'id' => 'gid',
-                  'table' => 'group_content_field_data',
-                  'field' => 'gid',
-                  'relationship' => 'group_content',
-                  'group_type' => 'group',
-                  'admin_label' => 'Group',
-                  'required' => FALSE,
-                  'entity_type' => 'group_content',
-                  'entity_field' => 'gid',
-                  'plugin_id' => 'standard',
                 ],
               ],
             ],
