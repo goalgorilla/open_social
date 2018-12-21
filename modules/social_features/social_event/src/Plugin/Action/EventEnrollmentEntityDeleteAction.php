ae318e50dad1fe54cc91dc774174fe4bf7fcee91
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-12-21 11:11:23 +0200

    Issue #3021721 by chmez: Improve the code of action for sending E-mails.

--- modules/social_features/social_event/src/Plugin/Action/EventEnrollmentEntityDeleteAction.php
+++ modules/social_features/social_event/src/Plugin/Action/EventEnrollmentEntityDeleteAction.php
@@ -3,8 +3,12 @@
 namespace Drupal\social_event\Plugin\Action;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\profile\Entity\ProfileInterface;
 use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Delete event enrollment entity action.
@@ -17,18 +21,67 @@ use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
  *   confirm_form_route_name = "social_event.views_bulk_operations.confirm",
  * )
  */
-class EventEnrollmentEntityDeleteAction extends ViewsBulkOperationsActionBase {
+class EventEnrollmentEntityDeleteAction extends ViewsBulkOperationsActionBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The event enrollment storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $storage;
+
+  /**
+   * The currently active route match object.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a ViewsBulkOperationSendEmail object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->storage = $entity_type_manager->getStorage('event_enrollment');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    $entities = \Drupal::entityTypeManager()->getStorage('event_enrollment')
-      ->loadByProperties([
-        'field_account' => $entity->uid->target_id,
-        'field_event' => $this->context['arguments'][0],
-      ]);
+    $entities = $this->storage->loadByProperties([
+      'field_account' => $entity->uid->target_id,
+      'field_event' => $this->context['arguments'][0],
+    ]);
 
+    /** @var \Drupal\social_event\EventEnrollmentInterface $entity */
     foreach ($entities as $entity) {
       $entity->delete();
     }
@@ -38,13 +91,17 @@ class EventEnrollmentEntityDeleteAction extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    $entities = \Drupal::entityTypeManager()->getStorage('event_enrollment')
-      ->loadByProperties([
+    if ($object instanceof ProfileInterface) {
+      $entities = $this->storage->loadByProperties([
         'field_account' => $object->uid->target_id,
         'field_event' => $this->context['arguments'][0],
       ]);
 
-    $access = AccessResult::allowedIf(!empty($entities));
+      $access = AccessResult::allowedIf(!empty($entities));
+    }
+    else {
+      $access = AccessResult::forbidden();
+    }
 
     return $return_as_object ? $access : $access->isAllowed();
   }
