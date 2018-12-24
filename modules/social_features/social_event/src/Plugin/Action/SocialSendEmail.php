d2c487d9bbf037cd2ac62bae47d91fd9a9c94188
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2018-12-24 11:21:41 +0200

    Issue #3021721 by chmez: Pre-configure action for sending E-mails.

--- modules/social_features/social_event/src/Plugin/Action/SocialSendEmail.php
+++ modules/social_features/social_event/src/Plugin/Action/SocialSendEmail.php
@@ -5,16 +5,16 @@ namespace Drupal\social_event\Plugin\Action;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\profile\Entity\ProfileInterface;
+use Drupal\social_event\EventEnrollmentInterface;
 use Drupal\social_user\Plugin\Action\SocialSendEmail as SocialSendEmailBase;
 
 /**
- * Send email to profile users.
+ * Send email to event enrollment users.
  *
  * @Action(
  *   id = "social_event_send_email_action",
- *   label = @Translation("Send email to profile users"),
- *   type = "profile",
+ *   label = @Translation("Send email to event enrollment users"),
+ *   type = "event_enrollment",
  *   confirm = TRUE,
  *   confirm_form_route_name = "social_event.views_bulk_operations.confirm",
  * )
@@ -25,17 +25,14 @@ class SocialSendEmail extends SocialSendEmailBase {
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    /** @var \Drupal\profile\Entity\ProfileInterface $entity */
-    parent::execute($entity->getOwner());
   }
 
   /**
    * {@inheritdoc}
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    if ($object instanceof ProfileInterface) {
-      /** @var \Drupal\group\Entity\GroupContentInterface $object */
-      $access = $object->getOwner()->access('view', $account, TRUE);
+    if ($object instanceof EventEnrollmentInterface) {
+      $access = AccessResult::allowed();
     }
     else {
       $access = AccessResult::forbidden();
