f9332ded301ead1d0ebe5b81e89756b66818cc08
ronaldtebrake <ronald@goalgorilla.com>
2019-01-08 15:26:55 +0100

    #3021721 by ronaldtebrake: Move field plugin alters to the view, we can just attach our views field to the view instead of altering it

--- modules/social_features/social_event/modules/social_event_managers/src/Form/SocialEventManagersViewsBulkOperationsConfirmAction.php
+++ modules/social_features/social_event/modules/social_event_managers/src/Form/SocialEventManagersViewsBulkOperationsConfirmAction.php
@@ -21,8 +21,8 @@ class SocialEventManagersViewsBulkOperationsConfirmAction extends ConfirmAction 
     if (isset($form_data['action_id'])) {
       $form['description'] = [
         '#markup' => $this->formatPlural($form_data['selected_count'],
-        'Are you sure you want to "%action" the following member?',
-        'Are you sure you want to "%action" the following %count members?',
+        'Are you sure you want to "%action" the following enrollee?',
+        'Are you sure you want to "%action" the following %count enrollees?',
         [
           '%action' => $form_data['action_label'],
           '%count' => $form_data['selected_count'],
@@ -33,8 +33,8 @@ class SocialEventManagersViewsBulkOperationsConfirmAction extends ConfirmAction 
       if (strpos($form_data['action_id'], 'mail') !== FALSE) {
         $form['description'] = [
           '#markup' => $this->formatPlural($form_data['selected_count'],
-            'Are you sure you want to send your email to the following member?',
-            'Are you sure you want to send your email to to the following %count members?',
+            'Are you sure you want to send your email to the following enrollee?',
+            'Are you sure you want to send your email to to the following %count enrollees?',
             [
               '%action' => $form_data['action_label'],
               '%count' => $form_data['selected_count'],
