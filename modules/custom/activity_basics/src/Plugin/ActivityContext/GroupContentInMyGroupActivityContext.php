a33e9beb5a6309baaf7ed459e93931b9695b3e78
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2019-01-18 17:37:39 +0200

    Issue #3026071 by chmez: Prohibit notification for the current user when he or she is a group manager.

--- modules/custom/activity_basics/src/Plugin/ActivityContext/GroupContentInMyGroupActivityContext.php
+++ modules/custom/activity_basics/src/Plugin/ActivityContext/GroupContentInMyGroupActivityContext.php
@@ -4,6 +4,10 @@ namespace Drupal\activity_basics\Plugin\ActivityContext;
 
 use Drupal\activity_creator\ActivityFactory;
 use Drupal\activity_creator\Plugin\ActivityContextBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\Sql\QueryFactory;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'GroupContentInMyGroupActivityContext' acitivy context.
@@ -14,6 +18,56 @@ use Drupal\activity_creator\Plugin\ActivityContextBase;
  * )
  */
 class GroupContentInMyGroupActivityContext extends ActivityContextBase {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a GroupContentInMyGroupActivityContext object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\Query\Sql\QueryFactory $entity_query
+   *   The query factory.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    QueryFactory $entity_query,
+    EntityTypeManagerInterface $entity_type_manager,
+    AccountProxyInterface $current_user
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_query, $entity_type_manager);
+
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.query.sql'),
+      $container->get('entity_type.manager'),
+      $container->get('current_user')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -33,9 +87,17 @@ class GroupContentInMyGroupActivityContext extends ActivityContextBase {
 
       $memberships = $group->getMembers($group->bundle() . '-group_manager');
 
+      // List of managers which shouldn't receive notifications.
+      $account_ids = [
+        // The current user when he/she is a manager.
+        $this->currentUser->id(),
+        // New group member with the "Group manager" role.
+        $group_content->getEntity()->id(),
+      ];
+
       /** @var \Drupal\group\GroupMembership $membership */
       foreach ($memberships as $membership) {
-        if ($membership->getUser()->id() != $group_content->getEntity()->id()) {
+        if (!in_array($membership->getUser()->id(), $account_ids)) {
           $recipients[] = [
             'target_type' => 'user',
             'target_id' => $membership->getUser()->id(),
