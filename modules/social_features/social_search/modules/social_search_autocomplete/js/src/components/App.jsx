d5873b63da4ad430611d3446ab0f6816b1323b9c
Alexander Varwijk <alexander@goalgorilla.com>
2019-01-21 15:58:05 +0100

    Set-up React rendering under the search block
    Adjusts the styling for search to work when more items such as the
    search suggestions field are added. Also adds weights to the search form
    to ensure that it can be properly sorted without rearranging its
    contents.
    
    Finally adds the compiled module javascript as a library and attaches it
    to the search form.

--- modules/social_features/social_search/modules/social_search_autocomplete/js/src/components/App.jsx
+++ modules/social_features/social_search/modules/social_search_autocomplete/js/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return <h1>Hello from React!</h1>;
 }
--- modules/social_features/social_search/modules/social_search_autocomplete/js/src/index.jsx
+++ modules/social_features/social_search/modules/social_search_autocomplete/js/src/index.jsx
@@ -1,5 +1,58 @@
+/**
+ * @file
+ * Javascript for the social search autocomplete.
+ */
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+(function($, Drupal) {
+  /**
+   * Initialises the React rendering for the search suggestions.
+   *
+   * Handles input events on the search field and React rendering in the
+   * provided container.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.initSocialSearchAutocomplete = {
+      attach(context, settings) {
+        console.log("Attached");
+        // TODO: Only load React if it's not yet loaded.
+        // TODO: Ideally only load when search is visible.
+
+        // Find all search suggestion containers to set-up.
+        const $search_suggests = $('.social-search-suggestions', context);
+
+        console.log("Looking for", $search_suggests);
+
+        if (!$search_suggests.length) {
+          return;
+        }
+
+
+
+        $search_suggests.each((index, element) => {
+          // Resolve the search field for this suggestions box.
+          const search_field_id = $(element).attr('data-search-suggestions-for');
+
+          if (!search_field_id) {
+            throw new Error(`Search suggestion element does not have an associated search field (${element}).`);
+          }
+
+          const $search_field = $(`#${search_field_id}`).get(0);
+
+          if (!$search_field) {
+            throw new Error(`Could not find search element with id '${search_field_id}'`);
+          }
+
+          // TODO: Add event listener to search field.
+
+          // Render our app.
+          console.log("Rendering in", element);
+          ReactDOM.render(<App />, element);
+        });
+
+      },
+  };
+})(jQuery, Drupal);
