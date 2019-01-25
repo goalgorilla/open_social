e6c0322bddf0379fe8fe5933b766d3c58816fc20
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2019-01-25 15:42:53 +0200

    Issue #3028367 by chmez: Allow change list of text formats which using the filter for converting URLs.

--- modules/social_features/social_embed/src/SocialEmbedEditorConfigOverride.php
+++ modules/social_features/social_embed/src/SocialEmbedEditorConfigOverride.php
@@ -2,61 +2,52 @@
 
 namespace Drupal\social_embed;
 
-use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Config\ConfigFactoryOverrideInterface;
-use Drupal\Core\Config\StorageInterface;
+use Drupal\Core\Config\Config;
 
 /**
  * Class SocialEmbedEditorConfigOverride.
  *
  * @package Drupal\social_embed
  */
-class SocialEmbedEditorConfigOverride implements ConfigFactoryOverrideInterface {
+class SocialEmbedEditorConfigOverride extends SocialEmbedConfigOverrideBase {
 
   /**
    * {@inheritdoc}
    */
-  public function loadOverrides($names) {
-    $overrides = [];
-    $config_names = [
-      'editor.editor.basic_html',
-      'editor.editor.full_html',
-    ];
-    foreach ($config_names as $config_name) {
-      if (in_array($config_name, $names)) {
-        /* @var \Drupal\Core\Config\ConfigFactory $config */
-        $config = \Drupal::service('config.factory')->getEditable($config_name);
-        if ($settings = $config->get('settings')) {
-          if (isset($settings['toolbar']['rows']) && is_array($settings['toolbar']['rows'])) {
-            $button_exists = FALSE;
-            foreach ($settings['toolbar']['rows'] as $row_id => $row) {
-              foreach ($row as $group_id => $group) {
-                foreach ($group['items'] as $button_id => $button) {
-                  if ($button === 'social_embed') {
-                    $button_exists = TRUE;
-                  }
-                }
-              }
-            }
-            if ($button_exists === FALSE) {
-              $row_array_keys = array_keys($settings['toolbar']['rows']);
-              $last_row_key = end($row_array_keys);
+  public function doOverride(Config $config, $config_name, $convert_url, array &$overrides) {
+    $settings = $config->get('settings');
 
-              $group = [];
-              $group['name'] = 'Embed';
-              $group['items'] = [];
-              $group['items'][] = 'social_embed';
-              $settings['toolbar']['rows'][$last_row_key][] = $group;
-              $overrides[$config_name] = [
-                'settings' => $settings,
-              ];
-            }
+    if (!($settings && isset($settings['toolbar']['rows']) && is_array($settings['toolbar']['rows']))) {
+      return;
+    }
+
+    $button_exists = FALSE;
+
+    foreach ($settings['toolbar']['rows'] as $row) {
+      foreach ($row as $group) {
+        foreach ($group['items'] as $button) {
+          if ($button === 'social_embed') {
+            $button_exists = TRUE;
+            break;
           }
         }
       }
     }
 
-    return $overrides;
+    if (!$button_exists) {
+      $row_array_keys = array_keys($settings['toolbar']['rows']);
+      $last_row_key = end($row_array_keys);
+
+      $group = [];
+      $group['name'] = 'Embed';
+      $group['items'] = [];
+      $group['items'][] = 'social_embed';
+      $settings['toolbar']['rows'][$last_row_key][] = $group;
+
+      $overrides[$config_name] = [
+        'settings' => $settings,
+      ];
+    }
   }
 
   /**
@@ -69,15 +60,8 @@ class SocialEmbedEditorConfigOverride implements ConfigFactoryOverrideInterface 
   /**
    * {@inheritdoc}
    */
-  public function getCacheableMetadata($name) {
-    return new CacheableMetadata();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
-    return NULL;
+  public function getPrefix() {
+    return 'editor.editor';
   }
 
 }
