6ad74e3629c47517678b3ec03dfc405f0453de01
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-01-28 17:11:58 +0100

    HGT-12 - Added Settings form and Event Subscriber for handling Flag events
    The Settings Form allows a user to require a mandatory reason (although not yet implemented) when reporting inappropriate content.
    Next to that a setting exists that unpublishes entities immediately after being reported as inappropriate.
    
    The Event Subscriber takes care of unpublishing entities when the setting is active.

---
+++ modules/social_features/social_content_moderation/src/EventSubscriber/FlagSubscriber.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\social_content_moderation\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\flag\Event\FlagEvents;
+use Drupal\flag\Event\FlaggingEvent;
+
+/**
+ * Class FlagSubscriber.
+ */
+class FlagSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Whether to unpublish the entity immediately on reporting or not.
+   *
+   * @var bool
+   */
+  protected $unpublishImmediately;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Creates a DiffFormatter to render diffs in a table.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity manager.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
+    $this->unpublishImmediately = $config_factory->get('social_content_moderation.settings')->get('unpublish_immediately');
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = [];
+    $events[FlagEvents::ENTITY_FLAGGED][] = ['onFlag'];
+    return $events;
+  }
+
+  /**
+   * Listener for flagging events.
+   *
+   * @param \Drupal\flag\Event\FlaggingEvent $event
+   *   The event when something is flagged.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function onFlag(FlaggingEvent $event) {
+    // Do nothing unless we need to unpublish the entity immediately.
+    // @todo Consider changing the strpos() to a custom hook.
+    if ($this->unpublishImmediately && strpos($event->getFlagging()->getFlagId(), 'report_') === 0) {
+      $flagging = $event->getFlagging();
+
+      // Retrieve the entity.
+      $entity = $this->entityTypeManager->getStorage($flagging->getFlaggable()->getEntityTypeId())
+        ->load($flagging->getFlaggable()->id());
+
+      try {
+        $entity->setPublished(FALSE);
+        $entity->save();
+      }
+      catch (EntityStorageException $exception) {
+        // @todo Log this exception.
+      }
+    }
+
+  }
+
+}
---
+++ modules/social_features/social_content_moderation/src/Form/ContentModerationSettingsForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\social_content_moderation\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class EventSettingsForm.
+ *
+ * @package Drupal\social_content_moderation\Form
+ */
+class ContentModerationSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'social_content_moderation.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'social_content_moderation_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('social_content_moderation.settings');
+
+    // Make reason at reporting mandatory.
+    $form['mandatory_reason'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Mandatory reason'),
+      '#description' => $this->t('Whether users should fill in a mandatory reason or if it is optional.'),
+      '#default_value' => $config->get('mandatory_reason'),
+    );
+
+    // Allow immediate unpublishing.
+    $form['unpublish_immediately'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Unpublished immediately'),
+      '#description' => $this->t('Whether the content is immediately unpublished if a user reports it as inappropriate.'),
+      '#default_value' => $config->get('unpublish_immediately'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->configFactory->getEditable('social_content_moderation.settings')
+      ->set('mandatory_reason', $form_state->get('mandatory_reason'))
+      ->set('unpublish_immediately', $form_state->get('unpublish_immediately'))
+      ->save();
+  }
+
+}
