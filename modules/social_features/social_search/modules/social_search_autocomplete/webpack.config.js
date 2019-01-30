1a234679d578674c43a2db7137146932dc26733c
Alexander Varwijk <alexander@goalgorilla.com>
2019-01-30 11:09:09 +0100

    Remove reference to yoastseo
    This was accidentally left behind by re-using a webpack configuration
    for the RTSEO.js project but is not needed here.

--- modules/social_features/social_search/modules/social_search_autocomplete/webpack.config.js
+++ modules/social_features/social_search/modules/social_search_autocomplete/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.m?jsx?$/,
-        exclude: /node_modules\/(?!yoastseo\/)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         }
