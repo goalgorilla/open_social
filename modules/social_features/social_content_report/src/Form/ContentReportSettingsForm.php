a8e1197e2dbb36a3db8a4a664f5e662b44e42fc9
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-02-01 10:58:41 +0100

    HGT-12 by bramtenhove: Added default terms on install, ability for SM to select with reasons need an extra description and rebuild logic and fields for the reasons

--- modules/social_features/social_content_report/src/Form/ContentReportSettingsForm.php
+++ modules/social_features/social_content_report/src/Form/ContentReportSettingsForm.php
@@ -32,23 +32,38 @@ class ContentReportSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('social_content_report.settings');
-
-    // Make reason at reporting mandatory.
-    $form['mandatory_reason'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Mandatory reason'),
-      '#description' => $this->t('Whether users should fill in a mandatory reason or if it is optional.'),
-      '#default_value' => $config->get('mandatory_reason'),
-    );
+    $config = $this->configFactory->get('social_content_report.settings');
 
     // Allow immediate unpublishing.
-    $form['unpublish_immediately'] = array(
+    $form['unpublish_immediately'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Unpublished immediately'),
       '#description' => $this->t('Whether the content is immediately unpublished if a user reports it as inappropriate.'),
       '#default_value' => $config->get('unpublish_immediately'),
-    );
+    ];
+
+    // A list of reason terms to display the reason textfield for.
+    // @todo Add dependency injection.
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('report_reasons');
+    foreach ($terms as $term) {
+      $reason_terms[$term->tid] = $term->name;
+    }
+
+    $form['reasons_with_text'] = [
+      '#type' => 'checkboxes',
+      '#options' => $reason_terms,
+      '#title' => $this->t('Terms with additional reason text'),
+      '#description' => $this->t('Select the terms that will show an additional field where users can describe their.'),
+      '#default_value' => $config->get('reasons_with_text'),
+    ];
+
+    // Make reason text at reporting mandatory.
+    $form['mandatory_reason'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Mandatory reason text'),
+      '#description' => $this->t('Whether users should fill in a mandatory reason or if it is optional.'),
+      '#default_value' => $config->get('mandatory_reason'),
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -59,9 +74,10 @@ class ContentReportSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $this->configFactory->getEditable('social_content_report.settings')
-      ->set('mandatory_reason', $form_state->get('mandatory_reason'))
-      ->set('unpublish_immediately', $form_state->get('unpublish_immediately'))
+    $this->config('social_content_report.settings')
+      ->set('unpublish_immediately', $form_state->getValue('unpublish_immediately'))
+      ->set('reasons_with_text', $form_state->getValue('reasons_with_text'))
+      ->set('mandatory_reason', $form_state->getValue('mandatory_reason'))
       ->save();
   }
 
