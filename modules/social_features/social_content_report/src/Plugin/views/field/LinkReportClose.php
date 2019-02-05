9f9165856319d477db6690f3eb36063096079c9c
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-02-05 16:58:12 +0100

    HGT-13 by bramtenhove: Added overview for content reports and the ability to close reports

---
+++ modules/social_features/social_content_report/src/Plugin/views/field/LinkReportClose.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\social_content_report\Plugin\views\field;
+
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\field\LinkBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to present a link to close a report.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("flagging_link_close")
+ */
+class LinkReportClose extends LinkBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getUrlInfo(ResultRow $row) {
+    /** @var \Drupal\flag\FlaggingInterface $flagging*/
+    $flagging = $this
+      ->getEntity($row);
+    return Url::fromRoute('social_content_report.close_report', [
+      'flagging' => $flagging->id(),
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDefaultLabel() {
+    return $this
+      ->t('Close');
+  }
+
+}
---
+++ modules/social_features/social_content_report/src/Plugin/views/field/ReportContentType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\social_content_report\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to present the entity type or bundle type for Nodes.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("flagging_entity_bundle_type")
+ */
+class ReportContentType extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {}
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $row) {
+    /** @var \Drupal\flag\FlaggingInterface $flagging*/
+    $flagging = $this
+      ->getEntity($row);
+
+    if ($flagging->getFlaggable()->getEntityTypeId() === 'node') {
+      /** @var \Drupal\node\NodeInterface $node */
+      $node = $flagging->getFlaggable();
+      return ucfirst($node->bundle());
+    }
+    else {
+      return $flagging->getFlaggable()->getEntityType()->getLabel();
+    }
+  }
+
+}
