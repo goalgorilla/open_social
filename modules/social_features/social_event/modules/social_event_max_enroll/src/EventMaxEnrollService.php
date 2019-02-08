bcc5cfbf1712994a671cf0aae34a060ab47057df
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-02-08 17:11:51 +0100

    Added function doc description and comments in the code. Improved source strings based on suggestions by Natasha and added a schema file for the settings.

--- modules/social_features/social_event/modules/social_event_max_enroll/src/EventMaxEnrollService.php
+++ modules/social_features/social_event/modules/social_event_max_enroll/src/EventMaxEnrollService.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\social_event_max_enroll;
 
-use Drupal\node\Entity\Node;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\node\NodeInterface;
 
 /**
  * Class EventMaxEnrollService.
@@ -41,9 +41,15 @@ class EventMaxEnrollService {
   }
 
   /**
-   * Returns number of all enrollments per event.
+   * Get count of enrollments per event.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return int
+   *   The enrollments count.
    */
-  public function getEnrollmentsNumber(Node $node) {
+  public function getEnrollmentsNumber(NodeInterface $node) {
     $query = $this->database->select('event_enrollment__field_enrollment_status', 'eefes');
     $query->join('event_enrollment__field_event', 'eefe', 'eefes.entity_id = eefe.entity_id');
     $query->condition('eefe.field_event_target_id', $node->id());
@@ -56,27 +62,44 @@ class EventMaxEnrollService {
   }
 
   /**
-   * Returns number of left enrollments per event.
+   * Get number of enrollments still possible per event.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return int
+   *   How many spots are left.
    */
-  public function getEnrollmentsLeft(Node $node) {
+  public function getEnrollmentsLeft(NodeInterface $node) {
     // Get max enrollment number.
     $max = $node->get('field_event_max_enroll_num')->value;
     // Take into account AN enrollments.
     $current = $this->getEnrollmentsNumber($node);
-    // Count how many places left.
+    // Count how many spots are left.
     return (int) $max - $current;
   }
 
   /**
    * Check if anonymous enrollment is allowed for given event.
    *
-   * Returns TRUE if feature is enabled and node is event and max enroll is set.
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return bool
+   *   Returns TRUE if feature is enabled, the node is an event and max enroll
+   *   is configured.
    */
-  public function isEnabled(Node $node) {
+  public function isEnabled(NodeInterface $node) {
+    // Check if feature is enabled.
     $is_global_enabled = $this->configFactory->get('social_event_max_enroll.settings')->get('max_enroll');
+
+    // Check if we're working with an event.
     $is_event = $node->getType() === 'event';
+
+    // Get enrollment configuration for this event.
     $is_event_max_enroll = !empty($node->get('field_event_max_enroll')->value);
     $is_event_max_enroll_num = isset($node->get('field_event_max_enroll_num')->value);
+
     return (bool) $is_global_enabled && $is_event && $is_event_max_enroll && $is_event_max_enroll_num;
   }
 
