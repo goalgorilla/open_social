37b92bcfff2f4d9f55cfe1aa352e8fb5260d5b6d
jaapjan <mail@jaapjan.nl>
2019-03-27 17:28:59 +0100

    DS-6373 - Enable module for existing installations, disable sharing optionally.

--- modules/custom/social_lets_connect/modules/social_lets_connect_usage/src/Form/LetsConnectUsageSettingsForm.php
+++ modules/custom/social_lets_connect/modules/social_lets_connect_usage/src/Form/LetsConnectUsageSettingsForm.php
@@ -30,20 +30,30 @@ class LetsConnectUsageSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['why'] = [
       '#type' => 'item',
-      '#markup' => $this->t('Open Social collects data to improve the product. We will never collect any personal identifiable information. You can choose what data you want to share with the Open Social team below. The data will be send once every 24 hours to the Open Social team.'),
+      '#markup' => $this->t('Open Social would like to collect non-personal data to improve the product and your experience. We will never collect any personal data. You can choose which data you want to share with the Open Social team below (or none at all). The usage data is sent every 24 hours to the team.'),
+    ];
+    $form['usage_data_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Share usage data with Open Social team'),
+      '#description' => $this->t("Tick the box to specify which data you want to share. Keep unchecked if you don’t want to share any data."),
+      '#default_value' => $this->config('social_lets_connect_usage.settings')->get('usage_data_enabled'),
     ];
     $form['usage_data'] = [
       '#type' => 'checkboxes',
       '#options' => [
-        'website_url' => $this->t('Website url'),
-        'entity_type_count' => $this->t('Entity type count (only which entities have how many items)'),
+        'website_url' => $this->t('Community URL'),
+        'entity_type_count' => $this->t('Number of items per entity (e.g., nr. of members, topics, events, etc.)'),
         'open_social_version' => $this->t('Open Social version'),
-        'system_data' => $this->t('System data, including PHP version'),
-        'modules_installed' => $this->t('Installed projects (which modules, profiles and themes are installed)'),
+        'system_data' => $this->t('System data (operating system, PHP version, and extensions)'),
+        'modules_installed' => $this->t('Add-on information (including installed modules, profiles, and themes)'),
       ],
-      '#title' => $this->t('Usage data'),
-      '#description' => $this->t('Select which usage data should be shared. We will not collect user data or content. We only collect the data you choose to share.'),
+      '#title' => $this->t('Select which usage data to share'),
       '#default_value' => $this->config('social_lets_connect_usage.settings')->get('usage_data'),
+      '#states' => [
+        'visible' => [
+          ':input[name="usage_data_enabled"]' => ['checked' => TRUE],
+        ],
+      ],
     ];
 
     return parent::buildForm($form, $form_state);
@@ -63,8 +73,10 @@ class LetsConnectUsageSettingsForm extends ConfigFormBase {
 
     // Save config.
     $config = $this->config('social_lets_connect_usage.settings');
-    $config->set('usage_data', $usage_data)
-      ->save();
+    $config->set('usage_data', $usage_data);
+    $usage_data_enabled = $form_state->getValue('usage_data_enabled');
+    $config->set('usage_data_enabled', $usage_data_enabled);
+    $config->save();
 
     parent::submitForm($form, $form_state);
   }
