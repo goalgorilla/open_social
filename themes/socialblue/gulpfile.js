c7428503c2f82e860743b3772498a598ab92f961
Serhii Myronets <myronets.serogka@gmail.com>
2019-03-28 17:25:29 +0200

    ECI-864: Move some templates, update gulpfile, add icons and add and enable variable for icons

--- themes/socialblue/gulpfile.js
+++ themes/socialblue/gulpfile.js
@@ -47,7 +47,9 @@ options.theme = {
   components : options.rootPath.theme + 'components/',
   build      : options.rootPath.theme + 'assets/',
   css        : options.rootPath.theme + 'assets/css/',
-  js         : options.rootPath.theme + 'assets/js/'
+  js         : options.rootPath.theme + 'assets/js/',
+  icons      : options.rootPath.theme + 'assets/icons/',
+  images     : options.rootPath.theme + 'assets/images/'
 };
 
 options.basetheme = {
@@ -260,6 +262,45 @@ gulp.task('scripts', function () {
 
 
 
+
+// #################
+//
+// Sprite icons
+//
+// #################
+//
+// svgmin minifies our SVG files and strips out unnecessary
+// code that you might inherit from your graphics editor.
+// svgstore binds them together in one giant SVG container called
+// icons.svg. Then cheerio gives us the ability to interact with
+// the DOM components in this file in a jQuery-like way. cheerio
+// in this case is removing any fill attributes from the SVG
+// elements (you’ll want to use CSS to manipulate them)
+// and adds a class of .hide to our parent SVG. It gets
+// deposited into our inc directory with the rest of the HTML partials.
+// ===================================================
+
+var svgmin        = require('gulp-svgmin'),
+  svgstore      = require('gulp-svgstore'),
+  cheerio       = require('gulp-cheerio');
+
+gulp.task('icons', function () {
+  return gulp.src(options.theme.images + '**/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe($.rename('icons.svg') )
+    .pipe(cheerio({
+      run: function ($, file) {
+        $('svg').addClass('hidden');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest(options.theme.icons))
+});
+
+
+
+
 // ##################
 // Build style guide.
 // ##################
@@ -431,7 +472,7 @@ gulp.task('lint:sass-with-fail', function () {
 //
 // ##############################
 
-gulp.task('watch', ['watch:css', 'watch:styleguide', 'watch:js'], function () {
+gulp.task('watch', ['watch:icons', 'watch:css', 'watch:styleguide', 'watch:js'], function () {
   if (!options.drupalURL) {
     return Promise.resolve();
   }
@@ -439,6 +480,10 @@ gulp.task('watch', ['watch:css', 'watch:styleguide', 'watch:js'], function () {
     proxy: options.drupalURL,
     noOpen: false
   });
+});
+
+gulp.task('watch:icons', ['icons'], function () {
+  return gulp.watch(options.theme.images + '**/*.svg', ['icons'] );
 });
 
 gulp.task('watch:css', ['styles'], function () {
