46cc2a18b33591925d181d0cf942bbfd2f8cff62
Taras Kruts <2ribel@gmail.com>
2019-04-11 16:57:51 +0300

    Issue #2747581 by ribel, bramtenhove, chmez, ronaldtebrake, jaapjan: max event participants (#1172)
    * Issue #2747581 by ribel: Create Social Event Max Enroll module
    
    * Issue #2747581 by ribel: Take into account enrollment status in counter query
    
    * Issue #2747581 by ribel: Improve code after review
    
    * Issue #2747581 by ribel: Fix spelling after Natasha's review
    
    * Issue #2747581 by ribel: Move max event enroll to separate settings. Add max enroll checkbox to event edit form
    
    * Issue #2747581 by ribel: Convert helper functions to service methods
    
    * Issue #2747581 by ribel: Fix usage of max event enroll service in event modules
    
    * Issue #2747581 by ribel: Add max enrollment info to the page hero title as a suffix
    
    * Issue #2747581 by ribel: Alter enroll forms in max enroll module
    
    * Added function doc description and comments in the code. Improved source strings based on suggestions by Natasha and added a schema file for the settings.
    
    * Issue #2747581 by bramtenhove: Improve inline comments
    
    * Issue #2747581 by bramtenhove: Make sure the spots left is never showing a negative number
    
    * Issue #2747581 by bramtenhove: Fix a bug where double 'enroll' buttons were shown when there were no spots left and the user enrolled anonymously
    
    * Issue #2747581 by bramtenhove: Just visually disable the option to set max number of enrollees for an event when it is mandatory anyway
    
    * Issue #2747581 by ribel: Add new field to Event that allows to disable enroll button
    
    * Issue #2747581 by bramtenhove: Fix issue where unknown module is enforced for newly added field storage
    
    * Issue #2747581 by chmez: Rename "participants" to "enrollees" in the field settings and update module information file.
    
    * Issue #2747581 by chmez: Fix conflict related to adding "Social Event AN Enroll" and "Event Max Enroll" module fields to the "Enrollment options" section of the event form when these two modules are enabled.
    
    * Issue #2747581 by chmez: Create service for getting enrollment status instead of using a custom function.
    
    * Issue #2747581 by chmez: Hide the "Maximum number of enrollees" field when enrollment functionality is disabled.
    
    * Issue #2747581 by chmez: Delete return type hints.
    
    * Issue #2747581 by chmez: Fix syntax of interfaces after deleting return type hints.
    
    * Issue #2747581 by chmez: Create a Behat test for checking settings form and existing new fields on the event form.
    
    * Issue #2747581 by chmez: Fix visibility of field for maximum number enrollees when the limitation is not mandatory.
    
    * Issue #2747581 by chmez: Disable the "Enroll" button only for not enrolled users when all enrollment user places were used.
    
    * Issue #2747581 by chmez: Extend Behat test for checking how working limitation for new enrollments.
    
    * Issue #2747581 by chmez: Add enroll status field to all views of the event.
    
    * Issue #2747581 by ribel: Move the enrolment options more closer to the visibility

---
+++ modules/social_features/social_event/modules/social_event_max_enroll/src/Service/EventMaxEnrollServiceInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\social_event_max_enroll\Service;
+
+use Drupal\node\NodeInterface;
+
+/**
+ * Interface EventMaxEnrollServiceInterface.
+ *
+ * @package Drupal\social_event_max_enroll\Service
+ */
+interface EventMaxEnrollServiceInterface {
+
+  /**
+   * Get count of enrollments per event.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return int
+   *   The enrollments count.
+   */
+  public function getEnrollmentsNumber(NodeInterface $node);
+
+  /**
+   * Get number of enrollments still possible per event.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return int
+   *   How many spots are left.
+   */
+  public function getEnrollmentsLeft(NodeInterface $node);
+
+  /**
+   * Check if anonymous enrollment is allowed for given event.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to check for.
+   *
+   * @return bool
+   *   Returns TRUE if feature is enabled, the node is an event and max enroll
+   *   is configured.
+   */
+  public function isEnabled(NodeInterface $node);
+
+}
