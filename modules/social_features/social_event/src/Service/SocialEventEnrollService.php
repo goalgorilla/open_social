05db214626f17ba0a79c96b31ae4b23d4122851d
Isaac Sierra <isaac_sierra@spe.sony.com>
2019-04-25 12:35:49 -0700

    Issue #3050093 by igasi: Fix create events after an update from previous versions of 5.x

--- modules/social_features/social_event/src/Service/SocialEventEnrollService.php
+++ modules/social_features/social_event/src/Service/SocialEventEnrollService.php
@@ -15,7 +15,7 @@ class SocialEventEnrollService implements SocialEventEnrollServiceInterface {
    * {@inheritdoc}
    */
   public function isEnabled(NodeInterface $node) {
-    if ($node->bundle() === 'event') {
+    if ($node->bundle() === 'event' && $node->hasField('field_event_enroll')) {
       $was_not_changed = $node->field_event_enroll->isEmpty();
       $is_enabled = $node->field_event_enroll->value;
 
