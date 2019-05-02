6cd09c18b3b4d6e3a4bc5282eef0ed9843d2f2f7
Alexander Varwijk <alexander@goalgorilla.com>
2019-05-02 15:34:31 +0200

    Issue #2898087 by Kingdutch: Remove the Secret Groups dependency on r4032login
    There's no need to extend the r4032login exception handler. We can use
    the power of the event system to turn the 403 exception into a 404
    exception when it suits us.
    
    This ensures that the presence of a secret group isn't leaked to users
    that are not allowed to view the group.

--- modules/social_features/social_group/modules/social_group_secret/src/EventSubscriber/SocialGroupSecretSubscriber.php
+++ modules/social_features/social_group/modules/social_group_secret/src/EventSubscriber/SocialGroupSecretSubscriber.php
@@ -2,24 +2,18 @@
 
 namespace Drupal\social_group_secret\EventSubscriber;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\EventSubscriber\CustomPageExceptionHtmlSubscriber;
-use Drupal\Core\EventSubscriber\DefaultExceptionHtmlSubscriber;
-use Drupal\Core\Path\PathMatcherInterface;
-use Drupal\Core\Routing\RedirectDestinationInterface;
+use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\group\Entity\GroupInterface;
-use Drupal\r4032login\EventSubscriber\R4032LoginSubscriber;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class SocialGroupSecretSubscriber.
  *
  * @package Drupal\social_group_secret\EventSubscriber
  */
-class SocialGroupSecretSubscriber extends R4032LoginSubscriber {
+class SocialGroupSecretSubscriber extends HttpExceptionSubscriberBase {
 
   /**
    * The currently active route match object.
@@ -29,54 +23,29 @@ class SocialGroupSecretSubscriber extends R4032LoginSubscriber {
   protected $routeMatch;
 
   /**
-   * The custom page HTML exception.
-   *
-   * @var \Drupal\Core\EventSubscriber\CustomPageExceptionHtmlSubscriber
-   */
-  protected $exceptionCustomPageHtml;
-
-  /**
-   * The default HTML exception.
-   *
-   * @var \Drupal\Core\EventSubscriber\DefaultExceptionHtmlSubscriber
-   */
-  protected $exceptionDefaultHtml;
-
-  /**
    * Constructs a new SocialGroupSecretSubscriber.
    *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   The current user.
-   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
-   *   The redirect destination service.
-   * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
-   *   The path matcher.
-   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
-   *   The event dispatcher.
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The currently active route match object.
-   * @param \Drupal\Core\EventSubscriber\CustomPageExceptionHtmlSubscriber $exception_custom_page_html
-   *   The custom page HTML exception.
-   * @param \Drupal\Core\EventSubscriber\DefaultExceptionHtmlSubscriber $exception_default_html
-   *   The default HTML exception.
    */
-  public function __construct(
-    ConfigFactoryInterface $config_factory,
-    AccountInterface $current_user,
-    RedirectDestinationInterface $redirect_destination,
-    PathMatcherInterface $path_matcher,
-    EventDispatcherInterface $event_dispatcher,
-    RouteMatchInterface $route_match,
-    CustomPageExceptionHtmlSubscriber $exception_custom_page_html,
-    DefaultExceptionHtmlSubscriber $exception_default_html
-  ) {
-    parent::__construct($config_factory, $current_user, $redirect_destination, $path_matcher, $event_dispatcher);
-
+  public function __construct(RouteMatchInterface $route_match) {
     $this->routeMatch = $route_match;
-    $this->exceptionCustomPageHtml = $exception_custom_page_html;
-    $this->exceptionDefaultHtml = $exception_default_html;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getHandledFormats() {
+    return ['html'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static function getPriority() {
+    // We need a higher priority than R4032Login so that it doesn't create a
+    // response to login.
+    return 100;
   }
 
   /**
@@ -86,18 +55,9 @@ class SocialGroupSecretSubscriber extends R4032LoginSubscriber {
     $group = $this->routeMatch->getParameter('group');
 
     // Show 404 page instead of 403 page for secret groups.
-    if ($group && $group instanceof GroupInterface && $group->bundle() === 'secret_group') {
-      $config = $this->configFactory->get('system.site');
-
-      if ($config->get('page.404')) {
-        $this->exceptionCustomPageHtml->on404($event);
-      }
-      else {
-        $this->exceptionDefaultHtml->on404($event);
-      }
-    }
-    else {
-      parent::on403($event);
+    if ($group instanceof GroupInterface && $group->bundle() === 'secret_group') {
+      // Change the exception to show as 404 instead of 403.
+      $event->setException(new NotFoundHttpException());
     }
   }
 
