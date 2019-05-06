2214e8ba8348948047bd2575081145e0acba39aa
Roy Harink <royharink@goalgorilla.com>
2019-05-06 12:02:35 +0200

    TB01894 - Added url from route to create the create url method.

--- modules/custom/social_magic_login/src/Service/MagicUrlCreate.php
+++ modules/custom/social_magic_login/src/Service/MagicUrlCreate.php
@@ -2,34 +2,53 @@
 
 namespace Drupal\social_magic_login\Service;
 
-
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\user\Entity\User;
+use Drupal\Core\Url;
+use Drupal\user\UserInterface;
 
 /**
  * Class MagicLoginCreate.
  */
 class MagicUrlCreate {
 
-  protected $entityTypeManager;
-
-  public function __construct() {
-  }
-
   /**
    * Create a magic login link.
    *
-   * @param int $uid
-   *   The uid of the user.
-   * @param string $destination
-   *   The destination.
+   * @param \Drupal\user\UserInterface $account
+   *   An object containing the user account.
+   * @param array $options
+   *   (optional) A keyed array of settings. Supported options are:
+   *   - langcode: A language code to be used when generating locale-sensitive
+   *    URLs. If langcode is NULL the users preferred language is used.
+   *   - destination: A redirect destination.
+   *    If destination is NULL it's not added.
+   *
+   * @return \Drupal\Core\GeneratedUrl|string
    */
-  public function create($uid, $destination) {
-    // 1. Get user and check if this is allowed to use a link.
-    // 2. Generate some kind of hash to identify the user.
-    // 3. Get the destination the user is supposed to go to.
-    // 4. Store the ID hash, destination and user ID.
-    // 5. Generate the link.
+  public function create(UserInterface $account, array $options) {
+    // Get url options and prerequisites.
+    $timestamp = \Drupal::time()->getRequestTime();
+    $lang_code = isset($options['langcode']) ? $options['langcode'] : $account->getPreferredLangcode();
+    $url_options = [
+      'absolute' => TRUE,
+      'language' => \Drupal::languageManager()->getLanguage($lang_code),
+    ];
+
+    // Add a destination if it's set.
+    if (NULL !== $options['destination']) {
+      $url_options['query']['destination'] = $options['destination'];
+    }
+
+    // Create url from route with the destination if it's set.
+    $url = Url::fromRoute('social_magic_login.login',
+      [
+        'user' => $account->id(),
+        'timestamp' => $timestamp,
+        'hashed_pass' => user_pass_rehash($account, $timestamp)
+      ],
+      $url_options
+    );
+
+    return $url->toUriString();
   }
 
 }
