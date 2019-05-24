53de82a176733557061ac72326d910ece494d591
Robert Ragas <robertragas@goalgorilla.com>
2019-05-24 16:53:51 +0200

    by robertragas: add check to see if entity is not empty, otherwise return right away

--- modules/custom/activity_basics/src/Plugin/ActivityContext/ContentInMyGroupActivityContext.php
+++ modules/custom/activity_basics/src/Plugin/ActivityContext/ContentInMyGroupActivityContext.php
@@ -68,6 +68,14 @@ class ContentInMyGroupActivityContext extends ActivityContextBase {
           'target_id' => $target_id,
         ];
         $group = Group::load($target_id);
+
+        // It could happen that a notification has been queued but the content
+        // has since been deleted. In that case we can find no additional
+        // recipients.
+        if (!$group) {
+          return $recipients;
+        }
+
         $memberships = $group->getMembers();
 
         foreach ($memberships as $membership) {
