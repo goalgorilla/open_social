8af0d135bd57d42cd7a86900f608182fee96a470
Roy Harink <royharink@goalgorilla.com>
2019-05-27 16:41:41 +0200

    TB-1894 - Add Social Magic Login module (#1382)
    Adds a service with interface (so others can implement the service as well) that allows developers to request a one-time login links. The logic for this login link is copied from Drupal 8's password reset functionality.
    
    It encodes the destination in base64 because the ?destination parameter didn't work as expected (causing the login controller to be bypassed). Base64 encoding makes the URL parts safe for transmission in URLs as a single argument.
    
    Typehints in the new service are used because it is code that will only run on PHP 7.2+ and has no existing dependants or dependencies.

---
+++ modules/custom/social_magic_login/src/Service/MagicUrl.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\social_magic_login\Service;
+
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Url;
+use Drupal\user\UserInterface;
+
+/**
+ * Service to generate one-time login links (a.k.a 'magic' #lama's).
+ */
+class MagicUrl implements MagicUrlInterface {
+
+  /**
+   * The path validator service.
+   *
+   * @var \Drupal\Core\Path\PathValidatorInterface
+   */
+  protected $pathValidator;
+
+  /**
+   * MagicUrlCreate constructor.
+   *
+   * @param \Drupal\Core\Path\PathValidatorInterface $pathValidator
+   *   The path validator.
+   */
+  public function __construct(PathValidatorInterface $pathValidator) {
+    $this->pathValidator = $pathValidator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function create(UserInterface $account, string $destination, array $options) : ?Url {
+    if (!isset($account, $destination)) {
+      return NULL;
+    }
+
+    // Check if path isn't external.
+    if (!$this->pathValidator->getUrlIfValidWithoutAccessCheck($destination)) {
+      return NULL;
+    }
+
+    // Get url options and prerequisites.
+    $timestamp = \Drupal::time()->getRequestTime();
+    $lang_code = isset($options['langcode']) ? $options['langcode'] : $account->getPreferredLangcode();
+    $url_options = [
+      'absolute' => TRUE,
+      'language' => \Drupal::languageManager()->getLanguage($lang_code),
+    ];
+
+    // Create url from route with the destination if it's set.
+    return Url::fromRoute('social_magic_login.login',
+      [
+        'uid' => $account->id(),
+        'timestamp' => $timestamp,
+        'hash' => user_pass_rehash($account, $timestamp),
+        'destination' => base64_encode($destination),
+      ],
+      $url_options
+    );
+  }
+
+}
