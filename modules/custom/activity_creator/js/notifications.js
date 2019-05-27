e87c821741ec693ba8514c7c36c337c72ca3c9cf
Roy Harink <royharink@goalgorilla.com>
2019-05-27 12:26:24 +0200

    #3056603 - Added _updateNotificationCount to Drupal JS behaviours.

--- modules/custom/activity_creator/js/notifications.js
+++ modules/custom/activity_creator/js/notifications.js
@@ -3,38 +3,40 @@
  * Update the notification bell badge.
  */
 
-(function ($) {
+(function ($, Drupal) {
   /**
    * Notification centre bell update behavior.
    */
   Drupal.behaviors.notificationUpdate = {
-    attach: function (context, settings) {
+    attach: function (context) {
+      $('.notification-bell', context)
+        .once('notificationUpdate')
+        .click(this._updateNotificationCount);
+    },
 
+    _updateNotificationCount: function () {
+      var $notificationCount = $('.notification-bell .badge');
+
+      // We won't proceed if the notification count is 0 or the dropdown is
+      // open.
       // @todo: Refactor to use data-* attributes and `jQuery.data()` to store
       //   the unread notification count.
-      $('.notification-bell', context).once('notificationUpdate').click(function (e) {
-        var $notificationCount = $('.notification-bell .badge');
+      if (!$notificationCount.html() || $notificationCount.html() === '0' || $(this).hasClass('open')) {
+        return;
+      }
 
-        // We won't proceed if the notification count is 0 or the dropdown is
-        // open.
-        if (!$notificationCount.html() || $notificationCount.html() === '0' || $(this).hasClass('open')) {
-          return;
+      // Post to the notification endpoint.
+      $.ajax({
+        method: 'POST',
+        url: '/ajax/notifications-mark-as-read',
+        data: { },
+        success: function (result) {
+          // Update the notification bell.
+          var $remainingNotifications = result['remaining_notifications'];
+
+          $notificationCount.html($remainingNotifications);
         }
-
-        // Post to the notification endpoint.
-        $.ajax({
-          method: 'POST',
-          url: '/ajax/notifications-mark-as-read',
-          data: { },
-          success: function (result) {
-            // Update the notification bell.
-            var $remainingNotifications = result['remaining_notifications'];
-
-            $notificationCount.html($remainingNotifications);
-          }
-        });
       });
-
     }
   };
-})(jQuery);
+})(jQuery, Drupal);
