fe9c71d5c2be3edbb6de72f2be47f01bb2e98e8e
Roy Harink <royharink@goalgorilla.com>
2019-07-08 11:08:15 +0200

    Issue #3019458 by royharink: Fixed an issue that prevented the social swift mailer settings not to save the applied text format.

--- modules/social_features/social_swiftmail/src/Form/SocialSwiftmailSettingsForm.php
+++ modules/social_features/social_swiftmail/src/Form/SocialSwiftmailSettingsForm.php
@@ -15,7 +15,7 @@ class SocialSwiftmailSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames(): array {
     return [
       'social_swiftmail.settings',
     ];
@@ -24,14 +24,14 @@ class SocialSwiftmailSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId(): string {
     return 'social_swiftmail_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state): array {
     $config = $this->config('social_swiftmail.settings');
 
     $form['remove_open_social_branding'] = [
@@ -49,15 +49,15 @@ class SocialSwiftmailSettingsForm extends ConfigFormBase {
     $form['template']['template_header'] = [
       '#title' => $this->t('Template header'),
       '#type' => 'text_format',
-      '#format' => 'mail_html',
       '#default_value' => $config->get('template_header') ?: '',
+      '#format' => $config->get('template_header_format') ?: 'mail_html',
       '#description' => $this->t('Enter information you want to show in the email notifications header'),
     ];
     $form['template']['template_footer'] = [
       '#title' => $this->t('Template footer'),
       '#type' => 'text_format',
-      '#format' => 'mail_html',
       '#default_value' => $config->get('template_footer') ?: '',
+      '#format' => $config->get('template_footer_format') ?: 'mail_html',
       '#description' => $this->t('Enter information you want to show in the email notifications footer'),
     ];
 
@@ -67,14 +67,23 @@ class SocialSwiftmailSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
     parent::submitForm($form, $form_state);
 
     // Save config.
     $config = $this->config('social_swiftmail.settings');
     $config->set('remove_open_social_branding', $form_state->getValue('remove_open_social_branding'));
-    $config->set('template_header', $form_state->getValue('template_header')['value']);
-    $config->set('template_footer', $form_state->getValue('template_footer')['value']);
+
+    // Get the template header settings.
+    $template_header = $form_state->getValue('template_header');
+    $config->set('template_header', $template_header['value']);
+    $config->set('template_header_format', $template_header['format']);
+
+    // Get the template footer settings.
+    $template_footer = $form_state->getValue('template_footer');
+    $config->set('template_footer', $template_footer['value']);
+    $config->set('template_footer_format', $template_footer['format']);
+
     $config->save();
   }
 
