1739d9c757bb68f9219ff73b71e8c22bf9b457b7
ronaldtebrake <ronald@goalgorilla.com>
2019-07-09 15:40:53 +0200

    TB-2351 - Custom access controle over group manage members

---
+++ modules/social_features/social_group/src/Controller/GroupManagersController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\social_group\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\group\Entity\GroupInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class GroupManagersController.
+ *
+ * @package Drupal\social_group\Controller
+ */
+class GroupManagersController extends ControllerBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Checks access for group management page.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Drupal\Core\Routing\RouteMatch $route_match
+   *   Current route.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   Check standard and custom permissions.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   *   Thrown if the entity type doesn't exist.
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   *   Thrown if the storage handler couldn't be loaded.
+   */
+  public function access(AccountInterface $account, RouteMatch $route_match) {
+    // CM+ are allowed!
+    if ($account->hasPermission('administer members')) {
+      return AccessResult::allowed();
+    }
+
+    /** @var \Drupal\group\Entity\GroupInterface $group */
+    $group = _social_group_get_current_group();
+
+    // Lets allow group managers as well.
+    if ($group->hasPermission('administer members', $account)) {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::forbidden();
+  }
+
+}
