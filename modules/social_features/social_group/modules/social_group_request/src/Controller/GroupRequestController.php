cd9ef533c654f7f6251ac7d131fae7890bcde6bf
Mykhailo Denysiuk <denysyukm@gmail.com>
2019-07-31 10:38:25 +0300

    Issue #2752603 by mdeny: Request membership feature

---
+++ modules/social_features/social_group/modules/social_group_request/src/Controller/GroupRequestController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Drupal\social_group_request\Controller;
+
+use Drupal\Core\Cache\CacheTagsInvalidator;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityFormBuilderInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\group\Entity\GroupContent;
+use Drupal\group\Entity\GroupContentInterface;
+use Drupal\group\Entity\GroupInterface;
+use Drupal\social_group_request\Plugin\GroupContentEnabler\GroupMembershipRequest;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for Group request routes.
+ */
+class GroupRequestController extends ControllerBase {
+
+  /**
+   * The entity form builder service.
+   *
+   * @var \Drupal\Core\Entity\EntityFormBuilderInterface
+   */
+  protected $entityFormBuilder;
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The cache tags invalidator.
+   *
+   * @var \Drupal\Core\Cache\CacheTagsInvalidator
+   */
+  protected $cacheTagsInvalidator;
+
+  /**
+   * The controller constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityFormBuilderInterface $entity_form_builder
+   *   The entity form builder service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(EntityFormBuilderInterface $entity_form_builder, MessengerInterface $messenger, CacheTagsInvalidator $cache_tags_invalidator) {
+    $this->entityFormBuilder = $entity_form_builder;
+    $this->messenger = $messenger;
+    $this->cacheTagsInvalidator = $cache_tags_invalidator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.form_builder'),
+      $container->get('messenger'),
+      $container->get('cache_tags.invalidator')
+    );
+  }
+
+  /**
+   * Builds the form to create new membership on membership request approve.
+   */
+  public function approveRequest(GroupInterface $group, GroupContentInterface $group_content) {
+    /** @var \Drupal\group\Plugin\GroupContentEnablerInterface $plugin */
+    $plugin = $group->getGroupType()->getContentPlugin('group_membership');
+
+    // Pre-populate a group membership from Membership request.
+    $group_content = GroupContent::create([
+      'type' => $plugin->getContentTypeConfigId(),
+      'gid' => $group->id(),
+      'entity_id' => $group_content->get('entity_id')->getString(),
+    ]);
+
+    return $this->entityFormBuilder->getForm($group_content, 'add');
+  }
+
+  /**
+   * Callback to request membership.
+   */
+  public function requestMembership(GroupInterface $group) {
+    $contentTypeConfigId = $group
+      ->getGroupType()
+      ->getContentPlugin('group_membership_request')
+      ->getContentTypeConfigId();
+
+    $group_content = GroupContent::create([
+      'type' => $contentTypeConfigId,
+      'gid' => $group->id(),
+      'entity_id' => $this->currentUser()->id(),
+      'grequest_status' => GroupMembershipRequest::REQUEST_PENDING,
+    ]);
+    $result = $group_content->save();
+
+    if ($result) {
+      $this->messenger()->addMessage($this->t("Your request is waiting for Group Administrator's approval"));
+    }
+    else {
+      $this->messenger()->addMessage($this->t("Error creating request"), self::TYPE_ERROR);
+    }
+
+    $this->cacheTagsInvalidator->invalidateTags(['request-membership']);
+
+     return $this->redirect('social_group.stream', ['group' => $group->id()]);
+  }
+
+  /**
+   * Callback to cancel the request of membership.
+   */
+  public function cancelRequest(GroupInterface $group) {
+    $contentTypeConfigId = $group
+      ->getGroupType()
+      ->getContentPlugin('group_membership_request')
+      ->getContentTypeConfigId();
+
+    $requests = \Drupal::entityTypeManager()->getStorage('group_content')->loadByProperties([
+      'type' => $contentTypeConfigId,
+      'gid' => $group->id(),
+      'entity_id' => $this->currentUser()->id(),
+      'grequest_status' => GroupMembershipRequest::REQUEST_PENDING,
+    ]);
+
+    foreach ($requests as $request) {
+      $request->delete();
+    }
+
+    $this->messenger()->addMessage($this->t("Cancel request of membership has been done successfully."));
+
+    $this->cacheTagsInvalidator->invalidateTags(['request-membership']);
+
+    return $this->redirect('social_group.stream', ['group' => $group->id()]);
+  }
+
+}
---
+++ modules/social_features/social_group/modules/social_group_request/src/Plugin/GroupContentEnabler/GroupMembershipRequest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Drupal\social_group_request\Plugin\GroupContentEnabler;
+
+use Drupal\group\Access\GroupAccessResult;
+use Drupal\group\Entity\GroupInterface;
+use Drupal\group\Entity\GroupContentInterface;
+use Drupal\group\Plugin\GroupContentEnablerBase;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Url;
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a content enabler for users.
+ *
+ * @GroupContentEnabler(
+ *   id = "group_membership_request",
+ *   label = @Translation("Group membership request"),
+ *   description = @Translation("Adds users as requesters for the group."),
+ *   entity_type_id = "user",
+ *   pretty_path_key = "request",
+ *   reference_label = @Translation("Username"),
+ *   reference_description = @Translation("The name of the user you want to make a member"),
+ * )
+ */
+class GroupMembershipRequest extends GroupContentEnablerBase {
+
+  /**
+   * Invitation created and waiting for user's response.
+   */
+  const REQUEST_PENDING = 0;
+
+  /**
+   * Invitation accepted by user.
+   */
+  const REQUEST_ACCEPTED = 1;
+
+  /**
+   * Invitation rejected by user.
+   */
+  const REQUEST_REJECTED = 2;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGroupOperations(GroupInterface $group) {
+    $account = \Drupal::currentUser();
+    $operations = [];
+
+    if (!$group->getMember($account) && $group->hasPermission('request group membership', $account)) {
+      $operations['group-request-membership'] = [
+        'title' => $this->t('Request group membership'),
+        'url' => new Url(
+          'social_group_request.request_membership',
+          ['group' => $group->id()],
+          ['query' => ['destination' => Url::fromRoute('<current>')->toString()]]
+        ),
+        'weight' => 99,
+      ];
+    }
+
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getGroupContentPermissions() {
+    $permissions = parent::getGroupContentPermissions();
+
+    // Add extra permissions specific to membership group content entities.
+    $permissions['request group membership'] = [
+      'title' => 'Request group membership',
+      'allowed for' => ['outsider'],
+    ];
+
+    // These are handled by 'administer members'.
+    unset($permissions['update own group_membership_request content']);
+    unset($permissions['view group_membership_request content']);
+    unset($permissions['create group_membership_request content']);
+    unset($permissions['update any group_membership_request content']);
+    unset($permissions['delete any group_membership_request content']);
+    unset($permissions['delete own group_membership_request content']);
+
+    return $permissions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createAccess(GroupInterface $group, AccountInterface $account) {
+    return GroupAccessResult::allowedIfHasGroupPermission($group, $account, 'administer members');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function viewAccess(GroupContentInterface $group_content, AccountInterface $account) {
+    $group = $group_content->getGroup();
+    return GroupAccessResult::allowedIfHasGroupPermission($group, $account, 'administer members');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function updateAccess(GroupContentInterface $group_content, AccountInterface $account) {
+    $group = $group_content->getGroup();
+    return GroupAccessResult::allowedIfHasGroupPermission($group, $account, 'administer members');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function deleteAccess(GroupContentInterface $group_content, AccountInterface $account) {
+    $group = $group_content->getGroup();
+    return GroupAccessResult::allowedIfHasGroupPermission($group, $account, 'administer members');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityReferenceSettings() {
+    $settings = parent::getEntityReferenceSettings();
+    $settings['handler_settings']['include_anonymous'] = FALSE;
+    return $settings;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $configuration = parent::defaultConfiguration();
+    $configuration['entity_cardinality'] = 1;
+
+    return $configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postInstall() {
+    if (!\Drupal::isConfigSyncing()) {
+      $group_content_type_id = $this->getContentTypeConfigId();
+
+
+      // Add Status field.
+      FieldConfig::create([
+        'field_storage' => FieldStorageConfig::loadByName('group_content', 'grequest_status'),
+        'bundle' => $group_content_type_id,
+        'label' => $this->t('Request status'),
+        'required' => TRUE,
+        'default_value' => self::REQUEST_PENDING,
+      ])->save();
+      // Add "Updated by" field, to save reference to
+      // user who approved/denied request.
+      FieldConfig::create([
+        'field_storage' => FieldStorageConfig::loadByName('group_content', 'grequest_updated_by'),
+        'bundle' => $group_content_type_id,
+        'label' => $this->t('Approved/Rejected by'),
+        'settings' => [
+          'handler' => 'default',
+          'target_bundles' => NULL,
+        ],
+      ])->save();
+
+
+      // Build the 'default' display ID for both the entity form and view mode.
+      $default_display_id = "group_content.$group_content_type_id.default";
+      // Build or retrieve the 'default' view mode.
+      if (!$view_display = EntityViewDisplay::load($default_display_id)) {
+        $view_display = EntityViewDisplay::create([
+          'targetEntityType' => 'group_content',
+          'bundle' => $group_content_type_id,
+          'mode' => 'default',
+          'status' => TRUE,
+        ]);
+      }
+
+
+      // Assign display settings for the 'default' view mode.
+      $view_display
+        ->setComponent('grequest_status', [
+          'type' => 'number_integer',
+        ])
+        ->setComponent('grequest_updated_by', [
+          'label' => 'above',
+          'type' => 'entity_reference_label',
+          'settings' => [
+            'link' => 1,
+          ],
+        ])
+        ->save();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    // Disable the entity cardinality field as the functionality of this module
+    // relies on a cardinality of 1. We don't just hide it, though, to keep a UI
+    // that's consistent with other content enabler plugins.
+    $info = $this->t("This field has been disabled by the plugin to guarantee the functionality that's expected of it.");
+    $form['entity_cardinality']['#disabled'] = TRUE;
+    $form['entity_cardinality']['#description'] .= '<br /><em>' . $info . '</em>';
+
+    return $form;
+  }
+
+}
