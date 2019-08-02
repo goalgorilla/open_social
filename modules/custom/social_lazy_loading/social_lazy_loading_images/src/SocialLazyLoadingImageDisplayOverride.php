739a0ed845db611d35955b94a7b47084f23cec96
ronaldtebrake <ronald@goalgorilla.com>
2019-08-02 12:18:52 +0200

    Issue #3065010 by ronaldtebrake: add lazy loading for all OS related displays that have images

--- modules/custom/social_lazy_loading/social_lazy_loading_images/src/SocialLazyLoadingImageDisplayOverride.php
+++ modules/custom/social_lazy_loading/social_lazy_loading_images/src/SocialLazyLoadingImageDisplayOverride.php
@@ -38,34 +38,82 @@ class SocialLazyLoadingImageDisplayOverride implements ConfigFactoryOverrideInte
   public function loadOverrides($names) {
     $overrides = [];
 
+    $config_fields = [
+      // Event
+      'core.entity_view_display.node.event.teaser' => 'field_event_image',
+      'core.entity_view_display.node.event.hero' => 'field_event_image',
+      'core.entity_view_display.node.event.activity' => 'field_event_image',
+      'core.entity_view_display.node.event.activity_comment' => 'field_event_image',
+      // Topic
+      'core.entity_view_display.node.topic.teaser' => 'field_topic_image',
+      'core.entity_view_display.node.topic.activity' => 'field_topic_image',
+      'core.entity_view_display.node.topic.activity_comment' => 'field_topic_image',
+      // Page
+      'core.entity_view_display.node.page.teaser' => 'field_page_image',
+      'core.entity_view_display.node.page.activity' => 'field_page_image',
+      'core.entity_view_display.node.page.activity_comment' => 'field_page_image',
+      // Groups
+      'core.entity_view_display.group.open_group.teaser' => 'field_group_image',
+      'core.entity_view_display.group.open_group.hero' => 'field_group_image',
+      'core.entity_view_display.group.secret_group.teaser' => 'field_group_image',
+      'core.entity_view_display.group.secret_group.hero' => 'field_group_image',
+      'core.entity_view_display.group.flexible_group.teaser' => 'field_group_image',
+      'core.entity_view_display.group.flexible_group.hero' => 'field_group_image',
+      'core.entity_view_display.group.public_group.teaser' => 'field_group_image',
+      'core.entity_view_display.group.public_group.hero' => 'field_group_image',
+      'core.entity_view_display.group.closed_group.teaser' => 'field_group_image',
+      'core.entity_view_display.group.closed_group.hero' => 'field_group_image',
+      // Posts
+      'core.entity_view_display.post.photo.activity' => 'field_post_image',
+      'core.entity_view_display.post.photo.activity_comment' => 'field_post_image',
+      'core.entity_view_display.post.photo.default' => 'field_post_image',
+      // Comments dont have image fields.
+      // Books
+      'core.entity_view_display.node.book.teaser' => 'field_book_image',
+      'core.entity_view_display.node.book.activity' => 'field_book_image',
+      'core.entity_view_display.node.book.activity_comment' => 'field_book_image',
+      // Profile
+      'core.entity_view_display.profile.profile.compact' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.compact_notification' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.compact_teaser' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.hero' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.small' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.small_teaser' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.table' => 'field_profile_image',
+      'core.entity_view_display.profile.profile.teaser' => 'field_profile_image',
+    ];
+
+    foreach ($config_fields as $config_name => $field_name) {
+      $this->addImageOverride($config_name, $field_name, $overrides);
+    }
+
     return $overrides;
   }
 
   /**
    * Alters the filter settings for the text format.
    *
-   * @param string $text_format
-   *   A config name.
-   * @param bool $convert_url
-   *   TRUE if filter should be used.
+   * @param string $config_name
+   *   A config name to override.
+   * @param string $field_name
+   *   A field to override.
    * @param array $overrides
    *   An override configuration.
    */
-  protected function addFilterOverride($text_format, $convert_url, array &$overrides) {
-    $config_name = 'filter.format.' . $text_format;
-
-    if ($convert_url) {
+  protected function addImageOverride($config_name, $field_name, array &$overrides) {
+    if (!empty($config_name) && !empty($field_name)) {
       $config = $this->configFactory->getEditable($config_name);
       $dependencies = $config->getOriginal('dependencies.module');
       $overrides[$config_name]['dependencies']['module'] = $dependencies;
-      $overrides[$config_name]['dependencies']['module'][] = 'blazy';
+      $overrides[$config_name]['dependencies']['module'][] = 'lazy';
 
-      $overrides[$config_name]['filters']['lazy_filter'] = [
-        'id' => 'lazy_filter',
-        'provider' => 'lazy',
-        'status' => TRUE,
-        'weight' => 999,
-        'settings' => [],
+      $settings = $config->getOriginal('content.' . $field_name . 'third_party_settings');
+
+      $overrides[$config_name]['content'][$field_name]['third_party_settings'] = $settings;
+      $overrides[$config_name]['content'][$field_name]['third_party_settings'] = [
+        'lazy' => [
+          'lazy_image' => '1',
+        ],
       ];
     }
   }
