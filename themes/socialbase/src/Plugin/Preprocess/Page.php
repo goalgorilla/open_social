6c04f18f4477329af5de8fff1153303757a9492e
Taras Kruts <taras.kruts@lemberg.co.uk>
2019-09-02 10:27:57 +0300

    FLAV-22: Remove complementary condition about sidebars

--- themes/socialbase/src/Plugin/Preprocess/Page.php
+++ themes/socialbase/src/Plugin/Preprocess/Page.php
@@ -89,17 +89,17 @@ class Page extends PreprocessBase {
     }
 
     // Check complementary_top and complementary_bottom variables.
-    if ($variables['page']['complementary_top'] || $variables['page']['complementary_bottom']) {
+    if (!empty($variables['page']['complementary_top']) || !empty($variables['page']['complementary_bottom'])) {
       $attributes->addClass('layout--with-complementary');
       $variables['display_complementary'] = TRUE;
     }
     else {
       $variables['display_complementary'] = FALSE;
     }
-    // Check if sidebars are empty.
-    if (empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
-      $attributes->addClass('layout--with-complementary');
-    }
+//    // Check if sidebars are empty.
+//    if (empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
+//      $attributes->addClass('layout--with-complementary');
+//    }
     // Sidebars logic.
     if (empty($variables['page']['complementary_top']) && empty($variables['page']['complementary_bottom'])) {
       if ($variables['page']['sidebar_first'] && $variables['page']['sidebar_second']) {
