0f0437dea3ba6a7f17ec751a418d52fb95384254
Robert Ragas <robertragas@goalgorilla.com>
2019-09-30 10:34:04 +0200

    Issue #3084268 by robertragas: remove caching as views already does this for us

--- modules/social_features/social_group/src/SocialGroupMembersCount.php
+++ modules/social_features/social_group/src/SocialGroupMembersCount.php
@@ -13,13 +13,6 @@ use Drupal\group\Entity\GroupInterface;
 class SocialGroupMembersCount {
 
   /**
-   * A cache of group members count for a specific group.
-   *
-   * @var array
-   */
-  protected $cache;
-
-  /**
    * The database connection object.
    *
    * @var \Drupal\Core\Database\Connection
@@ -41,30 +34,18 @@ class SocialGroupMembersCount {
    *
    * @param \Drupal\group\Entity\GroupInterface $group
    *   The group entity.
-   * @param bool $read_cache
-   *   Whether the per request cache should be used.
    *
    * @return int
    *   Number of members in a group.
    */
-  public function getGroupMemberCount(GroupInterface $group, $read_cache = TRUE) {
-    $cache_type = $group->getGroupType()->id();
-    $cache_id = $group->id();
-
-    if ($read_cache && is_array($this->cache[$cache_type]) && isset($this->cache[$cache_type][$cache_id])) {
-      return $this->cache[$cache_type][$cache_id];
-    }
-
+  public function getGroupMemberCount(GroupInterface $group) {
+    // Additional caching not required since views does this for us.
     $query = $this->database->select('group_content_field_data', 'gcfd');
     $query->addField('gcfd', 'gid');
     $query->condition('gcfd.gid', $group->id());
     $query->condition('gcfd.type', $group->getGroupType()->id() . '-group_membership', 'LIKE');
-    $count = $query->countQuery()->execute()->fetchField();
 
-    // Cache the group id for this entity to optimise future calls.
-    $this->cache[$cache_type][$cache_id] = $count;
-
-    return $count;
+    return $query->countQuery()->execute()->fetchField();
   }
 
 }
