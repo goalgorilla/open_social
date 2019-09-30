86447c0fccb5897f70737d8cf9f97b1886e6e95c
Roy Harink <royharink@goalgorilla.com>
2019-09-30 15:45:09 +0200

    TB-3068 - Added form display widget for crop image.

---
+++ modules/custom/social_image_copyright/src/Plugin/Field/FieldWidget/ImageCropCopyrightAttributeWidget.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\social_image_copyright\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\image_widget_crop\Plugin\Field\FieldWidget\ImageCropWidget;
+
+/**
+ * Plugin implementation of the 'image crop copyright' attribute.
+ *
+ * @FieldWidget(
+ *   id = "image_crop_copyright_attribute",
+ *   label = @Translation("Image crop copyright"),
+ *   field_types = {
+ *     "advanced_image"
+ *   }
+ * )
+ */
+class ImageCropCopyrightAttributeWidget extends ImageCropWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'copyright_attribute' => FALSE,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This is the form display setting when this plugin is chosen as widget.
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element = parent::settingsForm($form, $form_state);
+
+    $element['copyright_attribute'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display copyright attribute'),
+      '#default_value' => $this->getSetting('copyright_attribute'),
+      '#description' => $this->t('The copyright will only be shown when it has been supplied'),
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This is the message that gets displayed as the chosen plugin as a widget.
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+
+    $summary[] = $this->t('Copyright attribute: @bool', [
+      '@bool' => $this->getSetting('copyright_attribute') ? $this->t('Yes') : $this->t('No'),
+    ]);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    // Get the field settings of this widget.
+    $field_settings = $this->getFieldSettings();
+    $element['#image_copyright_field'] = $field_settings['image_copyright_field'];
+    $element['#image_copyright_field_required'] = $field_settings['image_copyright_field_required'];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function process($element, FormStateInterface $form_state, $form) {
+    $item = $element['#value'];
+    $item['fids'] = $element['fids']['#value'];
+
+    $element['copyright'] = [
+      '#type' => 'textfield',
+      '#title' => t('Copyright text'),
+      '#default_value' => isset($item['copyright']) ? $item['copyright'] : '',
+      '#description' => t('The copyright attribute is used as a tooltip when the mouse hovers over the image.'),
+      '#maxlength' => 1024,
+      '#weight' => -10,
+      '#access' => (bool) $item['fids'] && $element['#image_copyright_field'],
+      '#required' => $element['#image_copyright_field_required'],
+      '#element_validate' => $element['#image_copyright_field_required'] == 1 ? [[get_called_class(), 'validateRequiredFields']] : [],
+    ];
+
+    return parent::process($element, $form_state, $form);
+  }
+}
