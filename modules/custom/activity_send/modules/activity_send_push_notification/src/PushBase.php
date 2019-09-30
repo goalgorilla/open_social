fe804dd20768ae3e7bfd6f10cd9f1af5bc113bbf
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2019-09-30 12:54:11 +0300

    Issue #3083976 by chmez: Add a few services to base class.

--- modules/custom/activity_send/modules/activity_send_push_notification/src/PushBase.php
+++ modules/custom/activity_send/modules/activity_send_push_notification/src/PushBase.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\activity_send_push_notification;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\PluginBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class PushBase.
@@ -10,6 +14,61 @@ use Drupal\Core\Plugin\PluginBase;
  * @package Drupal\activity_send_push_notification
  */
 abstract class PushBase extends PluginBase implements PushInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a PushBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    TranslationInterface $string_translation,
+    ConfigFactoryInterface $config_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->setStringTranslation($string_translation);
+
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('string_translation'),
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
