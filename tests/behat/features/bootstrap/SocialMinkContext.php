60e11da10105698c8307f958b322aa3fbeab5543
Taras Kruts <taras.kruts@lemberg.co.uk>
2019-09-30 22:24:56 +0300

    Add custom behat step to set alias even if social_path_manager module is enabled

--- tests/behat/features/bootstrap/SocialMinkContext.php
+++ tests/behat/features/bootstrap/SocialMinkContext.php
@@ -192,4 +192,22 @@ JS;
     }
   }
 
+  /**
+   * Set alias field as specified value
+   * Example: When I set alias as: "bwayne"
+   *
+   * @When /^(?:|I )set alias as "(?P<value>(?:[^"]|\\")*)"$/
+   */
+  public function iSetAlias($value) {
+    // Uncheck "Generate automatic URL alias" if social_path_manager is enabled.
+    if (\Drupal::service('module_handler')->moduleExists('social_path_manager')) {
+      $option = $this->fixStepArgument('Generate automatic URL alias');
+      $this->getSession()->getPage()->uncheckField($option);
+    }
+    // Fill in "URL alias" field with given value
+    $field = $this->fixStepArgument('URL alias');
+    $value = $this->fixStepArgument($value);
+    $this->getSession()->getPage()->fillField($field, $value);
+  }
+
 }
