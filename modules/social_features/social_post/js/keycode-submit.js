b6b90f17703a16d28c666ea9df4f7effe737b2fd
Alberto Alaejos <alberto@goalgorilla.com>
2019-10-14 23:32:31 -0300

    issue #2977781 by jyotisingh: create post using ctrl + enter

--- modules/social_features/social_post/js/keycode-submit.js
+++ modules/social_features/social_post/js/keycode-submit.js
@@ -10,29 +10,24 @@
   Drupal.behaviors.keycodeSubmit = {
     attach: function (context, settings) {
 
-      // Enable CTRL+Enter to submit a form.
-      var keySubmit = function (form, textarea, submit) {
-        textarea.on("keydown", function (e) {
-          // Make sure the textarea contains text.
-          if ($.trim(textarea.val()) != "") {
-            if ((e.keyCode == 13 && e.ctrlKey) || (e.keyCode == 13 && e.metaKey)){
-              e.preventDefault();
-              submit.prop('disabled', true);
-              form.submit();
-            }
-          }
-        });
-      }
-
       // Post form.
-      var postForm = $('#social-post-entity-form');
-      keySubmit($(postForm), $('.form-textarea', postForm), $('.form-submit', postForm));
-
-      // Comment forms.
-      $('.comment-form').each(function () {
-        keySubmit($(this), $('.form-textarea', this), $('.form-submit', this));
-      });
-
+        $('#social-post-entity-form .form-textarea')
+      .off('keydown', onTextAreaKeyDown)
+      .on('keydown', onTextAreaKeyDown);
+      // comment form
+        $('.comment-form .form-textarea').on('keydown')
+      .off('keydown', onTextAreaKeyDown)
+      .on('keydown', onTextAreaKeyDown);
     }
   };
+  function onTextAreaKeyDown(e) {
+    var textarea = $(this);
+    var submit = textarea.closest('form').find('.form-submit');
+    if ($.trim(textarea.val()) != '') {
+      if ((e.keyCode === 13 && e.ctrlKey) || (e.keyCode === 13 && e.metaKey)) {
+        e.preventDefault();
+        submit.trigger('click');
+      }
+    }
+  }
 })(jQuery);
