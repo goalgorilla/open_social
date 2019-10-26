c4ccfc1377bff9a94313a9a4d0d1ab48bebcc734
Ullrich Neiss <ullrich.neiss@crowdcreation.de>
2019-10-26 12:02:26 +0200

    Correct more coding standards

--- modules/social_features/social_comment/src/Form/SocialCommentSettings.php
+++ modules/social_features/social_comment/src/Form/SocialCommentSettings.php
@@ -48,17 +48,11 @@ class SocialCommentSettings extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
     $this->config('social_comment.settings')
       ->set('social_comment_level', $form_state->getValue('social_comment_level'))
       ->save();
   }
+
 }
