d362412a8c0d2b40f69f260dd4ab773798cbcb15
Roy Harink <royharink@goalgorilla.com>
2019-12-06 13:12:53 +0100

    Issue #3097392 by royharink; Load message and users in queue worker.

--- modules/social_features/social_user/src/Plugin/QueueWorker/UserMailQueueProcessor.php
+++ modules/social_features/social_user/src/Plugin/QueueWorker/UserMailQueueProcessor.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\social_user\Plugin\QueueWorker;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Mail\MailManagerInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
 use Psr\Log\LoggerInterface;
@@ -32,24 +33,51 @@ class UserMailQueueProcessor extends QueueWorkerBase {
   protected $logger;
 
   /**
+   * The entity storage.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $storage;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MailManagerInterface $mail_manager, LoggerInterface $logger) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MailManagerInterface $mail_manager, LoggerInterface $logger, EntityTypeManagerInterface $entity_type_manager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->mailManager = $mail_manager;
     $this->logger = $logger;
+    $this->storage = $entity_type_manager;
   }
 
   /**
    * {@inheritdoc}
    */
   public function processItem($data) {
-    $message = $this->mailManager->mail('system', 'action_send_email', $data['email_address'], $data['lang_code'], $data['params'], $data['config']);
-    // Error logging is handled by \Drupal\Core\Mail\MailManager::mail().
-    if ($message['result']) {
-      $this->logger->notice('Sent email to %recipient', [
-        '%recipient' => $data['email_address'],
-      ]);
+    if (!empty($data)) {
+      // Get the email content that needs to be sent.
+      /** @var \Drupal\social_queue_storage\Entity\QueueStorageEntityInterface $queue_storage */
+      $queue_storage = $this->storage->getStorage('queue_storage_entity')->load($data['mail']);
+      if ($queue_storage->bundle() === 'mail') {
+        // Load the users that are in the batch.
+        $users = $this->storage->getStorage('user')->loadMultiple($data['users']);
+
+        /** @var \Drupal\user\UserInterface $user */
+        foreach ($users as $user) {
+          // Send mail.
+          $message = $this->mailManager->mail('system', 'action_send_email', $user->getEmail(), $user->language()->getId(), [
+            'context' => [
+              'subject' => $queue_storage->get('field_subject')->value,
+              'message' => $queue_storage->get('field_message')->value,
+            ],
+          ], $queue_storage->get('field_reply_to')->value);
+          // Error logging is handled by \Drupal\Core\Mail\MailManager::mail().
+          if ($message['result']) {
+            $this->logger->notice('Sent email to %recipient', [
+              '%recipient' => $user->getEmail(),
+            ]);
+          }
+        }
+      }
     }
   }
 
