fdce0e976e3ca6f41bc6e872e1b56a97c2de0794
Roy Harink <royharink@goalgorilla.com>
2019-12-13 15:54:34 +0100

    Issue #3097392 by royharink: Added message template for background finished tasks and added a custom queue storage entity send activity condition for the message template.

---
+++ modules/custom/social_queue_storage/src/Plugin/ActivityEntityCondition/QueueStorageEntitySendActivityCondition.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Drupal\social_queue_storage\Plugin\ActivityEntityCondition;
+
+use Drupal\activity_creator\Plugin\ActivityEntityConditionBase;
+
+/**
+ * Provides a 'QueueStorageEntitySendActivityAction` activity action.
+ *
+ * @ActivityEntityCondition(
+ *   id = "queue_store_entity_send_activity_condition",
+ *   label = @Translation("Queue Store Entity has is finished status."),
+ *  entities = {"queue_storage_entity" = {}}
+ * )
+ */
+class QueueStorageEntitySendActivityCondition extends ActivityEntityConditionBase {
+
+}
