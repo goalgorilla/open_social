243403dfae5de3dae74d4828b0b06ee9b9e5e7d1
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2019-12-23 16:23:15 +0100

    Issue #3097392 by bramtenhove: Fix fresh install dependency issues

--- modules/social_features/social_core/social_core.post_update.php
+++ modules/social_features/social_core/social_core.post_update.php
@@ -8,7 +8,7 @@
 /**
  * Enable the queue storage entity module.
  */
-function social_core_post_update_enable_queue_storage() {
+function social_core_post_update_8701_enable_queue_storage() {
   \Drupal::service('module_installer')->install([
     'social_queue_storage',
   ]);
