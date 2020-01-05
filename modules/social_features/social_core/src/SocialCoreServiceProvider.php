9455d0f64d3e3c8601f93735d892b1d2ef7e09b7
ronaldtebrake <ronald@goalgorilla.com>
2020-01-05 15:26:07 +0100

    DS-7024 by ronaldtebrake: add our new layout service only when layout builder is enabled so as part of service provider alter

--- modules/social_features/social_core/src/SocialCoreServiceProvider.php
+++ modules/social_features/social_core/src/SocialCoreServiceProvider.php
@@ -4,6 +4,8 @@ namespace Drupal\social_core;
 
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Drupal\social_core\Service\LayoutService;
 
 /**
  * Class SocialCoreServiceProvider.
@@ -19,6 +21,15 @@ class SocialCoreServiceProvider extends ServiceProviderBase {
     // Overrides language_manager class to test domain language negotiation.
     $definition = $container->getDefinition('entity.autocomplete_matcher');
     $definition->setClass('Drupal\social_core\Entity\EntityAutocompleteMatcher');
+
+    $modules = $container->getParameter('container.modules');
+
+    // Check for installed layout_builder module.
+    if (isset($modules['layout_builder'])) {
+      // Add a normalizer service for file entities.
+      $service_definition = new Definition(LayoutService::class);
+      $container->setDefinition('social_core.layout', $service_definition);
+    }
   }
 
 }
