ffb5802e85ac575285c9931ee51a9c64a22e5dbd
Alexander Varwijk <alexander@getopensocial.com>
2020-02-05 11:11:29 +0100

    Mute all status messages in Social Demo
    There are more modules that get triggered by demo content creation. It's
    easier to just mute all messages (which we can assume is safe) rather
    than try to add them all in the Social Demo Messenger class.
    
    Only messages of type `status` are muted so that `error` and `warning`
    messages do get through to the user.

--- modules/custom/social_demo/src/Messenger.php
+++ modules/custom/social_demo/src/Messenger.php
@@ -3,13 +3,15 @@
 namespace Drupal\social_demo;
 
 use Drupal\Core\Messenger\Messenger as MessengerBase;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Provide a Messenger service for use during demo content generation.
  *
  * Mutes messages that we don't want users to see that can occur when
  * social_demo creates demo content on the platform.
+ *
+ * This class is intentionally broad and assumes the Social Demo module is
+ * disabled after creating demo content.
  */
 class Messenger extends MessengerBase {
 
@@ -17,9 +19,11 @@ class Messenger extends MessengerBase {
    * {@inheritdoc}
    */
   public function addMessage($message, $type = MessengerBase::TYPE_STATUS, $repeat = FALSE) {
-    // Skip messages from social_follow_content that are created when the demo
-    // module is enabled.
-    if ($type === 'status' && $message instanceof TranslatableMarkup && strpos($message->getUntranslatedString(), 'You are now automatically following this ') === 0) {
+    // Skip all status messages while the demo content module is enabled.
+    // This avoids follow and enrollment messages that are not needed for the
+    // admin. The demo content module provides its own way of showing progress
+    // in drush.
+    if ($type === MessengerBase::TYPE_STATUS) {
       return $this;
     }
 
