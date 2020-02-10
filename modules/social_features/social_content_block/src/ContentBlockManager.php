67602382c2f542b57088f253576e6d8b8cbe3996
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-02-10 14:44:21 +0200

    Issue #3111959 by chmez: Create a new plugin type for implementing dependencies between fields and add the first instance for topics

---
+++ modules/social_features/social_content_block/src/ContentBlockManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\social_content_block;
+
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Plugin\Context\ContextAwarePluginManagerTrait;
+use Drupal\Core\Plugin\DefaultPluginManager;
+use Drupal\social_content_block\Annotation\ContentBlock;
+
+/**
+ * Class ContentBlockManager.
+ *
+ * @package Drupal\social_content_block
+ */
+class ContentBlockManager extends DefaultPluginManager implements ContentBlockManagerInterface {
+
+  use ContextAwarePluginManagerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
+    parent::__construct(
+      'Plugin/ContentBlock',
+      $namespaces,
+      $module_handler,
+      ContentBlockPluginInterface::class,
+      ContentBlock::class
+    );
+
+    $this->alterInfo('content_block');
+    $this->setCacheBackend($cache_backend, 'content_block_plugins');
+  }
+
+}
