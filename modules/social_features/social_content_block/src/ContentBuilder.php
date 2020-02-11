9bfb60769494de3df9b74c948f47afed1e6110a0
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-02-11 19:00:34 +0200

    Issue #3111959 by chmez: Move filtering query to a method of new plugin type

--- modules/social_features/social_content_block/src/ContentBuilder.php
+++ modules/social_features/social_content_block/src/ContentBuilder.php
@@ -3,10 +3,8 @@
 namespace Drupal\social_content_block;
 
 use Drupal\block_content\BlockContentInterface;
-use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Database\Connection;
 use Drupal\Core\Link;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
@@ -36,99 +34,80 @@ class ContentBuilder implements ContentBuilderInterface {
   protected $moduleHandler;
 
   /**
-   * The module handler service.
+   * The content block manager.
    *
-   * @var \Drupal\Core\Database\Connection
+   * @var \Drupal\social_content_block\ContentBlockManagerInterface
    */
-  protected $connection;
+  protected $contentBlockManager;
 
   /**
    * ContentBuilder constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
-   * @param \Drupal\Core\Database\Connection $connection
-   *   Database Service Object.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
    *   The string translation.
+   * @param \Drupal\social_content_block\ContentBlockManagerInterface $content_block_manager
+   *   The content block manager.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
-    Connection $connection,
     ModuleHandlerInterface $module_handler,
-    TranslationInterface $string_translation
+    TranslationInterface $string_translation,
+    ContentBlockManagerInterface $content_block_manager
   ) {
     $this->entityTypeManager = $entity_type_manager;
-    $this->connection = $connection;
     $this->moduleHandler = $module_handler;
     $this->setStringTranslation($string_translation);
+    $this->contentBlockManager = $content_block_manager;
   }
 
   /**
-   * Function to get all the topics based on the filters.
+   * Function to get all the entities based on the filters.
    *
-   * @param \Drupal\block_content\Entity\BlockContent $block_content
+   * @param \Drupal\block_content\BlockContentInterface $block_content
    *   The block content where we get the settings from.
    *
-   * @return array|string
-   *   Return the topics found.
+   * @return array
+   *   Returns the entities found.
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  protected function getTopics(BlockContent $block_content) {
-    // Get topic type tags.
-    $topic_types_list = $block_content->get('field_topic_type')->getValue();
-    $topic_types = array_map(function ($topic_type) {
-      return $topic_type['target_id'];
-    }, $topic_types_list);
+  protected function getEntities(BlockContentInterface $block_content) {
+    $plugin_id = $block_content->field_plugin_id->value;
 
-    // Get group tags.
-    $group_tag_list = $block_content->get('field_group')->getValue();
-    $group_tags = array_map(function ($group_tag) {
-      return $group_tag['target_id'];
-    }, $group_tag_list);
-
-    // Get social tags.
-    $social_tag_list = $block_content->get('field_content_tags')->getValue();
-    $social_tags = array_map(function ($social_tag) {
-      return $social_tag['target_id'];
-    }, $social_tag_list);
-
-    // Use database select because we need joins
-    // which are not possible with entityQuery.
-    /** @var \Drupal\Core\Database\Query\SelectInterface $query */
-    $query = $this->connection->select('node_field_data', 'n')
-      ->fields('n', ['nid'])
-      ->condition('n.type', 'topic');
-
-    // Add topic type tags.
-    if (!empty($topic_types)) {
-      $query->innerJoin('node__field_topic_type', 'tt', 'tt.entity_id = n.nid');
-      $query->condition('tt.field_topic_type_target_id', $topic_types, 'IN');
+    if ($block_content->field_plugin_field->isEmpty()) {
+      $definition = $this->contentBlockManager->getDefinition($plugin_id);
+      $field_names = $definition['fields'];
+    }
+    else {
+      $field_names = [$block_content->field_plugin_field->value];
     }
 
-    // Add group tags.
-    if (!empty($group_tags)) {
-      $query->innerJoin('group_content_field_data', 'gd', 'gd.entity_id = n.nid');
-      $query->condition('gd.gid', $group_tags, 'IN');
+    $fields = [];
+
+    foreach ($field_names as $field_name) {
+      $fields[$field_name] = array_map(function ($item) {
+        return $item['target_id'];
+      }, $block_content->get($field_name)->getValue());
     }
 
-    if (!empty($social_tags)) {
-      $query->innerJoin('node__social_tagging', 'st', 'st.entity_id = n.nid');
-      $query->condition('st.social_tagging_target_id', $social_tags, 'IN');
-    }
+    /** @var \Drupal\social_content_block\ContentBlockPluginInterface $plugin */
+    $plugin = $this->contentBlockManager->createInstance($plugin_id);
+
+    $query = $plugin->query($fields);
 
     // Allow other modules to change the query to add additions.
     $this->moduleHandler->alter('social_content_block_query', $query, $block_content);
 
     // Add sorting.
-    $query->orderBy('n.' . $block_content->getFieldValue('field_sorting', 'value'));
+    $query->orderBy('base_table.' . $block_content->field_sorting->value);
 
     // Add range.
-    $query->range(0, $block_content->getFieldValue('field_item_amount', 'value'));
+    $query->range(0, $block_content->field_item_amount->value);
 
     // Execute the query to get the results.
     $entities = $query->execute()->fetchAllKeyed(0, 0);
@@ -150,13 +129,13 @@ class ContentBuilder implements ContentBuilderInterface {
   /**
    * Function to generate the read more link.
    *
-   * @param \Drupal\block_content\Entity\BlockContent $block_content
+   * @param \Drupal\block_content\BlockContentInterface $block_content
    *   The block content where we get the settings from.
    *
    * @return string
    *   The read more link.
    */
-  protected function getLink(BlockContent $block_content) {
+  protected function getLink(BlockContentInterface $block_content) {
     $field = $block_content->field_link;
 
     if (!$field->isEmpty()) {
@@ -188,15 +167,18 @@ class ContentBuilder implements ContentBuilderInterface {
     $block_content = $this->entityTypeManager->getStorage('block_content')
       ->load($entity_id);
 
-    if (!$block_content instanceof BlockContentInterface || $block_content->bundle() !== 'custom_content_list') {
+    if (
+      !$block_content instanceof BlockContentInterface ||
+      $block_content->bundle() !== 'custom_content_list'
+    ) {
       return [];
     }
 
     $data = [
       '#theme' => 'social_content_block',
       '#title' => $block_content->label(),
-      '#subtitle' => $block_content->getFieldValue('field_subtitle', 'value'),
-      '#topics' => $this->getTopics($block_content),
+      '#subtitle' => $block_content->field_subtitle->value,
+      '#topics' => $this->getEntities($block_content),
       '#link' => $this->getLink($block_content),
     ];
 
--- modules/social_features/social_content_block/src/Plugin/ContentBlock/TopicContentBlock.php
+++ modules/social_features/social_content_block/src/Plugin/ContentBlock/TopicContentBlock.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\social_content_block\Plugin\ContentBlock;
 
+use Drupal\social_content_block\ContentBlockBase;
+
 /**
  * Provides a content block for topics.
  *
@@ -15,6 +17,39 @@ namespace Drupal\social_content_block\Plugin\ContentBlock;
  *   },
  * )
  */
-class TopicContentBlock {
+class TopicContentBlock extends ContentBlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query(array $fields) {
+    /** @var \Drupal\Core\Database\Query\SelectInterface $query */
+    $query = $this->connection->select('node_field_data', 'base_table')
+      ->fields('base_table', ['nid'])
+      ->condition('base_table.type', 'topic');
+
+    foreach ($fields as $field_name => $entity_ids) {
+      switch ($field_name) {
+        // Add topic type tags.
+        case 'field_topic_type':
+          $query->innerJoin('node__field_topic_type', 'tt', 'tt.entity_id = base_table.nid');
+          $query->condition('tt.field_topic_type_target_id', $entity_ids, 'IN');
+          break;
+
+        // Add group tags.
+        case 'field_group':
+          $query->innerJoin('group_content_field_data', 'gd', 'gd.entity_id = base_table.nid');
+          $query->condition('gd.gid', $entity_ids, 'IN');
+          break;
+
+        case 'field_content_tags':
+          $query->innerJoin('node__social_tagging', 'st', 'st.entity_id = base_table.nid');
+          $query->condition('st.social_tagging_target_id', $entity_ids, 'IN');
+          break;
+      }
+    }
+
+    return $query;
+  }
 
 }
