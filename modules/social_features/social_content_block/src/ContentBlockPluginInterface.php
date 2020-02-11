f6073562a299dacdede38363c17bad81f1ce394e
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-02-11 19:00:34 +0200

    Issue #3111959 by chmez: Move filtering query to a method of new plugin type

--- modules/social_features/social_content_block/src/ContentBlockPluginInterface.php
+++ modules/social_features/social_content_block/src/ContentBlockPluginInterface.php
@@ -2,11 +2,24 @@
 
 namespace Drupal\social_content_block;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
 /**
  * Interface ContentBlockPluginInterface.
  *
  * @package Drupal\social_content_block
  */
-interface ContentBlockPluginInterface {
+interface ContentBlockPluginInterface extends ContainerFactoryPluginInterface {
+
+  /**
+   * Create filtering query.
+   *
+   * @param array $fields
+   *   The fields.
+   *
+   * @return \Drupal\Core\Database\Query\SelectInterface
+   *   The query.
+   */
+  public function query(array $fields);
 
 }
