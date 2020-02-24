b2bc4356c8d21362574f9e0c6c81cb6bbb434af0
Volodymyr Martsynenko <volodymyr.martsynenko@lemberg.co.uk>
2020-02-24 11:13:40 +0200

    YANG-2047: Add Follow taxonomy functionality and Follow tag module

---
+++ modules/social_features/social_follow_taxonomy/modules/social_follow_tag/src/Plugin/ActivityContext/FollowTagActivityContext.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\social_follow_tag\Plugin\ActivityContext;
+
+use Drupal\social_follow_taxonomy\Plugin\ActivityContext\FollowTaxonomyActivityContext;
+
+/**
+ * Provides a 'FollowTagActivityContext' activity context plugin.
+ *
+ * @ActivityContext(
+ *  id = "follow_tag_activity_context",
+ *  label = @Translation("Following tag activity context"),
+ * )
+ */
+class FollowTagActivityContext extends FollowTaxonomyActivityContext {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function taxonomyTermsList($entity) {
+    $taxonomy_ids = [];
+
+    if ($entity->hasField('social_tagging')) {
+      if (!empty($entity->get('social_tagging')->getValue())) {
+        $tags = $entity->get('social_tagging')->getValue();
+
+        foreach ($tags as $tag) {
+          $taxonomy_ids[] = $tag['target_id'];
+        }
+      }
+    }
+
+    return $taxonomy_ids;
+  }
+
+}
