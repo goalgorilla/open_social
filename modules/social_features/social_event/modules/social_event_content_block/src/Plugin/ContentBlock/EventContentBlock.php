758e553f2c7d30849f723660be57c17d82da3742
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-03-10 18:46:54 +0200

    Issue #3118987 by chmez: Create a new module that contains a plugin for events

---
+++ modules/social_features/social_event/modules/social_event_content_block/src/Plugin/ContentBlock/EventContentBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\social_event_content_block\Plugin\ContentBlock;
+
+use Drupal\Core\Database\Query\SelectInterface;
+use Drupal\social_content_block\ContentBlockBase;
+
+/**
+ * Provides a content block for events.
+ *
+ * @ContentBlock(
+ *   id = "event_content_block",
+ *   entityTypeId = "node",
+ *   bundle = "event",
+ *   fields = {
+ *     "field_event_group",
+ *   },
+ * )
+ */
+class TopicContentBlock extends ContentBlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query(SelectInterface $query, array $fields) {
+    foreach ($fields as $field_name => $entity_ids) {
+      switch ($field_name) {
+        case 'field_event_group':
+          $query->innerJoin('group_content_field_data', 'gc', 'gc.entity_id = base_table.nid');
+          $query->condition('gc.type', '%' . $query->escapeLike('-group_node-event'), 'LIKE');
+          $query->condition('gc.gid', $entity_ids, 'IN');
+          break;
+      }
+    }
+  }
+
+}
