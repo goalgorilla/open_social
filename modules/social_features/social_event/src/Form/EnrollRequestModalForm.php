afa0354d466d1e6b55efeb214d98ed7b24b88a19
Robert Ragas <robertragas@goalgorilla.com>
2020-03-16 13:57:05 +0100

    Issue #3118222 by robertragas: fix ajax callback issue to stop submitting the form twice

--- modules/social_features/social_event/src/Form/EnrollRequestModalForm.php
+++ modules/social_features/social_event/src/Form/EnrollRequestModalForm.php
@@ -55,18 +55,20 @@ class EnrollRequestModalForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    $form['actions'] = ['#type' => 'actions'];
-    $form['actions']['send'] = [
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    // Add a submit button that handles the submission of the form.
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Send request'),
       '#attributes' => [
-        'class' => [
-          'use-ajax',
-          'btn-primary',
-        ],
+        'class' => ['btn', 'btn-primary'],
       ],
       '#ajax' => [
-        'callback' => [$this, 'submitForm'],
+        'callback' => [$this, 'submitModalFormAjax'],
         'event' => 'click',
       ],
     ];
@@ -79,7 +81,7 @@ class EnrollRequestModalForm extends FormBase {
   /**
    * AJAX callback handler that displays any errors or a success message.
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitModalFormAjax(array $form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
 
     if ($form_state->getErrors()) {
@@ -121,6 +123,16 @@ class EnrollRequestModalForm extends FormBase {
     \Drupal::messenger()->addStatus(t('Your request has been sent successfully'));
     return $response->addCommand(new RedirectCommand($currentURL->toString()));
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
 
   /**
    * Gets the configuration names that will be editable.
