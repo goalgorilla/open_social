d806d8e5c86d35a76f086260936cdacdb75370aa
ronaldtebrake <ronald@goalgorilla.com>
2020-03-25 22:13:47 +0100

    Issue #3119327 by ronaldtebrake: 5,6 manage invites - make sure the redirect after inviting members goes to the new overview

---
+++ modules/social_features/social_group/modules/social_group_invite/src/EventSubscriber/CustomRedirects.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Drupal\social_group_invite\EventSubscriber;
+
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\Core\Url;
+use Drupal\group\Entity\Group;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class CustomRedirects.
+ *
+ * @package Drupal\social_group_invite\EventSubscriber
+ */
+class CustomRedirects implements EventSubscriberInterface {
+
+  /**
+   * The current route.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRoute;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+  /**
+   * CustomRedirects construct.
+   *
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   The current route.
+   * @param \Drupal\Core\Session\AccountProxy $current_user
+   *   The current user.
+   */
+  public function __construct(CurrentRouteMatch $route_match, AccountProxy $current_user) {
+    $this->currentRoute = $route_match;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * Get the request events.
+   *
+   * @return mixed
+   *   Returns request events.
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['customRedirect'];
+    return $events;
+  }
+
+  /**
+   * This method is called when the KernelEvents::REQUEST event is dispatched.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   The event.
+   */
+  public function customRedirect(GetResponseEvent $event) {
+
+    // First check if the current route is the group canonical.
+    $routeMatch = $this->currentRoute->getRouteName();
+
+    // Not group canonical, then we leave.
+    if ($routeMatch !== 'view.group_invitations.page_1') {
+      return;
+    }
+
+    // Fetch the group parameter and check if's an actual group.
+    $group = $this->currentRoute->getParameter('group');
+    // Not group, then we leave.
+    if (!$group instanceof Group) {
+      return;
+    }
+
+    // Determine the URL we want to redirect to.
+    $url = Url::fromRoute('view.social_group_invitations.page_1', ['group' => $group->id()]);
+
+    // If it's not set, set to canonical, or the current user has no access.
+    if ($url->access($this->currentUser) === FALSE) {
+      // This basically means that the normal flow remains intact.
+      return;
+    }
+    // Redirect.
+    $event->setResponse(new RedirectResponse($url->toString()));
+  }
+
+}
