6f4d957b8a42bdf2360255a33d42fe6676a5d41d
Robert Ragas <robertragas@goalgorilla.com>
2020-03-28 15:34:29 +0100

    Issue #31207979 by robertragas: add service to check enrollments for a user

---
+++ modules/social_features/social_event/modules/social_event_invite/src/SocialEventInviteStatusHelper.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\social_event_invite;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
+
+/**
+ * Class SocialEventInviteStatusHelper
+ */
+class SocialEventInviteStatusHelper {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * EventInvitesAccess constructor.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Configuration factory.
+   */
+  public function __construct(RouteMatchInterface $routeMatch, EntityTypeManagerInterface $entityTypeManager, AccountProxyInterface $currentUser, ConfigFactoryInterface $configFactory) {
+    $this->routeMatch = $routeMatch;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->currentUser = $currentUser;
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * Custom check to see if a user has enrollments
+   *
+   * @param $trigger string
+   *   The email or userid you want to check on.
+   * @return bool|\Drupal\Core\Entity\EntityInterface|mixed
+   */
+  public function userEnrollments($trigger) {
+    $current_user = $this->currentUser;
+    $uid = $current_user->id();
+    $nid = $this->routeMatch->getRawParameter('node');
+
+    // If there is no trigger get the enrollment for the current user.
+    $conditions = [
+      'field_account' => $uid,
+      'field_event' => $nid,
+    ];
+
+    if($trigger) {
+      // Always assume the trigger is emails unless the ID is a user.
+      $conditions = [
+        'field_email' => $trigger,
+        'field_event' => $nid,
+      ];
+
+      /** @var \Drupal\user\Entity\User $user */
+      $user = User::load($trigger);
+      if ($user InstanceOf UserInterface) {
+        $conditions = [
+          'field_account' => $user->id(),
+          'field_event' => $nid,
+        ];
+      }
+    }
+
+    $enrollments = $this->entityTypeManager->getStorage('event_enrollment')->loadByProperties($conditions);
+
+    return $enrollments;
+  }
+}
