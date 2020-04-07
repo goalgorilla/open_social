33f2443865c476254567ad329dfaf7256ab65207
ronaldtebrake <ronald@goalgorilla.com>
2020-04-07 09:59:01 +0200

    Issue #3120797 by ronaldtebrake: DS-7183 show message on the frontpages when there are pending invites in order to accomodate the user after logging in or registering but dont spam them everywhere

--- modules/social_features/social_core/src/EventSubscriber/SocialInviteSubscriber.php
+++ modules/social_features/social_core/src/EventSubscriber/SocialInviteSubscriber.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\social_core\EventSubscriber;
 
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Drupal\social_core\InviteService;
@@ -42,6 +44,27 @@ class SocialInviteSubscriber implements EventSubscriberInterface {
   protected $messenger;
 
   /**
+   * The current route.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRoute;
+
+  /**
+   * Protected var alternativeFrontpageSettings.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $alternativeFrontpageSettings;
+
+  /**
+   * Protected var siteSettings.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $siteSettings;
+
+  /**
    * Constructs SocialInviteSubscriber.
    *
    * @param \Drupal\social_core\InviteService $inviteService
@@ -50,11 +73,18 @@ class SocialInviteSubscriber implements EventSubscriberInterface {
    *   The current user.
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   The current route.
+   * @param \Drupal\Core\Config\ConfigFactory $config_factory
+   *   The config factory.
    */
-  public function __construct(InviteService $inviteService, AccountInterface $current_user, MessengerInterface $messenger) {
+  public function __construct(InviteService $inviteService, AccountInterface $current_user, MessengerInterface $messenger, CurrentRouteMatch $route_match, ConfigFactory $config_factory) {
     $this->inviteService = $inviteService;
     $this->currentUser = $current_user;
     $this->messenger = $messenger;
+    $this->currentRoute = $route_match;
+    $this->alternativeFrontpageSettings = $config_factory->get('alternative_frontpage.settings');
+    $this->siteSettings = $config_factory->get('system.site');
   }
 
   /**
@@ -64,14 +94,40 @@ class SocialInviteSubscriber implements EventSubscriberInterface {
    *   The GetResponseEvent to process.
    */
   public function notifyAboutPendingInvitations(GetResponseEvent $event) {
-    $route = $this->inviteService->getInviteData();
-    if (!empty($route['name']) && !empty($route['amount'])) {
-      $replacement_url = ['@url' => Url::fromRoute($route['name'], ['user' => $this->currentUser->id()])->toString()];
-      $message = $this->formatPlural($route['amount'],
-        'You have 1 pending invite <a href="@url">visit your invite overview</a> to see it.',
-        'You have @count pending invites <a href="@url">visit your invite overview</a> to see them.', $replacement_url);
+    $data = $this->inviteService->getInviteData();
+    /** @var \Symfony\Component\HttpFoundation\Request $request */
+    $request = $event->getRequest();
+    $request_path = $request->getPathInfo();
+    $route_name = $this->currentRoute->getRouteName();
+    $default_front = $this->siteSettings->get('page.front');
+    $frontpage_lu = $this->alternativeFrontpageSettings->get('frontpage_for_authenticated_user') ?: '';
 
-      $this->messenger->addMessage($message, 'warning', FALSE);
+    // Either allow on paths.
+    $paths_allowed = [
+      $default_front,
+      $frontpage_lu,
+      '/',
+    ];
+    // Or allow on route names.
+    $routes_allowed = [
+      'social_core.homepage',
+    ];
+
+    // We want to show this message either on:
+    // the default site front page
+    // the front page set for authenticated users
+    // or the stream being the social_core.homepage.
+    if (in_array($request_path, $paths_allowed) || in_array($route_name, $routes_allowed)) {
+      if (!empty($data['name']) && !empty($data['amount'])) {
+        $replacement_url = [
+          '@url' => Url::fromRoute($data['name'], ['user' => $this->currentUser->id()])->toString()
+        ];
+        $message = $this->formatPlural($data['amount'],
+          'You have 1 pending invite <a href="@url">visit your invite overview</a> to see it.',
+          'You have @count pending invites <a href="@url">visit your invite overview</a> to see them.', $replacement_url);
+
+        $this->messenger->addMessage($message, 'warning');
+      }
     }
   }
 
