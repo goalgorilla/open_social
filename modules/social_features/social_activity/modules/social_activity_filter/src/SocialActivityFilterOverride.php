b74cf6a4c33b988c991d9a9271438ed140a470fd
Volodymyr Sydor <volodymyr.sydor@lemberg.co.uk>
2020-04-14 01:25:09 +0300

    Issue #3123324 by SV: Update config override - activity_filter_tags.

--- modules/social_features/social_activity/modules/social_activity_filter/src/SocialActivityFilterOverride.php
+++ modules/social_features/social_activity/modules/social_activity_filter/src/SocialActivityFilterOverride.php
@@ -30,53 +30,60 @@ class SocialActivityFilterOverride implements ConfigFactoryOverrideInterface {
     foreach ($config_names as $config_name => $display) {
 
       if (in_array($config_name, $names)) {
-        $data = [
-          'id' => 'activity_filter_tags',
-          'table' => 'activity',
-          'field' => 'activity_filter_tags',
-          'relationship' => 'none',
-          'group_type' => 'group',
-          'admin_label' => '',
-          'operator' => '=',
-          'value' => '',
-          'group' => 1,
-          'exposed' => FALSE,
-          'expose' => [
-            'operator_id' => '',
-            'label' => '',
-            'description' => '',
-            'use_operator' => FALSE,
-            'operator' => '',
-            'operator_limit_selection' => FALSE,
-            'operator_list' => [],
-            'identifier' => '',
-            'required' => FALSE,
-            'remember' => FALSE,
-            'multiple' => FALSE,
-            'remember_roles' => [
-              'authenticated' => 'authenticated',
+        $overrides[$config_name] = [
+          'display' => [
+            $display => [
+              'display_options' => [
+                'filters' => [
+                  'activity_filter_tags' => [
+                    'id' => 'activity_filter_tags',
+                    'table' => 'activity',
+                    'field' => 'activity_filter_tags',
+                    'relationship' => 'none',
+                    'group_type' => 'group',
+                    'admin_label' => '',
+                    'operator' => '=',
+                    'value' => '',
+                    'group' => 1,
+                    'exposed' => FALSE,
+                    'expose' => [
+                      'operator_id' => '',
+                      'label' => '',
+                      'description' => '',
+                      'use_operator' => FALSE,
+                      'operator' => '',
+                      'operator_limit_selection' => FALSE,
+                      'operator_list' => [],
+                      'identifier' => '',
+                      'required' => FALSE,
+                      'remember' => FALSE,
+                      'multiple' => FALSE,
+                      'remember_roles' => [
+                        'authenticated' => 'authenticated',
+                      ],
+                    ],
+                    'is_grouped' => FALSE,
+                    'group_info' => [
+                      'label' => '',
+                      'description' => '',
+                      'identifier' => '',
+                      'optional' => TRUE,
+                      'widget' => 'select',
+                      'multiple' => FALSE,
+                      'remember' => FALSE,
+                      'default_group' => 'All',
+                      'default_group_multiple' => [],
+                      'group_items' => [],
+                    ],
+                    'entity_type' => 'activity',
+                    'plugin_id' => 'activity_filter_tags',
+                  ],
+                ],
+                'override_tags_filter' => TRUE,
+              ],
             ],
           ],
-          'is_grouped' => FALSE,
-          'group_info' => [
-            'label' => '',
-            'description' => '',
-            'identifier' => '',
-            'optional' => TRUE,
-            'widget' => 'select',
-            'multiple' => FALSE,
-            'remember' => FALSE,
-            'default_group' => 'All',
-            'default_group_multiple' => [],
-            'group_items' => [],
-          ],
-          'entity_type' => 'activity',
-          'plugin_id' => 'activity_filter_tags',
         ];
-
-        $display_options = &$overrides[$config_name]['display'][$display]['display_options'];
-        $display_options['filters']['activity_filter_tags'] = $data;
-        $display_options['override_tags_filter'] = TRUE;
       }
     }
 
