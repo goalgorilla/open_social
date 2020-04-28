a425737c2f20d04739ee9e08c2c9614f21888782
Robert Ragas <robertragas@goalgorilla.com>
2020-04-28 19:54:46 +0200

    Issue #3102763 by robertragas: change or condition to and

--- modules/social_features/social_group/modules/social_group_flexible_group/src/Plugin/views/filter/FlexibleGroupNodeAccess.php
+++ modules/social_features/social_group/modules/social_group_flexible_group/src/Plugin/views/filter/FlexibleGroupNodeAccess.php
@@ -33,7 +33,7 @@ class FlexibleGroupNodeAccess extends FilterPluginBase {
   public function query() {
     $account = $this->view->getUser();
     $group_access = NULL;
-    if (!$account->hasPermission('bypass node access') || !$account->hasPermission('administer nodes')) {
+    if (!$account->hasPermission('administer nodes') && !$account->hasPermission('bypass node access')) {
       // Ensure we check for group content.
       // Join node table(s).
       $configuration = [
