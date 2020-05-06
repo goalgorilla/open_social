506f90ad52e7aec493a783560483dc27fc968d9b
Volodymyr <volodymyr.sydor@lemberg.co.uk>
2020-05-06 13:59:45 +0300

    Issue #3123324 by SV: Add filtering for block_stream_homepage activity stream views block.

--- modules/social_features/social_activity/modules/social_activity_filter/src/SocialActivityFilterOverride.php
+++ modules/social_features/social_activity/modules/social_activity_filter/src/SocialActivityFilterOverride.php
@@ -23,67 +23,72 @@ class SocialActivityFilterOverride implements ConfigFactoryOverrideInterface {
 
     // Override activity_stream & community_activities views.
     $config_names = [
-      'views.view.activity_stream' => 'block_stream_homepage_without_post',
-      'views.view.community_activities' => 'block_stream_landing',
+      'views.view.activity_stream' => [
+        'block_stream_homepage',
+        'block_stream_homepage_without_post',
+      ],
+      'views.view.community_activities' => [
+        'block_stream_landing',
+      ],
     ];
 
-    foreach ($config_names as $config_name => $display) {
+    foreach ($config_names as $config_name => $displays) {
 
       if (in_array($config_name, $names)) {
-        $overrides[$config_name] = [
-          'display' => [
-            $display => [
-              'display_options' => [
-                'filters' => [
-                  'activity_filter_tags' => [
-                    'id' => 'activity_filter_tags',
-                    'table' => 'activity',
-                    'field' => 'activity_filter_tags',
-                    'relationship' => 'none',
-                    'group_type' => 'group',
-                    'admin_label' => '',
-                    'operator' => '=',
-                    'value' => '',
-                    'group' => 1,
-                    'exposed' => FALSE,
-                    'expose' => [
-                      'operator_id' => '',
-                      'label' => '',
-                      'description' => '',
-                      'use_operator' => FALSE,
-                      'operator' => '',
-                      'operator_limit_selection' => FALSE,
-                      'operator_list' => [],
-                      'identifier' => '',
-                      'required' => FALSE,
-                      'remember' => FALSE,
-                      'multiple' => FALSE,
-                      'remember_roles' => [
-                        'authenticated' => 'authenticated',
-                      ],
+
+        foreach ($displays as $display) {
+
+          $overrides[$config_name]['display'][$display] = [
+            'display_options' => [
+              'filters' => [
+                'activity_filter_tags' => [
+                  'id' => 'activity_filter_tags',
+                  'table' => 'activity',
+                  'field' => 'activity_filter_tags',
+                  'relationship' => 'none',
+                  'group_type' => 'group',
+                  'admin_label' => '',
+                  'operator' => '=',
+                  'value' => '',
+                  'group' => 1,
+                  'exposed' => FALSE,
+                  'expose' => [
+                    'operator_id' => '',
+                    'label' => '',
+                    'description' => '',
+                    'use_operator' => FALSE,
+                    'operator' => '',
+                    'operator_limit_selection' => FALSE,
+                    'operator_list' => [],
+                    'identifier' => '',
+                    'required' => FALSE,
+                    'remember' => FALSE,
+                    'multiple' => FALSE,
+                    'remember_roles' => [
+                      'authenticated' => 'authenticated',
                     ],
-                    'is_grouped' => FALSE,
-                    'group_info' => [
-                      'label' => '',
-                      'description' => '',
-                      'identifier' => '',
-                      'optional' => TRUE,
-                      'widget' => 'select',
-                      'multiple' => FALSE,
-                      'remember' => FALSE,
-                      'default_group' => 'All',
-                      'default_group_multiple' => [],
-                      'group_items' => [],
-                    ],
-                    'entity_type' => 'activity',
-                    'plugin_id' => 'activity_filter_tags',
                   ],
+                  'is_grouped' => FALSE,
+                  'group_info' => [
+                    'label' => '',
+                    'description' => '',
+                    'identifier' => '',
+                    'optional' => TRUE,
+                    'widget' => 'select',
+                    'multiple' => FALSE,
+                    'remember' => FALSE,
+                    'default_group' => 'All',
+                    'default_group_multiple' => [],
+                    'group_items' => [],
+                  ],
+                  'entity_type' => 'activity',
+                  'plugin_id' => 'activity_filter_tags',
                 ],
-                'override_tags_filter' => TRUE,
               ],
+              'override_tags_filter' => TRUE,
             ],
-          ],
-        ];
+          ];
+        }
       }
     }
 
