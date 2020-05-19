f5ea33536f3162fc77e5915bd089b6f86674371f
Volodymyr Martsynenko <volodymyr.martsynenko@lemberg.co.uk>
2020-05-19 16:04:42 +0300

    Issue #3117677 by makishima: Update templates related to follow terms functionality

--- modules/social_features/social_follow_taxonomy/assets/js/follow-action_link.js
+++ modules/social_features/social_follow_taxonomy/assets/js/follow-action_link.js
@@ -6,9 +6,17 @@
         return link.addEventListener('click', function (event) {
 
           if(event.target.closest('.flag-follow-term').classList.contains('action-flag')) {
+            // Increase the number of followers when the user click on follow term button.
+            event.target.closest('.teaser__content').querySelector('.teaser__followers-count').innerHTML =
+              event.target.closest('.teaser__content').querySelector('.teaser__followers-count').innerHTML.replace(/[0-9]+/, function(n){ return ++n });
+
             event.target.closest('.group-action').querySelector('a.btn-action__term').classList.add('term-followed');
           }
           if(event.target.closest('.flag-follow-term').classList.contains('action-unflag')) {
+            // Decrease the number of followers when the user click on unfollow term button.
+            event.target.closest('.teaser__content').querySelector('.teaser__followers-count').innerHTML =
+              event.target.closest('.teaser__content').querySelector('.teaser__followers-count').innerHTML.replace(/[0-9]+/, function (n) { return (n>0) ? --n : n; });
+
             event.target.closest('.group-action').querySelector('a.btn-action__term').classList.remove('term-followed');
           }
         });
