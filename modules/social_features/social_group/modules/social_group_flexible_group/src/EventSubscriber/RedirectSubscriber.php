ff5043e511e09bbb05b8fc914103e62e8bea2fe1
Roman Salo <frs.daddy@gmail.com>
2020-06-11 13:17:43 +0300

    Issue #3150451 by rolki: Use dependency injection in RedirectSubscriber

--- modules/social_features/social_group/modules/social_group_flexible_group/src/EventSubscriber/RedirectSubscriber.php
+++ modules/social_features/social_group/modules/social_group_flexible_group/src/EventSubscriber/RedirectSubscriber.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\social_group_flexible_group\EventSubscriber;
 
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Url;
+use Drupal\group\Entity\GroupInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
@@ -14,6 +17,34 @@ use Symfony\Component\HttpKernel\KernelEvents;
  * @package Drupal\social_group_flexible_group\EventSubscriber
  */
 class RedirectSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The current route.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRoute;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+
+  /**
+   * Redirectsubscriber construct.
+   *
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   The current route.
+   * @param \Drupal\Core\Session\AccountProxy $current_user
+   *   The current user.
+   */
+  public function __construct(CurrentRouteMatch $route_match, AccountProxy $current_user) {
+    $this->currentRoute = $route_match;
+    $this->currentUser = $current_user;
+  }
 
   /**
    * Get the request events.
@@ -34,11 +65,10 @@ class RedirectSubscriber implements EventSubscriberInterface {
    */
   public function checkForRedirection(GetResponseEvent $event) {
     // Check if there is a group object on the current route.
+    /** @var \Drupal\group\Entity\GroupInterface $group */
     $group = _social_group_get_current_group();
     // Get the current route name for the checks being performed below.
-    $routeMatch = \Drupal::routeMatch()->getRouteName();
-    // Get the current user.
-    $user = \Drupal::currentUser();
+    $routeMatch = $this->currentRoute->getRouteName();
     // The array of forbidden routes.
     $routes = [
       'entity.group.canonical',
@@ -49,12 +79,12 @@ class RedirectSubscriber implements EventSubscriberInterface {
     ];
 
     // If a group is set, and the type is flexible_group.
-    if ($group && $group->getGroupType()->id() === 'flexible_group') {
-      if ($user->hasPermission('manage all groups')) {
+    if ($group instanceof GroupInterface && $group->getGroupType()->id() === 'flexible_group') {
+      if ($this->currentUser->hasPermission('manage all groups')) {
         return;
       }
       // If the user is not an member of this group.
-      elseif (!$group->getMember($user) && in_array($routeMatch, $routes, FALSE)
+      elseif (!$group->getMember($this->currentUser) && in_array($routeMatch, $routes, FALSE)
         && !social_group_flexible_group_community_enabled($group)
         && !social_group_flexible_group_public_enabled($group)) {
         $event->setResponse(new RedirectResponse(Url::fromRoute('view.group_information.page_group_about', ['group' => $group->id()])
