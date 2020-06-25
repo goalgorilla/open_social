d1d9a63a2f78cd1cb0ceeaa26b4062287e0def3d
Dmytrii Kaiun <dmytrii.kaiun@lemberg.co.ua>
2020-06-25 15:47:47 +0300

    Issue #3154462 by tBKoT: Clear the comment form before insert a new mention

--- modules/social_features/social_mentions/js/social_mentions.js
+++ modules/social_features/social_mentions/js/social_mentions.js
@@ -150,10 +150,9 @@
           $(".mention-reply").on("click", function (e) {
             e.preventDefault();
 
-            var author = $(this).data("author"),
-              empty = editor ? !editor.getData().length : !$textarea.val().length;
+            var author = $(this).data("author");
 
-            if (author && empty) {
+            if (author) {
               if (!editor) {
                 $textarea.val(author.value);
 
@@ -169,6 +168,14 @@
                 $textarea.focus();
               }
               else {
+                if(editor.getData().length) {
+                  $("[data-drupal-selector=\"comment-form\"]")
+                    .find('iframe')
+                    .contents()
+                    .find('body')
+                    .empty();
+                  editor.updateElement();
+                }
                 mentionsInput.handler.refreshMentions();
 
                 mentionsInput.handler.editor.focus();
