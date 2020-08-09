e3464602ce09e418511d2c95336e1ca6a752501e
ronaldtebrake <ronald@goalgorilla.com>
2020-08-09 14:24:53 +0200

    Revert changes, not necessary statically cached works out fine and better performant

--- modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
+++ modules/social_features/social_group/src/Plugin/Block/GroupHeroBlock.php
@@ -64,7 +64,7 @@ class GroupHeroBlock extends BlockBase implements ContainerFactoryPluginInterfac
   public function build() {
     $build = [];
 
-    $group = _social_group_get_current_group(NULL, FALSE);
+    $group = _social_group_get_current_group();
 
     if (!empty($group)) {
       // Content.
