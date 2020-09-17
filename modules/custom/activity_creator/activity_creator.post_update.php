59a2216e8721f5784e3ca9ea86d8825486e03347
navneet0693 <navneetsingh0693@gmail.com>
2020-09-17 19:22:50 +0530

    Issue #317156333 by navneet0693: Moved logic from activity_creator_update_8802 to hook_post_update_name and refactored.

--- modules/custom/activity_creator/activity_creator.post_update.php
+++ modules/custom/activity_creator/activity_creator.post_update.php
@@ -5,6 +5,9 @@
  * Contains post update hook implementations.
  */
 
+use Drupal\activity_creator\ActivityInterface;
+use Drupal\Core\Site\Settings;
+
 /**
  * Migrate all the activity status information to new table.
  *
@@ -66,4 +69,96 @@ function activity_creator_post_update_8001_one_to_many_activities(&$sandbox) {
   $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
   // Print some progress.
   return t('@count activities data has been migrated to activity_notification_table.', ['@count' => $sandbox['current']]);
+}
+
+/**
+ * Remove activities notification status if related entity not exist.
+ *
+ * @throws \Drupal\Core\Entity\EntityStorageException
+ */
+function activity_creator_post_update_8802_remove_activities_with_no_related_entities(&$sandbox) {
+  // Fetching amount of data we need to process.
+  // Runs only once per update.
+  $database = \Drupal::database();
+  if (!isset($sandbox['total'])) {
+    // Get count of all the necessary fields information from current database.
+    /** @var \Drupal\Core\Database\Query\Select $query */
+    $query = $database->select('activity_notification_status', 'ans');
+    $number_of_activities = $query->countQuery()->execute()->fetchField();
+
+    // Write total of entities need to be processed to $sandbox.
+    $sandbox['total'] = $number_of_activities;
+    // Initiate default value for current processing of element.
+    $sandbox['current'] = 0;
+
+    // Get activity IDs.
+    /** @var \Drupal\Core\Database\Query\Select $query */
+    $activity_ids = $database->select('activity_notification_status', 'ans')
+      ->fields('ans', ['aid'])
+      ->execute()
+      ->fetchCol();
+
+    // We pass all the ids in sandbox to be processed in each batch.
+    // We will choose a chunk of ids from this ids and use $sanbox['current']
+    // as offset. We do this to ensure all the ids are processed because we are
+    // deleting the data from the same table we are choosing from.
+    $sandbox['activities_id'] = $activity_ids;
+  }
+
+  // Do not continue if no entities are found.
+  if (empty($sandbox['total'])) {
+    $sandbox['#finished'] = 1;
+    return t('No activities data to be processed.');
+  }
+
+  // Activities per one batch operation.
+  $activities_per_batch = Settings::get('activity_update_batch_size', 10000);
+  // Get activity storage.
+  $activity_storage = $activity = \Drupal::entityTypeManager()
+    ->getStorage('activity');
+
+  // Choose chunk of array to be processed.
+  $activity_ids = array_slice($sandbox['activities_id'], $sandbox['current'], $activities_per_batch);
+
+  // Prepare the array of ids for deletion.
+  foreach ($activity_ids as $aid) {
+    /** @var \Drupal\activity_creator\ActivityInterface $activity */
+    $activity = $activity_storage->load($aid);
+
+    // Add invalid ids for deletion.
+    if (!$activity instanceof ActivityInterface) {
+      $aids_for_delete[] = $aid;
+    }
+    // Add not required $activity.
+    elseif (is_null($activity->getRelatedEntity())) {
+      $aids_for_delete[] = $aid;
+      $activities_for_delete[$aid] = $activity;
+    }
+  }
+
+  // Remove notifications.
+  if (!empty($aids_for_delete)) {
+    \Drupal::service('activity_creator.activity_notifications')
+      ->deleteNotificationsbyIds($aids_for_delete);
+  }
+
+  // Delete not required activity entities.
+  if (!empty($activities_for_delete)) {
+    $activity_storage->delete($activities_for_delete);
+  }
+
+  // Increment currently processed entities.
+  // Check if current starting point is less than our range selection.
+  if ($sandbox['total'] - $sandbox['current'] > $activities_per_batch) {
+    $sandbox['current'] += $activities_per_batch;
+  }
+  else {
+    // If we have less number of results to process, we increment by difference.
+    $sandbox['current'] += ($sandbox['total'] - $sandbox['current']);
+  }
+
+  // The batch will finish when '#finished' will become '1'.
+  $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
+  // Print some progress.
+  return t('@count activities data has been cleaned up.', ['@count' => $sandbox['current']]);
 }
