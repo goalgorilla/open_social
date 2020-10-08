7984f3e5bc3f533df4484127a0250c38b272bdda
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-10-08 10:11:12 +0300

    Issue #3172477 by chmez: Add conditions to checking relationships between entities in the access checker of the page for deleting the image from the post

--- modules/social_features/social_album/src/Controller/SocialAlbumController.php
+++ modules/social_features/social_album/src/Controller/SocialAlbumController.php
@@ -5,6 +5,7 @@ namespace Drupal\social_album\Controller;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
+use Drupal\social_post\Entity\PostInterface;
 
 /**
  * Class SocialAlbumController.
@@ -35,8 +36,39 @@ class SocialAlbumController extends ControllerBase {
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function checkAccess(NodeInterface $node) {
+  public function checkAlbumAccess(NodeInterface $node) {
     return AccessResult::allowedIf($node->bundle() === 'album');
+  }
+
+  /**
+   * Checks access to the page for deleting the image from the post.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node entity object.
+   * @param \Drupal\social_post\Entity\PostInterface $post
+   *   The post entity object.
+   * @param int $fid
+   *   The file entity ID.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function checkImageAccess(NodeInterface $node, PostInterface $post, $fid) {
+    if (
+      $this->checkAlbumAccess($node)->isAllowed() &&
+      $post->bundle() === 'photo' &&
+      !$post->field_album->isEmpty() &&
+      $post->field_album->target_id === $node->id() &&
+      !$post->field_post_image->isEmpty()
+    ) {
+      foreach ($post->field_post_image->getValue() as $item) {
+        if ($item['target_id'] === $fid) {
+          return AccessResult::allowed();
+        }
+      }
+    }
+
+    return AccessResult::forbidden();
   }
 
 }
