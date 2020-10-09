14b88a8b24a0a63e70ff43f5aba2bf853f240b64
Serhii Myronets <serhii.myronets@lemberg.co.uk>
2020-10-09 15:11:35 +0300

    YANG-3713: Update album popup

---
+++ modules/social_features/social_post/modules/social_post_album/assets/js/social-post-album--popup-slider.js
@@ -0,0 +1,18 @@
+(function ($) {
+  Drupal.behaviors.socialPostAlbumPopupSlider = {
+    attach: function () {
+      var albumSlider = $('.social-post-album--popup-slider-wrapper');
+
+      // Init slick slider.
+      albumSlider.slick({
+        infinite: false,
+        nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button">' +
+          '<svg><use xlink:href="#arrow-forward"></use></svg>' +
+          '</button>',
+        prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button">' +
+          '<svg><use xlink:href="#arrow-back"></use></svg>' +
+          '</button>',
+      });
+    }
+  };
+})(jQuery);
