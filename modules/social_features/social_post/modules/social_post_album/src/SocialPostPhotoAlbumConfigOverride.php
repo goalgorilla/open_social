9d056a8ca455b6f1ea739da3aeb391afbe3bd967
Roman Salo <frs.daddy@gmail.com>
2020-10-09 17:44:44 +0300

    YANG-3713: Override view mode for images during posting

---
+++ modules/social_features/social_post/modules/social_post_album/src/SocialPostPhotoAlbumConfigOverride.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\social_post_album;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Example configuration override.
+ */
+class SocialPostPhotoAlbumConfigOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * Returns config overrides.
+   *
+   * @param array $names
+   *   A list of configuration names that are being loaded.
+   *
+   * @return array
+   *   An array keyed by configuration name of override data. Override data
+   *   contains a nested array structure of overrides.
+   * @codingStandardsIgnoreStart
+   */
+  public function loadOverrides($names) {
+    // @codingStandardsIgnoreEnd
+    $overrides = [];
+
+    $config_name = 'core.entity_form_display.post.photo.group';
+
+    if (in_array($config_name, $names)) {
+      $overrides[$config_name]['content']['field_post_image']['settings']['preview_image_style'] = 'large';
+    }
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'SocialPostPhotoAlbumConfigOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
