2fbc135f8a878f9d305c7182570986ced960d824
ronaldtebrake <ronald@goalgorilla.com>
2020-10-09 16:48:39 +0200

    Issue #3108608 by ronaldtebrake: only update the defaults we want, not all

--- modules/custom/social_ajax_comments/src/Routing/RouteSubscriber.php
+++ modules/custom/social_ajax_comments/src/Routing/RouteSubscriber.php
@@ -18,9 +18,9 @@ class RouteSubscriber extends RouteSubscriberBase {
     // We need this custom implementation because we need to remove and invoke
     // additional ajax commands due to our theme.
     if ($route = $collection->get('ajax_comments.cancel')) {
-      $route->setDefaults([
-        '_controller' => '\Drupal\social_ajax_comments\Controller\AjaxCommentsController::socialCancel',
-      ]);
+      $defaults = $route->getDefaults();
+      $defaults['_controller'] = '\Drupal\social_ajax_comments\Controller\AjaxCommentsController::socialCancel';
+      $route->setDefaults($defaults);
     }
 
     // Replace "ajax_comments.add" with our implementation.
@@ -29,9 +29,9 @@ class RouteSubscriber extends RouteSubscriberBase {
     // add form with a mention. Therefor we need to override the message display
     // so ajax comments understands where to put the status message.
     if ($route = $collection->get('ajax_comments.add')) {
-      $route->setDefaults([
-        '_controller' => '\Drupal\social_ajax_comments\Controller\AjaxCommentsController::socialAdd',
-      ]);
+      $defaults = $route->getDefaults();
+      $defaults['_controller'] = '\Drupal\social_ajax_comments\Controller\AjaxCommentsController::socialAdd';
+      $route->setDefaults($defaults);
     }
   }
 
