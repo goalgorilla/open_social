b53f1687b4aea7051f08d3036c2b654be8b2e274
Roman Salo <frs.daddy@gmail.com>
2020-10-12 16:44:17 +0300

    YANG-3713: Show the image in pop-up that was clicked

--- modules/social_features/social_post/modules/social_post_album/src/Controller/ImagePopupController.php
+++ modules/social_features/social_post/modules/social_post_album/src/Controller/ImagePopupController.php
@@ -19,23 +19,32 @@ class ImagePopupController extends ControllerBase {
    *   Ids of images.
    * @param string $pid
    *   The post ID.
+   * @param string $first_fid
+   *   The first file ID.
    *
    * @return array
    *   Return render array.
    *
    * @see https://git.drupalcode.org/project/image_popup/-/blob/2.x/src/Controller/ImagePopup.php
    */
-  public function render($fids, $pid) {
+  public function render($fids, $pid, $first_fid) {
     $absolute_path = [];
     $fids = explode(',', $fids);
 
     foreach ($fids as $fid) {
+      if ($fid == $first_fid) {
+        continue;
+      }
       $file = $this->entityTypeManager()->getStorage('file')->load($fid);
       $image_uri = $file->getFileUri();
 
       // Get absolute path for original image.
       $absolute_path[] = Url::fromUri(file_create_url($image_uri))->getUri();
     }
+
+    $first_file = $this->entityTypeManager()->getStorage('file')->load($first_fid);
+    $first_image_uri = $first_file->getFileUri();
+    array_unshift($absolute_path, Url::fromUri(file_create_url($first_image_uri))->getUri());
 
     return [
       '#theme' => 'image_popup_details',
