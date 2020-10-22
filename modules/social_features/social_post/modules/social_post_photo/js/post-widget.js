32e442d6b7078b87efc92e9b34c8c4dbd607e675
Robert Ragas <robert@goalgorilla.com>
2020-10-22 13:23:53 +0200

    Merge pull request #2045 from goalgorilla/bugfix/3171502-remove-image-post-button-follow-up
    Issue #3171502 by Kingdutch, robertragas: Change the js of uploading …

--- modules/social_features/social_post/modules/social_post_photo/js/post-widget.js
+++ modules/social_features/social_post/modules/social_post_photo/js/post-widget.js
@@ -10,18 +10,24 @@
   Drupal.behaviors.postPhotoWidget = {
     attach: function (context, settings) {
 
-      $(document, context).once('field-post-image-add').on('click', '#post-photo-add', function(e) {
-        $('input[data-drupal-selector^="edit-field-post-image-0-upload"]', context).trigger('click');
+      $(document).once('field-post-image-add').on('click', '#post-photo-add', function (e) {
+        $(this)
+        .parents('.container-post-image')
+        .find('input[data-drupal-selector^="edit-field-post-image-0-upload"]')
+        .trigger('click');
         e.preventDefault();
       });
 
-      $(document, context).once('field-post-image-remove').on('click', '#post-photo-remove', function(e) {
-        $('button[data-drupal-selector^="edit-field-post-image-0-remove-button"]', context).trigger('mousedown');
+      $(document).once('field-post-image-remove').on('click', '#post-photo-remove', function (e) {
+        $(this)
+        .parents('.container-post-image')
+        .find('button[data-drupal-selector^="edit-field-post-image-0-remove-button"]')
+        .trigger('mousedown');
         e.preventDefault();
       });
 
       // Change placeholder text when someone adds a photo.
-      $('[data-drupal-selector^="edit-field-post-image-0-upload"]').change(function(e) {
+      $('[data-drupal-selector^="edit-field-post-image-0-upload"]', context).change(function (e) {
         $('#edit-field-post-0-value', context).attr("placeholder", Drupal.t('Say something about this photo'));
         $('[data-drupal-selector^="edit-field-post-image-wrapper"] .spinner', context).remove();
         $('[data-drupal-selector^="edit-field-post-image-wrapper"] .form-group .form-group', context).prepend('<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
