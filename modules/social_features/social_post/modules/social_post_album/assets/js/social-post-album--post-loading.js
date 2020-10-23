0f0de5a9edacd6e1767dd2b62eca388e1db657ed
Serhii Myronets <serhii.myronets@lemberg.co.uk>
2020-10-23 13:26:18 +0300

    YANG-3713: Add icon loading for the post button

---
+++ modules/social_features/social_post/modules/social_post_album/assets/js/social-post-album--post-loading.js
@@ -0,0 +1,21 @@
+(function ($) {
+  Drupal.behaviors.socialPostAlbumPostLoading = {
+    attach: function () {
+      var postForm = $('.social-post-album--form form');
+      var postBtn = postForm.find('> .form-submit');
+      var postTextField = postForm.find('.mentions-input input[type="hidden"]');
+
+      postBtn.wrapInner('<span class="text"></span>');
+      postBtn.append('<span class="post-loading"><svg class="loader" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">\n' +
+        '  <circle class="internal-circle" cx="60" cy="60" r="30"></circle>\n' +
+        '</svg></span>');
+
+      postBtn.on('click', function () {
+        if (postTextField.val() !== '') {
+          console.log(123);
+          $(this).addClass('post-loading-active');
+        }
+      });
+    }
+  };
+})(jQuery);
