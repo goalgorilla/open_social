a07783a38f35a5f0f2d8f04f88c5a731b2a198c0
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-11-05 18:49:50 +0200

    Issue #3156708 by chmez: Implement an unlinking account from the user settings page

--- modules/custom/social_auth_apple_extra/src/AppleUserManager.php
+++ modules/custom/social_auth_apple_extra/src/AppleUserManager.php
@@ -21,16 +21,39 @@ class AppleUserManager extends UserManager {
   /**
    * {@inheritdoc}
    */
-  public function getAccountId() {
-    $entities = $this->entityTypeManager->getStorage('social_auth')->loadByProperties([
-      'user_id' => $this->account->id(),
-    ]);
+  public function setAccountId($account_id) {
+    if ($account_id === NULL) {
+      foreach ($this->getEntities() as $entity) {
+        $entity->delete();
+      }
+    }
+    else {
+      parent::setAccountId($account_id);
+    }
+  }
 
-    if ($entities) {
+  /**
+   * {@inheritdoc}
+   */
+  public function getAccountId() {
+    if ($entities = $this->getEntities()) {
       return reset($entities)->provider_user_id->value;
     }
 
     return parent::getAccountId();
+  }
+
+  /**
+   * Returns list of the auth entities for a selected user.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   The entities list.
+   */
+  protected function getEntities() {
+    return $this->entityTypeManager->getStorage('social_auth')->loadByProperties([
+      'user_id' => $this->account->id(),
+      'plugin_id' => 'social_auth_apple',
+    ]);
   }
 
 }
