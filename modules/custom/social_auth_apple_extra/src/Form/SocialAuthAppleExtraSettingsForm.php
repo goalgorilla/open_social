e045fe4e54f2d00eebfce1e50297cf76bdea7945
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-11-09 14:23:33 +0200

    Issue #3156708 by chmez: Make the settings form more similar to the settings forms of other social networks

---
+++ modules/custom/social_auth_apple_extra/src/Form/SocialAuthAppleExtraSettingsForm.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\social_auth_apple_extra\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\social_auth_apple\Form\AppleAuthSettingsForm;
+
+/**
+ * Class SocialAuthAppleExtraSettingsForm.
+ *
+ * @package Drupal\social_auth_apple_extra\Form
+ */
+class SocialAuthAppleExtraSettingsForm extends AppleAuthSettingsForm {
+
+  /**
+   * The additional routes list for extending a set of redirect URLs.
+   */
+  const ROUTE_NAMES = [
+    'social_auth_apple_extra.user_register_callback',
+    'social_auth_apple_extra.user_link_callback',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    $section = &$form['apple_settings'];
+    $section['#title'] = $this->t('Apple App settings');
+
+    $section['authorized_redirect_url']['#type'] = 'textarea';
+    $section['authorized_redirect_url']['#rows'] = 1;
+
+    foreach (self::ROUTE_NAMES as $route_name) {
+      $url = Url::fromRoute($route_name)->setAbsolute();
+      $section['authorized_redirect_url']['#default_value'] .= ',' . $url->toString();
+    }
+
+    $section['status'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Active'),
+      '#default_value' => $this->config('social_auth_apple.settings')->get('status'),
+      '#description' => $this->t('Determines whether this social network can be used.'),
+    ];
+
+    $form['social_auth']['#access'] = FALSE;
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('social_auth_apple.settings')
+      ->set('status', $form_state->getValue('status'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
--- modules/custom/social_auth_apple_extra/src/Routing/RouteSubscriber.php
+++ modules/custom/social_auth_apple_extra/src/Routing/RouteSubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\social_auth_apple_extra\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Drupal\social_auth_apple_extra\Controller\AppleAuthExtraController;
+use Drupal\social_auth_apple_extra\Form\SocialAuthAppleExtraSettingsForm;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
@@ -19,6 +20,12 @@ class RouteSubscriber extends RouteSubscriberBase {
   protected function alterRoutes(RouteCollection $collection) {
     if ($route = $collection->get('social_auth_apple.callback')) {
       $route->setDefault('_controller', AppleAuthExtraController::class . '::callback');
+    }
+
+    if ($route = $collection->get('social_auth_apple.settings_form')) {
+      $route
+        ->setDefault('_form', SocialAuthAppleExtraSettingsForm::class)
+        ->setDefault('_title', 'Social Auth Apple settings');
     }
   }
 
