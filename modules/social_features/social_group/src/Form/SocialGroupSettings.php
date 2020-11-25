4e83f7d0aa0da1fa6ba75d11506208b5265e2c97
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2020-11-25 10:40:18 +0200

    Issue #3181951 by chmez: Update field labels and reorder these fields on the settings page

--- modules/social_features/social_group/src/Form/SocialGroupSettings.php
+++ modules/social_features/social_group/src/Form/SocialGroupSettings.php
@@ -36,34 +36,35 @@ class SocialGroupSettings extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('social_group.settings');
 
-    $form['create'] = [
+    $form['permissions'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Group permissions'),
+    ];
+
+    $form['permissions']['allow_group_create'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Create'),
+      '#title' => $this->t('Allow regular users to create new groups'),
       '#default_value' => $config->get('allow_group_create'),
     ];
 
-    $form['allow_group_selection_in_node'] = [
+    $form['permissions']['allow_group_selection_in_node'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Allow logged-in users to change or remove a group when editing content'),
-      '#description' => $this->t('When checked, logged-in users can also move content to or out of a group after the content is created. Users can only move content to a group the author is a member of.'),
+      '#title' => $this->t('Allow regular users to change the group their content belong to'),
       '#default_value' => $config->get('allow_group_selection_in_node'),
     ];
 
+    $form['permissions']['address_visibility_settings'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Only show the group address to the group members'),
+      '#default_value' => $config->get('address_visibility_settings'),
+    ];
+
     $form['default_hero'] = [
-      '#type' => 'select',
-      '#title' => $this->t('The default hero image.'),
+      '#type' => 'radios',
+      '#title' => $this->t('Default group hero size'),
       '#description' => $this->t('The default hero size used on this platform. Only applicable when logged-in users cannot choose a different hero size on each group.'),
       '#default_value' => $config->get('default_hero'),
       '#options' => $this->getCropTypes(),
-    ];
-
-    $form['address_visibility_settings'] = [
-      '#type' => 'checkboxes',
-      '#title' => $this->t('Address visibility settings'),
-      '#options' => [
-        'street_code_private' => $this->t('Only show street and postal code to group members'),
-      ],
-      '#default_value' => (array) $config->get('address_visibility_settings'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -76,7 +77,7 @@ class SocialGroupSettings extends ConfigFormBase {
     parent::submitForm($form, $form_state);
 
     $this->config('social_group.settings')
-      ->set('allow_group_create', $form_state->getValue('create'))
+      ->set('allow_group_create', $form_state->getValue('allow_group_create'))
       ->set('allow_group_selection_in_node', $form_state->getValue('allow_group_selection_in_node'))
       ->set('default_hero', $form_state->getValue('default_hero'))
       ->set('address_visibility_settings', $form_state->getValue('address_visibility_settings'))
