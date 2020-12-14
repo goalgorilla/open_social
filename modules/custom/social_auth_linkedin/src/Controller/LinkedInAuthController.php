66488bd3896176b311d41c04575bdd4fe449bc58
dmytriikaiun <dmytrii.kaiun@lemberg.co.ua>
2020-12-14 13:08:20 +0200

    Issue #3180468 by tBKoT: Change redirection for Social Login from profile to the front page (#2052)

--- modules/custom/social_auth_linkedin/src/Controller/LinkedInAuthController.php
+++ modules/custom/social_auth_linkedin/src/Controller/LinkedInAuthController.php
@@ -113,12 +113,10 @@ class LinkedInAuthController extends ControllerBase {
       return $this->redirect('user.login');
     }
 
-    // Authorize the user and redirect him to the account page.
+    // Authorize the user and redirect him to the front page.
     user_login_finalize($account);
 
-    return $this->redirect('entity.user.canonical', [
-      'user' => $account->id(),
-    ]);
+    return $this->redirect('<front>');
   }
 
   /**
