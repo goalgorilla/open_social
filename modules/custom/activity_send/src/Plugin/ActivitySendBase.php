a633f7f86dc2c460027b82444023c52d2a3d243e
jochemvn <jochemvn@gmail.com>
2021-01-20 12:20:16 +0100

    #3134077 by jochemvn: activity_send > DI issues fixed

--- modules/custom/activity_send/src/Plugin/ActivitySendBase.php
+++ modules/custom/activity_send/src/Plugin/ActivitySendBase.php
@@ -3,10 +3,21 @@
 namespace Drupal\activity_send\Plugin;
 
 use Drupal\Component\Plugin\PluginBase;
+use Drupal\Core\Queue\QueueFactory;
 
 /**
  * Base class for Activity send plugins.
  */
 abstract class ActivitySendBase extends PluginBase implements ActivitySendInterface {
 
+  /**
+   * @var \Drupal\Core\Queue\QueueFactory
+   */
+  protected $queue;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueueFactory $queue) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->queue = $queue;
+  }
 }
