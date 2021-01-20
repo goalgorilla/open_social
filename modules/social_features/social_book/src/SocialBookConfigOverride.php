4ead80fe1997fc33c35865081107f2774cb72a3c
ronaldtebrake <ronald@goalgorilla.com>
2021-01-20 13:22:09 +0100

    Issue #3134077 by ronaldtebrake: Fix Access to constant DEFAULT_COLLECTION on an unknown class Drupal\social_mentions\StorageInterface

--- modules/social_features/social_book/src/SocialBookConfigOverride.php
+++ modules/social_features/social_book/src/SocialBookConfigOverride.php
@@ -4,6 +4,7 @@ namespace Drupal\social_book;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
 
 /**
  * Class SocialBookConfigOverride.
