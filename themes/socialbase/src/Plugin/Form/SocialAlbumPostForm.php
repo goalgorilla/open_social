ad92a172ab0472b8c430c831725a31a524942f80
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2021-02-02 13:54:55 +0200

    Issue #3177902 by chmez: Fix losing CSS classes of the "Go to album" button on the page for adding a post to album after deleting the first image

--- themes/socialbase/src/Plugin/Form/SocialAlbumPostForm.php
+++ themes/socialbase/src/Plugin/Form/SocialAlbumPostForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\socialbase\Plugin\Form;
 
 use Drupal\bootstrap\Utility\Element;
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -18,7 +19,10 @@ class SocialAlbumPostForm extends SocialAlbumImageForm {
    * {@inheritdoc}
    */
   public function alterFormElement(Element $form, FormStateInterface $form_state, $form_id = NULL) {
-    if ($form_state->get('form_display')->getOriginalMode() === 'album') {
+    if (in_array($form_state->get('form_display')->getOriginalMode(), [
+      EntityDisplayRepositoryInterface::DEFAULT_DISPLAY_MODE,
+      'album',
+    ])) {
       parent::alterFormElement($form, $form_state, $form_id);
     }
   }
