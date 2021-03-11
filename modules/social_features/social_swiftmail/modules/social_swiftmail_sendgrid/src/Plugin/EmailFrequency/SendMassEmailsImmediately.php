4888305e3c2a8e779067e1df97d0b136faaa14df
ronaldtebrake <ronald@goalgorilla.com>
2021-03-11 16:00:44 +0100

    Start on the Immediately plugin to update our activity entity email to use sendgrid as well

---
+++ modules/social_features/social_swiftmail/modules/social_swiftmail_sendgrid/src/Plugin/EmailFrequency/SendMassEmailsImmediately.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\social_swiftmail_sendgrid\Plugin\EmailFrequency;
+
+use Drupal\activity_creator\Entity\Activity;
+use Drupal\activity_send_email\Plugin\ActivityDestination\EmailActivityDestination;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\message\Entity\Message;
+use Drupal\user\Entity\User;
+use Drupal\activity_send_email\Plugin\EmailFrequency\Immediately;
+
+/**
+ * Sends emails to users Immediately using Sendgrids substitution tokens.
+ */
+class SendMassEmailsImmediately extends Immediately {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem(Activity $activity, Message $message, User $target) {
+    if ($target->isBlocked()) {
+      return;
+    }
+
+    // Continue if we have text to send and the user is currently offline.
+    if (isset($activity->field_activity_output_text) && EmailActivityDestination::isUserOffline($target)) {
+      $langcode = $target->getPreferredLangcode();
+      $body_text = EmailActivityDestination::getSendEmailOutputText($message, $langcode);
+
+      if ($langcode && !empty($body_text)) {
+        $this->sendEmail($body_text, $langcode, $target);
+      }
+    }
+  }
+
+  /**
+   * Send an email with a single notification.
+   *
+   * @param string $body_text
+   *   The text to send to the target user.
+   * @param string $langcode
+   *   The langcode of the target user.
+   * @param \Drupal\user\Entity\User $target
+   *   The target account to send the email to.
+   */
+  protected function sendEmail($body_text, $langcode, User $target) {
+    // Translating frequency instance in the language of the user.
+    // @codingStandardsIgnoreStart
+    $frequency_translated = t($this->getName()->getUntranslatedString(), [], ['langcode' => $langcode]);
+    // @codingStandardsIgnoreEnd
+
+    // Construct the render array.
+    $notification = [
+      '#theme' => 'directmail',
+      '#notification' => $body_text,
+      '#notification_settings' => t('Based on your @settings, the notification above is sent to you <strong>:frequency</strong>', [
+        '@settings' => Link::fromTextAndUrl(t('email notification settings', [], ['langcode' => $langcode]), Url::fromRoute('entity.user.edit_form', ['user' => $target->id()])->setAbsolute())->toString(),
+        ':frequency' => $frequency_translated,
+      ],
+      ['langcode' => $langcode]),
+    ];
+
+    $params['body'] = \Drupal::service('renderer')->renderRoot($notification);
+
+    // Send the email.
+    $mail_manager = \Drupal::service('plugin.manager.mail');
+    $mail_manager->mail(
+      'activity_send_email',
+      'activity_send_email',
+      $target->getEmail(),
+      $langcode,
+      $params,
+      NULL,
+      TRUE
+    );
+  }
+
+}
