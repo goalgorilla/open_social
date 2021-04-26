97c331f6fe8366a88d6352005e3474239d31db63
Alexander Varwijk <alexander@getopensocial.com>
2021-04-26 10:50:04 +0200

    Split GraphQL comment tests per field
    This splits up the comment test for `Query.comment` and `Query.comments`
    and also changes the names to make them more easily recognizable as
    such.
    
    This allows us to simplify the test names slightly so that it's
    hopefully easier to see what is broken when a test fails.

--- modules/social_features/social_comment/tests/src/Kernel/GraphQL/QueryCommentsTest.php
+++ modules/social_features/social_comment/tests/src/Kernel/GraphQL/QueryCommentsTest.php
@@ -11,17 +11,12 @@ use Drupal\Tests\social_graphql\Kernel\SocialGraphQLTestBase;
 use Drupal\Tests\user\Traits\UserCreationTrait;
 
 /**
- * Tests the comments endpoint added to the Open Social schema by this module.
- *
- * This uses the GraphQLTestBase which extends KernelTestBase since this class
- * is interested in testing the implementation of the GraphQL schema that's a
- * part of this module. We're not interested in the HTTP functionality since
- * that is covered by the graphql module itself. Thus BrowserTestBase is not
- * needed.
+ * Tests the comments field on the Query type.
  *
  * @group social_graphql
+ * @group social_comment
  */
-class GraphQLCommentsEndpointTest extends SocialGraphQLTestBase {
+class QueryCommentsTest extends SocialGraphQLTestBase {
 
   use CommentTestTrait;
   use NodeCreationTrait;
@@ -71,7 +66,7 @@ class GraphQLCommentsEndpointTest extends SocialGraphQLTestBase {
   /**
    * Test that platform comments can be fetched using platform pagination.
    */
-  public function testPaginatedQueryComments(): void {
+  public function testSupportsRelayPagination(): void {
     // Act as a user that can create and view published comments and contents.
     $this->setUpCurrentUser([], array_merge(['skip comment approval', 'access comments'], $this->userPermissions()));
 
@@ -96,72 +91,9 @@ class GraphQLCommentsEndpointTest extends SocialGraphQLTestBase {
   }
 
   /**
-   * Test that a specific comment and its contents can be fetched by uuid.
-   */
-  public function testCanQueryOwnPublishedComment() : void {
-    // Create a node to comment on.
-    $node = $this->createNode();
-
-    // Set up a user that can create a published comment and view it.
-    // The default publishing status for comments looks at the current user
-    // rather than the comment author.
-    $user = $this->setUpCurrentUser([], array_merge(['skip comment approval', 'access comments'], $this->userPermissions()));
-
-    // We expect our bodyHtml to come out processed. This includes a linebreak
-    // that seems to be added by the renderer for funsies.
-    $raw_comment_body = "This is a link test: https://social.localhost";
-    $html_comment_body = '<div><p>This is a link test: <a href="https://social.localhost">https://social.localhost</a></p>
-</div>';
-    $comment = $this->createComment(
-      $node,
-      NULL,
-      ['field_comment_body' => $raw_comment_body]
-    );
-
-    $query = '
-      query ($id: ID!) {
-        comment(id: $id) {
-          id
-          author {
-            displayName
-          }
-          bodyHtml
-          created {
-            timestamp
-          }
-        }
-      }
-    ';
-
-    $this->assertResults(
-      $query,
-      ['id' => $comment->uuid()],
-      [
-        'comment' => [
-          'id' => $comment->uuid(),
-          'author' => [
-            'displayName' => $user->getDisplayName(),
-          ],
-          'bodyHtml' => $html_comment_body,
-          'created' => [
-            'timestamp' => $comment->getCreatedTime(),
-          ],
-        ],
-      ],
-      $this->defaultCacheMetaData()
-        ->addCacheableDependency($node)
-        ->addCacheableDependency($user)
-        ->addCacheableDependency($comment)
-        ->addCacheTags(['config:filter.format.plain_text', 'config:filter.settings'])
-        // @todo It's unclear why this cache context is added.
-        ->addCacheContexts(['languages:language_interface'])
-    );
-  }
-
-  /**
    * Test that the comments endpoint respects the access comments permission.
    */
-  public function testCommentListRequiresAccessCommentsPermission() {
+  public function testUserRequiresAccessCommentsPermission() {
     // Create a published comment on a node.
     $node = $this->createNode();
     $this->setUpCurrentUser([], array_merge(['skip comment approval', 'access comments'], $this->userPermissions()));
@@ -191,93 +123,12 @@ class GraphQLCommentsEndpointTest extends SocialGraphQLTestBase {
   }
 
   /**
-   * Test that the comment endpoint respects the access comments permission.
-   */
-  public function testCommentRequiresAccessCommentsPermission() {
-    // Create a published comment on a node.
-    $node = $this->createNode();
-    $this->setUpCurrentUser([], array_merge(['skip comment approval', 'access comments'], $this->userPermissions()));
-    $comment = $this->createComment($node);
-
-    // Create a user that is not allowed to view comments.
-    $this->setUpCurrentUser([], $this->userPermissions());
-
-    $this->assertResults('
-        query ($id: ID!) {
-          comment(id: $id) {
-            id
-          }
-        }
-      ',
-      ['id' => $comment->uuid()],
-      ['comment' => NULL],
-      $this->defaultCacheMetaData()
-        ->addCacheableDependency($comment)
-        ->addCacheContexts(['languages:language_interface'])
-    );
-  }
-
-  /**
-   * Test that a user can view their own unpublished comment.
-   */
-  public function testCanViewOwnUnpublishedComment() {
-    // Create an unpublished comment on a node.
-    $node = $this->createNode();
-    $this->setUpCurrentUser([], array_merge(['access comments'], $this->userPermissions()));
-    $comment = $this->createComment($node);
-
-    $this->assertResults('
-        query ($id: ID!) {
-          comment(id: $id) {
-            id
-          }
-        }
-      ',
-      ['id' => $comment->uuid()],
-      [
-        'comment' => [
-          'id' => $comment->uuid(),
-        ],
-      ],
-      $this->defaultCacheMetaData()
-        ->addCacheableDependency($comment)
-        ->addCacheContexts(['languages:language_interface'])
-    );
-  }
-
-  /**
-   * Test that a user can not view another person's unpublished comment.
-   */
-  public function testCanNotViewOtherUnpublishedComment() {
-    // Create an unpublished comment on a node.
-    $node = $this->createNode();
-    $this->setUpCurrentUser([], array_merge(['access comments'], $this->userPermissions()));
-    $comment = $this->createComment($node);
-
-    // Create another user to try and view the comment.
-    $this->setUpCurrentUser([], array_merge(['access comments'], $this->userPermissions()));
-
-    $this->assertResults('
-        query ($id: ID!) {
-          comment(id: $id) {
-            id
-          }
-        }
-      ',
-      ['id' => $comment->uuid()],
-      ['comment' => NULL],
-      $this->defaultCacheMetaData()
-        ->addCacheContexts(['languages:language_interface'])
-    );
-  }
-
-  /**
    * Test that a user can only see comments they're allowed to see in the list.
    *
    * - Any published comment
    * - Their own unpublished comment.
    */
-  public function testCommentListingAccessChecks() {
+  public function testUserCanViewOnlyOwnOrOtherPublishedComments() {
     $node = $this->createNode();
 
     // A user to create some other comments with.
