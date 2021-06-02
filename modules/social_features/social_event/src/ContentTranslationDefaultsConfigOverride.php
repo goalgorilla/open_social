d86ada1b6f639e6884eeeffa295d5fe5a77b6ecf
Vitaliy Nechay <vitally.nechai@lemberg.co.ua>
2021-06-02 11:51:31 +0300

    Issue #3216884 by vnech: Improve translations for "Event" node type; Add translation support for enrollments
    Related tickets: YANG-5636, YANG-5638, YANG-5647

--- modules/social_features/social_event/src/ContentTranslationDefaultsConfigOverride.php
+++ modules/social_features/social_event/src/ContentTranslationDefaultsConfigOverride.php
@@ -31,6 +31,75 @@ class ContentTranslationDefaultsConfigOverride extends ContentTranslationConfigO
    * {@inheritdoc}
    */
   protected function getTranslationOverrides() {
+    $views_plugin_content_translation_settings = [
+      'display_options' => [
+        'rendering_language' => '***LANGUAGE_language_interface***',
+      ],
+    ];
+
+    $langcode_filter_settings = [
+      'id' => 'langcode',
+      'table' => 'node_field_data',
+      'field' => 'langcode',
+      'relationship' => 'field_event',
+      'group_type' => 'group',
+      'admin_label' => '',
+      'operator' => 'in',
+      'value' => [
+        '***LANGUAGE_language_interface***' => '***LANGUAGE_language_interface***',
+      ],
+      'group' => 1,
+      'exposed' => FALSE,
+      'expose' => [
+        'operator_id' => '',
+        'label' => '',
+        'description' => '',
+        'use_operator' => FALSE,
+        'operator' => '',
+        'operator_limit_selection' => FALSE,
+        'operator_list' => [
+        ],
+        'identifier' => '',
+        'required' => FALSE,
+        'remember' => FALSE,
+        'multiple' => FALSE,
+        'remember_roles' => [
+          'authenticated' => 'authenticated',
+        ],
+        'reduce' => FALSE,
+      ],
+      'is_grouped' => FALSE,
+      'group_info' => [
+        'label' => '',
+        'description' => '',
+        'identifier' => '',
+        'optional' => TRUE,
+        'widget' => 'select',
+        'multiple' => FALSE,
+        'remember' => FALSE,
+        'default_group' => 'All',
+        'default_group_multiple' => [
+        ],
+        'group_items' => [
+        ],
+      ],
+      'entity_type' => 'node',
+      'entity_field' => 'langcode',
+      'plugin_id' => 'language',
+    ];
+
+    $default_display_langcode_filter_options = [
+      'display' => [
+        'default' => [
+          'display_options' => [
+            'filters' => [
+              'langcode' => $langcode_filter_settings,
+            ]
+          ],
+        ],
+      ],
+    ];
+
     return [
       'language.content_settings.node.event' => [
         'third_party_settings' => [
@@ -51,6 +120,12 @@ class ContentTranslationDefaultsConfigOverride extends ContentTranslationConfigO
       'field.field.node.event.body' => [
         'translatable' => TRUE,
       ],
+      'field.field.node.event.field_event_address' => [
+        'translatable' => TRUE,
+      ],
+      'field.field.node.event.field_files' => [
+        'translatable' => TRUE,
+      ],
       'field.field.node.event.field_event_image' => [
         'third_party_settings' => [
           'content_translation' => [
@@ -63,6 +138,31 @@ class ContentTranslationDefaultsConfigOverride extends ContentTranslationConfigO
         ],
         'translatable' => TRUE,
       ],
+      'views.view.upcoming_events' => [
+        'display' => [
+          'block_community_events' => $views_plugin_content_translation_settings,
+          'block_my_upcoming_events' => $views_plugin_content_translation_settings,
+          'page_community_events' => $views_plugin_content_translation_settings,
+          'upcoming_events_group' => $views_plugin_content_translation_settings,
+        ],
+      ],
+      'views.view.events' => [
+        'display' => [
+          'events_overview' => $views_plugin_content_translation_settings,
+          'block_events_on_profile' => $views_plugin_content_translation_settings,
+        ],
+      ],
+      'views.view.group_events' => [
+        'display' => [
+          'page_group_events' => $views_plugin_content_translation_settings,
+        ],
+      ],
+      'views.view.event_manage_enrollments' => $default_display_langcode_filter_options,
+      'views.view.user_event_invites' => $default_display_langcode_filter_options,
+      'views.view.event_enrollments' => $default_display_langcode_filter_options,
+      'views.view.manage_enrollments' => $default_display_langcode_filter_options,
+      'views.view.event_manage_enrollment_invites' => $default_display_langcode_filter_options,
+      'views.view.event_manage_enrollment_requests' => $default_display_langcode_filter_options,
     ];
   }
 
