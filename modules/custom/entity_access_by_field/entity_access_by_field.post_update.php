808d541f7424984a21ffa25191e0f88af3ccf334
navneet0693 <navneetsingh0693@gmail.com>
2021-06-23 13:06:49 +0530

    Issue #3209094 by Ressinel, navneet0693: Removed code for re-indexing search indexes.

--- modules/custom/entity_access_by_field/entity_access_by_field.post_update.php
+++ modules/custom/entity_access_by_field/entity_access_by_field.post_update.php
@@ -5,36 +5,9 @@
  * Post update functions for Entity Access By Field.
  */
 
-use Drupal\Core\Entity\EntityStorageException;
-use Drupal\search_api\Entity\Index;
-
 /**
  * Rebuild node access.
  */
 function entity_access_by_field_post_update_10101_rebuild_node_access() {
   node_access_rebuild(TRUE);
-}
-
-/**
- * Update Search index.
- */
-function entity_access_by_field_post_update_10102_update_search_index() {
-  try {
-    $indexes = [
-      'social_all',
-      'social_content',
-    ];
-
-    foreach ($indexes as $index) {
-      $index = Index::load($index);
-      if ($index !== NULL && $index->status()) {
-        $index->save();
-        $index->clear();
-        $index->reindex();
-      }
-    }
-  }
-  catch (EntityStorageException $e) {
-    \Drupal::logger('entity_access_by_field')->info($e->getMessage());
-  }
 }
