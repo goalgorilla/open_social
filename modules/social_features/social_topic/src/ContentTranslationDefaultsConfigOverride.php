f0f9af6938007bda51a8a4b5390ad483624edacc
Vitaliy Nechay <nechai.dev@gmail.com>
2021-07-06 16:47:25 +0300

    Issue #3219417 by vnech: Add compatibility with the latest changes in social_content_translation module
    See PR - https://github.com/goalgorilla/open_social/pull/2395

--- modules/social_features/social_topic/src/ContentTranslationDefaultsConfigOverride.php
+++ modules/social_features/social_topic/src/ContentTranslationDefaultsConfigOverride.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\social_topic;
 
-use Drupal\social_content_translation\ContentTranslationConfigOverrideBase;
+use Drupal\social_core\ContentTranslationConfigOverrideBase;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
