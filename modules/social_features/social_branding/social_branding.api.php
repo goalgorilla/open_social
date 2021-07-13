7d94de4305b857a684471d3f39163b66382ad6f2
nachosalvador <nachosalvador83@gmail.com>
2021-07-13 10:48:13 +0200

    Issue #3222386 by nachosalvador: Add preferredFeatures data producers.

---
+++ modules/social_features/social_branding/social_branding.api.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Social Branding module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter the list of preferred features.
+ *
+ * @param array $preferred_features
+ *   An array with the following key/value pairs:
+ *   - weight: The weight of the preferred feature.
+ *   - name: The machine name of the preferred feature.
+ *
+ * @ingroup social_branding_api
+ */
+function hook_social_branding_preferred_features_alter(array &$preferred_features) {
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
