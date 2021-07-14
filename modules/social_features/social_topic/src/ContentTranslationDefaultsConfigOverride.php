ed923c52773405a3fd7895f728e0a67d3c3448a3
Vitaliy Nechay <nechai.dev@gmail.com>
2021-07-14 11:42:48 +0300

    Issue #3219417 by SV, vnech: Make translations config overrides depended from "Content Translation" module

--- modules/social_features/social_topic/src/ContentTranslationDefaultsConfigOverride.php
+++ modules/social_features/social_topic/src/ContentTranslationDefaultsConfigOverride.php
@@ -2,60 +2,80 @@
 
 namespace Drupal\social_topic;
 
-use Drupal\social_core\ContentTranslationConfigOverrideBase;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
 
 /**
  * Provides content translation defaults for the event content type.
  *
  * @package Drupal\social_topic
  */
-class ContentTranslationDefaultsConfigOverride extends ContentTranslationConfigOverrideBase {
-
-  use StringTranslationTrait;
+class ContentTranslationDefaultsConfigOverride implements ConfigFactoryOverrideInterface {
 
   /**
    * {@inheritdoc}
    */
-  protected function getModule() {
-    return 'social_topic';
-  }
+  public function loadOverrides($names) {
+    $overrides = [];
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getDisplayName() {
-    return $this->t('Topics');
-  }
+    // If the module "content_translation" is enabled let make translations
+    // enabled for content provided by the module by default.
+    $is_content_translations_enabled = \Drupal::moduleHandler()
+      ->moduleExists('content_translation');
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getTranslationOverrides() {
-    return [
-      'language.content_settings.node.topic' => [
+    if (!$is_content_translations_enabled) {
+      return $overrides;
+    }
+
+    // Translations for "Topic" node type.
+    $config_name = 'language.content_settings.node.topic';
+    if (in_array($config_name, $names)) {
+      $overrides[$config_name] = [
         'third_party_settings' => [
           'content_translation' => [
             'enabled' => TRUE,
           ],
         ],
-      ],
-      'core.base_field_override.node.topic.title' => [
-        'translatable' => TRUE,
-      ],
-      'core.base_field_override.node.topic.menu_link' => [
-        'translatable' => TRUE,
-      ],
-      'core.base_field_override.node.topic.path' => [
-        'translatable' => TRUE,
-      ],
-      'core.base_field_override.node.topic.uid' => [
-        'translatable' => TRUE,
-      ],
-      'core.base_field_override.node.topic.status' => [
-        'translatable' => TRUE,
-      ],
+      ];
+    }
+    $config_names = [
+      'core.base_field_override.node.topic.title',
+      'core.base_field_override.node.topic.menu_link',
+      'core.base_field_override.node.topic.path',
+      'core.base_field_override.node.topic.uid',
+      'core.base_field_override.node.topic.status',
     ];
+    foreach ($config_names as $config_name) {
+      if (in_array($config_name, $names)) {
+        $overrides[$config_name] = [
+          'translatable' => TRUE,
+        ];
+      }
+    }
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'social_topic.content_translation_defaults_config_override';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
   }
 
 }
