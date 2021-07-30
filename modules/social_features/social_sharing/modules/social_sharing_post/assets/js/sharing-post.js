76c114e624648a7c1277fa195e0a809fb13d80f1
Roman Salo <frs.daddy@gmail.com>
2021-07-30 11:26:11 +0300

    Issue #3225997: Display modal under the link instead of center of window

--- modules/social_features/social_sharing/modules/social_sharing_post/assets/js/sharing-post.js
+++ modules/social_features/social_sharing/modules/social_sharing_post/assets/js/sharing-post.js
@@ -8,45 +8,55 @@
 
   Drupal.behaviors.social_sharing_post = {
     attach: function (context, _) {
-      // Get the modal.
-      var modal = document.getElementById('share-modal');
+      // Get the modals.
+      var modals = document.getElementsByClassName('modal-share');
       // Get the button that opens the modal.
-      var btn = document.getElementById('share-button');
+      var btn = document.getElementsByClassName('share-button');
       // Get the <span> element that closes the modal.
-      var span = document.getElementsByClassName('close')[0];
+      var span = document.getElementsByClassName('close');
 
-      // When the user clicks on the button, open the modal.
-      btn.onclick = function() {
-        modal.style.display = 'block';
-      }
+      Array.from(btn).forEach(element => {
+        element.addEventListener('click', (event) => {
+          Array.from(modals).forEach(element => {
+            element.style.display = 'none';
+          });
 
-      // When the user clicks on <span> (x), close the modal.
-      span.onclick = function() {
-        modal.style.display = 'none';
-      }
+          event.target.nextElementSibling.style.display = 'block';
+        });
+      });
 
-      // When the user clicks anywhere outside of the modal, close it.
-      window.onclick = function(event) {
-        if (event.target === modal) {
-          modal.style.display = 'none';
+      Array.from(span).forEach(element => {
+        element.addEventListener('click', (event) => {
+          element.closest('.modal-share').style.display = 'none';
+        });
+      });
+
+      window.onclick = function (event) {
+        if ($(event.target).is('.modal-share') || $(event.target).closest('.modal-share').length || $(event.target).is('.share-button')) {
+
+        } else {
+          Array.from(modals).forEach(element => {
+            element.style.display = 'none';
+          });
         }
       }
 
       // Get the button that copies the link.
-      var copyLink = document.getElementById('copy-link-clipboard');
+      var copyLink = document.getElementsByClassName('copy-link-clipboard');
 
-      // When the user clicks on the button, copy the link.
-      copyLink.onclick = function() {
-        var copyText = document.getElementById('input-url');
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand('copy');
+      Array.from(copyLink).forEach(element => {
+        element.addEventListener('click', (event) => {
+          var copyText = $(event.target.parentNode.parentNode.parentNode).find('input.input-url')[0];
 
-        var x = document.getElementById('snackbar');
-        x.className = 'show';
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-      }
+          copyText.select();
+          copyText.setSelectionRange(0, 99999);
+          document.execCommand('copy');
 
+          var x = document.getElementById('snackbar');
+          x.className = 'show';
+          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        });
+      });
     }
   }
 })(jQuery);
