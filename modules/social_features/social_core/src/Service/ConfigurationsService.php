a5cf858ad7551cede7a6ecbc74097e0fe1db73d3
Andrii Chyrskyi <andrii.chyrskyi@lemberg.co.ua>
2021-09-06 08:33:34 +0300

    Issue #3215146 by Ressinel: Improvement for helper function to import static configurations.

--- modules/social_features/social_core/src/Service/ConfigurationsService.php
+++ modules/social_features/social_core/src/Service/ConfigurationsService.php
@@ -16,22 +16,32 @@ class ConfigurationsService {
    *
    * @param array $configs
    *   Array of configuration file names and their folders.
-   * @param string $module_name
-   *   The module name for which we import configurations.
-   * @param int $n
-   *   The hook update number.
+   * @param string $func_update_name
+   *   The hook_update_N function full name.
    */
-  public static function importStaticConfigurations(array $configs, string $module_name, int $n) {
-    foreach ($configs as $config) {
-      $config_file = drupal_get_path('module', $module_name) . "/config/static/{$config}_$n.yml";
+  public static function importStaticConfigurations(array $configs, string $func_update_name) {
+    // Retrieve module_name and update number.
+    $func_update_name = explode('_update_', $func_update_name);
 
-      if (is_file($config_file)) {
-        $settings = Yaml::parse(file_get_contents($config_file));
-        if (is_array($settings)) {
-          $update_config = \Drupal::configFactory()
-            ->getEditable($config);
+    // We must have at least two items in retrieved data.
+    if (is_array($func_update_name) && count($func_update_name) === 2) {
+      // Module name have to be first item in retrieved data.
+      $module_name = $func_update_name[0];
+      // Update number have to be second item in retrieved data.
+      $n = $func_update_name[1];
 
-          $update_config->setData($settings)->save(TRUE);
+      // Set configurations.
+      foreach ($configs as $config) {
+        $config_file = drupal_get_path('module', $module_name) . "/config/static/{$config}_$n.yml";
+
+        if (is_file($config_file)) {
+          $settings = Yaml::parse(file_get_contents($config_file));
+          if (is_array($settings)) {
+            $update_config = \Drupal::configFactory()
+              ->getEditable($config);
+
+            $update_config->setData($settings)->save(TRUE);
+          }
         }
       }
     }
