b339da25b8cd6c018d0f5e967a3fbcf719505adf
Alexander Varwijk <alexander@getopensocial.com>
2021-09-15 17:05:08 +0200

    Switch visibility settings edit form and profile rendering to new system
    This is how long a line can be if you were to type it in manually rather
    This commit removes the old visibility settings fields (that used a form
    alter) from the user settings page. This form is replaced by the profile
    form rendered in the `visibility` form mode.
    
    The profile was previously rendered by a view which caused field level
    access checks to be ignored. To solve this the route is replaced with a
    controller that simply renders the profile in the full view mode. This
    ensures that field level access checks are correctly applied.
    
    As part of the access check improvements the information stored about
    fields in the field storage third party settings is changed slightly.
    Rather than storing the field storage ID we now only store the field
    name (for the corresponding visibility or value field). This makes
    handling the value slightly easier and makes it applicable across both
    FieldConfig and FieldStorage entities. *We don’t currently support
    storing visibility settings on a different entity-type/-bundle than the
    corresponding value field.*
    
    The information storage change allows to make the FieldManager class
    slightly more readable and provides clearer `isX` functions. Finally we
    also generalise some of the entity type and bundle checks which in
    theory makes this usable on other entity types or profile bundles too
    (although the required hooks for field config/storage synchronisation)
    still need to be implemented.
    
    While testing both the user visibility settings editing and the profile
    field display access checks it was discovered that some changes were
    needed there too. Specifically returning Neutral from a field_access
    hook will make the access check default to allowed. So in our case we
    want to expressly allow or forbid rather than return neutral if we know
    something about the access at all.

--- modules/social_features/social_profile/src/FieldManager.php
+++ modules/social_features/social_profile/src/FieldManager.php
@@ -6,7 +6,7 @@ namespace Drupal\social_profile;
 
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\field\FieldConfigInterface;
+use Drupal\Core\Field\FieldConfigInterface;
 use Drupal\field\FieldStorageConfigInterface;
 
 /**
@@ -34,54 +34,107 @@ class FieldManager implements EntityFieldManagerInterface {
   /**
    * Get the fields available on the profile that we manage visibility for.
    *
-   * Modules can opt-out for field management by this module by setting the
-   * `social_profile` third-party setting `managed_access` to FALSE for the
-   * field definition. An empty value is treated as TRUE.
-   *
    * @param string $bundle
-   *   Optional bundle to get fields for (defaults to 'profile').
+   *   Bundle to get fields for (defaults to 'profile').
    *
-   * @return \Drupal\field\FieldConfigInterface[]
+   * @return \Drupal\Core\Field\FieldConfigInterface[]
    *   A list of managed field configurations, keyed by field name.
+   *
+   * @see \Drupal\social_profile\FieldManager::isOptedOutOfFieldAccessManagement()
    */
   public function getManagedProfileFieldDefinitions(string $bundle = 'profile') : array {
     return array_filter(
       $this->getFieldDefinitions('profile', $bundle),
-      fn (FieldDefinitionInterface $fieldDefinition) => $fieldDefinition instanceof FieldConfigInterface && $fieldDefinition->getThirdPartySetting('social_profile', 'managed_access', TRUE)
+      // Indirection until https://github.com/phpstan/phpstan/issues/8528.
+      fn (FieldDefinitionInterface $definition) => static::isManagedValueField($definition)
     );
   }
 
   /**
-   * Get the field that stores user visibility choices for a field.
+   * Whether the field has opted out of our access management.
    *
-   * @param \Drupal\field\FieldConfigInterface $field
-   *   The field for which to find the visibility field.
+   * Modules can opt-out for field management by this module by setting the
+   * `social_profile` third-party setting `managed_access` to FALSE for the
+   * field definition. Fields without value will be managed.
    *
-   * @return \Drupal\Core\Field\FieldDefinitionInterface|null
-   *   The field storing field visibility choices or NULL if the provided field
-   *   doesn't support changing visibility.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field definition for which we are checking.
+   *
+   * @return bool
+   *   Whether the field has opted out of access management by this module.
    */
-  public function getVisibilityFieldFor(FieldConfigInterface $field) : ?FieldDefinitionInterface {
-    $bundle = $field->getTargetBundle();
-    if ($bundle === NULL) {
+  public static function isOptedOutOfFieldAccessManagement(FieldDefinitionInterface $field_definition) : bool {
+    return !self::fieldIsConfigurable($field_definition) || !self::getFieldStorageDefinition($field_definition)->getThirdPartySetting('social_profile', 'managed_access', TRUE);
+  }
+
+  /**
+   * Get the name of the field that this field contains visibility info for.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field that contains visibility data.
+   *
+   * @return string|null
+   *   The name of the field or NULL if $field_definition is not a field that
+   *   contains field visibility data.
+   */
+  public static function getManagedValueFieldName(FieldDefinitionInterface $field_definition) : ?string {
+    // Visibility can only be managed for fieldable fields.
+    if (!self::fieldIsConfigurable($field_definition)) {
       return NULL;
     }
 
-    $field_storage = $field->getFieldStorageDefinition();
-    if (!$field_storage instanceof FieldStorageConfigInterface) {
+    return self::getFieldStorageDefinition($field_definition)->getThirdPartySetting('social_profile', 'visibility_for');
+  }
+
+  /**
+   * Get the name of the field that contains visibility data.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field to get the visibility field name for.
+   *
+   * @return string|null
+   *   The name of the field containing visibility data or NULL if
+   *   $field_definition is not a field that manages visibility for another
+   *   field.
+   */
+  public static function getVisibilityFieldName(FieldDefinitionInterface $field_definition) : ?string {
+    // Visibility can only be managed by fieldable fields.
+    if (!self::fieldIsConfigurable($field_definition)) {
       return NULL;
     }
 
-    $visibility_storage_id = $field_storage->getThirdPartySetting('social_profile', 'visibility_stored_by');
-    if ($visibility_storage_id === NULL) {
-      return NULL;
-    }
+    return self::getFieldStorageDefinition($field_definition)->getThirdPartySetting('social_profile', 'visibility_stored_by');
+  }
 
-    $config_name_parts = explode(".", $visibility_storage_id);
-    $field_name = end($config_name_parts);
-    $bundle_fields = $this->getFieldDefinitions($field->getTargetEntityTypeId(), $bundle);
+  /**
+   * Whether the provided field manages visibility data for another field.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field to check.
+   *
+   * @return bool
+   *   Whether the provided field manages visibility data for another field.
+   *
+   * @phpstan-assert-if-true \Drupal\Core\Field\FieldConfigInterface $field_definition
+   */
+  public static function isVisibilityField(FieldDefinitionInterface $field_definition) : bool {
+    return static::getManagedValueFieldName($field_definition) !== NULL;
+  }
 
-    return $bundle_fields[$field_name] ?? NULL;
+  /**
+   * Whether the provided field has another field that manages visibility data.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The field to check.
+   *
+   * @return bool
+   *   Whether the provided field has another field that manages visibility
+   *   data.
+   *
+   * @phpstan-assert-if-true \Drupal\Core\Field\FieldConfigInterface $field_definition
+   */
+  public static function isManagedValueField(FieldDefinitionInterface $field_definition) : bool {
+    return !static::isOptedOutOfFieldAccessManagement($field_definition) && static::getVisibilityFieldName($field_definition) !== NULL;
   }
 
   /**
@@ -146,6 +199,47 @@ class FieldManager implements EntityFieldManagerInterface {
    */
   public function getExtraFields($entity_type_id, $bundle) {
     return $this->entityFieldManager->getExtraFields($entity_type_id, $bundle);
+  }
+
+  /**
+   * Type-checker satisfaction function.
+   *
+   * A \Drupal\field\FieldConfigInterface will always have a
+   * \Drupal\field\FieldStorageConfigInterface instance as storage. This is
+   * guarded by the implementation. However the interface of the original
+   * interface is not adjusted so the type-checker gets confused.
+   *
+   * @param \Drupal\Core\Field\FieldConfigInterface $field_definition
+   *   The field definition to call `getFieldStorageDefinition` on.
+   *
+   * @return \Drupal\field\FieldStorageConfigInterface
+   *   The field storage config entity.
+   *
+   * @see \Drupal\field\Entity\FieldConfig::getFieldStorageDefinition()
+   * @see https://www.drupal.org/project/drupal/issues/2399301
+   * @see https://www.drupal.org/project/drupal/issues/2818877
+   */
+  private static function getFieldStorageDefinition(FieldConfigInterface $field_definition) : FieldStorageConfigInterface {
+    /** @var \Drupal\field\FieldStorageConfigInterface $fsd */
+    $fsd = $field_definition->getFieldStorageDefinition();
+    return $fsd;
+  }
+
+  /**
+   * Check that a field definition is configurable.
+   *
+   * It must be configurable so that we can add our own metadata.
+   *
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition to check.
+   *
+   * @return bool
+   *   Whether the class implements \Drupal\Core\Field\FieldConfigInterface.
+   *
+   * @phpstan-assert-if-true \Drupal\Core\Field\FieldConfigInterface $field_definition
+   */
+  private static function fieldIsConfigurable(FieldDefinitionInterface $field_definition) : bool {
+    return $field_definition instanceof FieldConfigInterface;
   }
 
 }
