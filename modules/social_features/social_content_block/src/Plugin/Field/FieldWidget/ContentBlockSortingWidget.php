5422be43b73be8eb804651b3c2f6899137e8b779
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2021-09-24 17:39:24 +0300

    Issue #3236733 by chmez: Fix coding standards after changing the checking level

--- modules/social_features/social_content_block/src/Plugin/Field/FieldWidget/ContentBlockSortingWidget.php
+++ modules/social_features/social_content_block/src/Plugin/Field/FieldWidget/ContentBlockSortingWidget.php
@@ -39,9 +39,9 @@ class ContentBlockSortingWidget extends OptionsSelectWidget {
    *   The plugin implementation definition.
    * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
    *   The definition of the field to which the widget is associated.
-   * @param array $settings
+   * @param mixed[] $settings
    *   The widget settings.
-   * @param array $third_party_settings
+   * @param mixed[] $third_party_settings
    *   Any third party settings.
    * @param \Drupal\social_content_block\ContentBlockManagerInterface $manager
    *   The content block manager.
@@ -66,9 +66,21 @@ class ContentBlockSortingWidget extends OptionsSelectWidget {
   }
 
   /**
-   * {@inheritdoc}
+   * Creates an instance of the plugin.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container to pull out services used in the plugin.
+   * @param mixed[] $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   *   Returns an instance of this plugin.
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
     return new static(
       $plugin_id,
       $plugin_definition,
@@ -80,14 +92,33 @@ class ContentBlockSortingWidget extends OptionsSelectWidget {
   }
 
   /**
-   * {@inheritdoc}
+   * Returns the form for a single field widget.
+   *
+   * @param \Drupal\Core\Field\FieldItemListInterface|mixed[] $items
+   *   Array of default values for this field.
+   * @param int $delta
+   *   The order of this item in the array of sub-elements (0, 1, 2, etc.).
+   * @param mixed[] $element
+   *   A form element array containing basic properties for the widget.
+   * @param mixed[] $form
+   *   The form structure where widgets are being attached to. This might be a
+   *   full form structure, or a sub-element of a larger form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return mixed[]
+   *   The form elements for a single widget for this field.
    */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state): array {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    if (!is_string($name = $items->getName())) {
+      return $element;
+    }
 
     $element['#description'] = $names = [];
 
-    $selector = $this->manager->getSelector($items->getName(), NULL, NULL, TRUE);
+    $selector = $this->manager->getSelector($name, NULL, NULL, TRUE);
 
     foreach (array_keys($this->manager->getDefinitions()) as $plugin_id) {
       $plugin = $this->manager->createInstance($plugin_id);
