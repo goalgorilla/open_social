baadf6245a67631a81f7b8c7fa8a368aa5d796ff
Andrii Chyrskyi <andrii.chyrskyi@lemberg.co.ua>
2021-09-27 21:40:45 +0300

    Fix PHPStan error.

--- modules/social_features/social_user/social_user.post_update.php
+++ modules/social_features/social_user/social_user.post_update.php
@@ -23,9 +23,15 @@ function social_user_post_update_convert_navigation_settings() {
  * Give all existing users on the platform the new role to ensure backward
  * compatibility (even blocked users).
  *
+ * @param array<array> $sandbox
+ *   Stores information for batch updates.
+ *
+ * @return bool
+ *   TRUE if the operation was finished, FALSE otherwise.
+ *
  * @throws \Drupal\Core\Entity\EntityStorageException
  */
-function social_user_post_update_10101_add_verified_role_to_existing_users(&$sandbox) {
+function social_user_post_update_10101_add_verified_role_to_existing_users(array &$sandbox) : bool {
   /** @var \Drupal\user\UserStorageInterface $user_storage */
   $user_storage = \Drupal::entityTypeManager()->getStorage('user');
 
@@ -34,9 +40,11 @@ function social_user_post_update_10101_add_verified_role_to_existing_users(&$san
     $sandbox['ids'] = \Drupal::entityQuery('user')
       ->accessCheck(FALSE)
       ->execute();
-    $sandbox['count'] = count($sandbox['ids']);
+
+    $sandbox['count'] = count((array) $sandbox['ids']);
   }
 
+  $sandbox['ids'] = (array) $sandbox['ids'];
   $ids = array_splice($sandbox['ids'], 0, 50);
 
   // Load accounts by users IDs.
@@ -51,5 +59,7 @@ function social_user_post_update_10101_add_verified_role_to_existing_users(&$san
     }
   }
 
-  $sandbox['#finished'] = empty($sandbox['ids']) ? 1 : ($sandbox['count'] - count($sandbox['ids'])) / $sandbox['count'];
+  $sandbox['#finished'] = empty($sandbox['ids']) ? 1 : (((int) $sandbox['count']) - count($sandbox['ids'])) / ($sandbox['count']);
+
+  return (bool) $sandbox['#finished'];
 }
