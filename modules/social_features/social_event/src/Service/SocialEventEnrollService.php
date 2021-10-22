58a709f980b0325db52fd8226473ffb882d89420
dmytriikaiun <dmytrii.kaiun@lemberg.co.ua>
2021-10-22 17:41:58 +0300

    Issue #3244274 by tBKoT: Update get value process to avoid errors on the event view/edit page (#2553)

--- modules/social_features/social_event/src/Service/SocialEventEnrollService.php
+++ modules/social_features/social_event/src/Service/SocialEventEnrollService.php
@@ -38,19 +38,19 @@ class SocialEventEnrollService implements SocialEventEnrollServiceInterface {
       $this->eventSettings->get('disable_event_enroll') ||
       $node->bundle() !== 'event' ||
       !$node->hasField('field_event_enroll') ||
-      (!$node->get('field_event_enroll')->isEmpty() && !(bool) $node->get('field_event_enroll')->first()->getValue()['value'])
+      (!$node->get('field_event_enroll')->isEmpty() && !(bool) $node->get('field_event_enroll')->getString())
     ) {
       return FALSE;
     }
 
     $was_not_changed = $node->get('field_event_enroll')->isEmpty();
-    $is_enabled = $node->get('field_event_enroll')->first()->getValue()['value'];
+    $is_enabled = (bool) $node->get('field_event_enroll')->getString();
 
     // Make an exception for the invite enroll method.
     // This doesn't allow people to enroll themselves, but get invited.
     if (
       !$node->get('field_enroll_method')->isEmpty() &&
-      (int) $node->get('field_enroll_method')->first()->getValue()['value'] === EventEnrollmentInterface::ENROLL_METHOD_INVITE
+      (int) $node->get('field_enroll_method')->getString() === EventEnrollmentInterface::ENROLL_METHOD_INVITE
     ) {
       $is_enabled = TRUE;
     }
