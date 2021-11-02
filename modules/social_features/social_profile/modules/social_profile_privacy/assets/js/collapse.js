14a012850bf9e321f81f15851fe088029e31cf30
Serogka Myronets <myronets.serogka@gmail.com>
2021-11-02 23:29:08 +0200

    Issue #3247243 by agami4: Improve collapse behaviour

--- modules/social_features/social_profile/modules/social_profile_privacy/assets/js/collapse.js
+++ modules/social_features/social_profile/modules/social_profile_privacy/assets/js/collapse.js
@@ -10,16 +10,18 @@
           var $icon = $this.find('.icon');
           var $iconUse = $icon.find('use');
 
-          if ($this.hasClass('collapsed')) {
-            $icon.removeClass('icon-expand_less')
-              .addClass('icon-expand_more');
-            $iconUse.attr('href', '#icon-expand_more');
-          }
-          else {
-            $icon.removeClass('icon-expand_more')
-              .addClass('icon-expand_less');
-            $iconUse.attr('href', '#icon-expand-less');
-          }
+          setTimeout(function () {
+            if ($this.hasClass('collapsed')) {
+              $icon.removeClass('icon-expand_less')
+                .addClass('icon-expand_more');
+              $iconUse.attr('href', '#icon-expand_more');
+            }
+            else {
+              $icon.removeClass('icon-expand_more')
+                .addClass('icon-expand_less');
+              $iconUse.attr('href', '#icon-expand-less');
+            }
+          }, 150);
         });
       });
     }
