01aa726a1cf3940bdef04093e01beb79358b8b94
navneet0693 <navneetsingh0693@gmail.com>
2021-12-01 09:19:07 +0530

    Added test for checking flood control.
        1. Added two tests for checking flood control for anonymous and authenticated user.
        2. Added patch for https://www.drupal.org/project/drupal/issues/2910000#comment-14304192.

--- modules/social_features/social_embed/tests/src/Kernel/SocialUserEmbedTest.php
+++ modules/social_features/social_embed/tests/src/Kernel/SocialUserEmbedTest.php
@@ -3,8 +3,11 @@
 namespace Drupal\social_embed\Tests;
 
 use Drupal\Component\Uuid\Php;
+use Drupal\Core\Site\Settings;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\Tests\user\Traits\UserCreationTrait;
+use Drupal\user\Entity\Role;
+use Drupal\user\RoleInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -39,24 +42,29 @@ class SocialUserEmbedTest extends KernelTestBase {
   protected function setUp(): void {
     parent::setUp();
 
+    $this->installConfig(['user']);
     $this->installSchema('system', ['sequences']);
     $this->installSchema('user', ['users_data']);
     $this->installEntitySchema('user');
     $this->installEntitySchema('profile');
+    $settings = Settings::getAll();
+    $settings['social_embed_flood_retries'] = 5;
+    $settings['social_embed_flood_time_window'] = 10;
+    new Settings($settings);
+
+    // Give anonymous users permission to view test entities.
+    if ($anonymous_role = Role::load(RoleInterface::ANONYMOUS_ID)) {
+      $anonymous_role->grantPermission('generate social embed content')
+        ->save();
+    }
   }
 
   /**
-   * Test generate embed content of a given URL.
-   *
-   * Test maximum number of times each user can generate embed content of a
-   * given URL.
+   * Test flood control generate embed content URL for LU.
    */
-  public function testSocialUserEmbed(): void {
+  public function testFloodControlForAuthenticatedUser(): void {
     // Create a user with the necessary permission.
-    /** @var \Drupal\Core\Session\AccountInterface $user */
-    $user = $this->createUser(['generate social embed content']);
-    // Set created user as the current user.
-    \Drupal::currentUser()->setAccount($user);
+    $this->setUpCurrentUser([], ['generate social embed content']);
 
     // URL needed for the request.
     $url = 'https://www.youtube.com/watch?v=kgE9QNX8f3c';
@@ -64,7 +72,34 @@ class SocialUserEmbedTest extends KernelTestBase {
     $uuid = (new Php())->generate();
 
     // The maximum number of times each user can do this event per time window.
-    $retries = 50;
+    $retries = Settings::get('social_embed_flood_retries');
+
+    /** @var \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel */
+    $http_kernel = $this->container->get('http_kernel');
+
+    for ($i = 0; $i <= $retries; $i++) {
+      // Create request.
+      $request = Request::create("/api/opensocial/social-embed/generate?url=$url&uuid=$uuid");
+      // Handle request.
+      $response = $http_kernel->handle($request);
+
+      // All requests should be ok except last one as it will be $retries + 1.
+      $status_code = ($i === $retries) ? Response::HTTP_FORBIDDEN : Response::HTTP_OK;
+      $this->assertEqual($response->getStatusCode(), $status_code);
+    }
+  }
+
+  /**
+   * Test flood control generate embed content URL for AN.
+   */
+  public function testFloodControlForAnonymousUser(): void {
+    // URL needed for the request.
+    $url = 'https://www.youtube.com/watch?v=kgE9QNX8f3c';
+    // Generate random UUID.
+    $uuid = (new Php())->generate();
+
+    // The maximum number of times each user can do this event per time window.
+    $retries = Settings::get('social_embed_flood_retries');
 
     /** @var \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel */
     $http_kernel = $this->container->get('http_kernel');
