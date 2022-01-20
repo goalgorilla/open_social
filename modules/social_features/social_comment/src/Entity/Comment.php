b62310b0190ca57a81070ce8f60a1b0ac08a43ea
dmytriikaiun <dmytrii.kaiun@lemberg.co.ua>
2022-01-20 15:48:17 +0200

    Issue #3258809 by tBKoT: Use postdelete instead of entity delete hook

--- modules/social_features/social_comment/src/Entity/Comment.php
+++ modules/social_features/social_comment/src/Entity/Comment.php
@@ -3,6 +3,8 @@
 namespace Drupal\social_comment\Entity;
 
 use Drupal\comment\Entity\Comment as CommentBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\social_core\EntityUrlLanguageTrait;
 
 /**
@@ -10,5 +12,19 @@ use Drupal\social_core\EntityUrlLanguageTrait;
  */
 class Comment extends CommentBase {
   use EntityUrlLanguageTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function postDelete(EntityStorageInterface $storage, array $entities) {
+    parent::postDelete($storage, $entities);
+    // Always invalidate the cache tag for the commented entity.
+    /** @var \Drupal\comment\CommentInterface $entity */
+    foreach ($entities as $entity) {
+      if ($commented_entity = $entity->getCommentedEntity()) {
+        Cache::invalidateTags($commented_entity->getCacheTagsToInvalidate());
+      }
+    }
+  }
 
 }
