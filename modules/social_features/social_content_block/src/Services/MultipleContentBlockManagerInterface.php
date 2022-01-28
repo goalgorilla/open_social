cdc7087b653eee43d2c68f8b22601856fb47a0cd
dmytriikaiun <dmytrii.kaiun@lemberg.co.ua>
2022-01-28 15:05:30 +0200

    Issue #3260885 by tBKoT: Creates a new conent list block for multiple content types

---
+++ modules/social_features/social_content_block/src/Services/MultipleContentBlockManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\social_content_block\Services;
+
+/**
+ * Defines the multiple content block manager.
+ *
+ * @package Drupal\social_content_block
+ */
+interface MultipleContentBlockManagerInterface {
+
+}
