3535a7410fb44a2cb2545d94a7b59533fc1db40d
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2022-02-23 14:21:50 +0200

    Issue #3265949 by chmez: Remember requested profiles and rendered dialogue windows

--- modules/social_features/social_follow_user/assets/js/social_follow_user.preview.js
+++ modules/social_features/social_follow_user/assets/js/social_follow_user.preview.js
@@ -1,27 +1,63 @@
 (function ($, Drupal) {
   Drupal.behaviors.socialFollowUser = {
     attach: function attach(context) {
-      $(context).find('.preview > img').once('preview').each(function (i, element) {
-        var $element = $(element);
-        var id = $element.parent().attr('id');
+      var dialogs = [];
+      var profiles = [];
 
-        Drupal.ajax({
-          progress: false,
-          dialogType: 'modal',
-          dialog: {
-            title: $element.attr('alt'),
-            width: '50%',
-            position: {
-              my: 'left top',
-              at: 'right top',
-              of: '#' + id
+      $(context).find('img[id^="profile-preview"]').on('mouseover', function () {
+        var $element = $(this);
+        var selector = $element.attr('id');
+        var identifier = $element.data('profile');
+
+        var dialog = function () {
+          dialogs[selector] = Drupal.dialog(
+            '<div>'.concat(profiles[identifier], '</div>'),
+            {
+              title: $element.attr('alt'),
+              width: '50%',
+              position: {
+                my: 'left top',
+                at: 'right top',
+                of: '#' + selector
+              },
+              create: function () {
+                var $wrapper = $(this).closest('.ui-dialog');
+
+                $wrapper.find('.ui-dialog-titlebar-close').remove();
+
+                $wrapper.on('mouseleave', function () {
+                  dialogs[selector].close();
+                });
+              }
             }
-          },
-          base: id,
-          element: element,
-          url: '/user/2/information',
-          event: 'mouseover'
-        });
+          );
+
+          console.log(dialogs[selector]);
+
+          dialogs[selector].showModal();
+        };
+
+        if (dialogs[selector] !== undefined) {
+          dialogs[selector].showModal();
+        }
+        else if (profiles[identifier] !== undefined) {
+          dialog();
+        }
+        else {
+          var ajax = Drupal.ajax({
+            url: Drupal.url('user/' + $element.data('profile') + '/information')
+          });
+
+          ajax.commands.insert = function (ajax, response, status) {
+            if (response.method === null) {
+              profiles[identifier] = response.data;
+
+              dialog();
+            }
+          };
+
+          ajax.execute();
+        }
       });
     }
   };
