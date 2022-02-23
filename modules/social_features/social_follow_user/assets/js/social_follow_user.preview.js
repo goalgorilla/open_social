82187a95815e367a9ee27273df1c1cc34d52e6eb
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2022-02-23 18:59:42 +0200

    Issue #3265949 by chmez: Add delay before loading pop-up

--- modules/social_features/social_follow_user/assets/js/social_follow_user.preview.js
+++ modules/social_features/social_follow_user/assets/js/social_follow_user.preview.js
@@ -1,64 +1,66 @@
-(function ($, Drupal) {
+(function ($, Drupal, window) {
   Drupal.behaviors.socialFollowUser = {
     attach: function attach(context) {
-      var dialogs = [];
-      var profiles = [];
+      var timeouts = [], dialogs = [], profiles = [];
 
-      $(context).find('img[id^="profile-preview"]').on('mouseover', function () {
-        var $element = $(this);
-        var selector = $element.attr('id');
-        var identifier = $element.data('profile');
+      $(context).find('img[id^="profile-preview"]')
+        .on('mouseover', function () {
+          var $element = $(this);
+          var selector = $element.attr('id');
 
-        var dialog = function () {
-          dialogs[selector] = Drupal.dialog(
-            '<div>'.concat(profiles[identifier], '</div>'),
-            {
-              title: $element.attr('alt'),
-              width: '50%',
-              position: {
-                my: 'left top',
-                at: 'right top',
-                of: '#' + selector
-              },
-              create: function () {
-                var $wrapper = $(this).closest('.ui-dialog');
+          timeouts[selector] = window.setTimeout(function () {
+            var identifier = $element.data('profile');
 
-                $wrapper.find('.ui-dialog-titlebar-close').remove();
+            var dialog = function () {
+              dialogs[selector] = Drupal.dialog(
+                '<div>'.concat(profiles[identifier], '</div>'),
+                {
+                  title: $element.attr('alt'),
+                  width: '50%',
+                  position: {
+                    my: 'left top',
+                    at: 'right top',
+                    of: '#' + selector
+                  },
+                  create: function () {
+                    $(this).closest('.ui-dialog')
+                      .on('mouseleave', function () {
+                        dialogs[selector].close();
+                      })
+                      .find('.ui-dialog-titlebar-close').remove();
+                  }
+                }
+              );
 
-                $wrapper.on('mouseleave', function () {
-                  dialogs[selector].close();
-                });
-              }
+              dialogs[selector].showModal();
+            };
+
+            if (dialogs[selector] !== undefined) {
+              dialogs[selector].showModal();
             }
-          );
-
-          console.log(dialogs[selector]);
-
-          dialogs[selector].showModal();
-        };
-
-        if (dialogs[selector] !== undefined) {
-          dialogs[selector].showModal();
-        }
-        else if (profiles[identifier] !== undefined) {
-          dialog();
-        }
-        else {
-          var ajax = Drupal.ajax({
-            url: Drupal.url('user/' + $element.data('profile') + '/information')
-          });
-
-          ajax.commands.insert = function (ajax, response, status) {
-            if (response.method === null) {
-              profiles[identifier] = response.data;
-
+            else if (profiles[identifier] !== undefined) {
               dialog();
             }
-          };
+            else {
+              var ajax = Drupal.ajax({
+                url: Drupal.url('user/' + $element.data('profile') + '/information')
+              });
 
-          ajax.execute();
-        }
-      });
+              ajax.commands.insert = function (ajax, response, status) {
+                if (response.method === null) {
+                  profiles[identifier] = response.data;
+
+                  dialog();
+                }
+              };
+
+              ajax.execute();
+            }
+          }, 200);
+        })
+        .on('mouseout', function () {
+          window.clearTimeout(timeouts[$(this).attr('id')]);
+        });
     }
   };
-}(jQuery, Drupal));
+}(jQuery, Drupal, window));
