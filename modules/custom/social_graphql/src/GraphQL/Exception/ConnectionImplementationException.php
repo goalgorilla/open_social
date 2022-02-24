abc0589bc8120dbc7a20af00c186ccd57bddd68c
navneet0693 <navneetsingh0693@gmail.com>
2022-02-24 19:46:11 +0530

    Added a custom a exception class for social_graphQL.

---
+++ modules/custom/social_graphql/src/GraphQL/Exception/ConnectionImplementationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Drupal\social_graphql\GraphQL\Exception;
+
+/**
+ * Exception thrown when an DataProducer is missing cacheability metadata.
+ */
+class ConnectionImplementationException extends \Exception {}
