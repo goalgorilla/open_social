942a4e346941e9ccbd27758168ca8fdfc61787f1
Anna Maltseva <anna.maltseva@nxte.nl>
2022-03-08 11:49:13 +0100

    Issue #3266651 by belatrix22: added missing js file

--- modules/social_features/social_emoji/assets/js/social_emoji_picker.js
+++ modules/social_features/social_emoji/assets/js/social_emoji_picker.js
@@ -1,10 +1,64 @@
 (function ($) {
 
-  // Drupal.behaviors.social_emoji_picker = {
-  //   attach: function(context, _) {
-      document.querySelector('emoji-picker')
-        .addEventListener('emoji-click', event => console.log(event.detail));
-    // }
-  // }
+  const emojiPickerTriggers = document.getElementsByClassName("emoji-trigger")
+
+  function emoji_picker(event) {
+    let targetElement = event.target;
+    targetElement.innerHTML = '<emoji-picker></emoji-picker>';
+
+    targetElement.addEventListener('emoji-click', e => {
+      let parentElementId = targetElement.parentElement.id;
+      let pos = parentElementId.lastIndexOf('-wrapper');
+      let hiddenInputAttribute = parentElementId.slice(0, pos);
+      let inputFieldId = parentElementId.replace('-wrapper', '-0-value');
+      $('#' + inputFieldId).focus();
+      let textToInsert = e.detail.unicode;
+      let curPos;
+      let curValue;
+
+      if (typeof CKEDITOR != "undefined" && CKEDITOR.instances[inputFieldId]) {
+        CKEDITOR.instances[inputFieldId].insertText(textToInsert);
+      }
+      else {
+        curPos = document.getElementById(inputFieldId).selectionStart;
+        curValue = $('#' + inputFieldId).val();
+        $('#' + inputFieldId).val(curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos));
+        let hiddenInputs = $('input[type=hidden]');
+        for (let i = 0; i < hiddenInputs.length; i++) {
+          if (hiddenInputs[i].getAttribute('data-drupal-selector').includes(hiddenInputAttribute)) {
+            hiddenInputs[i].setAttribute('value', curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos));
+          }
+        }
+      }
+    });
+
+  }
+
+  for (let i = 0; i < emojiPickerTriggers.length; i++) {
+    emojiPickerTriggers[i].addEventListener('click', emoji_picker);
+  }
+
+  $(document).click(function(event) {
+    let target = $(event.target);
+
+    if(!target.closest('.emoji-trigger').length
+      && $('.emoji-trigger').children().first().prop('tagName') == 'EMOJI-PICKER') {
+      $('.emoji-trigger').html("Emoji");
+    }
+  });
+
+  $("#add-new-custom-emoji-button").click(function(event) {
+    const picker = new Picker({
+      customEmoji: [
+        {
+          name: $("#custom-emoji-name").val(),
+          shortcodes: $("#custom-emoji-shortcode").val(),
+          url: $("#custom-emoji-url").val(),
+          category: $("#custom-emoji-category").val()
+        }
+      ]
+    });
+  });
+
 
 })(jQuery);
