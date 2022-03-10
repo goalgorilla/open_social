c2cc4a8c9153a0eab18c9b9025adf24ca3a1ffd6
Vitaliy Nechay <nechai.dev@gmail.com>
2022-03-10 22:19:00 +0200

    Issue #3260416 by vnech: Removed redundant code

--- modules/social_features/social_follow_user/src/Service/SocialFollowUserHelper.php
+++ modules/social_features/social_follow_user/src/Service/SocialFollowUserHelper.php
@@ -5,47 +5,11 @@ namespace Drupal\social_follow_user\Service;
 use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\profile\Entity\ProfileInterface;
-use Drupal\user\RoleInterface;
 
 /**
  * Defines the helper service.
  */
 class SocialFollowUserHelper implements SocialFollowUserHelperInterface {
-
-  /**
-   * Set default permissions.
-   */
-  public static function setPermissions(): void {
-    $roles = \Drupal::entityQuery('user_role')
-      ->condition('id', 'administrator', '<>')
-      ->execute();
-
-    foreach ($roles as $role) {
-      user_role_grant_permissions($role, self::getPermissions($role));
-    }
-  }
-
-  /**
-   * Set default permissions.
-   */
-  public static function getPermissions(string $role): array {
-    // Anonymous.
-    $permissions[RoleInterface::ANONYMOUS_ID] = [];
-
-    // Authenticated.
-    $permissions[RoleInterface::AUTHENTICATED_ID] = array_merge($permissions[RoleInterface::ANONYMOUS_ID], [
-      'flag follow_user',
-      'unflag follow_user',
-    ]);
-
-    // Content manager.
-    $permissions['contentmanager'] = array_merge($permissions[RoleInterface::AUTHENTICATED_ID], []);
-
-    // Site manager.
-    $permissions['sitemanager'] = array_merge($permissions['contentmanager'], []);
-
-    return $permissions[$role] ?? [];
-  }
 
   /**
    * {@inheritdoc}
