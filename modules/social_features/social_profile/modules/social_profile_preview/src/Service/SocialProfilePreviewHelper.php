57eaf5fdd75ed835e049e1c8bc10a21bf804caaa
serhii.myronets <serhii.myronets@lemberg.co.uk>
2022-03-31 14:11:06 +0300

    Issue #3268943 by agami4: Improve counters and buttons

--- modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelper.php
+++ modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelper.php
@@ -39,6 +39,7 @@ class SocialProfilePreviewHelper implements SocialProfilePreviewHelperInterface 
 
       $variables['#attached']['library'][] = 'social_profile_preview/base';
       $variables['#attached']['library'][] = 'social_follow_user/follow_user_counter';
+      $variables['#attached']['library'][] = 'social_follow_user/follow_user_btn';
     }
   }
 
