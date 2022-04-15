07670411ca51a4f72b934aa8e7104bf808391bba
Andrii Chyrskyi <ressinel@gmail.com>
2022-04-15 18:44:26 +0300

    Merge pull request #2867 from goalgorilla/feature/3268943-users-follow
    Issue #3268943 by SV: As a LU I want to be able to see the list of the followers & following users

--- modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelper.php
+++ modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelper.php
@@ -5,11 +5,27 @@ namespace Drupal\social_profile_preview\Service;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Template\Attribute;
 use Drupal\profile\Entity\ProfileInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 
 /**
  * Defines the helper service.
  */
 class SocialProfilePreviewHelper implements SocialProfilePreviewHelperInterface {
+
+  /**
+   * The module handler.
+   */
+  private ModuleHandlerInterface $moduleHandler;
+
+  /**
+   * SocialProfilePreviewHelper constructor.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
 
   /**
    * {@inheritdoc}
@@ -38,6 +54,11 @@ class SocialProfilePreviewHelper implements SocialProfilePreviewHelperInterface 
       }
 
       $variables['#attached']['library'][] = 'social_profile_preview/base';
+
+      if ($this->moduleHandler->moduleExists('social_follow_user')) {
+        $variables['#attached']['library'][] = 'social_follow_user/counter';
+        $variables['#attached']['library'][] = 'social_follow_user/button';
+      }
     }
   }
 
