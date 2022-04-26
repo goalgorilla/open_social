732a2c566f8c3f7cbc96b21b706ae23ca122f68b
Anna Maltseva <anna.maltseva@nxte.nl>
2022-04-26 15:07:33 +0200

    Issue #3265651 by belatrix: fixed multiple emoji input instead of one; added functionality to close empji-picker on click outside of button; added drupal behavior

--- modules/social_features/social_emoji/assets/js/social_emoji_picker.js
+++ modules/social_features/social_emoji/assets/js/social_emoji_picker.js
@@ -1,50 +1,57 @@
-(function (Drupal) {
+(function (Drupal, $) {
 
-  var emojiTriggers = document.getElementsByClassName("emoji-trigger");
-  var emojiPickers = document.getElementsByClassName("emoji-picker");
+  Drupal.behaviors.socialEmojiPicker = {
+    attach: function (context, settings) {
 
-  function emoji_picker(event)
-  {
-    var targetElement = event.target;
-    var emojiPicker = targetElement.nextSibling;
-    emojiPicker.classList.toggle('shown');
+      var emojiTriggers = document.getElementsByClassName("emoji-trigger");
+      var emojiPickers = document.getElementsByClassName("emoji-picker");
 
-    if (emojiPicker.getAttribute('listener') !== 'true') {
-      emojiPicker.addEventListener(
-        'emoji-click', e => {
+      function emoji_picker(event) {
+        var targetElement = event.target;
+        var emojiPicker = targetElement.nextSibling;
+        emojiPicker.classList.toggle('shown');
+
+        if (emojiPicker.getAttribute('listener') !== 'true') {
           emojiPicker.setAttribute('listener', true);
-          var parentElementId = emojiPicker.parentElement.id;
-          var pos = parentElementId.lastIndexOf('-wrapper');
-          var hiddenInputAttribute = parentElementId.slice(0, pos);
-          var inputFieldId = parentElementId.replace('-wrapper', '-0-value');
-          var textToInsert = e.detail.unicode;
-          var curPos;
-          var curValue;
-          if (typeof CKEDITOR != "undefined" && CKEDITOR.instances[inputFieldId]) {
-            CKEDITOR.instances[inputFieldId].insertText(textToInsert);
-          }
-          else
-          {
-            curPos = document.getElementById(inputFieldId).selectionStart;
-            curValue = document.getElementById(inputFieldId).value;
-            document.getElementById(inputFieldId).value = curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos);
-            var hiddenInputs = document.querySelectorAll('input[type=hidden]');
-            for (var i = 0; i < hiddenInputs.length; i++)
-            {
-              if (hiddenInputs[i].getAttribute('data-drupal-selector').includes(hiddenInputAttribute)) {
-                hiddenInputs[i].setAttribute('value', curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos));
+          emojiPicker.addEventListener(
+            'emoji-click', e => {
+              var parentElementId = emojiPicker.parentElement.id;
+              var pos = parentElementId.lastIndexOf('-wrapper');
+              var hiddenInputAttribute = parentElementId.slice(0, pos);
+              var inputFieldId = parentElementId.replace('-wrapper', '-0-value');
+              var textToInsert = e.detail.unicode;
+              var curPos;
+              var curValue;
+              if (typeof CKEDITOR != "undefined" && CKEDITOR.instances[inputFieldId]) {
+                CKEDITOR.instances[inputFieldId].insertText(textToInsert);
+              } else {
+                curPos = document.getElementById(inputFieldId).selectionStart;
+                curValue = document.getElementById(inputFieldId).value;
+                document.getElementById(inputFieldId).value = curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos);
+                var hiddenInputs = document.querySelectorAll('input[type=hidden]');
+                for (var i = 0; i < hiddenInputs.length; i++) {
+                  if (hiddenInputs[i].getAttribute('data-drupal-selector').includes(hiddenInputAttribute)) {
+                    hiddenInputs[i].setAttribute('value', curValue.slice(0, curPos) + textToInsert + curValue.slice(curPos));
+                  }
+                }
               }
             }
-          }
+          );
         }
-      );
+      }
+
+      for (var i = 0; i < emojiTriggers.length; i++) {
+        emojiPickers[i].innerHTML = '<emoji-picker></emoji-picker>';
+        emojiTriggers[i].addEventListener('click', emoji_picker);
+      }
+
+      $(document).on('click', function (e) {
+        if ($(e.target).closest(".emoji-picker").length === 0 && $(e.target).closest(".emoji-trigger").length === 0) {
+          $(".emoji-picker").removeClass('shown');
+        }
+      });
+
     }
   }
 
-  for (var i = 0; i < emojiTriggers.length; i++)
-  {
-    emojiPickers[i].innerHTML = '<emoji-picker></emoji-picker>';
-    emojiTriggers[i].addEventListener('click', emoji_picker);
-  }
-
-})(Drupal);
+})(Drupal, jQuery);
