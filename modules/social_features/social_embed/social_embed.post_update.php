1afa690b80568b5516e604892831a765e3efe24f
navneet0693 <navneetsingh0693@gmail.com>
2022-05-03 18:53:01 +0530

    Added conditions to check for existing consent of users.
    See: https://github.com/goalgorilla/open_social/pull/2913/files#r863616985
    
    As said, as this feature was release already, and we are improving the hook_post_update_name later. There is possibility that users might have already given the consent. So, we want to preserve their preferences.
    
    Added a new query to check for existing ids in the table. If there are ids existing we take a difference of ids and process them.

--- modules/social_features/social_embed/social_embed.post_update.php
+++ modules/social_features/social_embed/social_embed.post_update.php
@@ -37,13 +37,36 @@ function social_embed_post_update_11002_populate_field_embed_content_settings(ar
         ->fields('u', ['uid'])
         ->condition('uid', '0', '>')
         ->execute();
-      if (!empty($query)) {
-        // If 'count' is empty, we have nothing to process.
-        if (!empty($uids = $query->fetchCol())) {
+
+      // If 'count' is empty, we have nothing to process.
+      if (!empty($query)
+        && !empty($uids = $query->fetchCol())
+      ) {
+
+        // As this feature was release in Open Social 11, and because we are
+        // moving this code to a new update, there can be a scenario where
+        // users might have already set their preferences. So, we don't want
+        // to process such uids to keep them safe.
+        $existing_uids_query = $database->select('user__field_user_embed_content_consent', 'ufu')
+          ->fields('ufu', ['entity_id'])
+          ->condition('entity_id', '0', '>')
+          ->execute();
+
+        if (!empty($existing_uids_query)
+          && !empty($existing_uids = $existing_uids_query->fetchCol())
+        ) {
+          $uids_diff = array_diff($uids, $existing_uids);
+
           // Let's store the user IDs and their count.
+          $sandbox['uids'] = $uids_diff;
+          $sandbox['count'] = count($uids_diff);
+        }
+        else {
           $sandbox['uids'] = $uids;
           $sandbox['count'] = count($uids);
+        }
 
+        if ($sandbox['count'] > 0) {
           // 'progress' will represent the current progress of our processing.
           $sandbox['progress'] = 0;
         }
@@ -51,6 +74,10 @@ function social_embed_post_update_11002_populate_field_embed_content_settings(ar
           $sandbox['#finished'] = 1;
           return;
         }
+      }
+      else {
+        $sandbox['#finished'] = 1;
+        return;
       }
     }
 
@@ -83,8 +110,6 @@ function social_embed_post_update_11002_populate_field_embed_content_settings(ar
         )
         ->execute();
       $sandbox['progress']++;
-      \Drupal::messenger()
-        ->addMessage($uid . ' user processed.');
     }
 
     // Drupal’s Batch API will stop executing our update hook as soon as
