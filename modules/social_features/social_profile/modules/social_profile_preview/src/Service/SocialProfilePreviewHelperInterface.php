9a26ed2c440efc5c7807a321f400c4efbc34a20d
Taras Kruts <taras.kruts@lemberg.co.uk>
2022-05-09 13:59:45 +0300

    Merge pull request #2886 from goalgorilla/bugfix/3275467
    Issue #3275467 by chmez: Profile preview for event organizers

--- modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelperInterface.php
+++ modules/social_features/social_profile/modules/social_profile_preview/src/Service/SocialProfilePreviewHelperInterface.php
@@ -19,11 +19,15 @@ interface SocialProfilePreviewHelperInterface {
    * @param array|string $path
    *   (optional) The list of keys to the location of the attributes in
    *   preprocessing variables. Defaults to 'attributes'.
+   * @param bool $return_as_object
+   *   (optional) TRUE if attributes set should be returned as an object even if
+   *   it was an array. Defaults to FALSE.
    */
   public function alter(
     ProfileInterface $profile,
     array &$variables,
-    $path = 'attributes'
+    $path = 'attributes',
+    $return_as_object = FALSE
   ): void;
 
 }
