d710f3dfeb5da745e3d8dd435f057b4c44ed0ab6
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2022-06-08 17:38:53 +0300

    Issue #3254715 by chmez: Update patch for the "Social Blue" theme and refresh the actual release version in name of the update hook and deprecated hook

--- modules/social_features/social_group/modules/social_group_request/social_group_request.api.php
+++ modules/social_features/social_group/modules/social_group_request/social_group_request.api.php
@@ -16,7 +16,7 @@
  * @param array $group_types
  *   List of group types used in open social.
  *
- * @deprecated in social:11.2.0 and is removed from social:12.0.0. Use
+ * @deprecated in social:11.4.0 and is removed from social:12.0.0. Use
  *   hook_social_group_join_method_usage instead.
  *
  * @see https://www.drupal.org/node/3254715
@@ -26,3 +26,7 @@
 function hook_social_group_request_alter(array &$group_types) {
   $group_types[] = 'flexible_group';
 }
+
+/**
+ * @} End of "addtogroup hooks".
+ */
