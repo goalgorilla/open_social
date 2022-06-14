14b4b38e767d4c12b3b949fc33ad6596372a311b
Oleksandr Horbatiuk <oleksandr.horbatiuk@lemberg.co.uk>
2022-06-14 21:04:38 +0300

    Issue #3285045 by chmez: Make to be able to use the same format of creation page title of entity of any entity type

--- modules/social_features/social_group/src/Routing/RouteSubscriber.php
+++ modules/social_features/social_group/src/Routing/RouteSubscriber.php
@@ -3,12 +3,11 @@
 namespace Drupal\social_group\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\social_group\Controller\SocialGroupController;
+use Drupal\social_group\Form\SocialGroupAddForm;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
- * Class RouteSubscriber.
- *
- * @package Drupal\social_group\Routing
  * Listens to the dynamic route events.
  */
 class RouteSubscriber extends RouteSubscriberBase {
@@ -19,52 +18,51 @@ class RouteSubscriber extends RouteSubscriberBase {
   protected function alterRoutes(RouteCollection $collection) {
     // Route the group view page to group/{group}/timeline.
     if ($route = $collection->get('entity.group.canonical')) {
-      $route->setPath('/group/{group}/stream');
-      $defaults = $route->getDefaults();
-      $defaults['_entity_view'] = 'group.stream';
-      $defaults['_title_callback'] = '\Drupal\social_group\Controller\SocialGroupController::groupStreamTitle';
-      $route->setDefaults($defaults);
-    }
-
-    // Override the node creation page.
-    if ($route = $collection->get('entity.group_content.create_form')) {
-      $defaults = $route->getDefaults();
-      $defaults['_title_callback'] = '\Drupal\social_group\Controller\SocialGroupController::createFormTitle';
-      $route->setDefaults($defaults);
+      $route
+        ->setPath('/group/{group}/stream')
+        ->setDefault('_entity_view', 'group.stream')
+        ->setDefault(
+          '_title_callback',
+          SocialGroupController::class . '::groupStreamTitle',
+        );
     }
 
     // Override default title for Group Members page.
     if ($route = $collection->get('view.group_members.page_group_members')) {
-      $defaults = $route->getDefaults();
-      $defaults['_title_callback'] = '\Drupal\social_group\Controller\SocialGroupController::groupMembersTitle';
-      $route->setDefaults($defaults);
+      $route->setDefault(
+        '_title_callback',
+        SocialGroupController::class . '::groupMembersTitle',
+      );
     }
 
     // Override default title for Groups "Add Member" page.
     if ($route = $collection->get('entity.group_content.add_form')) {
-      $defaults = $route->getDefaults();
-      $defaults['_title_callback'] = '\Drupal\social_group\Controller\SocialGroupController::groupAddMemberTitle';
-      $route->setDefaults($defaults);
+      $route->setDefault(
+        '_title_callback',
+        SocialGroupController::class . '::groupAddMemberTitle',
+      );
     }
 
     // Override default title for Groups "Delete Content" page.
     if ($route = $collection->get('entity.group_content.delete_form')) {
-      $defaults = $route->getDefaults();
-      $defaults['_title_callback'] = '\Drupal\social_group\Controller\SocialGroupController::groupRemoveContentTitle';
-      $route->setDefaults($defaults);
+      $route->setDefault(
+        '_title_callback',
+        SocialGroupController::class . '::groupRemoveContentTitle',
+      );
     }
 
     if ($route = $collection->get('entity.group.add_page')) {
       $defaults = $route->getDefaults();
       unset($defaults['_controller']);
-      $defaults['_form'] = '\Drupal\social_group\Form\SocialGroupAddForm';
+      $defaults['_form'] = SocialGroupAddForm::class;
       $route->setDefaults($defaults);
     }
 
     if ($route = $collection->get('view.groups.page_user_groups')) {
-      $requirements = $route->getRequirements();
-      $requirements['_custom_access'] = "\Drupal\social_group\Controller\SocialGroupController::myGroupAccess";
-      $route->setRequirements($requirements);
+      $route->setRequirement(
+        '_custom_access',
+        SocialGroupController::class . '::myGroupAccess',
+      );
     }
 
   }
