07f66a74e7ee7275939cbed9ec892df6529d83ef
Alexander Varwijk <alexander@getopensocial.com>
2023-01-17 11:15:45 +0100

    Test against the sky theme by default
    We install platforms with the Sky theme and we want all of our tests to
    move that way so we test what we're actually shipping and providing to
    our users. Any tests that don't yet support sky have the step `the theme
    is set to old` added so that it's clear they need work to support Sky.

--- tests/behat/features/bootstrap/ThemeContext.php
+++ tests/behat/features/bootstrap/ThemeContext.php
@@ -7,26 +7,8 @@ use Behat\MinkExtension\Context\RawMinkContext;
 
 /**
  * Defines test steps around theming.
- *
- * Also sets our default sky theme back to the legacy theme for which tests were
- * designed.
  */
 class ThemeContext extends RawMinkContext {
-
-  /**
-   * By default all tests run in the old theme.
-   *
-   * @param \Behat\Behat\Hook\Scope\BeforeScenarioScope $scope
-   *   The before scenario scope.
-   *
-   * @BeforeScenario
-   */
-  public function before(BeforeScenarioScope $scope) {
-    // Since we enable Sky theme by default we should make sure we run our
-    // tests on the old theme. In another case, it will break all our tests.
-    // @see https://www.drupal.org/project/socialblue/issues/3251299
-    $this->withTheme("old");
-  }
 
   /**
    * Allows a test to use a specific theme for a scenario.
