9f736545db19afb5e092fd05f206e6c7f2667e57
nkoporec <me@nkoporec.com>
2023-03-07 18:03:52 +0100

    Update setContext to be compatible with the parent

--- modules/social_features/social_user/src/Plugin/Action/SocialSendEmail.php
+++ modules/social_features/social_user/src/Plugin/Action/SocialSendEmail.php
@@ -136,7 +136,7 @@ class SocialSendEmail extends ViewsBulkOperationsActionBase implements Container
   /**
    * {@inheritdoc}
    */
-  public function setContext(array &$context) {
+  public function setContext(array &$context): void {
     parent::setContext($context);
     // @todo make the batch size configurable.
     $context['batch_size'] = Settings::get('social_mail_batch_size', 25);
