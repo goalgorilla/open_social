94c2a600b5fa23397deedd0c22a5a017d58d0ef7
Tiago Siqueira <tiago@getopensocial.com>
2023-03-20 14:34:49 +0100

    Revert "issuer #3345231 by zanivdmar: Missing option to accept invite when group joining option is only by request while user is invited by group/site manager."
    This reverts commit 0507997918c32353b5014b4723978e5a984267db.

--- modules/social_features/social_group/modules/social_group_request/src/Plugin/Join/SocialGroupRequestJoin.php
+++ modules/social_features/social_group/modules/social_group_request/src/Plugin/Join/SocialGroupRequestJoin.php
@@ -6,7 +6,6 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\ginvite\GroupInvitationLoaderInterface;
-use Drupal\ginvite\Plugin\GroupContentEnabler\GroupInvitation as GroupInvitationEnabler;
 use Drupal\grequest\Plugin\GroupContentEnabler\GroupMembershipRequest;
 use Drupal\social_group\EntityMemberInterface;
 use Drupal\social_group\JoinBase;
@@ -81,9 +80,8 @@ class SocialGroupRequestJoin extends JoinBase {
     // If user has a pending invite we should skip the request button.
     if ($this->loader !== NULL) {
       $group_invites = $this->loader->loadByProperties([
-        'entity_id' => $this->currentUser->id(),
-        'gid' => $entity->id(),
-        'invitation_status' => GroupInvitationEnabler::INVITATION_PENDING,
+        'gid' => $group->id(),
+        'uid' => $this->currentUser->id(),
       ]);
 
       if ($group_invites !== []) {
