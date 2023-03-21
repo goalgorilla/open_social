16b9c1586f2334b4a86d8f2941402bf166e867f6
ronaldtebrake <ronald@goalgorilla.com>
2023-03-21 13:27:33 +0100

    Performance improvements for the Toolbar by overriding core toolbar early and stopping others to try and tweak
    By unsetting implementations of the hook_toolbar, we stop for example Gin trying to alter our toolbar.
    By making sure we render social_core.dashboard as menu instead of subtreeing it in the Drupal admin menu
    We make sure we don't need many roundtrips of access checks, it's only two levels deep with menu links we provide.

--- modules/custom/social_admin_menu/src/Menu/SocialAdminMenuAdministratorMenuLinkTreeManipulators.php
+++ modules/custom/social_admin_menu/src/Menu/SocialAdminMenuAdministratorMenuLinkTreeManipulators.php
@@ -47,88 +47,6 @@ class SocialAdminMenuAdministratorMenuLinkTreeManipulators extends DefaultMenuLi
   }
 
   /**
-   * Performs access checks of a menu tree.
-   *
-   * Sets the 'access' property to AccessResultInterface objects on menu link
-   * tree elements. Descends into subtrees if the root of the subtree is
-   * accessible. Inaccessible subtrees are deleted, except the top-level
-   * inaccessible link, to be compatible with render caching.
-   *
-   * (This means that top-level inaccessible links are *not* removed; it is up
-   * to the code doing something with the tree to exclude inaccessible links,
-   * just like MenuLinkTree::build() does. This allows those things to specify
-   * the necessary cacheability metadata.)
-   *
-   * This is compatible with render caching, because of cache context bubbling:
-   * conditionally defined cache contexts (i.e. subtrees that are only
-   * accessible to some users) will bubble just like they do for render arrays.
-   * This is why inaccessible subtrees are deleted, except at the top-level
-   * inaccessible link: if we didn't keep the first (depth-wise) inaccessible
-   * link, we wouldn't be able to know which cache contexts would cause those
-   * subtrees to become accessible again, thus forcing us to conclude that that
-   * subtree is unconditionally inaccessible.
-   *
-   * @param \Drupal\Core\Menu\MenuLinkTreeElement[] $tree
-   *   The menu link tree to manipulate.
-   *
-   * @return \Drupal\Core\Menu\MenuLinkTreeElement[]
-   *   The manipulated menu link tree.
-   */
-  public function checkAccess(array $tree) {
-
-    if ($this->account->id() != 1) {
-      $account_roles = $this->account->getRoles();
-      // Define routes to hide for a role.
-      // 'content' => 'system.admin_content',
-      // 'structure' => 'system.admin_structure',
-      // 'config' => 'system.admin_config',
-      // 'reports' => 'system.admin_reports',
-      // 'themes' => 'system.themes_page',
-      // 'people' => 'entity.user.collection',
-      // 'help' => 'help.main'.
-      $routes = [
-        'contentmanager' => [
-          'config' => 'system.admin_config',
-          'reports' => 'system.admin_reports',
-          'help' => 'help.main',
-        ],
-        'sitemanager' => [
-          'help' => 'help.main',
-        ],
-      ];
-
-      // Define which routes to hide based on hierarchy.
-      if (in_array('sitemanager', $account_roles)) {
-        $hide_routes = $routes['sitemanager'];
-      }
-      elseif (in_array('contentmanager', $account_roles)) {
-        $hide_routes = $routes['contentmanager'];
-      }
-      else {
-        $hide_routes = [];
-      }
-
-      foreach ($tree as $key => $element) {
-        // Always hide the admin_toolbar_tools.help.
-        $plugin_id = $tree[$key]->link->getPluginId();
-        if ($plugin_id === 'admin_toolbar_tools.help') {
-          unset($tree[$key]);
-          continue;
-        }
-
-        $route = $tree[$key]->link->getRouteName();
-        if (in_array($route, $hide_routes)) {
-          unset($tree[$key]);
-          continue;
-        }
-
-      }
-    }
-
-    return $tree;
-  }
-
-  /**
    * Renders the toolbar's administration tray.
    *
    * This is a adoption of core's
@@ -142,17 +60,16 @@ class SocialAdminMenuAdministratorMenuLinkTreeManipulators extends DefaultMenuLi
    */
   public function renderForm() {
     $parameters = new MenuTreeParameters();
-    $parameters->setRoot('system.admin')
+    $parameters->setRoot('social_core.dashboard')
       ->excludeRoot()
-      ->setMaxDepth(4)
+      ->setMaxDepth(2)
       ->onlyEnabledLinks();
     $manipulators = [
       ['callable' => 'menu.default_tree_manipulators:checkAccess'],
       ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-      ['callable' => 'toolbar_tools_menu_navigation_links'],
-      ['callable' => 'social_admin_menu.administrator_menu_tree_manipulators:checkAccess'],
+      ['callable' => 'gin_toolbar_tools_menu_navigation_links'],
     ];
-    $tree = $this->toolbarMenuLinkTree->load(NULL, $parameters);
+    $tree = $this->toolbarMenuLinkTree->load('social_core.dashboard', $parameters);
     $tree = $this->toolbarMenuLinkTree->transform($tree, $manipulators);
     $element['toolbar_administration'] = [
       '#type' => 'container',
