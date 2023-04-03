952d970c6db3a77dbb6597043c4cfe8f93b99d37
Tiago Siqueira <tiago@getopensocial.com>
2023-04-03 10:49:24 +0200

    Revert "Issue #3341238: Upgrade Drupal version to Drupal 9.5"

--- modules/custom/entity_access_by_field/tests/src/Kernel/EntityAccessNodeAccessTestBase.php
+++ modules/custom/entity_access_by_field/tests/src/Kernel/EntityAccessNodeAccessTestBase.php
@@ -16,7 +16,7 @@ abstract class EntityAccessNodeAccessTestBase extends EntityKernelTestBase {
    *
    * @var array
    */
-  protected static $modules = ['group', 'options', 'entity', 'variationcache', 'node', 'gnode', 'social_group', 'flag', 'address', 'image', 'file', 'entity_access_by_field'];
+  public static $modules = ['group', 'options', 'entity', 'variationcache', 'node', 'gnode', 'social_group', 'flag', 'address', 'image', 'file', 'entity_access_by_field'];
 
   /**
    * The entity type manager service.
