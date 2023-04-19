b83071424e6da9bdcfdb91c1825803ea3d192df7
Alexander Varwijk <alexander@getopensocial.com>
2023-04-19 14:11:27 +0200

    Move phpstan-* to normal docblock
    With the change in coder we can deduplicate some of our docblock
    annotations since Coder now accepts more complex PHPStan types that it
    would previously choke on.

--- tests/behat/features/bootstrap/TopicContext.php
+++ tests/behat/features/bootstrap/TopicContext.php
@@ -31,15 +31,14 @@ class TopicContext extends RawMinkContext {
    * the ID if we already have it in the step or the title otherwise. We avoid
    * looking up the topic because a user may be testing an error state.
    *
-   * @var array
-   * @phpstan-var array<int|string>
+   * @var array<int|string>
    */
   private array $created = [];
 
   /**
    * Topic data that was changed in a previous step.
    *
-   * @phpstan-var array<string, mixed>
+   * @var array<string, mixed>
    */
   private array $updatedTopicData = [];
 
