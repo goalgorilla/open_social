8a6b79ed988bfe4759e6934ca0fff01dda03bf95
Alexander Varwijk <alexander@getopensocial.com>
2023-06-23 13:43:10 +0200

    Ensure the mail-spool directory exists for Behat tests
    We previously created this in the GitHub workflow, but this makes new
    developer project set-up more difficult (because then when running an
    email test the folder doesn't exist).
    
    By adding it to the repository we ensure it's present for developers and
    we can remove some steps from the CI.
    
    We rename it from `swiftmailer-spool` to `mail-spool` because we know in
    the future we want to move away from swiftmailer.
    
    We ignore all messages in the director to make sure we don't
    accidentally commit debug output.

--- tests/behat/features/bootstrap/EmailContext.php
+++ tests/behat/features/bootstrap/EmailContext.php
@@ -84,7 +84,7 @@ class EmailContext implements Context {
    *   The path where the spooled emails are stored.
    */
   protected function getSpoolDir() {
-    $path = \Drupal::service('extension.list.profile')->getPath('social') . '/tests/behat/features/swiftmailer-spool';
+    $path = \Drupal::service('extension.list.profile')->getPath('social') . '/tests/behat/mail-spool';
     if (!file_exists($path)) {
       mkdir($path, 0777, true);
     }
