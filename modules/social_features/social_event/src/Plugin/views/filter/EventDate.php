238cd7a8c1c24946fa9350039f40de81004288d9
Vitalii Koval <vitalii.koval@lembergsolutions.com>
2023-07-03 16:27:02 +0300

    Issue PROD-22452: Fix ongoing events when checkbox all day is activated
    Issue PROD-22452: Change comment
    
    Issue #3276434: Add behat tests for 'Filter community events' feature
    
    PROD-22452: Change the plugin filter for events, change behat tests for event filtering
    
    PROD-22452: Minor fix in behat test for filtering of events
    
    PROD-22452: Minor fixes in behat test

--- modules/social_features/social_event/src/Plugin/views/filter/EventDate.php
+++ modules/social_features/social_event/src/Plugin/views/filter/EventDate.php
@@ -49,11 +49,21 @@ class EventDate extends InOperator {
     if (empty($value)) {
       return;
     }
-
     $this->ensureMyTable();
     $now = $this->query->getDateFormat('NOW()', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, TRUE);
+    // Get the time at past midnight and next midnight time.
+    $past_midnight = $this->query->getDateFormat('NOW()', 'Y-m-d\T00:00:00', TRUE);
+    $next_midnight = $this->query->getDateFormat('NOW()', 'Y-m-d\T23:59:59', TRUE);
+
     $configuration = [
       'table' => 'node__field_event_date_end',
+      'field' => 'entity_id',
+      'left_table' => '',
+      'left_field' => 'nid',
+    ];
+
+    $configuration_all_day = [
+      'table' => 'node__field_event_all_day',
       'field' => 'entity_id',
       'left_table' => '',
       'left_field' => 'nid',
@@ -63,17 +73,25 @@ class EventDate extends InOperator {
       ->createInstance('standard', $configuration);
     $alias = $this->query->addRelationship($configuration['table'], $join, 'node_field_data');
     $field_end = $this->query->getDateFormat($alias . '.field_event_date_end_value', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, TRUE);
+
+    $all_day_join = Views::pluginManager('join')
+      ->createInstance('standard', $configuration_all_day);
+    $all_day_alias = $this->query->addRelationship($configuration_all_day['table'], $all_day_join, 'node_field_data');
+    $all_day = $all_day_alias . '.field_event_all_day_value';
+
     $field = "{$this->tableAlias}.{$this->realField}";
     $field = $this->query->getDateFormat($field, DateTimeItemInterface::DATETIME_STORAGE_FORMAT, TRUE);
 
     switch ($value) {
       case self::UPCOMING_EVENTS:
-        $this->query->addWhereExpression($this->options['group'], "({$field} >= {$now}) OR ({$field} <= {$now} AND {$field_end} > {$now})");
+        $this->query->addWhereExpression($this->options['group'], "({$field} >= {$now}
+        OR
+        ({$now} >= {$field} AND ({$now} < {$field_end} OR ({$all_day} > 0 AND {$past_midnight} <= {$field_end} AND {$field_end} < {$next_midnight}))))");
         break;
 
       case self::PAST_EVENTS:
         $this->query->addWhereExpression($this->options['group'], "
-        ({$now} >= {$field_end})
+        (({$now} >= {$field_end}) AND ({$past_midnight} > {$field_end}))
         OR
         ({$field_end} IS NULL AND {$now} >= {$field})");
         break;
