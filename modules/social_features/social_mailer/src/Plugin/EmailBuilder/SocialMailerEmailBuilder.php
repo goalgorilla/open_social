c58dfa1dd53d03fab5ebd0cb58463e52f0d59ef5
Robert Ragas <robertragas@getopensocial.com>
2023-08-15 10:08:51 +0200

    update the dependencies to use the new plugin location as the symfony_mailer_bc is not being used anymore. Also update the plugin itself to extend the EmailBuilderBase like all the other plugins also do

--- modules/social_features/social_mailer/src/Plugin/EmailBuilder/SocialMailerEmailBuilder.php
+++ modules/social_features/social_mailer/src/Plugin/EmailBuilder/SocialMailerEmailBuilder.php
@@ -2,83 +2,56 @@
 
 namespace Drupal\social_mailer\Plugin\EmailBuilder;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Render\Markup;
-use Drupal\Core\Session\AccountProxyInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\symfony_mailer\EmailFactoryInterface;
 use Drupal\symfony_mailer\EmailInterface;
-use Drupal\symfony_mailer\Processor\EmailProcessorBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\symfony_mailer\Processor\EmailBuilderBase;
 
 /**
- * Defines the Email builder for the social_mailer module.
+ * Defines the Email Builder plug-in for user module.
  *
  * @EmailBuilder(
  *   id = "social_mailer",
- *   sub_types = {
- *     "test" = @Translation("Test email")
- *   }
+ *   sub_types = { "test" = @Translation("Test email") },
+ *   common_adjusters = {"email_subject", "email_body"},
  * )
  */
-class SocialMailerEmailBuilder extends EmailProcessorBase implements ContainerFactoryPluginInterface {
+class SocialMailerEmailBuilder extends EmailBuilderBase {
 
   /**
-   * The current user.
-   */
-  protected AccountProxyInterface $currentUser;
-
-  /**
-   * The config factory.
-   */
-  protected ConfigFactoryInterface $configFactory;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('current_user'),
-      $container->get('config.factory')
-    );
-  }
-
-  /**
-   * Constructs a UserEmailBuilder object.
+   * Saves the parameters for a newly created email.
    *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
-   *   The current user.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory.
+   * @param \Drupal\symfony_mailer\EmailInterface $email
+   *   The email to modify.
+   * @param mixed $params
+   *   The params containing the site name
+   * @param mixed $to
+   *   The to addresses, see Address::convert().
+   *
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxyInterface $current_user, ConfigFactoryInterface $config_factory) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->currentUser = $current_user;
-    $this->configFactory = $config_factory;
+  public function createParams(EmailInterface $email, $params = NULL, $to = NULL) {
+    $email->setParam('to', $to);
+    $email->setParam('site_name', $params['site_name']);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function preRender(EmailInterface $email): void {
-    parent::preRender($email);
+  public function fromArray(EmailFactoryInterface $factory, array $message) {
+    return $factory->newTypedEmail($message['module'], $message['key'], $message['params'], $message['to']);
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function build(EmailInterface $email) {
+    $email->setTo($email->getParam('to'));
     $email->setSubject($this->t('Social Mailer has been successfully configured!'));
 
-    $text[] = '<h3>' . $this->t('Dear @user,', [
-      '@user' => $this->currentUser->getDisplayName(),
-    ]) . '</h3>';
+    $text[] = '<h3>' . $this->t('Hello,') . '</h3>';
     $text[] = '<p>' . $this->t('This e-mail has been sent from @site by the Social Mailer module. The module has been successfully configured.', [
-      '@site' => $this->configFactory->get('system.site')->get('name'),
-    ]) . '</p>';
+        '@site' => $email->getParam('site_name'),
+      ]) . '</p>';
     $text[] = $this->t('Kind regards') . '<br /><br />';
     $text[] = $this->t('The Social Mailer module');
     $email->setBody(Markup::create(implode(PHP_EOL, $text)));
