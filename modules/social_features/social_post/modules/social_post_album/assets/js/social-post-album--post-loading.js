0acc23d5828bb4aeb66dbd91b27ade3a4df82c96
serhii.myronets <serhii.myronets@lembergsolutions.com>
2023-10-26 15:45:13 +0300

    '#3396909 by agami4: Improve js behavior of the post button on the post form'

--- modules/social_features/social_post/modules/social_post_album/assets/js/social-post-album--post-loading.js
+++ modules/social_features/social_post/modules/social_post_album/assets/js/social-post-album--post-loading.js
@@ -1,24 +1,25 @@
-(function ($) {
+(function (Drupal, $, once) {
   Drupal.behaviors.socialPostAlbumPostLoading = {
     attach: function (context) {
-      var postForm = $(context).find('.social-post-album--form form');
-          postBtn = postForm.find('> .form-submit');
-          postTextField = postForm.find('.mentions-input input[type="hidden"]');
+      $(once('socialPostAlbumPostLoading', '.social-post-album--form form', context)).each(function () {
+        var postBtn = $(this).find('> .form-submit');
+        var postTextField = $(this).find('.mentions-input input[type="hidden"]');
 
-      // Add wrapper to the text inside button.
-      postBtn.wrapInner('<span class="text"></span>');
+        // Add wrapper to the text inside button.
+        postBtn.wrapInner('<span class="text"></span>');
 
-      // Add load icon to the button.
-      postBtn.append('<span class="post-loading"><svg class="loader" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">\n' +
-        '  <circle class="internal-circle" cx="60" cy="60" r="30"></circle>\n' +
-        '</svg></span>');
+        // Add load icon to the button.
+        postBtn.append('<span class="post-loading"><svg class="loader" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">\n' +
+          '  <circle class="internal-circle" cx="60" cy="60" r="30"></circle>\n' +
+          '</svg></span>');
 
-      // Show load icon and hide text button on click.
-      postBtn.on('click', function () {
-        if (postTextField.val() !== '') {
-          $(this).addClass('post-loading-active');
-        }
+        // Show load icon and hide text button on click.
+        postBtn.on('click', function () {
+          if (postTextField.val() !== '') {
+            $(this).addClass('post-loading-active');
+          }
+        });
       });
     }
   };
-})(jQuery);
+})(Drupal, jQuery, once);
