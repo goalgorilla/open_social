97b88234e76e42c327ba8808f8ecb71a5da403bf
ronaldtebrake <ronald@goalgorilla.com>
2023-10-29 22:04:17 +0100

    Fix ParseError: syntax error, unexpected 'private' (T_PRIVATE), expecting variable (T_VARIABLE) in Composer\Autoload\includeFile()

--- modules/social_features/social_tagging/src/Plugin/Field/FieldFormatter/TaggingUsageFormatter.php
+++ modules/social_features/social_tagging/src/Plugin/Field/FieldFormatter/TaggingUsageFormatter.php
@@ -22,10 +22,16 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class TaggingUsageFormatter extends FormatterBase {
 
   /**
+   * The tagging service.
+   */
+  protected SocialTaggingServiceInterface $taggingService;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, private SocialTaggingServiceInterface $taggingService) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, SocialTaggingServiceInterface $taggingService) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->taggingService = $taggingService;
   }
 
   /**
