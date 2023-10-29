6642828e6c6c9d8e7a2058e29be228bdf537d6c4
ronaldtebrake <ronald@goalgorilla.com>
2023-10-29 08:48:24 +0100

    Fix assertion on destination for redirect response based on query get.
     ------ --------------------------------------------------------------------------------------------------------------------------------------------
      Line   modules/social_features/social_event/src/Controller/UpdateEnrollRequestController.php
     ------ --------------------------------------------------------------------------------------------------------------------------------------------
      104    Parameter #1 $url of class Symfony\Component\HttpFoundation\RedirectResponse constructor expects string, bool|float|int|string|null given.
    
    and also ensure we return a invalid argument exception if thats not the case.

--- modules/social_features/social_event/src/Controller/UpdateEnrollRequestController.php
+++ modules/social_features/social_event/src/Controller/UpdateEnrollRequestController.php
@@ -100,7 +100,7 @@ class UpdateEnrollRequestController extends ControllerBase {
 
     // Get the redirect destination we're given in the request for the response.
     $destination = $this->requestStack->getCurrentRequest()->query->get('destination');
-
+    assert(is_string($destination), new \InvalidArgumentException());
     return new RedirectResponse($destination);
   }
 
