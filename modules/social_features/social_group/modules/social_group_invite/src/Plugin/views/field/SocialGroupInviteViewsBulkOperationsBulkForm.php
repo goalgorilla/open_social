e009824b51a325b9fbe311a0d7cf450adb318bcc
Vinicius Rodrigues Pereira <rpereira.vcs@gmail.com>
2024-01-04 12:13:34 -0300

    Removed third parameter from calculateEntityBulkFormKey invoke function

--- modules/social_features/social_group/modules/social_group_invite/src/Plugin/views/field/SocialGroupInviteViewsBulkOperationsBulkForm.php
+++ modules/social_features/social_group/modules/social_group_invite/src/Plugin/views/field/SocialGroupInviteViewsBulkOperationsBulkForm.php
@@ -61,8 +61,7 @@ class SocialGroupInviteViewsBulkOperationsBulkForm extends ViewsBulkOperationsBu
             if ($entity = $this->getEntity($row)) {
               $bulk_form_keys[$row_index] = self::calculateEntityBulkFormKey(
                 $entity,
-                $row->{$base_field},
-                $row_index
+                $row->{$base_field}
               );
             }
           }
