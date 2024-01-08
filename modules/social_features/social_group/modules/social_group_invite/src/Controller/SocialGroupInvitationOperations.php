065ae5b5d17a70e72c9ba15a302e0138fcecdd7b
Volodymyr Sydor <volodymyr.sydor@lemberg.co.uk>
2024-01-08 14:06:38 +0200

    Issue #3403634 by SV: Adjust functionality in: activity_filter_personalised_homepage, social_group_invite and related modules

--- modules/social_features/social_group/modules/social_group_invite/src/Controller/SocialGroupInvitationOperations.php
+++ modules/social_features/social_group/modules/social_group_invite/src/Controller/SocialGroupInvitationOperations.php
@@ -3,8 +3,8 @@
 namespace Drupal\social_group_invite\Controller;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\group\Entity\GroupContent;
-use Drupal\group\Entity\GroupContentInterface;
+use Drupal\group\Entity\GroupRelationship;
+use Drupal\group\Entity\GroupRelationshipInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\ginvite\Controller\InvitationOperations;
@@ -19,33 +19,30 @@ class SocialGroupInvitationOperations extends InvitationOperations {
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   HTTP request.
-   * @param \Drupal\group\Entity\GroupContentInterface $group_content
+   * @param \Drupal\group\Entity\GroupRelationshipInterface $group_content
    *   Invitation entity.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
    *   A redirect response object.
    */
-  public function accepted(Request $request, GroupContentInterface $group_content) {
+  public function accepted(Request $request, GroupRelationshipInterface $group_content) {
     $group = $group_content->getGroup();
-    $contentTypeConfigId = $group_content->getGroup()
-      ->getGroupType()
-      ->getContentPlugin('group_membership')
-      ->getContentTypeConfigId();
 
     // Check if user already is a member.
     $membership = $this->membershipLoader->load($group, $this->currentUser());
+    $relation_type_id = $this->entityTypeManager()->getStorage('group_content_type')->getRelationshipTypeId($group_type->id(), 'group_membership');
 
     if (!$membership) {
-      /** @var \Drupal\group\Entity\GroupContentInterface $group_content */
-      $group_content = GroupContent::create([
-        'type' => $contentTypeConfigId,
-        'entity_id' => $group_content->get('entity_id')->getString(),
+      /** @var \Drupal\group\Entity\GroupRelationshipInterface $group_content */
+      $group_membership = GroupRelationship::create([
+        'type' => $relation_type_id,
+        'entity_id' => $group_content->getEntityId(),
         'content_plugin' => 'group_membership',
         'gid' => $group->id(),
         'uid' => $group_content->getOwnerId(),
         'group_roles' => $group_content->get('group_roles')->getValue(),
       ]);
-      $group_content->save();
+      $group_membership->save();
     }
     else {
       $this->messenger->addStatus($this->t('You are already a member of the @group.', [
@@ -59,8 +56,8 @@ class SocialGroupInvitationOperations extends InvitationOperations {
   /**
    * {@inheritdoc}
    */
-  public function checkAccess(GroupContentInterface $group_content) {
-    $invited = $group_content->get('entity_id')->getString();
+  public function checkAccess(GroupRelationshipInterface $group_content) {
+    $invited = $group_content->getEntityId();
 
     // Only allow user accept/decline own invitations.
     if ($invited == $this->currentUser()->id()) {
