6f4bfcea61f22797362d26dd8b64c6fb3c2ba0fa
Robert Ragas <robertragas@getopensocial.com>
2024-01-10 16:13:20 +0100

    When using the token to replace the mentions into displaynames it can cause the render context to be empty. We can fix this by doing the conversion manually and skipping the token for this part when it's run in the terminal.

--- modules/custom/mentions/src/Plugin/Mentions/Entity.php
+++ modules/custom/mentions/src/Plugin/Mentions/Entity.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\mentions\Plugin\Mentions;
 
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Utility\Token;
 use Drupal\mentions\MentionsPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\profile\Entity\ProfileInterface;
 
 /**
  * The Mention entity.
@@ -32,16 +34,26 @@ class Entity implements MentionsPluginInterface {
   private EntityTypeManagerInterface $entityTypeManager;
 
   /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  private $config;
+
+  /**
    * Entity constructor.
    *
    * @param \Drupal\Core\Utility\Token $token
    *   The token service.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\Config\ConfigFactory $config
+   *   The config factory.
    */
-  public function __construct(Token $token, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(Token $token, EntityTypeManagerInterface $entity_type_manager, ConfigFactory $config) {
     $this->tokenService = $token;
     $this->entityTypeManager = $entity_type_manager;
+    $this->config = $config;
   }
 
   /**
@@ -50,9 +62,11 @@ class Entity implements MentionsPluginInterface {
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $token = $container->get('token');
     $entity_type_manager = $container->get('entity_type.manager');
+    $config = $container->get('config.factory');
     return new static(
       $token,
-      $entity_type_manager
+      $entity_type_manager,
+      $config
     );
   }
 
@@ -63,6 +77,31 @@ class Entity implements MentionsPluginInterface {
     $entity = $this->entityTypeManager->getStorage($mention['target']['entity_type'])
       ->load($mention['target']['entity_id']);
     $output = [];
+
+    // If the mentions is run with cron, replace the output ourself.
+    if (PHP_SAPI === 'cli') {
+      if ($entity instanceof ProfileInterface) {
+        $entity = $entity->getOwner();
+
+        // Get the output value according to the config settings.
+        $config = $this->config->get('mentions.settings');
+        switch ($config->get('suggestions_format')) {
+          case SOCIAL_PROFILE_SUGGESTIONS_FULL_NAME:
+          case SOCIAL_PROFILE_SUGGESTIONS_ALL:
+            $output['value'] = $entity->getDisplayName();
+        }
+        if (empty($output['value'])) {
+          $output['value'] = $entity->getAccountName();
+        }
+
+        $output['render_plain'] = !$entity->access('view');
+        // Convert to the correct output link based on the mention config.
+        // Ex: user/[user:uid] will replace between the brackets for OwnerId.
+        $output['link'] = preg_replace("/\[([^\[\]]++|(?R))*+\]/", (string) $entity->id(), $settings['rendertextbox']);
+      }
+      return $output;
+    }
+
     $output['value'] = $this->tokenService->replace($settings['value'], [$mention['target']['entity_type'] => $entity]);
     $output['render_plain'] = $entity && !$entity->access('view');
     if ($settings['renderlink']) {
