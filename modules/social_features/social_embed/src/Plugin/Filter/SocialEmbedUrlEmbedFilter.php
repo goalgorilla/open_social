3723759c227eeafe25014869ee8a62cca7eab998
VBaldych <volodymyr.baldych@lembergsolutions.com>
2024-02-13 17:54:52 +0200

    Issue #3421184: Fix incorrect URL embedding

--- modules/social_features/social_embed/src/Plugin/Filter/SocialEmbedUrlEmbedFilter.php
+++ modules/social_features/social_embed/src/Plugin/Filter/SocialEmbedUrlEmbedFilter.php
@@ -143,7 +143,8 @@ class SocialEmbedUrlEmbedFilter extends UrlEmbedFilter {
           }
         }
         catch (\Exception $e) {
-          watchdog_exception('url_embed', $e);
+          $logger = $this->loggerFactory->get('social_embed');
+          Error::logException($logger, $e);
         } finally {
           // If the $url_output is empty, that means URL is non-embeddable.
           // So, we return the original url instead of blank output.
