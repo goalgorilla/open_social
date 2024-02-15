03767e1d0c2e3dd964b03d53e7732b6bc44b4660
Nechai <nechai.dev@gmail.com>
2024-02-15 11:07:26 +0100

    Issue #3421348: Remove redundant group type filters from group views
    For views.view.group_events we remove specific references to group
    types. To ensure we only show event content we make the relationship to
    the events required. We can also remove the validation of the group
    type.
    
    The view is also moved to `config/optional` so that it’s only available
    if the views module and the social_event module are enabled. This brings
    us closer to not requiring a dependency on `social_event` from
    `social_group`. The other changes are relatively small but out of scope
    for this issue.
    
    For views.view.group_topics we make similar changes, but we add
    `social_topic` as a dependency for the view since it contains the
    required node bundle. Again this is moved to `config/optional` in a
    first step to decouple `social_group` from `social_topic`.
    
    For views.view.group_manage_members we also remove the filters. There
    might be multiple different relationships between a user and a group.
    For example “invites”, “membership requests” and actual “memberships”.
    The `gc__user` relationship ensures that we only show actual memberships.
    This also provides an option to unify the invites/request/member views
    by playing with these relationships.
    
    The same goes for views.view.group_members. However, here the
    relationship doesn’t exist yet so we add it to ensure we only show
    full-members.
    
    For views.view.group_managers we make use of the naming for group roles
    being `<group_type>-<role_name>` and the fact that we use `group_manager`
    as role name consistently across our existing group types. This allows
    us to match for a role `-group_manager` regardless of which group the
    role belongs to. While we’re at it we simplify the groupings of this
    filter.
    
    There are other views that we leave as non-optional for future changes.
    
    SocialGroupAdminPeopleConfigOverride is removed because the modifications
    it makes to views are no longer needed.
    
    The relevant parts of the Flexible Group and Secret Group Config
    Overrides are also removed because they cause errors in the filters.

--- modules/social_features/social_group/modules/social_group_flexible_group/src/SocialGroupFlexibleGroupConfigOverride.php
+++ modules/social_features/social_group/modules/social_group_flexible_group/src/SocialGroupFlexibleGroupConfigOverride.php
@@ -90,76 +90,6 @@ class SocialGroupFlexibleGroupConfigOverride implements ConfigFactoryOverrideInt
       }
     }
 
-    $config_names = [
-      'views.view.group_members',
-      'views.view.group_manage_members',
-    ];
-
-    foreach ($config_names as $config_name) {
-      if (in_array($config_name, $names)) {
-        $overrides[$config_name] = [
-          'dependencies' => [
-            'config' => [
-              'flexible_group-group_membership' => 'group.content_type.flexible_group-group_membership',
-            ],
-          ],
-          'display' => [
-            'default' => [
-              'display_options' => [
-                'filters' => [
-                  'type' => [
-                    'value' => [
-                      'flexible_group-group_membership' => 'flexible_group-group_membership',
-                    ],
-                  ],
-                ],
-              ],
-            ],
-          ],
-        ];
-      }
-    }
-
-    $config_names = [
-      'views.view.group_events' => 'flexible_group-group_node-event',
-      'views.view.group_topics' => 'flexible_group-group_node-topic',
-    ];
-
-    foreach ($config_names as $config_name => $content_type) {
-      if (in_array($config_name, $names)) {
-        $overrides[$config_name] = [
-          'dependencies' => [
-            'config' => [
-              'group-content-type' => 'group.content_type.' . $content_type,
-              'group-type' => 'group.type',
-            ],
-          ],
-          'display' => [
-            'default' => [
-              'display_options' => [
-                'arguments' => [
-                  'gid' => [
-                    'validate_options' => [
-                      'bundles' => [
-                        'flexible_group' => 'flexible_group',
-                      ],
-                    ],
-                  ],
-                ],
-                'filters' => [
-                  'type' => [
-                    'value' => [
-                      $content_type => $content_type,
-                    ],
-                  ],
-                ],
-              ],
-            ],
-          ],
-        ];
-      }
-    }
-
     $config_name = 'views.view.newest_groups';
 
     $displays = [
@@ -261,62 +191,6 @@ class SocialGroupFlexibleGroupConfigOverride implements ConfigFactoryOverrideInt
         [
           'group_content-flexible_group-group_membership' => 'group_content-flexible_group-group_membership',
         ];
-    }
-
-    $config_name = 'views.view.group_managers';
-
-    if (in_array($config_name, $names, FALSE)) {
-      $overrides[$config_name] = [
-        'display' => [
-          'default' => [
-            'display_options' => [
-              'filters' => [
-                'group_roles_target_id_3' => [
-                  'id' => 'group_roles_target_id_3',
-                  'table' => 'group_content__group_roles',
-                  'field' => 'group_roles_target_id',
-                  'relationship' => 'group_content',
-                  'group_type' => 'group',
-                  'admin_label' => '',
-                  'operator' => '=',
-                  'value' => 'flexible_group-group_manager',
-                  'group' => 1,
-                  'exposed' => FALSE,
-                  'expose' => [
-                    'operator_id' => '',
-                    'label' => '',
-                    'description' => '',
-                    'use_operator' => FALSE,
-                    'operator' => '',
-                    'identifier' => '',
-                    'required' => FALSE,
-                    'remember' => FALSE,
-                    'multiple' => FALSE,
-                    'remember_roles' => [
-                      'authenticated' => 'authenticated',
-                    ],
-                    'placeholder' => '',
-                  ],
-                  'is_grouped' => FALSE,
-                  'group_info' => [
-                    'label' => '',
-                    'description' => '',
-                    'identifier' => '',
-                    'optional' => TRUE,
-                    'widget' => 'select',
-                    'multiple' => FALSE,
-                    'remember' => FALSE,
-                    'default_group' => 'All',
-                    'default_group_multiple' => [],
-                    'group_items' => [],
-                  ],
-                  'plugin_id' => 'string',
-                ],
-              ],
-            ],
-          ],
-        ],
-      ];
     }
 
     // Add join options to the all-groups and search groups views.
