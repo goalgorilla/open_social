bce0316db26cbdd467051eff8452a11ce42e3841
ronaldtebrake <ronald@goalgorilla.com>
2024-03-18 14:48:41 +0100

    The linked issue is tackled, this is quite intensive between scenario's lets see if we can clear it

--- tests/behat/features/bootstrap/SearchContext.php
+++ tests/behat/features/bootstrap/SearchContext.php
@@ -40,37 +40,6 @@ class SearchContext extends RawMinkContext {
   }
 
   /**
-   * When the database is loaded, delete all data from SOLR.
-   *
-   * The search_api and search_api_solr modules create specific delete queries
-   * for SOLR data based on the indexes. However, the site hash that they use
-   * may change in between databases which can cause old test data not to be
-   * cleaned up. This can cause issues when the data matches and the modules
-   * try to load it.
-   *
-   * See https://www.drupal.org/project/search_api_solr/issues/3218868.
-   */
-  public function onDatabaseLoaded() {
-    /** @var \Drupal\search_api\IndexInterface[] $indexes */
-    $indexes = \Drupal::service("entity_type.manager")
-      ->getStorage('search_api_index')
-      ->loadMultiple();
-
-    foreach ($indexes as $index_id => $index) {
-      /** @var \Drupal\search_api\ServerInterface $server */
-      $server = $index->getServerInstance();
-      $backend = $server->getBackend();
-      if (!$backend instanceof SearchApiSolrBackend) {
-        continue;
-      }
-      $connector = $backend->getSolrConnector();
-      $update_query = $connector->getUpdateQuery();
-      $update_query->addDeleteQuery("*:*");
-      $connector->update($update_query, $backend->getCollectionEndpoint($index));
-    }
-  }
-
-  /**
    * @Given Search indexes are up to date
    */
   public function updateSearchIndexes() {
