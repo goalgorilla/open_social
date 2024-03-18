ed298e247c5bc319b32d438a5c834eab26efc51e
ronaldtebrake <ronald@goalgorilla.com>
2024-03-18 15:43:15 +0100

    Revert the timeout changes, now we also have removed betweenscenario delete queries

--- tests/default.settings.php
+++ tests/default.settings.php
@@ -816,10 +816,7 @@ $config['symfony_mailer.mailer_transport.sendmail']['configuration']['port'] = '
  * solr container is quite slow to respond.
  * To make sure our tests don't time out we increase the index timeout.
  */
-$config['search_api.server.social_solr']['backend_config']['connector_config']['index_timeout'] = '100';
-$config['search_api.server.social_solr']['backend_config']['connector_config']['timeout'] = '100';
-$config['search_api.server.social_solr']['backend_config']['connector_config']['optimize_timeout'] = '100';
-$config['search_api.server.social_solr']['backend_config']['connector_config']['finalize_timeout'] = '100';
+$config['search_api.server.social_solr']['backend_config']['connector_config']['index_timeout'] = '50';
 
 /**
  * Load local development override configuration, if available.
