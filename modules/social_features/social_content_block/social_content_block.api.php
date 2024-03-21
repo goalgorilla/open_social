2fca4b6280c288bd1584fc2bf1e8df527e928335
Vinicius Rodrigues Pereira <rpereira.vcs@gmail.com>
2024-03-21 10:12:22 -0300

    Removed deprecateion function from Social Content Block modules

--- modules/social_features/social_content_block/social_content_block.api.php
+++ modules/social_features/social_content_block/social_content_block.api.php
@@ -9,41 +9,6 @@ use Drupal\block_content\BlockContentInterface;
 use Drupal\Core\Database\Query\SelectInterface;
 
 /**
- * Provides a method to alter the query to get content.
- *
- * @param \Drupal\Core\Database\Query\SelectInterface $query
- *   The query to alter the results.
- * @param \Drupal\block_content\BlockContentInterface $block_content
- *   The current block content.
- *
- * @deprecated in social:11.1.0 and is removed from social:13.0.0. Use
- *   hook_query_social_content_block_alter instead.
- *
- * @see https://www.drupal.org/node/3258202
- *
- * @ingroup social_content_block_api
- */
-function hook_social_content_block_query_alter(SelectInterface $query, BlockContentInterface $block_content): void {
-  /** @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $field */
-  $field = $block_content->field_topic_type;
-
-  // Add topic type tags.
-  if (!$field->isEmpty()) {
-    $alias = $query->innerJoin(
-      'node__field_topic_type',
-      'tt',
-      '%alias.entity_id = n.nid',
-    );
-
-    $query->condition(
-      "$alias.field_topic_type_target_id",
-      array_column($field->getValue(), 'target_id'),
-      'IN',
-    );
-  }
-}
-
-/**
  * Alter the list of content block plugin definitions.
  *
  * @param array $info
