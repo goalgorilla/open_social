b82be9f6effd222d16ca455eb2055a1460b0d4a2
Nechai <nechai.dev@gmail.com>
2024-04-01 22:05:32 +0300

    Issue #3403634 by SV: Users should be able to see own invites

---
+++ modules/social_features/social_group/modules/social_group_invite/src/Plugin/Group/RelationHandler/SocialGroupInviteAccessControl.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\social_group_invite\Plugin\Group\RelationHandler;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\group\Entity\GroupRelationshipInterface;
+use Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface;
+use Drupal\group\Plugin\Group\RelationHandler\AccessControlTrait;
+
+/**
+ * Checks access for the group_invitation relation plugin.
+ */
+class SocialGroupInviteAccessControl implements AccessControlInterface {
+
+  use AccessControlTrait;
+
+  /**
+   * Constructs a new GroupInvitationAccessControl.
+   *
+   * @param \Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface $parent
+   *   The parent access control handler.
+   */
+  public function __construct(AccessControlInterface $parent) {
+    $this->parent = $parent;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function relationshipAccess(GroupRelationshipInterface $group_relationship, $operation, AccountInterface $account, $return_as_object = FALSE): bool|AccessResultInterface {
+    // User who receives the invite should be able to view it.
+    if ($group_relationship->getEntityId() === $account->id()) {
+      return AccessResult::allowed();
+    }
+
+    return $this->parent->relationshipAccess($group_relationship, $operation, $account, $return_as_object);
+  }
+
+}
