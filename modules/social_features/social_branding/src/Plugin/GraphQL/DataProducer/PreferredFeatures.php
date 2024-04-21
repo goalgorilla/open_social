83ed7cc62db86f2b2c7f23976113777927bafbef
ronaldtebrake <ronald@goalgorilla.com>
2024-04-21 08:25:02 +0200

    Add Configuration to influence the order of the bottom bar features
    This gives admins a way to deal with the bottom bar features for the App.
    This is just a proof of concept, later on we can add SM features when those details are clear.
    Right now we use the existing hook as input for the ConfigForm which preferred features are available.
    We add a draggable table to the form, which shows all available features.
    And we change the place where the hook is invoked, for GraphQl, to the configuration from the form submitted.

--- modules/social_features/social_branding/src/Plugin/GraphQL/DataProducer/PreferredFeatures.php
+++ modules/social_features/social_branding/src/Plugin/GraphQL/DataProducer/PreferredFeatures.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\social_branding\Plugin\GraphQL\DataProducer;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
+use Drupal\social_branding\PreferredFeature;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -23,20 +25,24 @@ class PreferredFeatures extends DataProducerPluginBase implements ContainerFacto
 
   /**
    * The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
-  protected $moduleHandler;
+  protected ModuleHandlerInterface $moduleHandler;
+
+  /**
+   * The config factory.
+   */
+  protected ConfigFactoryInterface $config;
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): PreferredFeatures|ContainerFactoryPluginInterface|static {
     return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('module_handler')
+      $container->get('module_handler'),
+      $container->get('config.factory')
     );
   }
 
@@ -51,10 +57,13 @@ class PreferredFeatures extends DataProducerPluginBase implements ContainerFacto
    *   The plugin implementation definition.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The config factory.
    */
-  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, ModuleHandlerInterface $module_handler) {
+  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config,) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->moduleHandler = $module_handler;
+    $this->config = $config;
   }
 
   /**
@@ -65,6 +74,21 @@ class PreferredFeatures extends DataProducerPluginBase implements ContainerFacto
    */
   public function resolve() : array {
     $preferred_features = $this->moduleHandler->invokeAll('social_branding_preferred_features');
+    // Grab the saved config, if there is config already saved.
+    $config = $this->config->getEditable('social_branding.settings');
+    $features = $config->get('features');
+    if (!empty($features)) {
+      // If we have saved the config, this means at this moment in time,
+      // even if there are things provided in the hook, we expect the
+      // form to have been submitted so that will take priority.
+      $preferred_features = [];
+      foreach ($features as $name => $weight) {
+        $weight = $weight['weight'];
+        $preferred_features[] = new PreferredFeature($name, $weight);
+      }
+    }
+
+    // Make sure the alter hook still works.
     $this->moduleHandler->alter('social_branding_preferred_features', $preferred_features);
 
     // Order ascending by weight.
