48af57c1b9d0e088f9896997dacd296dc42ae485
Alexander Varwijk <alexander@getopensocial.com>
2024-04-23 19:03:44 +0200

    Implement cacheability propagation for secret stream wrapper
    Drupal does not provide a way to propagate cacheability information from
    a stream wrapper to the caller and above. I came up with a workaround
    using the renderer to propagate the max age of the link to a surrounding
    render context so that the dynamic context can be updated.
    
    We also expose it in the Drupal settings attachment so that we can
    propagate it to a response subscriber which sets the caching for the
    entire response.

---
+++ modules/social_features/social_core/src/SecretResponseCacheSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\social_core;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Render\AttachmentsInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Modifies the cache lifecycle for responses that contain secret files.
+ *
+ * There's a disconnect between the cache information for a secret file link and
+ * the rest of a Drupal page. To solve this, we manually set the page cache
+ * time.
+ */
+class SecretResponseCacheSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Create a new subscriber instance.
+   *
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The Drupal time service.
+   */
+  public function __construct(
+    protected TimeInterface $time,
+  ) {
+  }
+
+  /**
+   * Set the cache max age based on image cache lifetimes.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\ResponseEvent $event
+   *   The response event, which contains created response.
+   */
+  public function onResponse(ResponseEvent $event) : void {
+    $response = $event->getResponse();
+
+    // We limit our response to responses which can have images attached.
+    if (!$response instanceof AttachmentsInterface) {
+      return;
+    }
+
+    $attachments = $response->getAttachments();
+    if (!isset($attachments['drupalSettings']['secretFiles'])) {
+      return;
+    }
+
+    // Don't send the secretFiles attachment to the browser.
+    $attachedFiles = $attachments['drupalSettings']['secretFiles'];
+    unset($attachments['drupalSettings']['secretFiles']);
+    $response->setAttachments($attachments);
+
+    // Find the lowest maximum age.
+    $expires_at = NULL;
+    foreach ($attachedFiles as $file_expires_at) {
+      $expires_at = min($expires_at ?? $file_expires_at, $file_expires_at);
+    }
+
+    // Propagate the maximum age from the contained images to the response.
+    // This is needed until Drupal properly propagates max ages from components
+    // to the page: https://www.drupal.org/node/2352009.
+    // https://www.lullabot.com/articles/common-max-age-pitfalls-with-drupal-cache
+    // for a solid explanation.
+    $response->setCache([
+      'max_age' => $expires_at - $this->time->getCurrentTime(),
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    $events[KernelEvents::RESPONSE][] = ['onResponse', 0];
+
+    return $events;
+  }
+
+}
--- modules/social_features/social_core/src/SocialCoreServiceProvider.php
+++ modules/social_features/social_core/src/SocialCoreServiceProvider.php
@@ -6,6 +6,7 @@ use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Site\Settings;
 use Drupal\social_core\StreamWrapper\SecretStream;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Class SocialCoreServiceProvider.
@@ -35,6 +36,10 @@ class SocialCoreServiceProvider extends ServiceProviderBase {
     // Only register the secret file stream wrapper if a private file path has
     // been set.
     if (Settings::get('file_private_path')) {
+      $container->register('social_core.secret_response_cache_subscriber', SecretResponseCacheSubscriber::class)
+        ->addArgument(new Reference('datetime.time'))
+        ->addTag('event_subscriber');
+
       $container->register('stream_wrapper.secret', SecretStream::class)
         ->addTag('stream_wrapper', ['scheme' => 'secret']);
     }
