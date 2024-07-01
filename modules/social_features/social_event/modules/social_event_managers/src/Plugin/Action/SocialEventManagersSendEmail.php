9725daedba4fdaaaa974629dff8209918a9f9263
Vitalii Nechai <nechai.dev@gmail.com>
2024-07-01 16:13:09 +0300

    Revert changes

--- modules/social_features/social_event/modules/social_event_managers/src/Plugin/Action/SocialEventManagersSendEmail.php
+++ modules/social_features/social_event/modules/social_event_managers/src/Plugin/Action/SocialEventManagersSendEmail.php
@@ -2,26 +2,19 @@
 
 namespace Drupal\social_event_managers\Plugin\Action;
 
-use Drupal\activity_send\Plugin\SendActivityDestinationBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Queue\QueueFactory;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\Core\Utility\Token;
 use Drupal\node\NodeInterface;
-use Drupal\social_event\Entity\EventEnrollment;
 use Drupal\social_event\EventEnrollmentInterface;
 use Drupal\social_user\Plugin\Action\SocialSendEmail;
 use Drupal\user\Entity\User;
-use Drupal\user\UserInterface;
-use Drupal\views_bulk_operations\Form\ViewsBulkOperationsFormTrait;
 use Egulias\EmailValidator\EmailValidator;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Send email to event enrollment users.
@@ -38,35 +31,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class SocialEventManagersSendEmail extends SocialSendEmail {
 
-  use ViewsBulkOperationsFormTrait;
-
   /**
    * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected EntityTypeManagerInterface $entityTypeManager;
-
-  /**
-   * The Drupal module handler service.
-   */
-  protected ModuleHandler $moduleHandler;
-
-  /**
-   * The tempstore service.
-   */
-  protected PrivateTempStoreFactory $tempStoreFactory;
-
-  /**
-   * The current user object.
-   */
-  protected AccountInterface $currentUser;
+  protected $entityTypeManager;
 
   /**
    * {@inheritdoc}
    */
   public function __construct(
     array $configuration,
-          $plugin_id,
-          $plugin_definition,
+    $plugin_id,
+    $plugin_definition,
     Token $token,
     EntityTypeManagerInterface $entity_type_manager,
     LoggerInterface $logger,
@@ -94,17 +72,6 @@ class SocialEventManagersSendEmail extends SocialSendEmail {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->moduleHandler = $container->get('module_handler');
-    $instance->tempStoreFactory = $container->get('tempstore.private');
-    $instance->currentUser = $container->get('current_user');
-    return $instance;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function executeMultiple(array $objects) {
     $users = [];
     // Process the event enrollment chunks. These need to be converted to users.
@@ -114,30 +81,13 @@ class SocialEventManagersSendEmail extends SocialSendEmail {
 
       // Get the user from the even enrollment.
       /** @var \Drupal\user\Entity\User $user */
-      $user = User::load((int) $enrollment->getAccount());
-
-      // Prevent sending emails for all enrollees if all selected users
-      // are unsubscribed from receiving emails.
-      if ($this->isUnsubscribedFromEmails($user)) {
-        continue;
-      }
-
+      $user = User::load($enrollment->getAccount());
       $entities[] = $this->execute($user);
 
       $users += $this->entityTypeManager->getStorage('user')->loadMultiple($entities);
     }
-
-    // Pass it back to our parent who handles the creation of queue items.
+    // Pass it back to our parent who handles creation of queue items.
     return parent::executeMultiple($users);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function createQueueItem($name, array $data): void {
-    $data['bulk_mail_footer'] = TRUE;
-
-    parent::createQueueItem($name, $data);
   }
 
   /**
@@ -172,25 +122,7 @@ class SocialEventManagersSendEmail extends SocialSendEmail {
   /**
    * {@inheritdoc}
    */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
-    if (!empty($this->context['selected_removed'])) {
-      $this->messenger()
-        ->addWarning($this->t('Your email will be sent to members who have opted to receive community updates and announcements'));
-      $this->messenger()
-        ->addWarning($this->formatPlural($this->context['selected_removed'],
-          "1 member won't receive this email due to their communication preferences.",
-          "@count members won't receive this email due to their communication preferences."
-        ));
-    }
-
-    // If all selected users have unsubscribed from emails, we should return
-    // empty form.
-    if (!empty($this->context['selected_removed']) && empty($this->context['list'])) {
-      return [
-        '#markup' => $this->t('No items selected. Go back and try again.'),
-      ];
-    }
-
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     // Add title to the form as well.
     if ($form['#title'] !== NULL) {
       $selected_count = $this->context['selected_count'];
@@ -210,135 +142,6 @@ class SocialEventManagersSendEmail extends SocialSendEmail {
     }
 
     return parent::buildConfigurationForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setContext(array &$context): void {
-    parent::setContext($context);
-
-    // @todo Rely on something more solid then dynamic data for batch.
-    // We don't want to run this code if batch was started.
-    // "prepopulated" key is adding exactly on batch start.
-    if (isset($context['prepopulated'])) {
-      return;
-    }
-
-    if (!isset($context['list'], $context['bulk_form_keys'])) {
-      // Probably, the method is executed on batch processing.
-      return;
-    }
-
-    // Filter enrollees that have disabled bulk mailing based on their profile
-    // settings.
-    // We need to remove the selected users from temporary data and update
-    // form data passed through the further forms.
-    if ($select_all = empty($context['list'])) {
-      $selected_options = $context['bulk_form_keys'];
-    }
-    else {
-      $selected_options = array_keys($context['list']);
-    }
-
-    if (!$selected_options) {
-      return;
-    }
-
-    $origin = [...$selected_options];
-
-    // Go through each enrollment and check if a user doesn't have
-    // a disabled bulk mailing.
-    foreach ($selected_options as $key => $name) {
-      $item = (array) $this->getListItem($name);
-      if (!in_array('event_enrollment', $item)) {
-        continue;
-      }
-
-      // First element is the enrollment ID.
-      $eid = $item[0] ?? 0;
-      $enrollment = EventEnrollment::load($eid);
-      if (!$enrollment) {
-        continue;
-      }
-
-      $account = $enrollment->getAccountEntity();
-
-      // Check user frequency settings for event bulk mailing.
-      // If a user has disabled mailing, we remove enrollment from
-      // the selected list.
-      // Only authenticated users have frequency settings.
-      if ($account && $this->isUnsubscribedFromEmails($account)) {
-        unset($selected_options[$key]);
-      }
-    }
-
-    // All selected users can receive the email.
-    if (!$removed = array_diff($origin, $selected_options)) {
-      $context['selected_removed'] = 0;
-      return;
-    }
-
-    // If some of the selected enrollees unsubscribed from emails, we should
-    // prevent executing action for all enrolles.
-    if ($select_all) {
-      $context['exclude_mode'] = FALSE;
-    }
-
-    $context['selected_removed'] = count($removed);
-    $context['selected_count'] = count($selected_options);
-    $context['bulk_form_keys'] = $selected_options;
-
-    // If event managers pressed "Select all" button, and we found the
-    // enrollees that have disabled bulk mailing, we should change below
-    // options to prevent sending emails for all users.
-    if ($select_all) {
-      foreach ($selected_options as $name) {
-        $context['list'][$name] = $this->getListItem($name);
-      }
-    }
-    else {
-      foreach ($removed as $name) {
-        unset($context['list'][$name]);
-      }
-    }
-
-    // Prevent sending emails for all users.
-    if (empty($context['list'])) {
-      $context['bulk_form_keys'] = [];
-    }
-
-    // As context was changed, we need to update the appropriate tempstore.
-    $this->setTempstoreData($context, view_id: 'event_manage_enrollments', display_id: 'page_manage_enrollments');
-    parent::setContext($context);
-  }
-
-  /**
-   * Gets the current user.
-   *
-   * @return \Drupal\Core\Session\AccountInterface
-   *   The current user.
-   */
-  protected function currentUser(): AccountInterface {
-    return $this->currentUser;
-  }
-
-  /**
-   * Helps to check if a user is unsubscribed or not from bulk mailing.
-   *
-   * @param \Drupal\user\UserInterface $user
-   *   The user entity.
-   *
-   * @return bool
-   *   If unsubscribed TRUE, otherwise FALSE.
-   */
-  private function isUnsubscribedFromEmails(UserInterface $user): bool {
-    if ($user->isAuthenticated() && $this->moduleHandler->moduleExists('activity_send_email')) {
-      $frequency = SendActivityDestinationBase::getSendUserSettings(destination: 'email', account: $user, type: 'bulk_mailing');
-      return !empty($frequency['event_enrollees']) && $frequency['event_enrollees'] === FREQUENCY_NONE;
-    }
-
-    return FALSE;
   }
 
 }
