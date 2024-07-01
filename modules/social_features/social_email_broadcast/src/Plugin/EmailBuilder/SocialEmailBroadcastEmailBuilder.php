560fc492a5aed1b8c68455ab8f2bcb342593a3a0
Vitalii Nechai <nechai.dev@gmail.com>
2024-07-01 17:26:23 +0300

    Issue #3449423: Users can unsubscribe on events and groups bulk emails

---
+++ modules/social_features/social_email_broadcast/src/Plugin/EmailBuilder/SocialEmailBroadcastEmailBuilder.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\social_email_broadcast\Plugin\EmailBuilder;
+
+use Drupal\symfony_mailer\EmailFactoryInterface;
+use Drupal\symfony_mailer\EmailInterface;
+use Drupal\symfony_mailer\Processor\EmailBuilderBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+
+/**
+ * Defines the Email Builder plugin for the "social_email_broadcast" module.
+ *
+ * @EmailBuilder(
+ *   id = "social_email_broadcast",
+ *   sub_types = {
+ *     "social_email_broadcast" = @Translation("Bulk emails")
+ *   },
+ *   common_adjusters = {},
+ *   import = @Translation("Bulk emails"),
+ * )
+ */
+class SocialEmailBroadcastEmailBuilder extends EmailBuilderBase implements ContainerFactoryPluginInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The config factory.
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Constructs an ActivitySendEmailBuilder object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preRender(EmailInterface $email): void {
+    $params = $email->getParam('params');
+    if ($subject = $params['subject']) {
+      $email->setSubject(strip_tags($subject));
+    }
+    else {
+      $site_name = $this->configFactory->get('system.site')->get('name');
+      $email->setSubject($this->t('Notification from @site_name', [
+        '@site_name' => $site_name,
+      ], [
+        'langcode' => $email->getLangcode(),
+      ]));
+    }
+
+    $email->setBody($params['body']);
+
+    if (!empty($params['reply-to'])) {
+      $email->setReplyTo($params['reply-to']);
+    }
+  }
+
+  /**
+   * Saves the parameters for a newly created email.
+   *
+   * @param \Drupal\symfony_mailer\EmailInterface $email
+   *   The email to modify.
+   * @param mixed $params
+   *   The params containing the site name.
+   * @param mixed $to
+   *   The to addresses, see Address::convert().
+   */
+  public function createParams(EmailInterface $email, $params = NULL, $to = NULL): void {
+    $email->setParam('params', $params);
+    $email->setParam('to', $to);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fromArray(EmailFactoryInterface $factory, array $message): EmailInterface {
+    return $factory->newTypedEmail($message['module'], $message['key'], $message['params'], $message['to']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(EmailInterface $email): void {
+    $email->setTo($email->getParam('to'));
+  }
+
+}
--- modules/social_features/social_user/src/Plugin/AdvancedQueue/JobType/UserMailQueueJob.php
+++ modules/social_features/social_user/src/Plugin/AdvancedQueue/JobType/UserMailQueueJob.php
@@ -8,11 +8,15 @@ use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
 use Drupal\Component\Utility\EmailValidatorInterface;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Mail\MailManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\Core\Url;
 use Drupal\social_queue_storage\Entity\QueueStorageEntity;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -66,9 +70,35 @@ class UserMailQueueJob extends JobTypeBase implements ContainerFactoryPluginInte
   protected $emailValidator;
 
   /**
+   * The Drupal module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandler
+   */
+  public $moduleHandler;
+
+  /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MailManagerInterface $mail_manager, EntityTypeManagerInterface $entity_type_manager, TranslationInterface $string_translation, Connection $database, LanguageManagerInterface $language_manager, EmailValidatorInterface $email_validator) {
+  public function __construct(
+    array $configuration,
+          $plugin_id,
+          $plugin_definition,
+    MailManagerInterface $mail_manager,
+    EntityTypeManagerInterface $entity_type_manager,
+    TranslationInterface $string_translation,
+    Connection $database,
+    LanguageManagerInterface $language_manager,
+    EmailValidatorInterface $email_validator,
+    ModuleHandler $module_handler,
+    RendererInterface $renderer,
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->mailManager = $mail_manager;
     $this->storage = $entity_type_manager;
@@ -76,12 +106,14 @@ class UserMailQueueJob extends JobTypeBase implements ContainerFactoryPluginInte
     $this->setStringTranslation($string_translation);
     $this->languageManager = $language_manager;
     $this->emailValidator = $email_validator;
+    $this->moduleHandler = $module_handler;
+    $this->renderer = $renderer;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
     return new static(
       $configuration,
       $plugin_id,
@@ -91,14 +123,16 @@ class UserMailQueueJob extends JobTypeBase implements ContainerFactoryPluginInte
       $container->get('string_translation'),
       $container->get('database'),
       $container->get('language_manager'),
-      $container->get('email.validator')
+      $container->get('email.validator'),
+      $container->get('module_handler'),
+      $container->get('renderer')
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function process(Job $job) {
+  public function process(Job $job): JobResult {
     try {
       // Get the Job data.
       $data = $job->getPayload();
@@ -129,7 +163,7 @@ class UserMailQueueJob extends JobTypeBase implements ContainerFactoryPluginInte
             foreach ($data['user_mail_addresses'] as $mail_address) {
               if ($this->emailValidator->isValid($mail_address['email_address'])) {
                 // Attempt sending mail.
-                $this->sendMail($mail_address['email_address'], $this->languageManager->getDefaultLanguage()->getId(), $queue_storage, $mail_address['display_name']);
+                $this->sendMail($mail_address['email_address'], $this->languageManager->getDefaultLanguage()->getId(), $queue_storage, $mail_address['display_name'], include_mail_footer: $data['bulk_mail_footer'] ?? FALSE);
               }
             }
           }
@@ -156,42 +190,65 @@ class UserMailQueueJob extends JobTypeBase implements ContainerFactoryPluginInte
    * Send the email.
    *
    * @param string $user_mail
-   *   The recipient email address.
+   *   The recipient's email address.
    * @param string $langcode
    *   The recipient language.
    * @param \Drupal\social_queue_storage\Entity\QueueStorageEntity $mail_params
    *   The email content from the storage entity.
-   * @param string $display_name
-   *   In case of anonymous users a display name will be given.
+   * @param string|null $display_name
+   *   In the case of anonymous users, a display name will be given.
+   * @param bool $include_mail_footer
+   *   TRUE if need to include the email footer.
    */
-  protected function sendMail(string $user_mail, string $langcode, QueueStorageEntity $mail_params, $display_name = NULL) {
-    $subject = $mail_params->get('field_subject')->getValue();
-    $message = $mail_params->get('field_message')->getValue();
-    $reply_to = $mail_params->get('field_reply_to')->getValue();
-    $reply = NULL;
+  protected function sendMail(string $user_mail, string $langcode, QueueStorageEntity $mail_params, string $display_name = NULL, bool $include_mail_footer = FALSE): void {
+    $subject = $mail_params->get('field_subject')->value;
+    $body = $mail_params->get('field_message')->value;
+    $reply_to = $mail_params->get('field_reply_to')->getString();
 
-    if (!empty($reply_to)) {
-      $reply = $reply_to[0]['value'];
+    if ($include_mail_footer && $this->moduleHandler->moduleExists('social_email_broadcast')) {
+      $params = ['subject' => $subject, 'body' => $body];
+
+      $settings_link = Link::fromTextAndUrl($this->t('email notification settings', [], ['langcode' => $langcode]),
+        Url::fromRoute('activity_send_email.user_edit_page')->setAbsolute())->toString();
+
+      // Construct the render array for email.
+      $notification = [
+        '#theme' => 'directmail',
+        '#notification' => $params['body'],
+        '#notification_settings' => $this->t('You receive community updates and announcements emails according to your @settings', [
+          '@settings' => $settings_link,
+        ], ['langcode' => $langcode]),
+      ];
+
+      $params['body'] = $this->renderer->renderPlain($notification);
+      $params['reply-to'] = $reply_to;
+
+      // Attempt sending mail.
+      $this->mailManager->mail(
+        'social_email_broadcast',
+        'social_email_broadcast',
+        $user_mail,
+        $langcode,
+        $params
+      );
     }
+    else {
+      $context = ['subject' => $subject, 'message' => $body];
 
-    $context = [
-      'subject' => $subject[0]['value'],
-      'message' => $message[0]['value'],
-    ];
+      if ($display_name) {
+        $context['display_name'] = $display_name;
+      }
 
-    if ($display_name) {
-      $context['display_name'] = $display_name;
+      // Attempt sending mail.
+      $this->mailManager->mail(
+        'system',
+        'action_send_email',
+        $user_mail,
+        $langcode,
+        ['context' => $context],
+        $reply_to,
+      );
     }
-
-    // Attempt sending mail.
-    $this->mailManager->mail(
-      'system',
-      'action_send_email',
-      $user_mail,
-      $langcode,
-      ['context' => $context],
-      $reply,
-    );
   }
 
   /**
