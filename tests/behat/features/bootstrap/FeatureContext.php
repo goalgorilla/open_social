3127473ca56b803e21a82321756342725005d2b7
Vinicius Pereira <rpereira.vcs@gmail.com>
2024-08-14 16:13:28 -0300

    Add confirmation-logout at behat test logout function.
    The route user.logout (path /user/logout) now requires a CSRF token before logging out a user. If no CSRF token is provided in the URL, the user will be redirected to a confirmation form.
    Reference: https://www.drupal.org/node/2822514

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -797,9 +797,7 @@ class FeatureContext extends RawMinkContext {
       // Since Drupal 10.3 logout is redirect to confirm if is missing token.
       // I check if user is in confirmation page.
       $session = $this->getSession();
-      // Remove query string to avoid check destination parameter.
-      $url = strtok($session->getCurrentUrl(), '?');
-      if (!str_contains($url, '/user/logout/confirm')) {
+      if (!str_contains($session->getCurrentUrl(), '/user/logout/confirm')) {
         return;
       }
 
