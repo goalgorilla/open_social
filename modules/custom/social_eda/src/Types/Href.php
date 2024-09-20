736078eaef8571fc2b2973ec060d676e894b3dc1
Thiago Régis <tregismoreira@gmail.com>
2024-09-20 10:41:24 -0300

    PROD-30684: Reorganize the Social EDA modules to make all components compatible with Distro

---
+++ modules/custom/social_eda/src/Types/Href.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\social_eda\Types;
+
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Type class for Href data.
+ */
+class Href {
+
+  /**
+   * Constructs the Href type.
+   *
+   * @param string $canonical
+   *   The canonical url of the entity.
+   */
+  public function __construct(
+    public readonly string $canonical,
+  ) {}
+
+  /**
+   * Get formatted Href output.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   *
+   * @return self
+   *   The Href data object.
+   *
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  public static function fromEntity(EntityInterface $entity): self {
+    return new self(
+      canonical: $entity->toUrl('canonical', ['absolute' => TRUE])->toString(),
+    );
+  }
+
+}
