868df5964ac53dd2437f96313a4888b665dc4a70
Koval <vitalii.koval@lembergsolutions.com>
2024-09-24 14:47:17 +0300

    Issue #3476409:Add hook to alter query and conditions of activities visibility on homepage stream page

--- modules/custom/activity_viewer/activity_viewer.api.php
+++ modules/custom/activity_viewer/activity_viewer.api.php
@@ -5,6 +5,7 @@
  * Hooks provided by the Activity Viewer module.
  */
 
+use Drupal\Core\Database\Query\ConditionInterface;
 use Drupal\Core\Database\Query\SelectInterface;
 use Drupal\Core\Session\AccountInterface;
 
@@ -36,6 +37,20 @@ function hook_activity_viewer_available_nodes_query_alter(SelectInterface $query
 function hook_activity_viewer_available_posts_query_alter(SelectInterface $query, AccountInterface $user) {
 
 }
+
+/**
+ * Alter the query in filter.
+ *
+ * Sometimes we need to change visibility for custom entities in activities.
+ *
+ * @param \Drupal\views\Plugin\views\query\Sql $filter_query
+ *   Query from the filter.
+ * @param \Drupal\Core\Database\Query\ConditionInterface $or_condition
+ *   Current user.
+ * @param \Drupal\Core\Session\AccountInterface $user
+ *   Current user.
+ */
+function hook_activity_viewer_personalized_homepage_query_alter(SelectInterface $filter_query, ConditionInterface $or_condition,  AccountInterface $user) {}
 
 /**
  * @} End of "addtogroup hooks".
