cd378e225c84bb6a842fade118564c1808c53f38
Thiago Régis <tregismoreira@gmail.com>
2024-10-04 11:46:36 -0300

    PROD-30875: Implement "event published" event for the EDA

--- modules/social_features/social_event/src/EdaHandler.php
+++ modules/social_features/social_event/src/EdaHandler.php
@@ -4,15 +4,20 @@ namespace Drupal\social_event;
 
 use CloudEvents\V1\CloudEvent;
 use Drupal\Component\Uuid\UuidInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\node\NodeInterface;
 use Drupal\social_eda\Types\Address;
+use Drupal\social_eda\Types\Application;
 use Drupal\social_eda\Types\DateTime;
 use Drupal\social_eda\Types\Entity;
 use Drupal\social_eda\Types\Href;
 use Drupal\social_eda\Types\User;
 use Drupal\social_eda_dispatcher\Dispatcher as SocialEdaDispatcher;
-use Drupal\social_event\Event\EventCreateEventData;
+use Drupal\social_event\Event\EventEntityData;
+use Drupal\user\UserInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -21,14 +26,25 @@ use Symfony\Component\HttpFoundation\RequestStack;
 final class EdaHandler {
 
   /**
-   * The Kafka topic name.
+   * The current logged-in user.
+   *
+   * @var \Drupal\user\UserInterface|null
    */
-  const TOPIC_NAME = 'com.getopensocial.cms.event.create';
+  protected ?UserInterface $currentUser = NULL;
 
   /**
-   * The Event type.
+   * The source.
+   *
+   * @var string
    */
-  const EVENT_TYPE = 'com.getopensocial.cms.event.create';
+  protected string $source;
+
+  /**
+   * The current route name.
+   *
+   * @var string
+   */
+  protected string $routeName;
 
   /**
    * {@inheritDoc}
@@ -38,46 +54,68 @@ final class EdaHandler {
     private readonly UuidInterface $uuid,
     private readonly RequestStack $requestStack,
     private readonly ModuleHandlerInterface $moduleHandler,
-  ) {}
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly AccountProxyInterface $account,
+    private readonly RouteMatchInterface $routeMatch,
+  ) {
+    // Load the full user entity if the account is authenticated.
+    $account_id = $this->account->id();
+    if ($account_id && $account_id !== 0) {
+      $user = $this->entityTypeManager->getStorage('user')->load($account_id);
+      if ($user instanceof UserInterface) {
+        $this->currentUser = $user;
+      }
+    }
+
+    // Set source.
+    $request = $this->requestStack->getCurrentRequest();
+    $this->source = $request ? $request->getPathInfo() : '';
+
+    // Set route name.
+    $this->routeName = $this->routeMatch->getRouteName() ?: '';
+  }
 
   /**
    * Create event handler.
    */
   public function eventCreate(NodeInterface $node): void {
-    // Skip if required modules are not enabled.
-    if (!$this->moduleHandler->moduleExists('social_eda') || !$this->dispatcher) {
-      return;
-    }
+    $event_type = 'com.getopensocial.cms.event.create';
+    $topic_name = 'com.getopensocial.cms.event.create';
+    $this->dispatch($topic_name, $event_type, $node);
+  }
 
-    // Transform the node into a CloudEvent.
-    $event = $this->fromEntity($node);
-
-    // Dispatch the event to the message broker.
-    $this->dispatcher->dispatch(self::TOPIC_NAME, $event);
+  /**
+   * Publish event handler.
+   */
+  public function eventPublish(NodeInterface $node): void {
+    $event_type = 'com.getopensocial.cms.event.publish';
+    $topic_name = 'com.getopensocial.cms.event.publish';
+    $this->dispatch($topic_name, $event_type, $node);
   }
 
   /**
    * Transforms a NodeInterface into a CloudEvent.
    *
    * @throws \Drupal\Core\Entity\EntityMalformedException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    */
-  public function fromEntity(NodeInterface $node): CloudEvent {
-    // Get current request.
-    $request = $this->requestStack->getCurrentRequest();
+  public function fromEntity(NodeInterface $node, string $event_type): CloudEvent {
+    // Determine actors.
+    [$actor_application, $actor_user] = $this->determineActors();
 
     // List enrollment methods.
     $enrollment_methods = ['open', 'request', 'invite'];
 
     return new CloudEvent(
       id: $this->uuid->generate(),
-      source: $request ? $request->getUri() : '/node/add/event',
-      type: self::EVENT_TYPE,
+      source: $this->source,
+      type: $event_type,
       data: [
-        'event' => new EventCreateEventData(
+        'event' => new EventEntityData(
           id: $node->get('uuid')->value,
           created: DateTime::fromTimestamp($node->getCreatedTime())->toString(),
           updated: DateTime::fromTimestamp($node->getChangedTime())->toString(),
-          status: $node->get('status')->value,
+          status: $node->get('status')->value ? 'published' : 'unpublished',
           label: (string) $node->label(),
           visibility: $node->get('field_content_visibility')->value,
           group: !$node->get('groups')->isEmpty() ? Entity::fromEntity($node->get('groups')->getEntity()) : NULL,
@@ -98,8 +136,8 @@ final class EdaHandler {
           type: $node->hasField('field_event_type') && !$node->get('field_event_type')->isEmpty() ? $node->get('field_event_type')->getEntity()->label() : NULL,
         ),
         'actor' => [
-          'application' => NULL,
-          'user' => User::fromEntity($node->get('uid')->entity),
+          'application' => $actor_application ? Application::fromId($actor_application) : NULL,
+          'user' => $actor_user ? User::fromEntity($actor_user) : NULL,
         ],
       ],
       dataContentType: 'application/json',
@@ -107,6 +145,59 @@ final class EdaHandler {
       subject: NULL,
       time: DateTime::fromTimestamp($node->getCreatedTime())->toImmutableDateTime(),
     );
+  }
+
+  /**
+   * Determines the actor (application and user) for the CloudEvent.
+   *
+   * @return array
+   *   An array with two elements: the application and the user.
+   */
+  private function determineActors(): array {
+    $application = NULL;
+    $user = NULL;
+
+    switch ($this->routeName) {
+      case 'entity.node.edit_form':
+      case 'system.admin_content':
+        $user = $this->currentUser;
+        break;
+
+      case 'entity.ultimate_cron_job.run':
+        $application = 'cron';
+        break;
+    }
+
+    return [
+      $application,
+      $user,
+    ];
+  }
+
+  /**
+   * Dispatches the event.
+   *
+   * @param string $topic_name
+   *   The topic name.
+   * @param string $event_type
+   *   The event type.
+   * @param \Drupal\node\NodeInterface $node
+   *   The node object.
+   *
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
+   */
+  private function dispatch(string $topic_name, string $event_type, NodeInterface $node): void {
+    // Skip if required modules are not enabled.
+    if (!$this->moduleHandler->moduleExists('social_eda') || !$this->dispatcher) {
+      return;
+    }
+
+    // Build the event.
+    $event = $this->fromEntity($node, $event_type);
+
+    // Dispatch to message broker.
+    $this->dispatcher->dispatch($topic_name, $event);
   }
 
 }
