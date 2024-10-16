f600f904db1d9b17be5e36c3c9f1798720019c10
Denis Kolmerschlag <denis@getopensocial.com>
2024-10-16 10:15:39 +0200

    Replacing social_post_form_post_form_alter with hux approach.

--- modules/social_features/social_post/modules/social_post_photo/src/Plugin/Block/PostPhotoProfileBlock.php
+++ modules/social_features/social_post/modules/social_post_photo/src/Plugin/Block/PostPhotoProfileBlock.php
@@ -5,7 +5,9 @@ namespace Drupal\social_post_photo\Plugin\Block;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\social_group\CurrentGroupService;
 use Drupal\social_post\Plugin\Block\PostProfileBlock;
 
 /**
@@ -23,13 +25,14 @@ class PostPhotoProfileBlock extends PostProfileBlock {
    */
   public function __construct(
     array $configuration,
-    $plugin_id,
-    $plugin_definition,
+          $plugin_id,
+          $plugin_definition,
     EntityTypeManagerInterface $entity_type_manager,
     AccountProxyInterface $current_user,
     FormBuilderInterface $form_builder,
     ModuleHandlerInterface $module_handler,
-    $account
+    CurrentRouteMatch $route_match,
+    CurrentGroupService $current_group_service,
   ) {
     parent::__construct(
       $configuration,
@@ -39,7 +42,8 @@ class PostPhotoProfileBlock extends PostProfileBlock {
       $current_user,
       $form_builder,
       $module_handler,
-      $account
+      $route_match,
+      $current_group_service,
     );
 
     // Override the bundle type.
