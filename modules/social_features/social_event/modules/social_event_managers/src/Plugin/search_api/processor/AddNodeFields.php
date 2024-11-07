9dea2d1993626054690591d379a02ce369a5e6af
Vitalii Nechai <nechai.dev@gmail.com>
2024-11-07 13:07:50 +0200

    Node entity type search api query access rules for event managers

---
+++ modules/social_features/social_event/modules/social_event_managers/src/Plugin/search_api/processor/AddNodeFields.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\social_event_managers\Plugin\search_api\processor;
+
+use Drupal\social_node\SearchApiAddNodeFieldsProcessorBase;
+
+/**
+ * Adds node fields used for controlling node access on query alters.
+ *
+ * @SearchApiProcessor(
+ *   id = "social_event_managers_node_fields",
+ *   label = @Translation("Social Event Managers: Add requried node fields to index"),
+ *   description = @Translation("Add requried node fields to index."),
+ *   stages = {
+ *     "pre_index_save" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class AddNodeFields extends SearchApiAddNodeFieldsProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getNodeFieldsName(): array {
+    return [
+      'field_event_managers' => ['type' => 'integer'],
+    ];
+  }
+
+}
