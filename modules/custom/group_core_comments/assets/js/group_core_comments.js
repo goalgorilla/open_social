8e92d83cc52adcfcba521f818ece6272ce487970
Vitalii Nechai <nechai.dev@gmail.com>
2024-11-11 12:26:05 +0200

    Issue #3486730: Make "View group" button work in "group_core_comments" module

--- modules/custom/group_core_comments/assets/js/group_core_comments.js
+++ modules/custom/group_core_comments/assets/js/group_core_comments.js
@@ -1,32 +1,34 @@
 (function ($, Drupal) {
   Drupal.behaviors.groupCoreComments = {
     attach: function (context) {
-      var forbiddenPost = $('.forbidden-post-comments-wrapper', context);
-      var popup = forbiddenPost.find('.popup-info', context);
-      var popupH = popup.outerHeight();
-      var link = forbiddenPost.find('.description .btn-action__group');
+      $(once('forbidden-post-comments', '.forbidden-post-comments-wrapper', context)).each(function () {
+        let forbiddenPost = $(this);
+        let popup = forbiddenPost.find('.popup-info', context);
+        let popupH = popup.outerHeight();
+        let link = forbiddenPost.find('.description .btn-action__group');
+        console.log(link);
+        popup.css('top', (-popupH - 5));
 
-      popup.css('top', (-popupH - 5));
+        $(window).on('resize', function () {
+          let popupHResize = popup.outerHeight();
+          popup.css('top', (-popupHResize - 5));
+        });
 
-      $(window).on('resize', function () {
-        var popupHResize = popup.outerHeight();
-        popup.css('top', (-popupHResize - 5));
-      });
+        link.on('click', function (event) {
+          event.preventDefault();
+          $(this).toggleClass('open');
+        });
 
-      link.on('click', function (event) {
-        event.preventDefault();
-        $(this).toggleClass('open');
-      });
-
-      $(document).click(function (event) {
-        if ($(event.target).closest('.forbidden-post-comments-wrapper .description .btn-action__group').length) {
-          return;
-        }
-        if ($(event.target).closest('.forbidden-post-comments-wrapper .popup-info').length) {
-         return;
-        }
-        link.removeClass('open');
-        event.stopPropagation();
+        $(document).click(function (event) {
+          if ($(event.target).closest('.forbidden-post-comments-wrapper .description .btn-action__group').length) {
+            return;
+          }
+          if ($(event.target).closest('.forbidden-post-comments-wrapper .popup-info').length) {
+            return;
+          }
+          link.removeClass('open');
+          event.stopPropagation();
+        });
       });
     }
   };
