5ae7559ae9b68bd2f9174416b82be722d361f45e
Thiago Régis <tregismoreira@gmail.com>
2024-11-13 11:20:19 -0300

    Fix conflict with ef210a528230db199b404c1cd08334309bafc0bf

--- modules/social_features/social_user/tests/src/Unit/EdaHandlerTest.php
+++ modules/social_features/social_user/tests/src/Unit/EdaHandlerTest.php
@@ -467,7 +467,7 @@ class EdaHandlerTest extends UnitTestCase {
     $this->dispatcher->expects($this->once())
       ->method('dispatch')
       ->with(
-        $this->equalTo('com.getopensocial.cms.user.settings.email'),
+        $this->equalTo('com.getopensocial.cms.user.v1'),
         $this->equalTo($event)
       );
 
