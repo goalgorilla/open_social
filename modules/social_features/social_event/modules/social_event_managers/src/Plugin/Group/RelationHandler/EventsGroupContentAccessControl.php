goalgorilla/open_social

commit 3c4d6803a2f927c66b6e8890ffd23f1725189c7f

Issue #3403634 by SV: Refactor node access handler for event managers

modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\social_event_managers\Plugin\Group\RelationHandlerDefault;
+namespace Drupal\social_event_managers\Plugin\Group\RelationHandler;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessResultForbidden;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface;
 use Drupal\group\Plugin\Group\RelationHandler\AccessControlTrait;
+use Drupal\node\NodeInterface;
 use Drupal\social_event_managers\SocialEventManagersAccessHelper;
 
 /**
@@ -35,42 +35,19 @@ class EventsGroupContentAccessControl implements AccessControlInterface {
   public function entityAccess(EntityInterface $entity, $operation, AccountInterface $account, $return_as_object = FALSE) {
     // We only care about Update (/edit) of the Event content.
     if ($operation !== 'update') {
-      $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
+      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
     }
 
-    /** @var \Drupal\group\Entity\Storage\GroupRelationshipStorage $storage */
-    $storage = $this->entityTypeManager()->getStorage('group_content');
-    $group_contents = $storage->loadByEntity($entity, $this->pluginId);
-
-    // If this plugin is not being used by the entity, we have nothing to say.
-    if (empty($group_contents)) {
-      return AccessResult::neutral();
+    if (!$entity instanceof NodeInterface || $entity->bundle() !== 'event') {
+      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
     }
 
-    // We need to determine if the user has access based on group permissions.
-    $group_based_access = $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-
-    // Only when the access result is False we need to override,
-    // when a user already has access based on Group relation we're good.
-    if (!$group_based_access instanceof AccessResultForbidden) {
-      return $group_based_access;
+    /** @var \Drupal\node\NodeInterface $entity */
+    if ($entity->get('field_event_managers')->isEmpty()) {
+      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
     }
 
-    // Based on the EventManager access we can determine if a user
-    // is the owner, or an event manager/organizer and give out
-    // permissions.
-    foreach ($group_contents as $group_content) {
-      /** @var \Drupal\node\NodeInterface $node */
-      $node = $group_content->getEntity();
-      $result = SocialEventManagersAccessHelper::getEntityAccessResult($node, $operation, $account);
-      if ($result->isAllowed()) {
-        break;
-      }
-    }
-
-    if (!isset($result)) {
-      return $group_based_access;
-    }
+    $result = SocialEventManagersAccessHelper::getEntityAccessResult($entity, $operation, $account);
 
     // If we did not allow access, we need to explicitly forbid access to avoid
     // other modules from granting access where Group promised the entity would

modules/social_features/social_event/modules/social_event_managers/src/SocialEventManagersServiceProvider.php
@@ -1,0 +1,34 @@
+<?php
+
+namespace Drupal\social_event_managers;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\social_event_managers\Plugin\Group\RelationHandler\EventsGroupContentAccessControl;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Alters existing services for the Social Event Managers module.
+ */
+class SocialEventManagersServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function register(ContainerBuilder $container): void {
+    $modules = $container->getParameter('container.modules');
+    if (isset($modules['gnode'])) {
+      // Decorating group relation handlers.
+      $definition = new Definition(EventsGroupContentAccessControl::class);
+      $definition->setArguments([
+        new Reference('social_event_managers.relation_handler.access_control.group_node.decorator.inner'),
+      ]);
+      $definition->setPublic(FALSE);
+      $definition->setShared(FALSE);
+      $definition->setDecoratedService('group.relation_handler.access_control.group_node');
+      $container->setDefinition('social_event_managers.relation_handler.access_control.group_node.decorator', $definition);
+    }
+  }
+
+}

modules/social_features/social_event/modules/social_event_managers/social_event_managers.services.yml
@@ -1,6 +1,6 @@
 services:
   social_event_managers.overrider:
-    class: \Drupal\social_event_managers\SocialEventManagersConfigOverride
+    class: Drupal\social_event_managers\SocialEventManagersConfigOverride
     tags:
       - {name: config.factory.override, priority: 5}
 
@@ -14,8 +14,3 @@ services:
     class: 'Drupal\social_event_managers\Access\AddEnrolleeAccessCheck'
     tags:
       - { name: 'access_check', applies_to: '_enrollee_permission', priority: 249 }
-
-  group.relation_handler.access_control.group_node:
-    class: 'Drupal\social_event_managers\Plugin\Group\RelationHandlerDefault\EventsGroupContentAccessControl'
-    arguments: ['@group.relation_handler.access_control']
-    shared: false

modules/social_features/social_event/tests/src/Kernel/GraphQL/QueryEventTest.php
@@ -65,11 +65,6 @@ class QueryEventTest extends SocialGraphQLTestBase {
     'social_profile',
     'social_event_managers',
     'views_bulk_operations',
-    // @deprecated added since we use social_entity_url producer in the
-    // EventSchemaExtension, but social_entity_url marked as deprecated so,
-    // social_topic should be removed from $modules when
-    // https://github.com/drupal-graphql/graphql/pull/1220 is merged.
-    'social_topic',
     'variationcache',
     'better_exposed_filters',
   ];
@@ -97,7 +92,8 @@ class QueryEventTest extends SocialGraphQLTestBase {
     $this->installEntitySchema('profile');
 
     $this->installSchema('comment', 'comment_entity_statistics');
-    $this->installSchema('file', ['file_usage']);
+    $this->installSchema('file', 'file_usage');
+
     $this->installConfig([
       'node',
       'social_core',

commit 9d6be51884c64a30fc6e900f26cf7b96f98536dd

PROD-30741: Remove deprecated class which interferes with node access
The user with UID 47270 is a co-author on an event /group/greenpeace-hamburg-intern/veranstaltung/gesamtgruppenplenum-orga-rainbow-drums?check_logged_in=1
But this co-author doesn’t have access to “Edit” the page because when $operation = ‘update' which is “edit”, the piece of code shown in the screenshot assumes that because access “Neutral” and it is no ->isAllowed() , it adds `Access:forbidden`

This is somewhat caused by https://git.drupalcode.org/project/social/-/commit/3c4d6803a2f927c66b6e8890ffd23f1725189c7f and https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L59

The above mentioned changes assume that if the Group doesn’t provides access writes to the current user then it should deny as well, but social_collaboration is an exception.

https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L54  returns Access::Neutral but https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L60  turns into forbidden.

Upon further investigation we found that the class `EventsGroupContentAccessControl.php` was marked deprecated in https://github.com/goalgorilla/open_social/pull/3682/files#diff-5212a1356f72992e772fe8bcdc2588605afd18b77f6f0f49c1fd8359a7a82fa5R16 and also we shouldn't check access to group content out of group permissions context.

modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php
@@ -1,68 +1,0 @@
-<?php
-
-namespace Drupal\social_event_managers\Plugin\Group\RelationHandler;
-
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface;
-use Drupal\group\Plugin\Group\RelationHandler\AccessControlTrait;
-use Drupal\node\NodeInterface;
-use Drupal\social_event_managers\SocialEventManagersAccessHelper;
-
-/**
- * Provides access control for Event GroupContent entities.
- *
- * @todo Check if the access handler not need anymore if favor of "social_event_managers_node_access()".
- */
-class EventsGroupContentAccessControl implements AccessControlInterface {
-
-  use AccessControlTrait;
-
-  /**
-   * Constructs a new GroupMembershipAccessControl.
-   *
-   * @param \Drupal\group\Plugin\Group\RelationHandlerDefault\AccessControl $parent
-   *   The parent access control handler.
-   */
-  public function __construct(AccessControlInterface $parent) {
-    $this->parent = $parent;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function entityAccess(EntityInterface $entity, $operation, AccountInterface $account, $return_as_object = FALSE) {
-    if (!isset($this->parent)) {
-      throw new \LogicException('Using AccessControlTrait without assigning a parent or overwriting the methods.');
-    }
-
-    // We only care about Update (/edit) of the Event content.
-    if ($operation !== 'update') {
-      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-    }
-
-    if (!$entity instanceof NodeInterface || $entity->bundle() !== 'event') {
-      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-    }
-
-    /** @var \Drupal\node\NodeInterface $entity */
-    if ($entity->get('field_event_managers')->isEmpty()) {
-      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-    }
-
-    $result = SocialEventManagersAccessHelper::getEntityAccessResult($entity, $operation, $account);
-
-    // If we did not allow access, we need to explicitly forbid access to avoid
-    // other modules from granting access where Group promised the entity would
-    // be inaccessible.
-    if (!$result->isAllowed()) {
-      $result = AccessResult::forbidden()->addCacheContexts(['user.group_permissions']);
-    }
-
-    $result->cachePerUser();
-
-    return $return_as_object ? $result : $result->isAllowed();
-  }
-
-}

modules/social_features/social_event/modules/social_event_managers/src/SocialEventManagersServiceProvider.php
@@ -1,34 +1,0 @@
-<?php
-
-namespace Drupal\social_event_managers;
-
-use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\DependencyInjection\ServiceProviderBase;
-use Drupal\social_event_managers\Plugin\Group\RelationHandler\EventsGroupContentAccessControl;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
-
-/**
- * Alters existing services for the Social Event Managers module.
- */
-class SocialEventManagersServiceProvider extends ServiceProviderBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function register(ContainerBuilder $container): void {
-    $modules = $container->getParameter('container.modules');
-    if (isset($modules['gnode'])) {
-      // Decorating group relation handlers.
-      $definition = new Definition(EventsGroupContentAccessControl::class);
-      $definition->setArguments([
-        new Reference('social_event_managers.relation_handler.access_control.group_node.decorator.inner'),
-      ]);
-      $definition->setPublic(FALSE);
-      $definition->setShared(FALSE);
-      $definition->setDecoratedService('group.relation_handler.access_control.group_node');
-      $container->setDefinition('social_event_managers.relation_handler.access_control.group_node.decorator', $definition);
-    }
-  }
-
-}