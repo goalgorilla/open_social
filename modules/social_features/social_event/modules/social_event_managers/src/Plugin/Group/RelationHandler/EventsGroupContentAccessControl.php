d882dad118490de207544808b92140e737089c8c
Navneet Singh <navneetsingh0693@gmail.com>
2025-04-07 12:52:06 +0530

    PROD-30741: Removed explicit denial logic from EventsGroupContentAccessControl handler
    The social_collaboration module adds co-author logic for Nodes. Currently, this co-author doesn’t have access to “Edit” the page because when $operation = ‘update' which is “edit”, the piece of code shown in the screenshot assumes that because access “Neutral” and it is no ->isAllowed() , it adds Access:forbidden.
    
    This is somewhat caused by https://git.drupalcode.org/project/social/-/commit/3c4d6803a2f927c66b6e8890ffd23f1725189c7f and https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L59
    
    The above mentioned changes assume that if the Group doesn’t provides access writes to the current user then it should deny as well, but social_collaboration is an exception.
    
    https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L54 returns Access::Neutral but https://github.com/goalgorilla/cablecar/blob/main/applications/community_management_system/html/profiles/contrib/social/modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php#L60 turns into forbidden.
    
    This code was originally added in https://github.com/goalgorilla/open_social/pull/2574/files#diff-c1bd01e0afbe99721cf7d422512c4cf894499d3d9cf9e4ca3d6fe608b3df6e07 while working on https://www.drupal.org/project/social/issues/3162491 and altered in https://www.drupal.org/project/social/issues/3403634
    
    The code was borrowed from the Group module's default AccessControl hanlder. The EventsGroupContentAccessControl is only checking if an 'EventManager' is having acccess to edit the event, this should either pass allowed or denied only in case if checks result in that. If the check result in AccessResult::NEUTRAL, then we don't need to explicitly deny it. This can still be taken by Group module, also we need to give chance to other plugins for approval or denial.

--- modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php
+++ modules/social_features/social_event/modules/social_event_managers/src/Plugin/Group/RelationHandler/EventsGroupContentAccessControl.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\social_event_managers\Plugin\Group\RelationHandler;
 
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\group\Plugin\Group\RelationHandler\AccessControlInterface;
@@ -12,15 +11,13 @@ use Drupal\social_event_managers\SocialEventManagersAccessHelper;
 
 /**
  * Provides access control for Event GroupContent entities.
- *
- * @todo Check if the access handler not need anymore if favor of "social_event_managers_node_access()".
  */
 class EventsGroupContentAccessControl implements AccessControlInterface {
 
   use AccessControlTrait;
 
   /**
-   * Constructs a new GroupMembershipAccessControl.
+   * Constructs a new AccessControl for event manager permissions.
    *
    * @param \Drupal\group\Plugin\Group\RelationHandlerDefault\AccessControl $parent
    *   The parent access control handler.
@@ -37,30 +34,17 @@ class EventsGroupContentAccessControl implements AccessControlInterface {
       throw new \LogicException('Using AccessControlTrait without assigning a parent or overwriting the methods.');
     }
 
-    // We only care about Update (/edit) of the Event content.
-    if ($operation !== 'update') {
+    // We only care about the update of the Event content
+    // and if the field_event_managers is not empty.
+    if ($operation !== 'update' ||
+      !($entity instanceof NodeInterface) ||
+      $entity->bundle() !== 'event' ||
+      $entity->get('field_event_managers')->isEmpty()) {
       return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
     }
 
-    if (!$entity instanceof NodeInterface || $entity->bundle() !== 'event') {
-      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-    }
-
-    /** @var \Drupal\node\NodeInterface $entity */
-    if ($entity->get('field_event_managers')->isEmpty()) {
-      return $this->parent->entityAccess($entity, $operation, $account, $return_as_object);
-    }
-
+    // We need to make sure that event managers have the access.
     $result = SocialEventManagersAccessHelper::getEntityAccessResult($entity, $operation, $account);
-
-    // If we did not allow access, we need to explicitly forbid access to avoid
-    // other modules from granting access where Group promised the entity would
-    // be inaccessible.
-    if (!$result->isAllowed()) {
-      $result = AccessResult::forbidden()->addCacheContexts(['user.group_permissions']);
-    }
-
-    $result->cachePerUser();
 
     return $return_as_object ? $result : $result->isAllowed();
   }
