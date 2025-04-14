5d185cc7b14744320064561c71bbdae6a2b149c9
Vinicius Pereira <rpereira.vcs@gmail.com>
2025-04-14 12:00:25 -0300

    Create a logger for new private message

--- modules/social_features/social_private_message/src/Hooks/SocialPrivateMessageThreadLogger.php
+++ modules/social_features/social_private_message/src/Hooks/SocialPrivateMessageThreadLogger.php
@@ -50,22 +50,65 @@ class SocialPrivateMessageThreadLogger implements ContainerInjectionInterface {
    * @see hook_ENTITY_TYPE_insert()
    */
   #[Hook('private_message_thread_insert')]
-  public function createLogger(EntityInterface $entity): void {
+  public function createPrivateMessageThreadLogger(EntityInterface $entity): void {
     // Validation to make PHPStan happy.
     if (!method_exists($entity, 'getMembers')) {
       return;
     }
 
     // Get usernames from members of thread.
-    $usernames = [];
+    $user_ids = [];
     foreach ($entity->getMembers() as $member) {
       assert($member instanceof User);
-      $usernames[] = $member->id();
+      $user_ids[] = $member->id();
     }
 
     // Logger message for new threads.
     $this->loggerFactory->info('Private message thread created with members: @members.', [
-      '@members' => implode(', ', $usernames),
+      '@members' => implode(', ', $user_ids),
+    ]);
+  }
+
+  /**
+   * Create a logger for new private message.
+   *
+   * The private message will create without relationship with thread
+   * after private message is created, the thread is updated with
+   * private message id, because of that I am trigger the logger on
+   * private message thread update.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   *
+   * @see hook_ENTITY_TYPE_insert()
+   */
+  #[Hook('private_message_thread_update')]
+  public function createPrivateMessageLogger(EntityInterface $entity): void {
+    // Validation to make PHPStan happy.
+    if (
+      !method_exists($entity, 'getMembers')
+      || !method_exists($entity, 'getMessages')
+      || empty($entity->original)
+    ) {
+      return;
+    }
+
+    // When don't have new messages being added, return early.
+    if ($entity->getMessages() == $entity->original->getMessages()) {
+      return;
+    }
+
+    // Get usernames from members of thread.
+    $user_ids = [];
+    foreach ($entity->getMembers() as $member) {
+      assert($member instanceof User);
+      $user_ids[] = $member->id();
+    }
+
+    // Logger message for new threads.
+    $this->loggerFactory->info('Private message created on thread @thread-id with members: @members.', [
+      '@thread-id' => $entity->id(),
+      '@members' => implode(', ', $user_ids),
     ]);
   }
 
