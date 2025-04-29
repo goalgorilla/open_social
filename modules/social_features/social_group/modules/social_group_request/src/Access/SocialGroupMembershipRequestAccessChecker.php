540a5c015625f9f3d6b42d69c55f2a3f79c3b378
Taras Kruts <taras.kruts@lembergsolutions.com>
2025-04-29 18:32:49 +0300

    PROD-30018: Invalidate group cache tags after approve or reject of membership request

--- modules/social_features/social_group/modules/social_group_request/src/Access/SocialGroupMembershipRequestAccessChecker.php
+++ modules/social_features/social_group/modules/social_group_request/src/Access/SocialGroupMembershipRequestAccessChecker.php
@@ -29,24 +29,24 @@ class SocialGroupMembershipRequestAccessChecker extends GroupMembershipRequestAc
 
     if (($group = $route_match->getParameter('group')) instanceof GroupInterface) {
       $group_membership_request = $this->groupRequestManager->getMembershipRequest($account, $group);
-      
-      // If there's no request, allow access
+
+      // If there's no request, allow access.
       if (empty($group_membership_request)) {
         return AccessResult::allowed();
       }
 
-      // Get the request status if available
+      // Get the request status if available.
       $status = $group_membership_request->get(GroupMembershipRequest::STATUS_FIELD)->value;
-      // Allow new request if previous one was rejected
+      // Allow new request if previous one was rejected.
       if ($status === GroupMembershipRequest::REQUEST_REJECTED) {
         return AccessResult::allowed();
       }
 
-      // If there's an active/pending request, forbid access
+      // If there's an active/pending request, forbid access.
       return AccessResult::forbidden();
     }
 
     return AccessResult::forbidden();
   }
 
-} 
+}
--- modules/social_features/social_group/modules/social_group_request/src/Form/GroupRequestMembershipRejectForm.php
+++ modules/social_features/social_group/modules/social_group_request/src/Form/GroupRequestMembershipRejectForm.php
@@ -2,13 +2,62 @@
 
 namespace Drupal\social_group_request\Form;
 
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\grequest\Entity\Form\GroupMembershipRejectForm;
+use Drupal\grequest\MembershipRequestManager;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a confirmation form before rejecting membership.
  */
 class GroupRequestMembershipRejectForm extends GroupMembershipRejectForm {
+
+  /**
+   * The cache tags invalidator.
+   *
+   * @var \Drupal\Core\Cache\CacheTagsInvalidatorInterface
+   */
+  protected $cacheTagsInvalidator;
+
+  /**
+   * Constructs a new GroupRequestMembershipRequestForm.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\grequest\MembershipRequestManager $membership_request_manager
+   *   Membership request manager.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger.
+   * @param \Drupal\Core\Cache\CacheTagsInvalidatorInterface $cache_tags_invalidator
+   *   The cache tags invalidator.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, MembershipRequestManager $membership_request_manager, LoggerInterface $logger, CacheTagsInvalidatorInterface $cache_tags_invalidator) {
+    parent::__construct($entity_repository, $entity_type_bundle_info, $time, $membership_request_manager, $logger);
+    $this->cacheTagsInvalidator = $cache_tags_invalidator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('grequest.membership_request_manager'),
+      $container->get('logger.factory')->get('group_relationship'),
+      $container->get('cache_tags.invalidator')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -50,6 +99,31 @@ class GroupRequestMembershipRejectForm extends GroupMembershipRejectForm {
     }
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    parent::submitForm($form, $form_state);
+
+    /** @var \Drupal\group\Entity\GroupRelationshipInterface $group_relationship */
+    $group_relationship = $this->getEntity();
+    $group = $group_relationship->getGroup();
+
+    // Add cache tags for group views.
+    $tags = [
+      'group_list',
+      'group_content_list',
+      'group_view',
+      'group_content_view',
+    ];
+    // Add cache tags that are based on group id.
+    $tags[] = 'group_hero:' . $group->id();
+    $tags[] = 'group_block:' . $group->id();
+    // Add cache tags that are based on group content.
+    $tags[] = 'group_content:' . $this->getEntity()->id();
+    $this->cacheTagsInvalidator->invalidateTags($tags);
   }
 
 }
