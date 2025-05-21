08a52a1c70d9a0990bdf6af360e31031dfd12720
Vitalii Nechai <vitalii.nechai@lembergsolutions.com>
2025-05-21 18:49:58 +0300

    PROD-32818: Fix label for request to join popup

--- modules/social_features/social_group/modules/social_group_request/src/Routing/SocialGroupRequestRouteSubscriber.php
+++ modules/social_features/social_group/modules/social_group_request/src/Routing/SocialGroupRequestRouteSubscriber.php
@@ -14,27 +14,18 @@ class SocialGroupRequestRouteSubscriber extends RouteSubscriberBase {
   /**
    * {@inheritdoc}
    */
-  protected function alterRoutes(RouteCollection $collection) {
+  protected function alterRoutes(RouteCollection $collection): void {
     $request_membership_route = $collection->get('entity.group.group_request_membership');
     assert($request_membership_route !== NULL, "The group request membership route in grequest changed or the grequest module was not added to the social_group_request.info.yml file.");
-    $request_membership_route->setDefaults([
-      '_title_callback' => GroupRequestController::class . "::requestMembershipTitle",
-      ...$request_membership_route->getDefaults(),
-    ]);
+    $request_membership_route->setDefault('_title_callback', GroupRequestController::class . "::requestMembershipTitle");
 
     $approve_membership_route = $collection->get('entity.group_content.group_approve_membership');
     assert($approve_membership_route !== NULL, "The group approve membership route in grequest changed or the grequest module was not added to the social_group_request.info.yml file.");
-    $approve_membership_route->setDefaults([
-      '_title_callback' => GroupRequestController::class . "::getTitleApproveRequest",
-      ...$approve_membership_route->getDefaults(),
-    ]);
+    $approve_membership_route->setDefault('_title_callback', GroupRequestController::class . "::getTitleApproveRequest");
 
     $reject_membership_route = $collection->get('entity.group_content.group_reject_membership');
     assert($reject_membership_route !== NULL, "The group reject membership route in grequest changed or the grequest module was not added to the social_group_request.info.yml file.");
-    $reject_membership_route->setDefaults([
-      '_title_callback' => GroupRequestController::class . "::getTitleRejectRequest",
-      ...$reject_membership_route->getDefaults(),
-    ]);
+    $reject_membership_route->setDefault('_title_callback', GroupRequestController::class . "::getTitleRejectRequest");
 
     if ($route = $collection->get('view.group_pending_members.page_1')) {
       $route->setRequirements([
