2d67fb24e00e532b928d02e6305fb38ea4be8798
Navneet Singh <navneetsingh0693@gmail.com>
2025-06-09 18:02:38 +0530

    PROD-30741: Moved hook_ENTITY_TYPE_access() to HUX implementation Moved hook_ENTITY_TYPE_access() to HUX implementation

---
+++ modules/social_features/social_event/modules/social_event_managers/src/Hooks/NodeAccessHook.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\social_event_managers\Hooks;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\hux\Attribute\Hook;
+use Drupal\node\NodeInterface;
+use Drupal\social_event_managers\SocialEventManagersAccessHelper;
+
+/**
+ * Contains entity related hooks.
+ */
+final readonly class NodeAccessHook {
+
+  /**
+   * Implements hook_ENTITY_TYPE_access().
+   *
+   * Remember: if any module returns forbidden and denies access to certain node
+   * and operation, it will not allow the user to do the operation on the node.
+   *
+   * Initially, this hook was written for providing access in groups.
+   * But the group has moved to RelationShipHandler access control plugins.
+   * We will still keep this in place for the events which may be inside any
+   * group.
+   */
+  #[Hook('node_access')]
+  public function nodeAccess(EntityInterface $entity, string $operation, AccountInterface $account): AccessResultInterface {
+    assert($entity instanceof NodeInterface, 'Entity must be a node.');
+    if (SocialEventManagersAccessHelper::isEventNodeWithManagers($entity)) {
+      return SocialEventManagersAccessHelper::getEntityAccessResult($entity, $operation, $account);
+    }
+    return AccessResult::neutral();
+  }
+
+}
