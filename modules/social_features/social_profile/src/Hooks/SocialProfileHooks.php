aa6847baf3c525cbc9efe5ebe0f373233348e577
Zan Vidmar <hello@zanvidmar.si>
2025-06-13 23:01:47 +0200

    PROD-32938: Fix hook alter typo in social_profile.module
    In social_profile.module, there is typo introduced by PROD-30545.
    social_user_profile_view_alter has to be replaced with
    social_profile_profile_view_alter.
    Because preferred approach is to use Hux,
    this code is refactored to Hux class.

--- modules/social_features/social_profile/src/Hooks/SocialProfileHooks.php
+++ modules/social_features/social_profile/src/Hooks/SocialProfileHooks.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\social_profile\Hooks;
 
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\hux\Attribute\Alter;
+use Drupal\profile\Entity\Profile;
 use Drupal\social_profile\Entity\Bundle\SocialProfile;
 
 /**
@@ -11,6 +15,16 @@ use Drupal\social_profile\Entity\Bundle\SocialProfile;
  * @internal
  */
 class SocialProfileHooks {
+
+  /**
+   * SocialProfileHooks constructor.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(
+    protected AccountInterface $current_user,
+  ) {}
 
   /**
    * Bundle class manager.
@@ -26,6 +40,34 @@ class SocialProfileHooks {
     if (isset($bundles['profile']['profile'])) {
       $bundles['profile']['profile']['class'] = SocialProfile::class;
     }
+  }
+
+  /**
+   * Create our custom profile tag so we can also invalidate f.e. teasers.
+   *
+   * @param array $build
+   *   A render array.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Entity for which to update the tags.
+   * @param \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display
+   *   The display interface.
+   *
+   * @return void
+   *   Return void.
+   */
+  #[Alter('profile_view')]
+  public function socialProfileCustomTags(array &$build, EntityInterface $entity, EntityViewDisplayInterface $display): void {
+    if ($this->current_user->isAnonymous() || !$entity instanceof Profile) {
+      return;
+    }
+
+    $uid = $this->current_user->id();
+    $pid = $entity->id();
+
+    // Create our custom profile tag so we can also invalidate f.e. teasers.
+    $profile_tag = 'profile:' . $pid . '-' . $uid;
+    $build['#cache']['tags'][] = $profile_tag;
+    $build['#cache']['contexts'][] = 'user';
   }
 
 }
