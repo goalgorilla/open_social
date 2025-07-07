b2bd88abfc3a519953f64759735d2fc801c933eb
Alexander Varwijk <alexander@getopensocial.com>
2025-07-07 16:01:09 +0200

    PROD-33190 Fix memory leak in Behat test runner
    The AvoidCleanUpTrait was used to ensure we don't run database queries
    at the end of each scenario, since we start from a fresh database for
    each test so that's just wasted time cleaning up a database that gets
    discarded.
    
    However, while disabling that work, the cleanup of any arrays that point
    to PHP Objects were also disabled. This means that in test runs with a
    large number of scenarios these arrays may grow very large since
    contexts stay around between scenarios. With our new Docker set-up we're
    limiting any CLI processes to 1 GB in the CI which is now showing this
    leak.
    
    The AvoidCleanUpTrait is adjusted so that we clear out the arrays. This
    should ensure the references to the objects are removed and let PHP
    garbage collect them to reduce the amount of memory the test runner
    uses.

--- tests/behat/features/bootstrap/AvoidCleanupTrait.php
+++ tests/behat/features/bootstrap/AvoidCleanupTrait.php
@@ -15,30 +15,50 @@ trait AvoidCleanupTrait {
    * Disable cleaning up to allow failed test inspection.
    */
   public function cleanUsers() : void {
+    // The database is cleared for us automatically, but we must empty the PHP
+    // array that tracks the objects to ensure our Behat process doesn't leak
+    // memory while running multiple scenarios.
+    $this->userManager->clearUsers();
   }
 
   /**
    * Disable cleaning up to allow failed test inspection.
    */
   public function cleanNodes() : void {
+    // The database is cleared for us automatically, but we must empty the PHP
+    // array that tracks the objects to ensure our Behat process doesn't leak
+    // memory while running multiple scenarios.
+    $this->nodes = [];
   }
 
   /**
    * Disable cleaning up to allow failed test inspection.
    */
   public function cleanLanguages() : void {
+    // The database is cleared for us automatically, but we must empty the PHP
+    // array that tracks the objects to ensure our Behat process doesn't leak
+    // memory while running multiple scenarios.
+    $this->languages = [];
   }
 
   /**
    * Disable cleaning up to allow failed test inspection.
    */
   public function cleanRoles() : void {
+    // The database is cleared for us automatically, but we must empty the PHP
+    // array that tracks the objects to ensure our Behat process doesn't leak
+    // memory while running multiple scenarios.
+    $this->roles = [];
   }
 
   /**
    * Disable cleaning up to allow failed test inspection.
    */
   public function cleanTerms() : void {
+    // The database is cleared for us automatically, but we must empty the PHP
+    // array that tracks the objects to ensure our Behat process doesn't leak
+    // memory while running multiple scenarios.
+    $this->terms = [];
   }
 
 }
