298ce327bd493259465b65dddfe1c51e7fcbc68f
My Global Name <bram@getopensocial.com>
2025-11-08 22:51:08 +0100

    PROD-34372: Add our own namespace for UUIDv5 generation
    We considered using one of the standards like DNS namespace, or the URL one. But that all feels not semantically correct.
    Better then to create our own namespace.

---
+++ modules/custom/social_eda/src/UuidNamespace.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\social_eda;
+
+/**
+ * UUID namespace for generating deterministic CloudEvent IDs.
+ */
+final class UuidNamespace {
+
+  /**
+   * Namespace UUID for generating deterministic CloudEvent IDs.
+   *
+   * Generated from 'getopensocial.com' using DNS namespace.
+   */
+  public const NAMESPACE_OPENSOCIAL = '7fd9a946-0864-5ffd-8ec5-a0866f6bda1f';
+
+}
