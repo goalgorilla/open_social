e1e35f1dc66eaa96ad1413c6458f94336823d203
Alexander Varwijk <alexander@getopensocial.com>
2026-04-13 19:35:01 +0200

    Fix SocialResourceLibraryOverrides view modes and add test
    The SocialResourceLibraryOverrides had a few issues since 2022.
    
    - It did not have a cache tag defined which meant it did not properly
      respond to settings changes.
    - The `$view_modes` variable was never redeclared and the `view_mode`
      array entry was consistently overwritten. This meant that the
      `entity:node` view modes would not be set if a group besides
      `flexible_group` was added to the index and group would also not work
      if a media file was selected. This was because all overrides happened
      within the same configuration object. We now properly build an array
      before making the override once.
    - We fix the settings being loaded in the constructor which is an
      anti-pattern that we're also addressing with a PHPStan rule in
      https://github.com/goalgorilla/cablecar/pull/5090 but which we pull
      forward for this class to make sure it doesn't cause issues.
    
    This PR adds tests for the social_resouce_library to show that we can
    disable a content type (event) from the settings and that the even then
    doesn't show up. This is the test coverage that helps prevent a
    regression from https://github.com/goalgorilla/cablecar/pull/5166/

--- tests/behat/features/bootstrap/ConfigContext.php
+++ tests/behat/features/bootstrap/ConfigContext.php
@@ -16,4 +16,18 @@ class ConfigContext extends BaseConfigContext {
    */
   public function cleanConfig() : void {}
 
+  /**
+   * Clear config cache in behat.
+   *
+   * We sometimes make config changes in the Drupal site which may affect steps
+   * that we take in the Behat context (e.g. change the search index
+   * configuration). If we have cached config in our Behat side of things then
+   * this can be break steps in fun and unexpected ways.
+   *
+   * @Given clear the config cache in Behat
+   */
+  public function clearSearchConfigCacheInBehat() : void {
+    \Drupal::configFactory()->reset();
+  }
+
 }
